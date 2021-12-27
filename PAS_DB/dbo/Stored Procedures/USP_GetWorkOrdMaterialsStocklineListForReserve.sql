2a54178417edce3894e084a5c792787af8ef2e38
Hemant <hemant_patel1234@yahoo.co.in>
2021-12-27 11:25:58 +0530

    SP Changes

---
+++ PAS_DB/dbo/Stored Procedures/USP_GetWorkOrdMaterialsStocklineListForReserve.sql
@@ -0,0 +1,115 @@
+﻿
+/*************************************************************           
+ ** File:   [USP_GetWorkOrdMaterialsStocklineListForReserve]           
+ ** Author:   Hemant Saliya
+ ** Description: This SP is Used to get Stockline list to reserve Stockline    
+ ** Purpose:         
+ ** Date:   12/24/2021        
+          
+ ** PARAMETERS:           
+ @WorkFlowWorkOrderId BIGINT   
+         
+ ** RETURN VALUE:           
+  
+ **************************************************************           
+  ** Change History           
+ **************************************************************           
+ ** PR   Date         Author		Change Description            
+ ** --   --------     -------		--------------------------------          
+    1    12/24/2021   Hemant Saliya Created
+
+     
+ EXECUTE USP_GetWorkOrdMaterialsStocklineListForReserve 61
+
+**************************************************************/ 
+    
+CREATE PROCEDURE [dbo].[USP_GetWorkOrdMaterialsStocklineListForReserve]    
+(    
+@WorkFlowWorkOrderId BIGINT = NULL
+)    
+AS    
+BEGIN    
+
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+SET NOCOUNT ON    
+
+	BEGIN TRY
+		BEGIN TRANSACTION
+			BEGIN  
+				SELECT  WOM.WorkOrderId,
+						WOM.WorkFlowWorkOrderId,
+						WOM.WorkOrderMaterialsId,						
+						WOM.ItemMasterId,
+						WOM.ConditionCodeId AS ConditionId,
+						WOM.MasterCompanyId,
+						WOM.Quantity,
+						WOM.QuantityReserved,
+						WOM.QuantityIssued,
+						WOM.QtyOnOrder AS QuantityOnOrder,
+						(ISNULL(WOM.Quantity, 0) - (ISNULL(WOM.QuantityReserved, 0) + ISNULL(WOM.QuantityIssued, 0))) AS QtyToBeReserved,
+						WOM.UnitCost,
+						WOM.ExtendedCost,
+						WOM.TaskId,
+						WOM.ProvisionId,
+						IM.PartNumber,
+						IM.PartDescription, 
+						SL.StocklineId,
+						SL.Condition,
+						SL.StockLineNumber,
+						SL.ControlNumber,
+						SL.IdNumber,
+						SL.Manufacturer,
+						SL.SerialNumber,
+						SL.QuantityAvailable AS QuantityAvailable,
+						SL.QuantityOnHand AS QuantityOnHand,
+						ISNULL(SL.QuantityOnOrder, 0) AS StocklineQuantityOnOrder,
+						ISNULL(SL.QuantityTurnIn, 0) AS StocklineQuantityTurnIn,
+						SL.UnitOfMeasure,
+						P.Description AS Provision,
+						P.StatusCode AS ProvisionStatusCode,
+						CASE 
+						WHEN IM.IsPma = 1 and IM.IsDER = 1 THEN 'PMA&DER'
+						WHEN IM.IsPma = 1 and IM.IsDER = 0 THEN 'PMA'
+						WHEN IM.IsPma = 0 and IM.IsDER = 1 THEN 'DER'
+						ELSE 'OEM'
+						END AS StockType,
+						WOMS.Quantity AS MSQuantityRequsted,
+						WOMS.QtyReserved AS MSQuantityReserved,
+						WOMS.QtyIssued AS MSQuantityIssued,
+						CASE WHEN WOMS.WOMStockLineId > 0 THEN WOMS.UnitCost ELSE SL.UnitCost END AS SLUnitCost,
+						MSQunatityRemaining = ISNULL(WOMS.Quantity, 0) - (ISNULL(WOMS.QtyReserved, 0) + ISNULL(WOMS.QtyIssued, 0)),
+						SP.Description AS MatStlProvision,
+						CASE WHEN WOMS.WOMStockLineId > 0 THEN 1 ELSE 0 END AS IsStocklineAdded
+					FROM dbo.WorkOrderMaterials WOM WITH (NOLOCK)  
+						JOIN dbo.ItemMaster IM WITH (NOLOCK) ON IM.ItemMasterId = WOM.ItemMasterId
+						JOIN dbo.Stockline SL WITH (NOLOCK) ON WOM.ItemMasterId = SL.ItemMasterId AND WOM.ConditionCodeId = SL.ConditionId
+						LEFT JOIN dbo.WorkOrderMaterialStockLine WOMS WITH (NOLOCK) ON WOMS.WorkOrderMaterialsId = WOM.WorkOrderMaterialsId AND SL.StockLineId = WOMS.StockLineId
+						LEFT JOIN dbo.Provision P WITH (NOLOCK) ON P.ProvisionId = WOM.ProvisionId
+						LEFT JOIN dbo.Provision SP WITH (NOLOCK) ON SP.ProvisionId = WOMS.ProvisionId
+						LEFT JOIN dbo.UnitOfMeasure UOM WITH (NOLOCK) ON UOM.UnitOfMeasureId = WOM.UnitOfMeasureId
+					WHERE WOM.WorkFlowWorkOrderId = @WorkFlowWorkOrderId AND ISNULL(SL.QuantityAvailable,0) > 0 AND SL.IsParent = 1 AND WOM.IsDeleted = 0  
+			END
+		COMMIT  TRANSACTION
+
+		END TRY    
+		BEGIN CATCH      
+			IF @@trancount > 0
+				ROLLBACK TRAN;
+				DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
+
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+              , @AdhocComments     VARCHAR(150)    = 'USP_GetWorkOrdMaterialsStocklineListForReserve' 
+              , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ ISNULL(@WorkFlowWorkOrderId, '') + ''
+              , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+
+              exec spLogException 
+                       @DatabaseName			= @DatabaseName
+                     , @AdhocComments			= @AdhocComments
+                     , @ProcedureParameters		= @ProcedureParameters
+                     , @ApplicationName			= @ApplicationName
+                     , @ErrorLogID              = @ErrorLogID OUTPUT ;
+              RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)
+              RETURN(1);
+		END CATCH
+END

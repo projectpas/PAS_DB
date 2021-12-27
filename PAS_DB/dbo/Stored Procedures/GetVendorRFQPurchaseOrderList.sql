2e674c1644a55deca636d20983236da6fc02d140
Hemant <hemant_patel1234@yahoo.co.in>
2021-12-27 19:01:50 +0530

    24 12 2021 DB Script Check-In

---
+++ PAS_DB/dbo/Stored Procedures/GetVendorRFQPurchaseOrderList.sql
@@ -0,0 +1,144 @@
+﻿
+
+CREATE PROCEDURE [dbo].[GetVendorRFQPurchaseOrderList]
+@PageNumber int = 1,
+@PageSize int = 10,
+@SortColumn varchar(50)=NULL,
+@SortOrder int = NULL,
+@StatusID int = 1,
+@Status varchar(50) = 'Open',
+@GlobalFilter varchar(50) = '',	
+@VendorRFQPurchaseOrderNumber varchar(50) = NULL,	
+@OpenDate  datetime = NULL,
+@VendorName varchar(50) = NULL,
+@RequestedBy varchar(50) = NULL,
+@CreatedBy  varchar(50) = NULL,
+@CreatedDate datetime = NULL,
+@UpdatedBy  varchar(50) = NULL,
+@UpdatedDate  datetime = NULL,
+@IsDeleted bit = 0,
+@EmployeeId bigint=1,
+@MasterCompanyId bigint=1,
+@VendorId bigint =null
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED	
+		DECLARE @RecordFrom int;
+		DECLARE @IsActive bit=1
+		DECLARE @Count Int;
+		SET @RecordFrom = (@PageNumber-1)*@PageSize;
+
+		IF @IsDeleted IS NULL
+		BEGIN
+			SET @IsDeleted=0
+		END
+		IF @SortColumn IS NULL
+		BEGIN
+			SET @SortColumn=Upper('CreatedDate')
+		END 
+		ELSE
+		BEGIN 
+			Set @SortColumn=Upper(@SortColumn)
+		END
+		IF (@StatusID=6 AND @Status='All')
+		BEGIN			
+			SET @Status = ''
+		END
+		IF (@StatusID=6 OR @StatusID=0)
+		BEGIN
+			SET @StatusID = NULL			
+		END		
+		
+		BEGIN TRY
+		BEGIN TRANSACTION
+		BEGIN	
+
+		;WITH Result AS(									
+		   	 SELECT DISTINCT PO.VendorRFQPurchaseOrderId,
+		            PO.VendorRFQPurchaseOrderNumber,					
+                    PO.OpenDate,
+					PO.ClosedDate,
+					PO.CreatedDate,
+				    PO.CreatedBy,
+					PO.UpdatedDate,
+					PO.UpdatedBy,
+				    PO.IsActive,
+					PO.IsDeleted,
+					PO.StatusId,
+					PO.VendorId,
+					PO.VendorName,
+					PO.VendorCode,					
+					PO.[Status],
+					PO.Requisitioner AS RequestedBy							
+			  FROM VendorRFQPurchaseOrder PO WITH (NOLOCK)
+			  INNER JOIN dbo.EmployeeManagementStructure EMS WITH (NOLOCK) ON EMS.ManagementStructureId = PO.ManagementStructureId		              			  
+		 	  WHERE ((PO.IsDeleted = @IsDeleted) AND (@StatusID IS NULL OR PO.StatusId = @StatusID)) 
+			      AND EMS.EmployeeId = 	@EmployeeId AND PO.MasterCompanyId = @MasterCompanyId	
+				  AND  (@VendorId  IS NULL OR PO.VendorId = @VendorId)
+			), ResultCount AS(Select COUNT(VendorRFQPurchaseOrderId) AS totalItems FROM Result)
+			SELECT * INTO #TempResult FROM  Result
+			 WHERE ((@GlobalFilter <>'' AND ((VendorRFQPurchaseOrderNumber LIKE '%' +@GlobalFilter+'%') OR
+					(CreatedBy LIKE '%' +@GlobalFilter+'%') OR
+					(UpdatedBy LIKE '%' +@GlobalFilter+'%') OR	
+					(VendorName LIKE '%' +@GlobalFilter+'%') OR		
+					(RequestedBy LIKE '%' +@GlobalFilter+'%') OR						
+					([Status]  LIKE '%' +@GlobalFilter+'%')))
+					OR   
+					(@GlobalFilter='' AND (ISNULL(@VendorRFQPurchaseOrderNumber,'') ='' OR VendorRFQPurchaseOrderNumber LIKE '%' + @VendorRFQPurchaseOrderNumber+'%') AND 
+					(ISNULL(@CreatedBy,'') ='' OR CreatedBy LIKE '%' + @CreatedBy + '%') AND
+					(ISNULL(@UpdatedBy,'') ='' OR UpdatedBy LIKE '%' + @UpdatedBy + '%') AND					
+					(ISNULL(@VendorName,'') ='' OR VendorName LIKE '%' + @VendorName + '%') AND
+					(ISNULL(@RequestedBy,'') ='' OR RequestedBy LIKE '%' + @RequestedBy + '%') AND
+					(ISNULL(@Status,'') ='' OR Status LIKE '%' + @Status + '%') AND									
+					(ISNULL(@OpenDate,'') ='' OR CAST(OpenDate AS Date) = CAST(@OpenDate AS date)) AND									
+					(ISNULL(@CreatedDate,'') ='' OR CAST(CreatedDate AS Date)=CAST(@CreatedDate AS date)) AND
+					(ISNULL(@UpdatedDate,'') ='' OR CAST(UpdatedDate AS date)=CAST(@UpdatedDate AS date)))
+				   )
+
+			SELECT @Count = COUNT(VendorRFQPurchaseOrderId) FROM #TempResult			
+
+			SELECT *, @Count AS NumberOfItems FROM #TempResult
+			ORDER BY  
+			CASE WHEN (@SortOrder=1  AND @SortColumn='VendorRFQPurchaseOrderNumber')  THEN VendorRFQPurchaseOrderNumber END ASC,
+			CASE WHEN (@SortOrder=-1 AND @SortColumn='VendorRFQPurchaseOrderNumber')  THEN VendorRFQPurchaseOrderNumber END DESC,
+			CASE WHEN (@SortOrder=1  AND @SortColumn='OpenDate')  THEN OpenDate END ASC,
+			CASE WHEN (@SortOrder=-1 AND @SortColumn='OpenDate')  THEN OpenDate END DESC,
+			CASE WHEN (@SortOrder=1  AND @SortColumn='VendorName')  THEN VendorName END ASC,
+			CASE WHEN (@SortOrder=-1 AND @SortColumn='VendorName')  THEN VendorName END DESC,			
+			CASE WHEN (@SortOrder=1  AND @SortColumn='RequestedBy')  THEN RequestedBy END ASC,
+			CASE WHEN (@SortOrder=-1 AND @SortColumn='RequestedBy')  THEN RequestedBy END DESC,			         
+			CASE WHEN (@SortOrder=1  AND @SortColumn='UpdatedDate')  THEN UpdatedDate END ASC,
+			CASE WHEN (@SortOrder=-1 AND @SortColumn='UpdatedDate')  THEN UpdatedDate END DESC,
+			CASE WHEN (@SortOrder=1  AND @SortColumn='CreatedBy')  THEN CreatedBy END ASC,
+			CASE WHEN (@SortOrder=-1 AND @SortColumn='CreatedBy')  THEN CreatedBy END DESC,
+			CASE WHEN (@SortOrder=1  AND @SortColumn='UpdatedBy')  THEN UpdatedBy END ASC,
+			CASE WHEN (@SortOrder=-1 AND @SortColumn='UpdatedBy')  THEN UpdatedBy END DESC,
+			CASE WHEN (@SortOrder=1  AND @SortColumn='CreatedDate')  THEN CreatedDate END ASC,
+			CASE WHEN (@SortOrder=-1 AND @SortColumn='CreatedDate')  THEN CreatedDate END DESC
+			OFFSET @RecordFrom ROWS 
+			FETCH NEXT @PageSize ROWS ONLY
+		END
+		COMMIT  TRANSACTION
+	END TRY    
+	BEGIN CATCH      
+		IF @@trancount > 0
+			PRINT 'ROLLBACK'
+			ROLLBACK TRAN;
+			DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+            , @AdhocComments     VARCHAR(150)    = 'GetVendorRFQPurchaseOrderList' 
+            , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ ISNULL(@VendorRFQPurchaseOrderNumber, '') + ''
+            , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+            exec spLogException 
+                    @DatabaseName           = @DatabaseName
+                    , @AdhocComments          = @AdhocComments
+                    , @ProcedureParameters = @ProcedureParameters
+                    , @ApplicationName        =  @ApplicationName
+                    , @ErrorLogID             = @ErrorLogID OUTPUT ;
+            RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1, @ErrorLogID)
+            RETURN(1);
+	END CATCH
+END
---
+++ PAS_DB/dbo/Tables/VendorRFQPurchaseOrder.sql
@@ -0,0 +1,61 @@
+﻿CREATE TABLE [dbo].[VendorRFQPurchaseOrder] (
+    [VendorRFQPurchaseOrderId]     BIGINT         IDENTITY (1, 1) NOT NULL,
+    [VendorRFQPurchaseOrderNumber] VARCHAR (50)   NOT NULL,
+    [OpenDate]                     DATETIME2 (7)  NOT NULL,
+    [ClosedDate]                   DATETIME2 (7)  NULL,
+    [NeedByDate]                   DATETIME       NOT NULL,
+    [PriorityId]                   BIGINT         NOT NULL,
+    [Priority]                     VARCHAR (100)  NULL,
+    [VendorId]                     BIGINT         NOT NULL,
+    [VendorName]                   VARCHAR (100)  NULL,
+    [VendorCode]                   VARCHAR (100)  NULL,
+    [VendorContactId]              BIGINT         NOT NULL,
+    [VendorContact]                VARCHAR (100)  NULL,
+    [VendorContactPhone]           VARCHAR (50)   NULL,
+    [CreditTermsId]                INT            NULL,
+    [Terms]                        VARCHAR (500)  NULL,
+    [CreditLimit]                  DECIMAL (18)   NULL,
+    [RequestedBy]                  BIGINT         NOT NULL,
+    [Requisitioner]                VARCHAR (100)  NULL,
+    [StatusId]                     BIGINT         NOT NULL,
+    [Status]                       VARCHAR (100)  NULL,
+    [StatusChangeDate]             DATETIME2 (7)  NULL,
+    [Resale]                       BIT            NOT NULL,
+    [DeferredReceiver]             BIT            NOT NULL,
+    [Memo]                         NVARCHAR (MAX) NULL,
+    [Notes]                        NVARCHAR (MAX) NULL,
+    [ManagementStructureId]        BIGINT         NOT NULL,
+    [Level1]                       VARCHAR (200)  NULL,
+    [Level2]                       VARCHAR (200)  NULL,
+    [Level3]                       VARCHAR (200)  NULL,
+    [Level4]                       VARCHAR (200)  NULL,
+    [MasterCompanyId]              INT            NOT NULL,
+    [CreatedBy]                    VARCHAR (256)  NOT NULL,
+    [UpdatedBy]                    VARCHAR (256)  NOT NULL,
+    [CreatedDate]                  DATETIME2 (7)  NOT NULL,
+    [UpdatedDate]                  DATETIME2 (7)  NOT NULL,
+    [IsActive]                     BIT            CONSTRAINT [VendorRFQPurchaseOrder_DC_Active] DEFAULT ((1)) NOT NULL,
+    [IsDeleted]                    BIT            CONSTRAINT [DF__VendorRFQPurchaseOrder__IsDel__7A1D154F] DEFAULT ((0)) NOT NULL,
+    [PDFPath]                      NVARCHAR (100) NULL,
+    CONSTRAINT [PK_VendorRFQPurchaseOrder] PRIMARY KEY CLUSTERED ([VendorRFQPurchaseOrderId] ASC),
+    FOREIGN KEY ([StatusId]) REFERENCES [dbo].[VendorRFQStatus] ([VendorRFQStatusId]),
+    CONSTRAINT [FK_VendorRFQPurchaseOrder_ManagementStructure] FOREIGN KEY ([ManagementStructureId]) REFERENCES [dbo].[ManagementStructure] ([ManagementStructureId]),
+    CONSTRAINT [FK_VendorRFQPurchaseOrder_MasterCompany] FOREIGN KEY ([MasterCompanyId]) REFERENCES [dbo].[MasterCompany] ([MasterCompanyId]),
+    CONSTRAINT [FK_VendorRFQPurchaseOrder_Priority] FOREIGN KEY ([PriorityId]) REFERENCES [dbo].[Priority] ([PriorityId]),
+    CONSTRAINT [FK_VendorRFQPurchaseOrder_Vendor] FOREIGN KEY ([VendorId]) REFERENCES [dbo].[Vendor] ([VendorId]),
+    CONSTRAINT [FK_VendorRFQPurchaseOrder_VendorContact] FOREIGN KEY ([VendorContactId]) REFERENCES [dbo].[VendorContact] ([VendorContactId])
+);
+
+
+GO
+
+
+CREATE TRIGGER [dbo].[TrgVendorRFQPurchaseOrderAudit]
+   ON [dbo].[VendorRFQPurchaseOrder]
+   AFTER INSERT,DELETE,UPDATE
+AS 
+BEGIN
+	INSERT INTO [dbo].[VendorRFQPurchaseOrderAudit]
+	SELECT * FROM INSERTED
+	SET NOCOUNT ON;
+END

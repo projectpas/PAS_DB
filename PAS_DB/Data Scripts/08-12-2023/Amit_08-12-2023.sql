ab4c53f1fda507e7f5da90277f7b905a3b1fcaad
amitghediyasilverxis <amit.ghediya@SILVERXIS.com>
2023-12-08 16:36:27 +0530

    PN-6082 update stocktype position for default.

---
+++ PAS_DB/Data Scripts/08-12-2023/Amit_08-12-2023.sql
@@ -0,0 +1,3 @@
+﻿
+UPDATE FieldMaster SET FieldSortOrder = 1 WHERE FieldName = 'bulkStkLineAdjNumber' AND ModuleId = (SELECT GridModuleId FROM DBO.GridModule WHERE ModuleName = 'BulkStocklineAdjustmentList');
+UPDATE FieldMaster SET FieldSortOrder = 2 WHERE FieldName = 'adjustmentType' AND ModuleId = (SELECT GridModuleId FROM DBO.GridModule WHERE ModuleName = 'BulkStocklineAdjustmentList');

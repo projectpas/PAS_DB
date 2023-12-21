b9d80a095cf782c88e1cfee30ea51b2c50a733be
shrey.chandegara <shrey.chandegara@WIN-4VI6LDEMCIN>
2023-12-21 16:38:30 +0530

    PN-6283 Distribution Setup audit table and trigger

---
+++ PAS_Database/dbo/Tables/DistributionSetupAudit.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[DistributionSetupAudit] (
+    [DistributionSetupAuditId] BIGINT        IDENTITY (1, 1) NOT NULL,
+    [ID]                       BIGINT        NOT NULL,
+    [Name]                     VARCHAR (200) NOT NULL,
+    [GlAccountId]              BIGINT        NOT NULL,
+    [GlAccountNumber]          VARCHAR (200) NOT NULL,
+    [GlAccountName]            VARCHAR (200) NOT NULL,
+    [JournalTypeId]            BIGINT        NOT NULL,
+    [DistributionMasterId]     BIGINT        NOT NULL,
+    [IsDebit]                  BIT           NULL,
+    [DisplayNumber]            INT           NULL,
+    [MasterCompanyId]          INT           NOT NULL,
+    [CreatedBy]                VARCHAR (256) NOT NULL,
+    [UpdatedBy]                VARCHAR (256) NOT NULL,
+    [CreatedDate]              DATETIME2 (7) CONSTRAINT [DF_DistributionSetupAudit_CreatedDate] DEFAULT (getutcdate()) NOT NULL,
+    [UpdatedDate]              DATETIME2 (7) CONSTRAINT [DF_DistributionSetupAudit_UpdatedDate] DEFAULT (getutcdate()) NOT NULL,
+    [IsActive]                 BIT           CONSTRAINT [DF__DistributionSetupAudit__IsActive] DEFAULT ((1)) NOT NULL,
+    [IsDeleted]                BIT           CONSTRAINT [DF__DistributionSetupAudit__IsDeleted] DEFAULT ((0)) NOT NULL,
+    [CRDRType]                 INT           NULL,
+    [DistributionSetupCode]    VARCHAR (100) NOT NULL,
+    [IsManualText]             BIT           NULL,
+    [ManualText]               VARCHAR (100) NULL,
+    CONSTRAINT [PK_DistributionSetupAudit] PRIMARY KEY CLUSTERED ([DistributionSetupAuditId] ASC)
+);
+

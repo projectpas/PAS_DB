5240ac23f99882e8c1660d017e5b1afc3cc2747b
amitghediyasilverxis <amit.ghediya@SILVERXIS.com>
2024-02-08 09:47:35 +0530

    PN-6557 added for Proforma in SO billing SP.

--- PAS_Database/dbo/Stored Procedures/GetProformaInvoicingSODetails.sql
+++ PAS_Database/dbo/Stored Procedures/GetProformaInvoicingSODetails.sql
@@ -18,7 +18,44 @@ BEGIN
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 SET NOCOUNT ON;
 	BEGIN TRY
-			SELECT sop.SalesOrderId, 
+			IF(@soBillingInvoicingId > 0)
+			BEGIN
+				SELECT sop.SalesOrderId, 
+					   sop.SalesOrderPartId, 
+					   0 AS SalesOrderShippingId, 
+					   NULL AS ShipDate, 
+					   so.SalesOrderNumber, 
+					   CONCAT(emp.FirstName, ' ', emp.LastName) AS EmployeeName,
+					   so.EmployeeId, 
+					   so.OpenDate, 
+					   so.CustomerReference AS CustomerRef, 
+					   so.CustomerId, 
+					   CONCAT(empsp.FirstName, ' ', empsp.LastName) AS SalesPerson,
+					   so.SalesPersonId, 
+					   cf.CreditLimit, 
+					   cf.CreditTermsId, 
+					   ct.[Name] AS CreditTerm, 
+					   cf.CurrencyId,
+					   so.TypeId, 
+					   sotype.[Name] AS RevType, 
+					   (ISNULL(SOR.QtyToReserve, 0) - ISNULL(sobii.NoofPieces, 0)) AS NoofPieces,
+					   sobi.GrandTotal
+				FROM DBO.SalesOrderPart sop WITH (NOLOCK)
+				INNER JOIN DBO.SalesOrder so WITH (NOLOCK) ON so.SalesOrderId = sop.SalesOrderId
+				INNER JOIN DBO.Customer co WITH (NOLOCK) ON co.CustomerId = so.CustomerId
+				LEFT JOIN DBO.CustomerFinancial cf WITH (NOLOCK) ON cf.CustomerId = co.CustomerId
+				INNER JOIN DBO.CreditTerms ct WITH (NOLOCK) ON ct.CreditTermsId = cf.CreditTermsId
+				LEFT JOIN DBO.Employee emp WITH (NOLOCK) ON emp.EmployeeId = so.EmployeeId
+				LEFT JOIN DBO.Employee empsp WITH (NOLOCK) ON empsp.EmployeeId = so.SalesPersonId
+				INNER JOIN DBO.MasterSalesOrderQuoteTypes sotype WITH (NOLOCK) ON sotype.Id = so.TypeId
+				LEFT JOIN DBO.SalesOrderReserveParts SOR WITH (NOLOCK) ON SOR.SalesOrderPartId = sop.SalesOrderPartId
+				LEFT JOIN DBO.SalesOrderBillingInvoicingItem sobii WITH (NOLOCK) ON sobii.SalesOrderPartId = sop.SalesOrderPartId AND ISNULL(sobii.IsProforma,0) = 1
+				LEFT JOIN DBO.SalesOrderBillingInvoicing sobi WITH (NOLOCK) ON sobi.SOBillingInvoicingId = sobii.SOBillingInvoicingId AND ISNULL(sobi.IsProforma,0) = 1
+				WHERE sop.SalesOrderPartId = @SalesOrderPartId;
+			END
+			ELSE
+			BEGIN
+				SELECT sop.SalesOrderId, 
 				   sop.SalesOrderPartId, 
 				   0 AS SalesOrderShippingId, 
 				   NULL AS ShipDate, 
@@ -50,6 +87,7 @@ SET NOCOUNT ON;
 			LEFT JOIN DBO.SalesOrderBillingInvoicingItem sobii WITH (NOLOCK) ON sobii.SalesOrderPartId = sop.SalesOrderPartId AND ISNULL(sobii.IsProforma,0) = 1
 			LEFT JOIN DBO.SalesOrderBillingInvoicing sobi WITH (NOLOCK) ON sobi.SOBillingInvoicingId = sobii.SOBillingInvoicingId AND ISNULL(sobi.IsProforma,0) = 1
 			WHERE sop.SalesOrderPartId = @SalesOrderPartId AND sobii.SOBillingInvoicingId = @soBillingInvoicingId;
+			END
 
 	END TRY    
 	BEGIN CATCH      

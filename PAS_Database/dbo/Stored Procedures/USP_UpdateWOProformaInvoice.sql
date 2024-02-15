d15ee868e429f5c844bc77a4155c9a1eb6d875a5
devendra shekh <devendra@silverxis.com>
2024-02-15 17:14:34 +0530

    PN-7163

--- PAS_Database/dbo/Stored Procedures/USP_UpdateWOProformaInvoice.sql
+++ PAS_Database/dbo/Stored Procedures/USP_UpdateWOProformaInvoice.sql
@@ -16,6 +16,7 @@
  ** PR   	Date			Author					Change Description            
  ** --   	--------		-------				--------------------------------     
 	1		08/02/2024		Devendra Shekh			CREATED
+	2		15/02/2024		Devendra Shekh			Modified
 
 	EXEC [USP_UpdateWOProformaInvoice] 4270,3737
 
@@ -23,7 +24,8 @@
 
 CREATE   Procedure [dbo].[USP_UpdateWOProformaInvoice]
 	@WorkOrderId  BIGINT,
-	@WorkFlowWorkOrderId BIGINT
+	@WorkFlowWorkOrderId BIGINT,
+	@BillingInvoicingId BIGINT = NULL
 AS
 BEGIN
 
@@ -33,7 +35,7 @@ BEGIN
 		BEGIN TRANSACTION
 			BEGIN		
 
-				DECLARE @TotalRec BIGINT = 0,@StartCount BIGINT = 1;
+				DECLARE @TotalRec BIGINT = 0,@StartCount BIGINT = 1,@WorkOrderPartNoId BIGINT = 0, @WOProfomaBillingInvoicingId BIGINT = 0;
 
 				IF OBJECT_ID('tempdb.dbo.#tempProformaInvoice', 'U') IS NOT NULL
 					DROP TABLE #tempProformaInvoice; 
@@ -48,6 +50,11 @@ BEGIN
 				INSERT INTO #tempProformaInvoice([BillingInvoicingId], [WorkOrderId], [WorkFlowWorkOrderId])
 				SELECT [BillingInvoicingId],[WorkOrderId],[WorkFlowWorkOrderId] 
 				FROM [dbo].[WorkOrderBillingInvoicing] WITH(NOLOCK) WHERE [WorkOrderId] = @WorkOrderId AND [WorkFlowWorkOrderId] = @WorkFlowWorkOrderId AND ISNULL(IsPerformaInvoice, 0) = 1
+
+				SELECT TOP 1 @WorkOrderPartNoId = WorkOrderPartId FROM [dbo].[WorkOrderBillingInvoicingItem] WITH(NOLOCK) WHERE [BillingInvoicingId] = @BillingInvoicingId;
+
+				SELECT TOP 1 @WOProfomaBillingInvoicingId = BillingInvoicingId 
+				FROM [dbo].[WorkOrderBillingInvoicingItem] WITH(NOLOCK) WHERE WorkOrderPartId = @WorkOrderPartNoId AND ISNULL(IsPerformaInvoice, 0) = 1 AND ISNULL(IsVersionIncrease, 0) = 0;
 
 				SET @TotalRec = (SELECT MAX(Id) FROM #tempProformaInvoice);
 
@@ -72,6 +79,19 @@ BEGIN
 					END
 
 				END
+
+				IF(ISNULL(@WOProfomaBillingInvoicingId, 0) > 0)
+				BEGIN
+					PRINT @WOProfomaBillingInvoicingId
+					UPDATE [dbo].[WorkOrderBillingInvoicing]
+					SET IsInvoicePosted = 1
+					WHERE [BillingInvoicingId] = @WOProfomaBillingInvoicingId
+
+					UPDATE [dbo].[WorkOrderBillingInvoicingItem]
+					SET IsInvoicePosted = 1
+					WHERE [BillingInvoicingId] = @WOProfomaBillingInvoicingId
+				END
+
 			END
 		COMMIT  TRANSACTION
 

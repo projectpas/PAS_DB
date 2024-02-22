b6233248983d5c3b234a8e8c440f8d0d6992d402
Ekta <ekta@silverxis.com>
2024-02-22 19:22:53 +0530

    PN-7272  - @Module Id is added

--- PAS_Database/dbo/Stored Procedures/GetDocumentDetailsByReferenceId.sql
+++ PAS_Database/dbo/Stored Procedures/GetDocumentDetailsByReferenceId.sql
@@ -14,14 +14,16 @@
     3     30-01-2024   Ekta Chandegra	   Add masterCompanyId parameter
 	4	  12-02-2024   Ekta Chandegra      IsDeleted, IsActive fields are added for join  
 	5	  12-02-2024   Ekta Chandegra      AttachmentDetailId is retrieve for documents listing
+	6     22-02-2024   Ekta Chandegra      @ModuleId added
 
-EXEC [GetDocumentDetailsByReferenceId] 411, 1
+EXEC [GetDocumentDetailsByReferenceId] 73, 1 , 77
 
 **************************************************************/ 
 
 CREATE  PROCEDURE [dbo].[GetDocumentDetailsByReferenceId]
 	@ReferenceId BIGINT = NULL,
-	@MasterCompanyId BIGINT = NULL
+	@MasterCompanyId BIGINT = NULL,
+	@ModuleId BIGINT = NULL
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -32,7 +34,7 @@ BEGIN
 		ad.FileSize, ad.AttachmentId , ad.AttachmentDetailId
 		FROM DBO.AttachmentDetails ad 
 		INNER JOIN DBO.CommonDocumentDetails cdd WITH (NOLOCK) ON ad.AttachmentId = cdd.AttachmentId AND ad.IsActive = cdd.IsActive AND ad.IsDeleted = cdd.IsDeleted
-		WHERE cdd.MasterCompanyId = @MasterCompanyId AND cdd.ReferenceId = @ReferenceId AND cdd.IsActive = 1 AND cdd.IsDeleted = 0 
+		WHERE cdd.MasterCompanyId = @MasterCompanyId AND cdd.ReferenceId = @ReferenceId AND cdd.IsActive = 1 AND cdd.IsDeleted = 0 AND cdd.ModuleId = @ModuleId
 	END TRY
 	BEGIN CATCH
 			DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 

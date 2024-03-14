833197ca2412ac327eae9878732d7d09485f856d
amitghediyasilverxis <amit.ghediya@SILVERXIS.com>
2024-03-14 18:34:30 +0530

    PN-6767 setting table for vendor payments.

---
+++ PAS_Database/dbo/Tables/VendorPaymentSettingMasterAudit.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[VendorPaymentSettingMasterAudit] (
+    [VendorPaymentSettingAuditId] BIGINT        IDENTITY (1, 1) NOT NULL,
+    [VendorPaymentSettingId]      BIGINT        NOT NULL,
+    [IsEnforceApproval]           BIT           NOT NULL,
+    [Effectivedate]               DATETIME2 (7) NULL,
+    [MasterCompanyId]             INT           NOT NULL,
+    [CreatedBy]                   VARCHAR (256) NOT NULL,
+    [UpdatedBy]                   VARCHAR (256) NOT NULL,
+    [CreatedDate]                 DATETIME2 (7) NOT NULL,
+    [UpdatedDate]                 DATETIME2 (7) NOT NULL,
+    [IsActive]                    BIT           NOT NULL,
+    [IsDeleted]                   BIT           NOT NULL,
+    CONSTRAINT [PK_VendorPaymentSettingMasterAudit] PRIMARY KEY CLUSTERED ([VendorPaymentSettingAuditId] ASC)
+);
+

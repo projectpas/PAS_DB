7de54cf6c10de36963e8e7edde596ae6ada458ef
amitghediyasilverxis <amit.ghediya@SILVERXIS.com>
2024-03-18 20:13:37 +0530

    PN-6767 update sp & add new for get checkregister data.

--- PAS_Database/dbo/Stored Procedures/Procs1/GetLegalEntityDetailsFromMSId.sql
+++ PAS_Database/dbo/Stored Procedures/Procs1/GetLegalEntityDetailsFromMSId.sql
@@ -14,11 +14,13 @@
  ** PR   Date         Author		Change Description            
  ** --   --------     -------		--------------------------------          
     1    06/03/2023  Moin Bloch     Created
+	2    18/03/2024  AMIT GHEDIYA   Update for ger LE based on EmployeId.
      
 -- EXEC GetLegalEntityDetailsFromMSId 1
 ************************************************************************/
 CREATE   PROCEDURE [dbo].[GetLegalEntityDetailsFromMSId]
-@EntityStructureId bigint
+--@EntityStructureId bigint
+@EmployeeId BIGINT
 AS
 BEGIN
 	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
@@ -33,13 +35,21 @@ BEGIN
 		--WHERE EST.EntityStructureId = @EntityStructureId
 		--GROUP BY MSL.LegalEntityId,LE.[Name]
 
-		SELECT LE.[LegalEntityId],LE.[Name]
-		FROM [dbo].[ManagementStructureLevel] MSL
-		INNER JOIN [dbo].[EntityStructureSetup] EST ON MSL.ID = EST.Level1Id
-		INNER JOIN [dbo].[LegalEntity] LE ON MSL.LegalEntityId = LE.LegalEntityId
-		INNER JOIN [dbo].[RoleManagementStructure] RMS ON EST.EntityStructureId = RMS.EntityStructureId
-		WHERE RMS.RoleId IN (SELECT RoleId FROM [dbo].[RoleManagementStructure] WHERE EntityStructureId = @EntityStructureId)
-		GROUP BY LE.[LegalEntityId],LE.[Name]
+		--SELECT LE.[LegalEntityId],LE.[Name]
+		--FROM [dbo].[ManagementStructureLevel] MSL
+		--INNER JOIN [dbo].[EntityStructureSetup] EST ON MSL.ID = EST.Level1Id
+		--INNER JOIN [dbo].[LegalEntity] LE ON MSL.LegalEntityId = LE.LegalEntityId
+		--INNER JOIN [dbo].[RoleManagementStructure] RMS ON EST.EntityStructureId = RMS.EntityStructureId
+		--WHERE RMS.RoleId IN (SELECT RoleId FROM [dbo].[RoleManagementStructure] WHERE EntityStructureId = @EntityStructureId)
+		--GROUP BY LE.[LegalEntityId],LE.[Name]
+
+		SELECT DISTINCT LE.LegalEntityId, LE.[Name]
+		FROM RoleManagementStructure RMS WITH(NOLOCK) 
+		JOIN EmployeeUserRole EUR WITH(NOLOCK) ON RMS.RoleId = EUR.RoleId
+		JOIN EntityStructureSetup EST WITH(NOLOCK) ON EST.EntityStructureId = RMS.EntityStructureId
+		JOIN ManagementStructureLevel MSL WITH(NOLOCK) ON MSL.ID = EST.Level1Id
+		JOIN LegalEntity LE WITH(NOLOCK) ON MSL.LegalEntityId = LE.LegalEntityId
+		WHERE RMS.RoleId IN (SELECT DISTINCT RoleId FROM EmployeeUserRole WHERE EmployeeId = @EmployeeId)
 
 	END TRY 
 	BEGIN CATCH      
@@ -49,7 +59,7 @@ BEGIN
 				DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
 -----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
               , @AdhocComments     VARCHAR(150)    = 'GetLegalEntityDetailsFromMSId' 
-              , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ CAST(ISNULL(@EntityStructureId, '') AS varchar(100))													 
+              , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ CAST(ISNULL(@EmployeeId, '') AS varchar(100))													 
               , @ApplicationName VARCHAR(100) = 'PAS'
 -----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
               exec spLogException 

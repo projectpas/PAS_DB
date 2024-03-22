d92843eaecd9b58a4ccd48b0c55e1d66d3314906
Rajesh Gami <rajesh@silverxis.com>
2024-03-22 19:31:26 +0530

    ILS Script

--- PAS_Database/dbo/User Defined Types/IlsRfqQuoteDetailsType.sql
+++ PAS_Database/dbo/User Defined Types/IlsRfqQuoteDetailsType.sql
@@ -1,12 +1,16 @@
 ﻿CREATE TYPE [dbo].[IlsRfqQuoteDetailsType] AS TABLE (
-    [IlsQty]          INT             NULL,
-    [IlsTraceability] VARCHAR (50)    NULL,
-    [IlsUom]          VARCHAR (50)    NULL,
-    [IlsPrice]        DECIMAL (10, 2) NULL,
-    [IlsPriceType]    VARCHAR (50)    NULL,
-    [IlsTagDate]      DATETIME2 (7)   NULL,
-    [IlsLeadTime]     VARCHAR (50)    NULL,
-    [IlsMinQty]       INT             NULL,
-    [IlsComment]      VARCHAR (MAX)   NULL,
-    [IlsCondition]    VARCHAR (50)    NULL);
+    [CustomerRfqQuoteDetailsId] BIGINT          NULL,
+    [CustomerRfqQuoteId]        BIGINT          NULL,
+    [IlsQty]                    INT             NULL,
+    [IlsTraceability]           VARCHAR (50)    NULL,
+    [IlsUom]                    VARCHAR (50)    NULL,
+    [IlsPrice]                  DECIMAL (10, 2) NULL,
+    [IlsPriceType]              VARCHAR (50)    NULL,
+    [IlsTagDate]                DATETIME2 (7)   NULL,
+    [IlsLeadTime]               VARCHAR (50)    NULL,
+    [IlsMinQty]                 INT             NULL,
+    [IlsComment]                VARCHAR (MAX)   NULL,
+    [IlsCondition]              VARCHAR (50)    NULL);
+
+
 

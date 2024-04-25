bc84e2073e19bc9fcb5e0d8c08a970a4628e9a52
Vishal Suthar <vishal@silverxis.com>
2024-04-25 16:25:25 +0530

    Added missing History Views

---
+++ PAS_Database/dbo/Views/vw_EmployeeLeaveType_Audit.sql
@@ -0,0 +1,5 @@
+﻿CREATE   VIEW [dbo].[vw_EmployeeLeaveType_Audit]
+AS
+	SELECT C.AuditEmployeeLeaveTypeId AS PkID, C.EmployeeLeaveTypeId AS ID, LeaveType AS [Employee Leave Type], Description AS [Description], C.CreatedBy AS [Created By],
+	C.CreatedDate AS [Created Date], C.UpdatedBy AS [Updated By], C.UpdatedDate AS [Updated Date], C.IsActive AS [Is Active], C.IsDeleted AS [Is Deleted]
+	FROM [DBO].[EmployeeLeaveTypeAudit] C WITH (NOLOCK)

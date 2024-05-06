ea57658c07a38830d73631ca73ab64a7c34e9150
shrey.chandegara <shrey.chandegara@silverxis.com>
2024-05-06 17:20:22 +0530

    Integration View

---
+++ PAS_Database/dbo/Views/vw_IntegrationPortalAudit.sql
@@ -0,0 +1,8 @@
+﻿CREATE   VIEW [dbo].[vw_IntegrationPortalAudit]
+AS
+	SELECT ipor.IntegrationPortalAuditId  AS PkID, ipor.IntegrationPortalId AS ID	,ipor.Description,ipor.PortalURL [Portal URL]
+	,(EMP_CR.FirstName + ' ' + EMP_CR.LastName) AS [Created By],
+	ipor.CreatedDate AS [Created Date], (EMP_UP.FirstName + ' ' + EMP_UP.LastName) AS [Updated By], ipor.UpdatedDate AS [Updated Date], ipor.IsActive AS [Is Active], ipor.IsDeleted AS [Is Deleted]
+	FROM [DBO].IntegrationPortalAudit ipor WITH (NOLOCK) 	
+	LEFT JOIN [dbo].[Employee] EMP_CR WITH (NOLOCK) ON ipor.CreatedBy = EMP_CR.EmployeeId
+    LEFT JOIN [dbo].[Employee] EMP_UP WITH (NOLOCK) ON ipor.UpdatedBy = EMP_UP.EmployeeId

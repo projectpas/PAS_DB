597dc3645b5e133300dd86cf1f9f55074008f543
devendra <devendra@silverxis.com>
2024-05-07 13:04:59 +0530

    PN-7971

---
+++ PAS_Database/dbo/Views/vw_ScrapreasonAudit.sql
@@ -0,0 +1,6 @@
+﻿
+CREATE   VIEW [dbo].[vw_ScrapreasonAudit] 
+AS
+	SELECT	SRA.ScrapReasonAuditId AS PkID, Id AS ID, Reason AS 'Reason', SRA.Memo
+	,SRA.IsActive AS 'Active ?', SRA.IsDeleted AS 'Deleted ?', SRA.CreatedBy as 'Created By', SRA.UpdatedBy AS 'Updated By', SRA.CreatedDate AS 'Created On', SRA.UpdatedDate AS 'Updated On', SRA.MasterCompanyId
+	FROM dbo.ScrapReasonAudit AS SRA WITH (NOLOCK)
---
+++ PAS_Database/dbo/Views/vw_TeardownReasonAudit.sql
@@ -0,0 +1,7 @@
+﻿
+CREATE   VIEW [dbo].[vw_TeardownReasonAudit] 
+AS
+	SELECT	TRA.AuditTeardownReasonId AS PkID, TeardownReasonId AS ID, CTT.Name AS 'Teardown Type', Reason, TRA.Memo
+	,Sequence AS 'Sequence No', TRA.IsActive AS 'Active ?', TRA.IsDeleted AS 'Deleted ?', TRA.CreatedBy as 'Created By', TRA.UpdatedBy AS 'Updated By', TRA.CreatedDate AS 'Created On', TRA.UpdatedDate AS 'Updated On', TRA.MasterCompanyId
+	FROM dbo.TeardownReasonAudit AS TRA WITH (NOLOCK)
+	LEFT JOIN [dbo].[CommonTeardownType] CTT WITH(NOLOCK) ON CTT.CommonTeardownTypeId = TRA.CommonTeardownTypeId

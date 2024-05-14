172eac3a95f16f7f228ac433f047270c4c669f71
Moin Bloch <moin@silverxis.com>
2024-05-14 19:48:46 +0530

    PN-7946

--- PAS_Database/dbo/Stored Procedures/Procs1/sp_GetWOShippingChildList.sql
+++ PAS_Database/dbo/Stored Procedures/Procs1/sp_GetWOShippingChildList.sql
@@ -16,6 +16,7 @@
  ** --   --------     -------			--------------------------------  
 	1    
 	2    01/01/2024   Devendra Shekh	updated for serialnumber for MPN
+	3    14/05/2024   Moin Bloch	    updated for dublicate record issue PN-7946
 
 EXEC DBO.sp_GetWOShippingChildList @ItemMasterIdlist=20751,@WorkOrderId =618 ,@WorkOrderPartId=10
 **************************************************************/ 
@@ -29,53 +30,53 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
  SET NOCOUNT ON;  
   
   BEGIN TRY  
-   BEGIN TRANSACTION  
-    BEGIN  
-     SELECT   
-      wopt.PickTicketId as WOPickTicketId,  
-      wos.WorkOrderShippingId,  
-      wos.ShipDate,  
-      wos.WOShippingNum,  
-      wopt.PickTicketNumber as  WOPickTicketNumber,  
-      (ISNULL(wopt.QtyToShip,0) - ISNULL(wosi.QtyShipped,0)) as QtyToShip,  
-      wo.WorkOrderNum,  
-      CASE WHEN ISNULL(wop.RevisedItemmasterid, 0) > 0 THEN wop.RevisedPartNumber ELSE imt.PartNumber END as 'PartNumber',  
-               CASE WHEN ISNULL(wop.RevisedItemmasterid, 0) > 0 THEN wop.RevisedPartDescription ELSE imt.PartDescription END as 'PartDescription',   
-      sl.StockLineNumber,  
-      CASE WHEN ISNULL(wop.RevisedSerialNumber, '') = '' THEN sl.SerialNumber ELSE wop.RevisedSerialNumber END AS 'SerialNumber',cr.[Name] as CustomerName,  
-      woc.CustomsValue,woc.CommodityCode,  
-      ISNULL(wosi.QtyShipped,0) as QtyShipped,  
+   --BEGIN TRANSACTION  
+   -- BEGIN  
+     SELECT  
+      wopt.[PickTicketId] AS WOPickTicketId,  
+      wos.[WorkOrderShippingId],  
+      wos.[ShipDate],  
+      wos.[WOShippingNum],  
+      wopt.[PickTicketNumber] AS  WOPickTicketNumber,  
+      (ISNULL(wopt.[QtyToShip],0) - ISNULL(wosi.[QtyShipped],0)) AS QtyToShip,  
+      wo.[WorkOrderNum],  
+      CASE WHEN ISNULL(wop.[RevisedItemmasterid], 0) > 0 THEN wop.[RevisedPartNumber] ELSE imt.[PartNumber] END AS 'PartNumber',  
+               CASE WHEN ISNULL(wop.[RevisedItemmasterid], 0) > 0 THEN wop.[RevisedPartDescription] ELSE imt.[PartDescription] END AS 'PartDescription',   
+      sl.[StockLineNumber],  
+      CASE WHEN ISNULL(wop.[RevisedSerialNumber], '') = '' THEN sl.[SerialNumber] ELSE wop.[RevisedSerialNumber] END AS 'SerialNumber',cr.[Name] AS CustomerName,  
+      woc.[CustomsValue],woc.CommodityCode,  
+      ISNULL(wosi.[QtyShipped],0) AS QtyShipped,  
       1 ItemNo,  
-      wop.WorkOrderId,wop.ID as WorkOrderPartId,  
-      wop.ItemMasterId,  
-      wos.AirwayBill,  
-      wPB.PackagingSlipNo,wPB.PackagingSlipId,  
-      wobii.WorkOrderShippingId AS WOShippingId, 
-	  wosi.FedexPdfPath,
-	  ISNULL(wop.IsFinishGood,0) IsFinishGood
-     FROM DBO.WOPickTicket wopt WITH (NOLOCK)   
-      INNER JOIN DBO.WorkOrderPartNumber wop WITH (NOLOCK) on wop.WorkOrderId = wopt.WorkOrderId  and wop.id=wopt.OrderPartId  
-      LEFT JOIN DBO.WorkOrderShippingItem wosi on wosi.WorkOrderPartNumId = wop.ID AND wosi.WOPickTicketId = wopt.PickTicketId  
-      LEFT JOIN DBO.WorkOrderShipping wos on wos.WorkOrderShippingId = wosi.WorkOrderShippingId  
-      INNER JOIN DBO.WorkOrder wo WITH (NOLOCK) on wo.WorkOrderId = wop.WorkOrderId  
-      LEFT JOIN DBO.ItemMaster imt  WITH (NOLOCK) on imt.ItemMasterId = wop.ItemMasterId  
-      LEFT JOIN DBO.Stockline sl WITH (NOLOCK) on sl.StockLineId = wop.StockLineId  
-      LEFT JOIN DBO.WorkOrderCustomsInfo woc WITH (NOLOCK) on woc.WorkOrderShippingId = wos.WorkOrderShippingId  
-      LEFT JOIN DBO.Customer cr WITH (NOLOCK) on cr.CustomerId = wo.CustomerId  
-      LEFT JOIN DBO.WorkOrderPackaginSlipItems wPI  WITH (NOLOCK) ON wopt.PickTicketId = wPI.WOPickTicketId AND wPI.WOPartNoId = wop.id  
-      LEFT JOIN DBO.WorkOrderPackaginSlipHeader wPB  WITH (NOLOCK) ON wPB.PackagingSlipId = wPI.PackagingSlipId  
-      OUTER APPLY (SELECT TOP 1 WorkOrderShippingId FROM DBO.WorkOrderBillingInvoicing WOBI   
-          WHERE wosi.WorkOrderShippingId = WOBI.WorkOrderShippingId) AS wobii  
-        
-     WHERE wopt.WorkOrderId=@WorkOrderId AND wopt.IsConfirmed=1 AND wopt.OrderPartId=@WorkOrderPartId   
-    END  
-   COMMIT  TRANSACTION  
+      wop.[WorkOrderId],
+	  wop.[ID] AS WorkOrderPartId,  
+      wop.[ItemMasterId],  
+      wos.[AirwayBill],  
+      wPB.[PackagingSlipNo],wPB.PackagingSlipId,  
+      wobii.[WorkOrderShippingId] AS WOShippingId, 
+	  wosi.[FedexPdfPath],
+	  ISNULL(wop.[IsFinishGood],0) IsFinishGood
+     FROM [dbo].[WOPickTicket] wopt WITH (NOLOCK)   
+      INNER JOIN [dbo].[WorkOrderPartNumber] wop WITH (NOLOCK) ON wop.[WorkOrderId] = wopt.[WorkOrderId] AND wop.id=wopt.[OrderPartId]  
+       LEFT JOIN [dbo].[WorkOrderShippingItem] wosi WITH (NOLOCK) ON wosi.[WorkOrderPartNumId] = wop.ID AND wosi.[WOPickTicketId] = wopt.[PickTicketId]
+       LEFT JOIN [dbo].[WorkOrderShipping] wos WITH (NOLOCK) ON wos.[WorkOrderShippingId] = wosi.[WorkOrderShippingId]  
+      INNER JOIN [dbo].[WorkOrder] wo WITH (NOLOCK) ON wo.[WorkOrderId] = wop.[WorkOrderId]  
+       LEFT JOIN [dbo].[ItemMaster] imt  WITH (NOLOCK) ON imt.[ItemMasterId] = wop.[ItemMasterId]  
+       LEFT JOIN [dbo].[Stockline] sl WITH (NOLOCK) ON sl.[StockLineId] = wop.[StockLineId]  
+       LEFT JOIN [dbo].[WorkOrderCustomsInfo] woc WITH (NOLOCK) ON woc.[WorkOrderShippingId] = wos.[WorkOrderShippingId]  
+       LEFT JOIN [dbo].[Customer] cr WITH (NOLOCK) ON cr.[CustomerId] = wo.[CustomerId]  
+       LEFT JOIN [dbo].[WorkOrderPackaginSlipItems] wPI  WITH (NOLOCK) ON wopt.[PickTicketId] = wPI.[WOPickTicketId] AND wPI.[WOPartNoId] = wop.[id]  
+       LEFT JOIN [dbo].[WorkOrderPackaginSlipHeader] wPB  WITH (NOLOCK) ON wPB.[PackagingSlipId] = wPI.[PackagingSlipId]  
+      OUTER APPLY (SELECT TOP 1 [WorkOrderShippingId] FROM [dbo].[WorkOrderBillingInvoicing] WOBI WITH (NOLOCK) 
+          WHERE wosi.[WorkOrderShippingId] = WOBI.[WorkOrderShippingId] AND wos.[WorkOrderId] = WOBI.[WorkOrderId] AND ISNULL(WOBI.[IsPerformaInvoice],0) = 0) AS wobii          
+      WHERE wopt.[WorkOrderId]=@WorkOrderId AND wopt.[IsConfirmed]=1 AND wopt.[OrderPartId]=@WorkOrderPartId   
+   -- END  
+   --COMMIT  TRANSACTION  
   
   END TRY      
   BEGIN CATCH        
    IF @@trancount > 0  
     PRINT 'ROLLBACK'  
-    ROLLBACK TRAN;  
+    --ROLLBACK TRAN;  
     DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name()   
   
 -----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------  

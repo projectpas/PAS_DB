79c47a4ff0dcda56cd70b2803cca88738b0c7826
Bhargav Saliya <bhargav.saliya@silverxis.com>
2024-05-15 16:48:44 +0530

    Address Tag Type Single Screen AuditView

---
+++ PAS_Database/dbo/Views/vw_ContactTagAudit.sql
@@ -0,0 +1,14 @@
+﻿create    VIEW [dbo].[vw_ContactTagAudit]
+AS
+	SELECT  CT.AuditContactTagId AS [PkID],
+	        CT.ContactTagId AS [ID],
+			CT.TagName, 
+			CT.Description,
+			CT.SequenceNo,
+			CT.CreatedBy AS [Created By],
+			CT.UpdatedBy AS [Updated By],
+			CT.CreatedDate AS [Created Date],
+			CT.UpdatedDate AS [Updated Date],
+			CT.IsActive AS [Is Active],
+			CT.IsDeleted AS [Is Deleted]
+	FROM [DBO].[ContactTagAudit] CT WITH (NOLOCK)

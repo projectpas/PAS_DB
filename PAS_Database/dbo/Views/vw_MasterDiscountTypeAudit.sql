7f6ebbc353e94264d2e3531f7e1fd368d56ecb79
Moin Bloch <moin@silverxis.com>
2024-05-27 19:29:30 +0530

    Cash Receipt Single Screen Views

---
+++ PAS_Database/dbo/Views/vw_MasterDiscountTypeAudit.sql
@@ -0,0 +1,15 @@
+﻿CREATE   VIEW [dbo].[vw_MasterDiscountTypeAudit]
+AS
+SELECT DA.[MasterDiscountTypeAuditId]  AS PkID
+      ,DA.[Id] AS ID
+      ,DA.[Name]	  
+      ,DA.[Description]
+	  ,GL.[AccountName] AS 'GL Account'
+      ,DA.[CreatedBy] AS [Created By]
+      ,DA.[CreatedDate] AS [Created Date]
+      ,DA.[UpdatedBy] AS [Updated By]
+      ,DA.[UpdatedDate]  AS [Updated Date]
+      ,DA.[IsActive] 'Active ?'
+      ,DA.[IsDeleted] 'Deleted ?'	  
+  FROM [dbo].[MasterDiscountTypeAudit] DA WITH(NOLOCK)
+  JOIN [dbo].[GLAccount] GL ON  DA.GLAccountId = GL.GLAccountId

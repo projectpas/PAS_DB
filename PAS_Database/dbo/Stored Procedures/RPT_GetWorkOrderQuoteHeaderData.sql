b0e693eebaff5fac30b60feba556726eacc8b4f2
Moin Bloch <moin@silverxis.com>
2024-05-31 12:41:35 +0530

    WOQ Notes Changes

--- PAS_Database/dbo/Stored Procedures/RPT_GetWorkOrderQuoteHeaderData.sql
+++ PAS_Database/dbo/Stored Procedures/RPT_GetWorkOrderQuoteHeaderData.sql
@@ -17,6 +17,7 @@
 	3	 11/04/2024   Bhargav Saliya    creditTerms Changes
 	4	 15/04/2024   Moin Bloch        Added Max Length for Notes
 	5	 16/04/2024   Moin Bloch        Commnted Notes Changes
+	6	 31/05/2024   Moin Bloch        Added Max Length for Notes
 
 --EXEC [RPT_GetWorkOrderQuoteHeaderData] 2174
 **************************************************************/  
@@ -86,16 +87,16 @@ BEGIN
             woq.QuoteStatusId,
             TaxRate = ISNULL(custtax.TaxRate,0),
             CustomerAttention = sa.Attention,
-            WONotes = woq.Notes,
-			--CASE WHEN woq.Notes !='' 
-			--     THEN 
-			--		CASE WHEN LEN(ISNULL(woq.Notes,'')) < 560
-			--			 THEN ISNULL(woq.Notes,'')
-			--		ELSE
-			--			LEFT(ISNULL(woq.Notes,''),560) + '...'
-			--		END
-			--   ELSE ''
-			--END AS 'WONotes',
+            --WONotes = woq.Notes,
+			CASE WHEN woq.Notes !='' 
+			     THEN 
+					CASE WHEN LEN(ISNULL(woq.Notes,'')) < 750
+						 THEN ISNULL(woq.Notes,'')
+					ELSE
+						LEFT(ISNULL(woq.Notes,''),750) + '...'
+					END
+			   ELSE ''
+			END AS 'WONotes',
             WOCustomerRef = UPPER(wop.CustomerReference),
 			WorkScope = UPPER(wop.WorkScope)
 			 FROM dbo.WorkOrderQuote woq WITH(NOLOCK)

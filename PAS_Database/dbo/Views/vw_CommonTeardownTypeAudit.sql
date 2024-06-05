f92f6f4ab44130480d5ddc51dfced40fa47f8212
devendra <devendra@silverxis.com>
2024-06-05 19:06:55 +0530

    teardown history view changes for single screen

--- PAS_Database/dbo/Views/vw_CommonTeardownTypeAudit.sql
+++ PAS_Database/dbo/Views/vw_CommonTeardownTypeAudit.sql
@@ -1,6 +1,5 @@
-﻿
-CREATE   VIEW [dbo].[vw_CommonTeardownTypeAudit] 
+﻿CREATE     VIEW [dbo].[vw_CommonTeardownTypeAudit] 
 AS
-	SELECT	CTTA.CommonTeardownTypeAuditId AS PkID, CommonTeardownTypeId AS ID, Name, Description, IsTechnician AS 'Technician ?', CTTA.[IsDate] AS 'Technician Date ?', IsInspector AS 'Inspector ?', IsInspectorDate AS 'Inspector Date ?', IsDocument AS 'Document ?'
-	,Sequence AS 'Sequence No', CTTA.IsActive AS 'Active ?', CTTA.IsDeleted AS 'Deleted ?', CTTA.CreatedBy as 'Created By', CTTA.UpdatedBy AS 'Updated By', CTTA.CreatedDate AS 'Created On', CTTA.UpdatedDate AS 'Updated On', CTTA.MasterCompanyId
+	SELECT	CTTA.CommonTeardownTypeAuditId AS PkID, CommonTeardownTypeId AS ID, CommonTeardownTypeId AS 'TearDown Type Id', Name, Description, IsTechnician AS 'Technician ?', CTTA.[IsDate] AS 'Technician Date ?', IsInspector AS 'Inspector ?', IsInspectorDate AS 'Inspector Date ?', IsDocument AS 'Document ?'
+	,Sequence AS 'Sequence No', CTTA.IsActive AS 'Active ?', CTTA.IsDeleted AS 'Deleted ?', CTTA.CreatedBy as 'Created By', CTTA.UpdatedBy AS 'Updated By', CTTA.CreatedDate AS 'Created On', CTTA.UpdatedDate AS 'Updated On'--, CTTA.MasterCompanyId
 	FROM dbo.CommonTeardownTypeAudit AS CTTA WITH (NOLOCK)
--- PAS_Database/dbo/Views/vw_TeardownReasonAudit.sql
+++ PAS_Database/dbo/Views/vw_TeardownReasonAudit.sql
@@ -1,7 +1,6 @@
-﻿
-CREATE   VIEW [dbo].[vw_TeardownReasonAudit] 
+﻿CREATE     VIEW [dbo].[vw_TeardownReasonAudit] 
 AS
-	SELECT	TRA.AuditTeardownReasonId AS PkID, TeardownReasonId AS ID, CTT.Name AS 'Teardown Type', Reason, TRA.Memo
-	,Sequence AS 'Sequence No', TRA.IsActive AS 'Active ?', TRA.IsDeleted AS 'Deleted ?', TRA.CreatedBy as 'Created By', TRA.UpdatedBy AS 'Updated By', TRA.CreatedDate AS 'Created On', TRA.UpdatedDate AS 'Updated On', TRA.MasterCompanyId
+	SELECT	TRA.AuditTeardownReasonId AS PkID, TeardownReasonId AS ID, TeardownReasonId AS 'Teardown Reason Id', CTT.Name AS 'Teardown Type', Reason as 'Default Entries', TRA.Memo
+	,TRA.IsActive AS 'Active ?', TRA.IsDeleted AS 'Deleted ?', TRA.CreatedBy as 'Created By', TRA.UpdatedBy AS 'Updated By', TRA.CreatedDate AS 'Created On', TRA.UpdatedDate AS 'Updated On'--, TRA.MasterCompanyId
 	FROM dbo.TeardownReasonAudit AS TRA WITH (NOLOCK)
 	LEFT JOIN [dbo].[CommonTeardownType] CTT WITH(NOLOCK) ON CTT.CommonTeardownTypeId = TRA.CommonTeardownTypeId

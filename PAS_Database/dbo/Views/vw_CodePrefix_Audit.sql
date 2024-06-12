de7bfcf0a7ea9cc2e530102d501b6e75c7549cc0
shrey.chandegara <shrey.chandegara@silverxis.com>
2024-06-12 11:52:27 +0530

    code prefix

---
+++ PAS_Database/dbo/Views/vw_CodePrefix_Audit.sql
@@ -0,0 +1,7 @@
+﻿CREATE   VIEW [dbo].[vw_CodePrefix_Audit]
+AS
+	SELECT C.AuditCodePrefixId AS PkID, C.CodePrefixId AS ID, CodeType AS [Code Type], CodePrefix AS [Code Prefix], CodeSufix AS [Code Sufix], StartsFrom AS [Starts From], Description, (EMP_CR.FirstName + ' ' + EMP_CR.LastName) AS [Created By],
+	C.CreatedDate AS [Created Date], (EMP_UP.FirstName + ' ' + EMP_UP.LastName) AS [Updated By], C.UpdatedDate AS [Updated Date], C.IsActive AS [Is Active], C.IsDeleted AS [Is Deleted]
+	FROM [DBO].[CodePrefixesAudit] C WITH (NOLOCK) 
+	LEFT JOIN [dbo].[Employee] EMP_CR WITH (NOLOCK) ON C.CreatedBy = EMP_CR.CreatedBy
+    LEFT JOIN [dbo].[Employee] EMP_UP WITH (NOLOCK) ON C.UpdatedBy = EMP_UP.UpdatedBy

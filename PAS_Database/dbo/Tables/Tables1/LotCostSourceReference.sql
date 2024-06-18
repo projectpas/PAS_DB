4879909d6a1531fa995c1cb0d2883ca3b407cafb
Bhargav Saliya <bhargav.saliya@silverxis.com>
2024-06-18 16:00:30 +0530

    Resolved [PROD] Bug PN-7954

--- PAS_Database/dbo/Tables/Tables1/LotCostSourceReference.sql
+++ PAS_Database/dbo/Tables/Tables1/LotCostSourceReference.sql
@@ -6,10 +6,12 @@
     [MasterCompanyId] INT           NOT NULL,
     [CreatedBy]       VARCHAR (256) NOT NULL,
     [UpdatedBy]       VARCHAR (256) NOT NULL,
-    [CreatedDate]     DATETIME2 (7) NOT NULL,
-    [UpdatedDate]     DATETIME2 (7) NULL,
-    [IsActive]        BIT           NOT NULL,
-    [IsDeleted]       BIT           NOT NULL,
+    [CreatedDate]     DATETIME2 (7) CONSTRAINT [LotCostSourceReference_CD_UDate] DEFAULT (getdate()) NOT NULL,
+    [UpdatedDate]     DATETIME2 (7) CONSTRAINT [LotCostSourceReference_UD_UDate] DEFAULT (getdate()) NULL,
+    [IsActive]        BIT           CONSTRAINT [DF_LotCostSourceReference_IsActive] DEFAULT ((1)) NOT NULL,
+    [IsDeleted]       BIT           CONSTRAINT [DF_LotCostSourceReference_IsDeleted] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_LotCostSourceReference] PRIMARY KEY CLUSTERED ([LotSourceId] ASC)
 );
+
+
 

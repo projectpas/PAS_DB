2dd0008a48e2ff39d2e11b2b49647dee30c1137b
Bhargav Saliya <bhargav.saliya@silverxis.com>
2024-06-25 11:46:40 +0530

    Email TemplateList

---
+++ PAS_Database/dbo/Views/vw_EmailTemplateviewList.sql
@@ -0,0 +1,19 @@
+﻿CREATE   VIEW [dbo].[vw_EmailTemplateviewList]
+AS
+	SELECT
+	ET.EmailTemplateId,
+	ET.EmailTemplateTypeId,
+	ETT.EmailTemplateTypeName,
+	ET.SubjectName,
+	ET.EmailBody AS [EmailBody],
+	ET.RevNo as [RevNo],
+	ET.RevDate [RevDate],
+	ET.MasterCompanyId,
+	ET.CreatedBy AS [CreatedBy],
+	ET.CreatedDate AS [CreatedDate],
+	ET.UpdatedBy AS [UpdatedBy],
+	ET.UpdatedDate AS [UpdatedDate],
+	ET.IsActive AS [IsActive],
+	ET.IsDeleted AS [IsDeleted]
+	FROM [DBO].[EmailTemplate] ET WITH (NOLOCK) 
+	JOIN [DBO].[EmailTemplateType] ETT WITH (NOLOCK) ON ET.EmailTemplateTypeId = ETT.EmailTemplateTypeId

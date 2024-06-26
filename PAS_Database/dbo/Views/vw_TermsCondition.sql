b96e3d6addeb4b4c2cfdc90ac0c43f92c355689e
Bhargav Saliya <bhargav.saliya@silverxis.com>
2024-06-26 12:05:09 +0530

    Termscondition Single Screen

--- PAS_Database/dbo/Views/vw_TermsCondition.sql
+++ PAS_Database/dbo/Views/vw_TermsCondition.sql
@@ -1,8 +1,16 @@
-﻿
-
-CREATE VIEW [dbo].[vw_TermsCondition]
+﻿CREATE   VIEW [dbo].[vw_TermsCondition]
 AS
-SELECT	tc.TermsConditionId, tc.Description, ETT.EmailTemplateType, tc.Memo, tc.MasterCompanyId, tc.CreatedBy, tc.UpdatedBy, 
-        tc.CreatedDate, tc.UpdatedDate, tc.IsActive, tc.IsDeleted, tc.EmailTemplateTypeId 
-FROM  dbo.TermsCondition tc WITH (NOLOCK) INNER JOIN
-      dbo.EmailTemplateType ETT WITH (NOLOCK) ON ETT.EmailTemplateTypeId = tc.EmailTemplateTypeId
+	SELECT TC.TermsConditionId,
+	ET.EmailTemplateTypeId,
+	ET.EmailTemplateType,
+	TC.[Description],
+	TC.Memo,
+	TC.MasterCompanyId,
+	TC.CreatedDate,
+	TC.CreatedBy,
+	TC.UpdatedDate,
+	TC.UpdatedBy ,
+	TC.IsActive,
+	TC.IsDeleted
+	FROM [dbo].[TermsCondition] TC WITH (NOLOCK)
+	LEFT JOIN [dbo].[EmailTemplateType] ET WITH(NOLOCK) ON TC.EmailTemplateTypeId = ET.EmailTemplateTypeId

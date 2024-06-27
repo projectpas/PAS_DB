733a3aaba999fcba7752c112397bf24e6229cd9d
ekta <ekta@silverxis.com>
2024-06-28 02:00:52 +0530

    Single Screen - Journal Category , Journal Currency Type , Expenditure Category , GL A/C Class

---
+++ PAS_Database/dbo/Views/vw_GLAccountClassAudit.sql
@@ -0,0 +1,13 @@
+﻿CREATE   VIEW [DBO].[vw_GLAccountClassAudit]
+AS
+SELECT
+	GLAccountClassAuditId AS [PkID],
+	GLAccountClassId AS [ID],
+	GLAccountClassName AS [Name],
+	GLAccountClassMemo As [Memo],
+	CreatedBy AS [Created By],
+	UpdatedBy AS [Updated By],
+	UpdatedDate AS [Updated On],
+	IsActive AS [IsActive],
+	IsDeleted AS [IsDeleted]
+FROM [DBO].[GLAccountClassAudit]

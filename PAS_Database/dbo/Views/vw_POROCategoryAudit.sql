26ea9114311894cd9b507556e251b4c67e7cbef5
Bhargav Saliya <bhargav.saliya@silverxis.com>
2024-06-28 19:31:06 +0530

    Single Screens

---
+++ PAS_Database/dbo/Views/vw_POROCategoryAudit.sql
@@ -0,0 +1,11 @@
+﻿CREATE    VIEW [dbo].[vw_POROCategoryAudit]
+AS
+	SELECT ipor.POROCategoryAuditId  AS PkID,
+	ipor.POROCategoryId AS ID 
+	,ipor.CategoryName as [Category Name],
+	ipor.Memo,
+	Ipor.IsPO as [Is PO],
+	Ipor.IsRO as [Is RO]
+	,ipor.CreatedBy AS [Created By],
+	ipor.CreatedDate AS [Created Date], ipor.UpdatedBy AS [Updated By], ipor.UpdatedDate AS [Updated Date], ipor.IsActive AS [Is Active], ipor.IsDeleted AS [Is Deleted]
+	FROM [DBO].POROCategoryAudit ipor WITH (NOLOCK)

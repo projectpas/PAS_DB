8bd494e8e869364deaeabd0455b17c24628ca9b1
ekta <ekta@silverxis.com>
2024-06-29 02:14:20 +0530

    Single Screen - GL Account Category, ATA Sub Chapter, Tax Rate

---
+++ PAS_Database/dbo/Views/vw_ATASubChapterList.sql
@@ -0,0 +1,21 @@
+﻿CREATE   VIEW [DBO].[vw_ATASubChapterList]
+AS 
+SELECT
+	atasc.ATASubChapterId,
+	atacc.ATAChapterCategoryId,
+	atacc.CategoryName,
+	atasc.Description,
+	atac.ATAChapterId,
+	atac.ATAChapterName,
+	atasc.ATASubChapterCode,
+	atac.ATAChapterCode,
+	atasc.MasterCompanyId,
+	atasc.CreatedBy,
+	atasc.UpdatedBy,
+	atasc.CreatedDate,
+	atasc.UpdatedDate,
+	atasc.IsActive,
+	atasc.IsDeleted
+FROM [DBO].[ATASubChapter] atasc WITH (NOLOCK)
+LEFT JOIN [DBO].[ATAChapter] atac WITH (NOLOCK) ON atasc.ATAChapterId = atac.ATAChapterId
+LEFT JOIN [DBO].[ATAChapterCategory] atacc WITH (NOLOCK) ON atasc.ATAChapterCategoryId = atacc.ATAChapterCategoryId

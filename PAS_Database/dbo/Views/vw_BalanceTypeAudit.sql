d3da1879009458b432237c827d87b0bc6a878add
ekta <ekta@silverxis.com>
2024-06-30 01:31:36 +0530

    Single screen - Remaining screens of Accounting/Finance

---
+++ PAS_Database/dbo/Views/vw_BalanceTypeAudit.sql
@@ -0,0 +1,15 @@
+﻿CREATE   VIEW [DBO].[vw_BalanceTypeAudit]
+AS
+SELECT
+	BalanceTypeId AS [PkID],
+	ID AS [ID],
+	BalanceTypeName AS [Name],
+	Description AS [Description],
+	Memo AS [Memo],
+	CreatedBy AS [Created By],
+	UpdatedBy AS [Updated By],
+	CreatedDate AS [Created On],
+	UpdatedDate AS [Updated On],
+	IsActive AS [Is Active],
+	IsDeleted AS [Is Deleted]
+FROM [DBO].[BalanceTypeAudit]

f57224263bea540e928a44e63288cf5c42d93b74
Hemant <hemant_patel1234@yahoo.co.in>
2024-07-09 18:02:06 +0530

    Asset View Changes

--- PAS_Database/dbo/Views/vw_AssetAttributeTypeAudit.sql
+++ PAS_Database/dbo/Views/vw_AssetAttributeTypeAudit.sql
@@ -1,41 +1,35 @@
 ﻿
 
 
-
 CREATE VIEW [dbo].[vw_AssetAttributeTypeAudit]
 AS
-SELECT AAT.* ,
-ATY.TangibleClassName ,
-CT.Name AS ConventionTypeName,
-DM.AssetDepreciationMethodName,
-FI.Name AS AssetDepreciationFrequencyName,
-PER.PercentValue,
-AQGL.AccountName AS AcquiredGLAccount,
-DPGL.AccountName AS DeprExpenseGLAccount,
-ADGL.AccountName AS AdDepsGLAccount,
-ASGL.AccountName AS AssetSaleGLAccount,
-WOGL.AccountName AS AssetWriteOffGLAccount,
-WDGL.AccountName AS AssetWriteDownLAccount,
---LegalEntity = STUFF((SELECT ', ' + Name
---           FROM LegalEntityAssetAttributeType LEA 
---		   JOIN LegalEntity LE ON LEA.LegalEntityId=LE.LegalEntityId
---           WHERE LEA.AssetAttributeTypeId = AAT.AssetAttributeTypeId 
---          FOR XML PATH('')), 1, 2, '')
-LegalEntity = STUFF((SELECT ', ' + Name
-           FROM LegalEntity LE 
-		   --WHERE LE.LegalEntityId = AAT.SelectedCompanyIds
-		   WHERE LE.LegalEntityId IN (SELECT Item FROM DBO.SPLITSTRING(AAT.SelectedCompanyIds,','))
-          FOR XML PATH('')), 1, 2, '')
-FROM AssetAttributeType AAT
-JOIN TangibleClass ATY ON AAT.TangibleClassId=ATY.TangibleClassId
-JOIN ConventionType CT ON AAT.ConventionType=CT.ConventionTypeId
-JOIN AssetDepreciationMethod DM ON AAT.DepreciationMethod=DM.AssetDepreciationMethodId
---JOIN AssetDepreciationInterval DI ON AAT.DepreciationFrequencyId=DI.AssetDepreciationIntervalId
-JOIN AssetDepreciationFrequency FI ON AAT.DepreciationFrequencyId=FI.AssetDepreciationFrequencyId
-LEFT JOIN [Percent] PER ON AAT.ResidualPercentage=PER.PercentId
-JOIN GLAccount AQGL ON  AAT.AcquiredGLAccountId=AQGL.GLAccountId
-JOIN GLAccount DPGL ON  AAT.DeprExpenseGLAccountId=DPGL.GLAccountId
-JOIN GLAccount ADGL ON  AAT.AdDepsGLAccountId=ADGL.GLAccountId
-JOIN GLAccount ASGL ON  AAT.AssetSale=ASGL.GLAccountId
-JOIN GLAccount WOGL ON  AAT.AssetWriteOff=WOGL.GLAccountId
-JOIN GLAccount WDGL ON  AAT.AssetWriteDown=WDGL.GLAccountId
+SELECT --AAT.* , 
+	AAT.AssetAttributeTypeId AS PkID,
+	AAT.AssetAttributeTypeId AS ID,
+	ATY.TangibleClassName ,
+	CT.Name AS ConventionTypeName,
+	DM.AssetDepreciationMethodName,
+	FI.Name AS AssetDepreciationFrequencyName,
+	PER.PercentValue,
+	AQGL.AccountName AS AcquiredGLAccount,
+	DPGL.AccountName AS DeprExpenseGLAccount,
+	ADGL.AccountName AS AdDepsGLAccount,
+	ASGL.AccountName AS AssetSaleGLAccount,
+	WOGL.AccountName AS AssetWriteOffGLAccount,
+	WDGL.AccountName AS AssetWriteDownLAccount,
+	LegalEntity = STUFF((SELECT ', ' + Name
+			   FROM dbo.LegalEntity LE 
+			   WHERE LE.LegalEntityId IN (SELECT Item FROM DBO.SPLITSTRING(AAT.SelectedCompanyIds,','))
+			  FOR XML PATH('')), 1, 2, '')
+FROM dbo.AssetAttributeType AAT WITH(NOLOCK)
+	JOIN dbo.TangibleClass ATY WITH(NOLOCK) ON AAT.TangibleClassId=ATY.TangibleClassId
+	JOIN dbo.ConventionType CT WITH(NOLOCK) ON AAT.ConventionType=CT.ConventionTypeId
+	JOIN dbo.AssetDepreciationMethod DM WITH(NOLOCK) ON  AAT.DepreciationMethod=DM.AssetDepreciationMethodId
+	JOIN dbo.AssetDepreciationFrequency FI WITH(NOLOCK) ON AAT.DepreciationFrequencyId=FI.AssetDepreciationFrequencyId
+	JOIN dbo.GLAccount AQGL WITH(NOLOCK) ON  AAT.AcquiredGLAccountId=AQGL.GLAccountId
+	JOIN dbo.GLAccount DPGL WITH(NOLOCK) ON  AAT.DeprExpenseGLAccountId=DPGL.GLAccountId
+	JOIN dbo.GLAccount ADGL WITH(NOLOCK) ON  AAT.AdDepsGLAccountId=ADGL.GLAccountId
+	JOIN dbo.GLAccount ASGL WITH(NOLOCK) ON  AAT.AssetSale=ASGL.GLAccountId
+	JOIN dbo.GLAccount WOGL WITH(NOLOCK) ON  AAT.AssetWriteOff=WOGL.GLAccountId
+	JOIN dbo.GLAccount WDGL WITH(NOLOCK) ON  AAT.AssetWriteDown=WDGL.GLAccountId
+	LEFT JOIN dbo.[Percent] PER WITH(NOLOCK) ON AAT.ResidualPercentage=PER.PercentId

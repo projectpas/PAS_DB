bb31c4fba355ef3b1c51affc93235d61e75328a9
devendra <devendra@silverxis.com>
2024-07-22 13:07:15 +0530

    PN-8775

---
+++ PAS_Database/dbo/User Defined Types/BatchTriggerWorkOrderType.sql
@@ -0,0 +1,15 @@
+﻿CREATE TYPE [dbo].[BatchTriggerWorkOrderType] AS TABLE (
+    [DistributionMasterId] BIGINT          NULL,
+    [ReferenceId]          BIGINT          NULL,
+    [ReferencePartId]      BIGINT          NULL,
+    [ReferencePieceId]     BIGINT          NULL,
+    [InvoiceId]            BIGINT          NULL,
+    [StocklineId]          BIGINT          NULL,
+    [Qty]                  INT             NULL,
+    [LaborType]            VARCHAR (200)   NULL,
+    [Issued]               BIT             NULL,
+    [Amount]               DECIMAL (18, 2) NULL,
+    [ModuleName]           VARCHAR (200)   NULL,
+    [MasterCompanyId]      INT             NULL,
+    [UpdateBy]             VARCHAR (200)   NULL);
+

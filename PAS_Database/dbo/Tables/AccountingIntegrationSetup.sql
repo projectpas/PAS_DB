1e7547be680eabe6fd7fd1ad31d32bf6a6c977dd
Hemant <hemant_patel1234@yahoo.co.in>
2024-08-09 17:11:18 +0530

    Intergration Table Changes

---
+++ PAS_Database/dbo/Tables/AccountingIntegrationSetup.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[AccountingIntegrationSetup] (
+    [AccountingIntegrationSetupId] INT            IDENTITY (1, 1) NOT NULL,
+    [IntegrationId]                INT            NOT NULL,
+    [ClientId]                     VARCHAR (500)  NULL,
+    [ClientSecret]                 VARCHAR (500)  NULL,
+    [RedirectUrl]                  VARCHAR (5000) NULL,
+    [Environment]                  VARCHAR (200)  NULL,
+    [MasterCompanyId]              INT            NOT NULL,
+    [CreatedBy]                    VARCHAR (256)  NOT NULL,
+    [UpdatedBy]                    VARCHAR (256)  NOT NULL,
+    [CreatedDate]                  DATETIME2 (7)  NOT NULL,
+    [UpdatedDate]                  DATETIME2 (7)  NOT NULL,
+    [IsActive]                     BIT            NOT NULL,
+    [IsDeleted]                    BIT            NOT NULL,
+    CONSTRAINT [PK_AccountingIntegrationSetup] PRIMARY KEY CLUSTERED ([AccountingIntegrationSetupId] ASC)
+);
+

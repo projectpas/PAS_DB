2ea18399086008f2cbddab1441eecafde383635b
amitghediyasilverxis <amit.ghediya@SILVERXIS.com>
2024-08-12 23:48:18 +0530

    PN-8694 update sp & add for check email phone exist or not.

---
+++ PAS_Database/dbo/Stored Procedures/CheckEmailContactExists.sql
@@ -0,0 +1,71 @@
+﻿/*************************************************************               
+ ** File:   [CheckEmailContactExists]               
+ ** Author:  AMIT GHEDIYA    
+ ** Description:  This Store Procedure use to check customer emial & phone is exists.   
+ ** Purpose:             
+ ** Date:   12/08/2024          
+              
+ ** RETURN VALUE:               
+ **********************************************************               
+ ** check customer emial & phone exists.             
+ **********************************************************               
+ ** PR   Date			Author			Change Description                
+ ** --   --------		-------			--------------------------------              
+    1    12/08/2024  	AMIT GHEDIYA	Created     
+ 
+ EXEC [CheckEmailContactExists] 12
+********************************************************************/ 
+
+CREATE   PROCEDURE [dbo].[CheckEmailContactExists]
+	@customerId BIGINT = 0
+AS
+BEGIN
+		BEGIN TRY
+			
+			DECLARE @ReturnStatus INT = 0,
+					@ExistingCustomerPhone VARCHAR(100),
+					@ExistingEmail VARCHAR(100),
+					@ReturnMsg VARCHAR(150) = 'Contact number or email details missing.';
+
+			SELECT @ExistingCustomerPhone = [CustomerPhone], @ExistingEmail = [Email] FROM [dbo].[Customer] WITH(NOLOCK) WHERE CustomerId = @customerId;
+			IF(ISNULL(@ExistingCustomerPhone,'') = '')
+			BEGIN
+				 SET @ReturnStatus = -1;
+				 SET @ReturnMsg = @ReturnMsg;
+			END
+			ELSE IF(ISNULL(@ExistingEmail,'') = '')
+			BEGIN
+				 SET @ReturnStatus = -1;
+				 SET @ReturnMsg = @ReturnMsg;
+			END
+			ELSE
+			BEGIN
+				 SET @ReturnStatus = 1;
+				 SET @ReturnMsg = '';
+			END
+
+			SELECT @ReturnStatus AS Status, @ReturnMsg AS Msg;
+
+		END TRY    
+		BEGIN CATCH      
+			IF @@trancount > 0
+				--PRINT 'ROLLBACK'
+				ROLLBACK TRAN;
+				DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
+
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+              , @AdhocComments     VARCHAR(150)    = 'CheckEmailContactExists' 
+              , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ ISNULL(@customerId, '') + ''
+              , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+
+              exec spLogException 
+                       @DatabaseName			= @DatabaseName
+                     , @AdhocComments			= @AdhocComments
+                     , @ProcedureParameters		= @ProcedureParameters
+                     , @ApplicationName         = @ApplicationName
+                     , @ErrorLogID              = @ErrorLogID OUTPUT ;
+              RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)
+              RETURN(1);
+		END CATCH
+END

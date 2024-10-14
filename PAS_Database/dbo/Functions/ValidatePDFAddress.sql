08a89f6726b0a51da8abceec93edc425ee9279da
Hemant <hemant_patel1234@yahoo.co.in>
2024-10-14 15:45:25 +0530

    Corrected Function

--- PAS_Database/dbo/Functions/ValidatePDFAddress.sql
+++ PAS_Database/dbo/Functions/ValidatePDFAddress.sql
@@ -1,32 +1,14 @@
-﻿/*********************
- ** File:   [ValidatePDFAddress]
- ** Author:   Bhargav Saliya
- ** Description: Merge Address
- ** Purpose:
- ** Date:   08/20/2024
- **********************
-  ** Change History
- **********************
- ** PR   Date         Author				Change Description            
- ** --   --------     -------				--------------------------------          
-    1    08/20/2024   BHargav Saliya			Created
-    2    08/21/2024   Ekta Chandegra			Add Comma Separation
-	3    08/28/2024   AMIT GHEDIYA			    Edit for Ristrict '-'
-	4    09/02/2024   SAHDEV SALIYA             Address Hendal For Asset
-	5    09/09/2024   Ayushi Patel				Return Address In upper case
-
-**********************/ 
-CREATE   FUNCTION [dbo].[ValidatePDFAddress]
+﻿CREATE FUNCTION [dbo].[ValidatePDFAddress]
 (
     @Address1 NVARCHAR(255),
     @Address2 NVARCHAR(255),
-	@Address3 NVARCHAR(255),
+    @Address3 NVARCHAR(255),
     @City NVARCHAR(255),
     @StateOrProvince NVARCHAR(255),
     @PostalCode NVARCHAR(50),
     @Country NVARCHAR(255),
     @PhoneNumber NVARCHAR(50),
-	@PhoneExt NVARCHAR(50),
+    @PhoneExt NVARCHAR(50),
     @Email NVARCHAR(255)
 )
 RETURNS NVARCHAR(MAX)
@@ -35,103 +17,47 @@ BEGIN
     DECLARE @address NVARCHAR(MAX);
     SET @address = '';
 
-    -- Append Address1
-    IF (@Address1 IS NOT NULL AND @Address1 <> '' AND TRIM(@Address1) <> '-')
-    BEGIN
-        SET @address = @address + @Address1 + ', <br/>';
-    END
+    -- Helper variables for conditional new line
+    DECLARE @lineBreak NVARCHAR(10) = ', <br/>';
+    DECLARE @newLine NVARCHAR(10) = '<br/>';
 
-    -- Append Address2
-    IF (@Address2 IS NOT NULL AND @Address2 <> '' AND TRIM(@Address2) <> '-')
-    BEGIN
-        SET @address = @address + @Address2 + ', <br/>';
-    END
+    -- Append Address1, Address2, Address3
+    SET @address = COALESCE(NULLIF(TRIM(@Address1), '-'), '') + 
+                   COALESCE(NULLIF(TRIM(@Address2), '-'), '') +
+                   COALESCE(NULLIF(TRIM(@Address3), '-'), '');
 
-	 -- Append Address3
-    IF (@Address3 IS NOT NULL AND @Address3 <> '' AND TRIM(@Address3) <> '-')
-    BEGIN
-        SET @address = @address + @Address3 + ', <br/>';
-    END
-
-    -- Append City
-    IF (@City IS NOT NULL AND @City <> '' AND TRIM(@City) <> '-')
+    -- Append City, StateOrProvince, and PostalCode
+    IF (COALESCE(NULLIF(TRIM(@City), '-'), '') <> '')
     BEGIN
         SET @address = @address + @City;
     END
 
-    -- Append StateOrProvince
-    IF (@StateOrProvince IS NOT NULL AND @StateOrProvince <> '' AND TRIM(@StateOrProvince) <> '-')
+    IF (COALESCE(NULLIF(TRIM(@StateOrProvince), '-'), '') <> '')
     BEGIN
-		IF(@City IS NOT NULL AND @City <> '' AND TRIM(@City) <> '-')
-		BEGIN
-			SET @address = @address+ ', ' + @StateOrProvince ;
-		END
-		ELSE
-		BEGIN
-			SET @address = @address+ ' ' + @StateOrProvince ;
-		END
+        SET @address = @address + CASE WHEN LEN(@address) > 0 THEN ', ' ELSE '' END + @StateOrProvince;
     END
 
-    -- Append PostalCode
-    IF (@PostalCode IS NOT NULL AND @PostalCode <> '' AND TRIM(@PostalCode) <> '-')
+    IF (COALESCE(NULLIF(TRIM(@PostalCode), '-'), '') <> '')
     BEGIN
-		IF(@StateOrProvince IS NOT NULL AND @StateOrProvince <> '' AND TRIM(@StateOrProvince) <> '-')
-		BEGIN
-			SET @address = @address+ ', ' + @PostalCode + ', <br/>';
-		END
-		ELSE
-		BEGIN
-			IF(@City IS NOT NULL AND @City <> '' AND TRIM(@City) <> '-')
-			BEGIN
-				SET @address = @address + ', '+ @PostalCode + ', <br/>';
-			END
-			ELSE
-			BEGIN
-				SET @address = @address + ' '+ @PostalCode + ', <br/>';
-			END
-		END
+        SET @address = @address + CASE WHEN LEN(@address) > 0 THEN ', ' ELSE '' END + @PostalCode + @lineBreak;
     END
-	ELSE
-	BEGIN
-		IF(@City IS NOT NULL AND @City <> '' AND TRIM(@City) <> '-')
-		BEGIN
-			SET @address = @address + '<br/>';
-		END
-		ELSE IF(@StateOrProvince IS NOT NULL AND @StateOrProvince <> '' AND TRIM(@StateOrProvince) <> '-')
-		BEGIN 
-			SET @address = @address + '<br/>';
-		END
-		ELSE IF(@PostalCode IS NOT NULL AND @PostalCode <> '' AND TRIM(@PostalCode) <> '-')
-		BEGIN 
-			SET @address = @address + '<br/>';
-		END
-	END
 
     -- Append Country
-    IF (@Country IS NOT NULL AND @Country <> '' AND TRIM(@Country) <> '-')
+    SET @address = @address + COALESCE(NULLIF(TRIM(@Country), '-'), '') + CASE WHEN LEN(@address) > 0 THEN @newLine ELSE '' END;
+
+    -- Append PhoneNumber and PhoneExt
+    SET @address = @address + COALESCE(NULLIF(TRIM(@PhoneNumber), '-'), '') + CASE WHEN LEN(@address) > 0 THEN @newLine ELSE '' END;
+    
+    IF (COALESCE(NULLIF(TRIM(@PhoneExt), '-'), '') <> '')
     BEGIN
-        SET @address = @address + @Country + '<br/>';
+        SET @address = @address + ' ' + @PhoneExt + @newLine;
     END
 
-    -- Append PhoneNumber
-    IF (@PhoneNumber IS NOT NULL AND @PhoneNumber <> '' AND TRIM(@PhoneNumber) <> '-')
-    BEGIN
-        SET @address = @address + @PhoneNumber + '<br/>';
-		--+ (CASE WHEN @PhoneExt IS NULL OR @PhoneExt <> '' THEN '<br/>' ELSE '' END) ; 
-
-    END
-
-	-- Append PhoneExt
-    IF (@PhoneExt IS NOT NULL AND @PhoneExt <> '' AND TRIM(@PhoneExt) <> '-')
-    BEGIN
-        SET @address = @address + ' ' + @PhoneExt + '<br/>';
-    END
-	
     -- Append Email
-    IF (@Email IS NOT NULL AND @Email <> '' AND TRIM(@Email) <> '-')
+    IF (COALESCE(NULLIF(TRIM(@Email), '-'), '') <> '')
     BEGIN
-        SET @address =  @address + @Email;
+        SET @address = @address + @Email;
     END
 
-    RETURN UPPER( @address );
+    RETURN UPPER(@address);
 END;

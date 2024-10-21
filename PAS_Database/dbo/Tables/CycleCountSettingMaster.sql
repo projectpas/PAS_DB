673484974b7bcea256da35f80f25d5621d601952
Bhargav Saliya <bhargav.saliya@silverxis.com>
2024-10-21 15:32:21 +0530

    PN-9978 Cycle Count Table's

---
+++ PAS_Database/dbo/Tables/CycleCountSettingMaster.sql
@@ -0,0 +1,39 @@
+﻿CREATE TABLE [dbo].[CycleCountSettingMaster] (
+    [CycleCountSettingId] BIGINT        IDENTITY (1, 1) NOT NULL,
+    [IsEnforceApproval]   BIT           CONSTRAINT [DF_CycleCountSettingMaster_IsEnforceApproval] DEFAULT ((0)) NOT NULL,
+    [StatusId]            BIGINT        NOT NULL,
+    [Effectivedate]       DATETIME2 (7) NULL,
+    [MasterCompanyId]     INT           NOT NULL,
+    [CreatedBy]           VARCHAR (256) NOT NULL,
+    [UpdatedBy]           VARCHAR (256) NOT NULL,
+    [CreatedDate]         DATETIME2 (7) CONSTRAINT [DF_CycleCountSettingMaster_CreatedDate] DEFAULT (getutcdate()) NOT NULL,
+    [UpdatedDate]         DATETIME2 (7) CONSTRAINT [DF_CycleCountSettingMaster_UpdatedDate] DEFAULT (getutcdate()) NOT NULL,
+    [IsActive]            BIT           CONSTRAINT [DF_CycleCountSettingMaster_IsActive] DEFAULT ((1)) NOT NULL,
+    [IsDeleted]           BIT           CONSTRAINT [DF_CycleCountSettingMaster_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_CycleCountSettingMaster] PRIMARY KEY CLUSTERED ([CycleCountSettingId] ASC),
+    CONSTRAINT [FK_CycleCountSettingMaster_MasterCompany] FOREIGN KEY ([MasterCompanyId]) REFERENCES [dbo].[MasterCompany] ([MasterCompanyId])
+);
+
+
+GO
+CREATE   TRIGGER [dbo].[Trg_CycleCountSettingMasterAudit]
+
+   ON  [dbo].[CycleCountSettingMaster]
+
+   AFTER INSERT,UPDATE
+
+AS 
+
+BEGIN
+
+
+
+	INSERT INTO [dbo].[CycleCountSettingMasterAudit]
+
+	SELECT * FROM INSERTED
+
+	SET NOCOUNT ON;
+
+
+
+END
---
+++ PAS_Database/dbo/Tables/CycleCountStatus.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[CycleCountStatus] (
+    [CycleCountStatusId] INT           IDENTITY (1, 1) NOT NULL,
+    [Status]             VARCHAR (50)  NOT NULL,
+    [Description]        VARCHAR (50)  NULL,
+    [SequenceNo]         INT           NOT NULL,
+    [MasterCompanyId]    INT           NOT NULL,
+    [CreatedBy]          VARCHAR (256) NOT NULL,
+    [UpdatedBy]          VARCHAR (256) NOT NULL,
+    [CreatedDate]        DATETIME2 (7) CONSTRAINT [DF_CycleCountStatus_CreatedDate] DEFAULT (getdate()) NOT NULL,
+    [UpdatedDate]        DATETIME2 (7) CONSTRAINT [DF_CycleCountStatus_UpdatedDate] DEFAULT (getdate()) NOT NULL,
+    [IsActive]           BIT           CONSTRAINT [DF_CycleCountStatus_IsActive] DEFAULT ((1)) NOT NULL,
+    [IsDeleted]          BIT           CONSTRAINT [DF_CycleCountStatus_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_CycleCountStatus] PRIMARY KEY CLUSTERED ([CycleCountStatusId] ASC)
+);
+

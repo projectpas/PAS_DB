b3b0b8aaaafd7d4e1a7127ffcb2eea95ef26ee61
sahdev <sahdev@silverxis.com>
2024-10-30 17:15:18 +0530

    PN-9782 Resolve (WO History in MPN part Level)

--- PAS_Database/dbo/Tables/Tables2/WorkOrderPartNumberAudit.sql
+++ PAS_Database/dbo/Tables/Tables2/WorkOrderPartNumberAudit.sql
@@ -78,12 +78,11 @@
     [RevisedSerialNumber]        VARCHAR (50)    NULL,
     [IsROCreated]                BIT             NULL,
     [PartNumber]                 VARCHAR (200)   NULL,
-    [WorkOrderStatus]            VARCHAR (MAX)   NULL,
-    [WorkOrderStage]             VARCHAR (150)   NULL,
-    [ManufacturerName]           VARCHAR (250)   NULL,
-    [EmployeeStation]            VARCHAR (100)   NULL,
+    [WorkOrderStatus]            VARCHAR (256)   NULL,
     CONSTRAINT [PK_WorkOrderPartNumberAudit] PRIMARY KEY CLUSTERED ([WOPartNoAuditId] ASC)
 );
+
+
 
 
 

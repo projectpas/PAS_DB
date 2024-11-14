5a35ad6353c8326ac8ae254b8947f515f338d2f4
shrey.chandegara <shrey.chandegara@silverxis.com>
2024-11-14 19:38:53 +0530

    add dbo and with nolock

--- PAS_Database/dbo/Stored Procedures/GetSalesOrderQuotePrintData.sql
+++ PAS_Database/dbo/Stored Procedures/GetSalesOrderQuotePrintData.sql
@@ -1,5 +1,4 @@
-﻿
--- EXEC [DBO].[GetSalesOrderQuotePrintData] 766
+﻿-- EXEC [DBO].[GetSalesOrderQuotePrintData] 766
 CREATE   PROCEDURE [dbo].[GetSalesOrderQuotePrintData]
     @SalesQuoteId INT
 AS
@@ -46,27 +45,27 @@ BEGIN
         so.ManagementStructureId,
         -- Barcode Generation Logic (if needed can be implemented in SQL or outside of the SP)
         '' AS Barcode -- Placeholder for the barcode, should be handled in application code
-    FROM SalesOrderQuote so WITH (NOLOCK)
-    LEFT JOIN SalesOrderQuotePartV1 sop WITH (NOLOCK) ON so.SalesOrderQuoteId = sop.SalesOrderQuoteId
-    LEFT JOIN SalesOrderQuoteStocklineV1 stk WITH (NOLOCK) ON stk.SalesOrderQuotePartId = sop.SalesOrderQuotePartId
-    LEFT JOIN SalesOrderQuotePartCost sopc WITH (NOLOCK) ON sopc.SalesOrderQuotePartId = sop.SalesOrderQuotePartId
-    LEFT JOIN ItemMaster itemMaster WITH (NOLOCK) ON sop.ItemMasterId = itemMaster.ItemMasterId
-    LEFT JOIN UnitOfMeasure iu WITH (NOLOCK) ON itemMaster.ConsumeUnitOfMeasureId = iu.UnitOfMeasureId
-    LEFT JOIN Condition cp WITH (NOLOCK) ON sop.ConditionId = cp.ConditionId
-    LEFT JOIN Customer cust WITH (NOLOCK) ON so.CustomerId = cust.CustomerId
-    LEFT JOIN Address custAddress WITH (NOLOCK) ON cust.AddressId = custAddress.AddressId
-    LEFT JOIN CustomerFinancial cf WITH (NOLOCK) ON cust.CustomerId = cf.CustomerId
-    LEFT JOIN Employee emp WITH (NOLOCK) ON so.EmployeeId = emp.EmployeeId
-    LEFT JOIN Employee sp WITH (NOLOCK) ON so.SalesPersonId = sp.EmployeeId
-    LEFT JOIN Countries cont WITH (NOLOCK) ON custAddress.CountryId = cont.countries_id
-    LEFT JOIN Currency cur WITH (NOLOCK) ON so.CurrencyId = cur.CurrencyId
-    LEFT JOIN CreditTerms ct WITH (NOLOCK) ON cf.CreditTermsId = ct.CreditTermsId
-    LEFT JOIN StockLine sl WITH (NOLOCK) ON stk.StockLineId = sl.StockLineId
-    LEFT JOIN SalesOrderQuoteFreight soFreight WITH (NOLOCK) ON so.SalesOrderQuoteId = soFreight.SalesOrderQuoteId AND soFreight.IsActive = 1 AND soFreight.IsDeleted = 0
-    LEFT JOIN SalesOrderQuoteCharges soCharges WITH (NOLOCK) ON so.SalesOrderQuoteId = soCharges.SalesOrderQuoteId AND soCharges.IsActive = 1 AND soCharges.IsDeleted = 0
-    LEFT JOIN StockLine qs WITH (NOLOCK) ON stk.StockLineId = qs.StockLineId
-    LEFT JOIN PurchaseOrder po WITH (NOLOCK) ON qs.PurchaseOrderId = po.PurchaseOrderId
-    LEFT JOIN RepairOrder ro WITH (NOLOCK) ON qs.RepairOrderId = ro.RepairOrderId
+    FROM DBO.SalesOrderQuote so WITH (NOLOCK)
+    LEFT JOIN DBO.SalesOrderQuotePartV1 sop WITH (NOLOCK) ON so.SalesOrderQuoteId = sop.SalesOrderQuoteId
+    LEFT JOIN DBO.SalesOrderQuoteStocklineV1 stk WITH (NOLOCK) ON stk.SalesOrderQuotePartId = sop.SalesOrderQuotePartId
+    LEFT JOIN DBO.SalesOrderQuotePartCost sopc WITH (NOLOCK) ON sopc.SalesOrderQuotePartId = sop.SalesOrderQuotePartId
+    LEFT JOIN DBO.ItemMaster itemMaster WITH (NOLOCK) ON sop.ItemMasterId = itemMaster.ItemMasterId
+    LEFT JOIN DBO.UnitOfMeasure iu WITH (NOLOCK) ON itemMaster.ConsumeUnitOfMeasureId = iu.UnitOfMeasureId
+    LEFT JOIN DBO.Condition cp WITH (NOLOCK) ON sop.ConditionId = cp.ConditionId
+    LEFT JOIN DBO.Customer cust WITH (NOLOCK) ON so.CustomerId = cust.CustomerId
+    LEFT JOIN DBO.Address custAddress WITH (NOLOCK) ON cust.AddressId = custAddress.AddressId
+    LEFT JOIN DBO.CustomerFinancial cf WITH (NOLOCK) ON cust.CustomerId = cf.CustomerId
+    LEFT JOIN DBO.Employee emp WITH (NOLOCK) ON so.EmployeeId = emp.EmployeeId
+    LEFT JOIN DBO.Employee sp WITH (NOLOCK) ON so.SalesPersonId = sp.EmployeeId
+    LEFT JOIN DBO.Countries cont WITH (NOLOCK) ON custAddress.CountryId = cont.countries_id
+    LEFT JOIN DBO.Currency cur WITH (NOLOCK) ON so.CurrencyId = cur.CurrencyId
+    LEFT JOIN DBO.CreditTerms ct WITH (NOLOCK) ON cf.CreditTermsId = ct.CreditTermsId
+    LEFT JOIN DBO.StockLine sl WITH (NOLOCK) ON stk.StockLineId = sl.StockLineId
+    LEFT JOIN DBO.SalesOrderQuoteFreight soFreight WITH (NOLOCK) ON so.SalesOrderQuoteId = soFreight.SalesOrderQuoteId AND soFreight.IsActive = 1 AND soFreight.IsDeleted = 0
+    LEFT JOIN DBO.SalesOrderQuoteCharges soCharges WITH (NOLOCK) ON so.SalesOrderQuoteId = soCharges.SalesOrderQuoteId AND soCharges.IsActive = 1 AND soCharges.IsDeleted = 0
+    LEFT JOIN DBO.StockLine qs WITH (NOLOCK) ON stk.StockLineId = qs.StockLineId
+    LEFT JOIN DBO.PurchaseOrder po WITH (NOLOCK) ON qs.PurchaseOrderId = po.PurchaseOrderId
+    LEFT JOIN DBO.RepairOrder ro WITH (NOLOCK) ON qs.RepairOrderId = ro.RepairOrderId
     WHERE so.SalesOrderQuoteId = @SalesQuoteId 
       AND so.IsActive = 1 
       AND so.IsDeleted = 0;

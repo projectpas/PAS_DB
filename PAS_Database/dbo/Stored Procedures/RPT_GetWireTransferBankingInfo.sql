a161d8ebd61b5ae0367d7a196159cca214335d5f
ekta <ekta@silverxis.com>
2024-12-30 17:52:34 +0530

    PN-10819 , PN-10820 - Add IsPrimary flag

--- PAS_Database/dbo/Stored Procedures/RPT_GetWireTransferBankingInfo.sql
+++ PAS_Database/dbo/Stored Procedures/RPT_GetWireTransferBankingInfo.sql
@@ -14,7 +14,8 @@
  ** PR   Date         Author		Change Description            
  ** --   --------     -------		--------------------------------          
     1    01/10/2024  Amit Ghediya    Created
-     
+    2    30/12/2024	 EKTA CHANDEGRA	 Check IsPrimary 
+
 -- EXEC RPT_GetWireTransferBankingInfo 1
 ************************************************************************/
 CREATE       PROCEDURE [dbo].[RPT_GetWireTransferBankingInfo] 
@@ -42,7 +43,7 @@ BEGIN
 				dbo.EntityStructureSetup ess WITH(NOLOCK)
 				JOIN dbo.ManagementStructureLevel msl WITH(NOLOCK) ON ess.Level1Id = msl.ID
 				JOIN dbo.LegalEntity le WITH(NOLOCK) ON msl.LegalEntityId = le.LegalEntityId
-				LEFT JOIN dbo.LegalEntityInternationalWireBanking lb WITH(NOLOCK) ON le.LegalEntityId = lb.LegalEntityId
+				LEFT JOIN dbo.LegalEntityInternationalWireBanking lb WITH(NOLOCK) ON le.LegalEntityId = lb.LegalEntityId AND lb.IsPrimay = 1
 				LEFT JOIN dbo.InternationalWirePayment inter WITH(NOLOCK) ON lb.InternationalWirePaymentId = inter.InternationalWirePaymentId
 				LEFT JOIN dbo.Address ad WITH(NOLOCK) ON le.AddressId = ad.AddressId
 				LEFT JOIN dbo.Countries co WITH(NOLOCK) ON ad.CountryId = co.countries_id

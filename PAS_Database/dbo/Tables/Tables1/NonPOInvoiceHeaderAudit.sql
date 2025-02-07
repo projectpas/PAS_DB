ab214c8ce460eb63a4d31415ab99c0ebe985da8e
abhishekjir <abhishek@silverxis.com>
2025-02-07 17:48:07 +0530

    PN-11272  Vendor Bill (Non PO) integration with QUICKBOOKS

--- PAS_Database/dbo/Tables/Tables1/NonPOInvoiceHeaderAudit.sql
+++ PAS_Database/dbo/Tables/Tables1/NonPOInvoiceHeaderAudit.sql
@@ -32,10 +32,10 @@
     [ReferenceId]               BIGINT        NULL,
     [ReferenceModuleId]         INT           NULL,
     [ControlNumber]             VARCHAR (150) NULL,
+    [QuickBooksReferenceId]     VARCHAR (200) NULL,
+    [IsUpdated]                 BIT           NULL,
+    [LastSyncDate]              DATETIME2 (7) NULL,
+    [SyncToken]                 VARCHAR (200) NULL,
     CONSTRAINT [PK_NonPOInvoiceHeaderAudit] PRIMARY KEY CLUSTERED ([NonPOInvoiceHeaderAuditId] ASC)
 );
-
-
-
-
 

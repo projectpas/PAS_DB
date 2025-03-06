ba8a85b7b1f1c00627d4354c468797fc33298b52
devendra <devendra@silverxis.com>
2025-03-06 19:40:26 +0530

    PN-11672

--- PAS_Database/dbo/Stored Procedures/USP_GetWOTaskMaterialsByWOTaskId.sql
+++ PAS_Database/dbo/Stored Procedures/USP_GetWOTaskMaterialsByWOTaskId.sql
@@ -16,8 +16,9 @@
  ** --   --------			-------			--------------------------------
     1    01/17/2025			Vishal Suthar		Created
     2    05-March-2025		Devendra Shekh		Changes For New Fields(PartDescription, UnitOfMeasure, Condition, Quantity)
+    3    06-March-2025		Devendra Shekh		Modified (UOM issue resolved)
 
-EXEC [dbo].[USP_GetWOTaskMaterialsByWOTaskId] 755
+EXEC [dbo].[USP_GetWOTaskMaterialsByWOTaskId] 831
 **************************************************************/
 CREATE   PROCEDURE [dbo].[USP_GetWOTaskMaterialsByWOTaskId]
 	@WorkOrderTaskId bigint = 0
@@ -36,15 +37,17 @@ BEGIN
 				IM.PartNumber,
 				CASE WHEN ISNULL(MSTL.StockLIneId, 0) > 0 THEN ISNULL(MSTL.ExtendedCost, 0) ELSE ISNULL(WOM.ExtendedCost, 0) END AS BillingAmount,
 				IM.PartDescription,
-				UOM.ShortName AS UnitOfMeasure,
+				CASE WHEN SUOM.UnitOfMeasureId IS NOT NULL THEN SUOM.ShortName ELSE UOM.ShortName END AS UnitOfMeasure,
 				C.Description AS Condition,
 				ISNULL(WOM.Quantity, 0) AS Quantity 
 			FROM DBO.WorkOrderMaterials WOM WITH (NOLOCK) 
 			LEFT JOIN DBO.WorkOrderMaterialStockLine MSTL WITH (NOLOCK) ON MSTL.WorkOrderMaterialsId = WOM.WorkOrderMaterialsId AND MSTL.IsDeleted = 0
 			INNER JOIN DBO.ItemMaster IM WITH (NOLOCK) ON WOM.ItemMasterId = IM.ItemMasterId
 			LEFT JOIN DBO.WorkOrderTask WOT WITH (NOLOCK) ON WOT.WorkOrderTaskId = WOM.TaskId AND WOT.WorkOrderId = WOM.WorkOrderId
-			LEFT JOIN DBO.UnitOfMeasure UOM WITH (NOLOCK) ON UOM.UnitOfMeasureId = WOM.UnitOfMeasureId
+			LEFT JOIN DBO.UnitOfMeasure UOM WITH (NOLOCK) ON UOM.UnitOfMeasureId = IM.PurchaseUnitOfMeasureId
 			LEFT JOIN dbo.Condition C WITH (NOLOCK) ON C.ConditionId = WOM.ConditionCodeId
+			LEFT JOIN dbo.Stockline SL WITH (NOLOCK) ON SL.StockLineId = MSTL.StockLineId
+			LEFT JOIN dbo.UnitOfMeasure SUOM WITH (NOLOCK) ON SUOM.UnitOfMeasureId = SL.PurchaseUnitOfMeasureId
 			WHERE WOM.IsDeleted = 0 AND WOT.WorkOrderTaskId = @WorkOrderTaskId
     
 			UNION ALL
@@ -53,16 +56,19 @@ BEGIN
 				IM.PartNumber,
 				CASE WHEN ISNULL(MSTL.StockLIneId, 0) > 0 THEN ISNULL(MSTL.ExtendedCost, 0) ELSE ISNULL(WOMK.ExtendedCost, 0) END AS BillingAmount,
 				IM.PartDescription,
-				UOM.ShortName AS UnitOfMeasure,
-				C.Description AS Condition,
+				CASE WHEN SUOM.UnitOfMeasureId IS NOT NULL THEN SUOM.ShortName ELSE UOM.ShortName END AS UnitOfMeasure,
+				CASE WHEN Stk_C.ConditionId IS NOT NULL THEN Stk_C.Description ELSE C.Description END AS Condition,
 				ISNULL(WOMK.Quantity, 0) AS Quantity
 			FROM [DBO].[WorkOrderMaterialsKitMapping] WOMKIT WITH(NOLOCK)
 			INNER JOIN [dbo].[WorkOrderMaterialsKit] WOMK WITH(NOLOCK) ON WOMK.WorkOrderMaterialsKitMappingId = WOMKIT.WorkOrderMaterialsKitMappingId
 			LEFT JOIN dbo.WorkOrderMaterialStockLineKit MSTL WITH (NOLOCK) ON MSTL.WorkOrderMaterialsKitId = WOMK.WorkOrderMaterialsKitId AND MSTL.IsDeleted = 0
 			INNER JOIN DBO.ItemMaster IM WITH (NOLOCK) ON WOMK.ItemMasterId = IM.ItemMasterId
 			LEFT JOIN DBO.WorkOrderTask WOT WITH (NOLOCK) ON WOT.WorkOrderTaskId = WOMK.TaskId AND WOT.WorkOrderId = WOMK.WorkOrderId
-			LEFT JOIN DBO.UnitOfMeasure UOM WITH (NOLOCK) ON UOM.UnitOfMeasureId = WOMK.UnitOfMeasureId
+			LEFT JOIN DBO.UnitOfMeasure UOM WITH (NOLOCK) ON UOM.UnitOfMeasureId = IM.PurchaseUnitOfMeasureId
 			LEFT JOIN dbo.Condition C WITH (NOLOCK) ON C.ConditionId = WOMK.ConditionCodeId
+			LEFT JOIN dbo.Stockline SL WITH (NOLOCK) ON SL.StockLineId = MSTL.StockLineId
+			LEFT JOIN dbo.UnitOfMeasure SUOM WITH (NOLOCK) ON SUOM.UnitOfMeasureId = SL.PurchaseUnitOfMeasureId
+			LEFT JOIN dbo.Condition Stk_C WITH (NOLOCK) ON Stk_C.ConditionId = SL.ConditionId
 			WHERE WOMKIT.IsDeleted = 0 AND WOT.WorkOrderTaskId = @WorkOrderTaskId
 		) AS Parts;
 	END TRY

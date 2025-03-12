53b26ff161ead39fcca10c94092a72f82c703ae9
saliyabhargav <bhargav.saliya@silverxis.com>
2025-03-12 11:35:06 +0530

    PN-11716

---
+++ PAS_Database/dbo/Views/vw_VendorAuditTypeAudit.sql
@@ -0,0 +1,16 @@
+﻿
+
+CREATE   VIEW [dbo].[vw_VendorAuditTypeAudit]
+AS
+	SELECT 
+	VAA.VendorAuditTypeAuditId AS [PkID],
+	VAA.VendorAuditTypeId AS [ID],
+	VAA.VendorAuditType AS [Vendor Audit Type],
+	VAA.Memo,
+	VAA.CreatedDate AS [Created On],
+	VAA.CreatedBy AS [Created By],
+	VAA.UpdatedDate AS [Updated On],
+	VAA.UpdatedBy AS [Updated By],
+	VAA.IsActive AS [Is Active],
+	VAA.IsDeleted AS [Is Deleted]
+	FROM [dbo].[VendorAuditTypeAudit] VAA WITH (NOLOCK)

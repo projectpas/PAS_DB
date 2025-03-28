d9a3167aa667ba51922699f1c7c499c4186506d5
Moin Bloch <moin@silverxis.com>
2025-03-28 17:28:58 +0530

    PN-11992

--- PAS_Database/dbo/Stored Procedures/UpdateSubWorkOrderMPNCostDetail.sql
+++ PAS_Database/dbo/Stored Procedures/UpdateSubWorkOrderMPNCostDetail.sql
@@ -10,9 +10,9 @@ EXEC [usp_IssueSubWorkOrderMaterialsStockline]
 ** PR   Date        Author          Change Description  
 ** --   --------    -------         --------------------------------
 ** 1    02/13/2025  HEMANT SALIYA    Update Sub WO MPN Cost Details
-
+** 2    03/28/2025  Moin Bloch       Fixed For Issue Sub WorkOrder And Format SP
 **************************************************************/ 
-CREATE   PROCEDURE UpdateSubWorkOrderMPNCostDetail
+CREATE   PROCEDURE [dbo].[UpdateSubWorkOrderMPNCostDetail]
     @workOrderId BIGINT,
     @subWorkOrderId BIGINT,
     @subWOPartNoId BIGINT,
@@ -20,7 +20,11 @@ CREATE   PROCEDURE UpdateSubWorkOrderMPNCostDetail
     @masterCompanyId INT
 AS
 BEGIN
-    SET NOCOUNT ON;
+   SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+		BEGIN TRY
+			BEGIN TRANSACTION
+				BEGIN
     
     DECLARE @overheadCost DECIMAL(18,2) = 0,
             @labourCost DECIMAL(18,2) = 0,
@@ -40,37 +44,36 @@ BEGIN
             @freightCost DECIMAL(18,2) = 0;
     
     -- Calculate Parts Cost
-    SELECT @partsCost = SUM(ISNULL(wms.UnitCost * wms.QtyIssued, 0))
-    FROM dbo.SubWorkOrderMaterials wm WITH(NOLOCK)
-    JOIN SubWorkOrderMaterialStockLine wms ON wm.SubWorkOrderMaterialsId = wms.SubWorkOrderMaterialsId
-    WHERE wm.SubWOPartNoId = @subWOPartNoId AND wm.IsDeleted = 0 AND wms.QtyIssued > 0;
+    SELECT @partsCost = SUM(ISNULL(wms.[UnitCost] * wms.[QtyIssued], 0))
+    FROM [dbo].[SubWorkOrderMaterials] wm WITH(NOLOCK)
+    JOIN [dbo].[SubWorkOrderMaterialStockLine] wms WITH(NOLOCK) ON wm.[SubWorkOrderMaterialsId] = wms.[SubWorkOrderMaterialsId]
+    WHERE wm.[SubWOPartNoId] = @subWOPartNoId AND wm.[IsDeleted] = 0 AND wms.[QtyIssued] > 0;
     
     -- Calculate Charges Cost
-    SELECT @chargesCost = SUM(ISNULL(UnitCost * Quantity, 0))
-    FROM dbo.SubWorkOrderCharges WITH(NOLOCK)
-    WHERE SubWOPartNoId = @subWOPartNoId AND IsDeleted = 0;
+    SELECT @chargesCost = SUM(ISNULL([UnitCost] * [Quantity], 0))
+    FROM [dbo].[SubWorkOrderCharges] WITH(NOLOCK)
+    WHERE [SubWOPartNoId] = @subWOPartNoId AND [IsDeleted] = 0;
     
     -- Calculate Freight Cost
-    SELECT @freightCost = SUM(ISNULL(Amount, 0))
-    FROM SubWorkOrderFreight
-    WHERE SubWOPartNoId = @subWOPartNoId AND IsDeleted = 0;
+    SELECT @freightCost = SUM(ISNULL([Amount], 0))
+    FROM [dbo].[SubWorkOrderFreight] WITH(NOLOCK)
+    WHERE [SubWOPartNoId] = @subWOPartNoId AND [IsDeleted] = 0;
     
     -- Calculate Labor Cost and Overhead Cost
     DECLARE @labourHeaderId BIGINT;
-    SELECT @labourHeaderId = SubWorkOrderLaborHeaderId
-    FROM SubWorkOrderLaborHeader
-    WHERE SubWOPartNoId = @subWOPartNoId AND IsDeleted = 0;
+    SELECT @labourHeaderId = [SubWorkOrderLaborHeaderId]
+    FROM [dbo].[SubWorkOrderLaborHeader] WITH(NOLOCK)
+    WHERE [SubWOPartNoId] = @subWOPartNoId AND [IsDeleted] = 0;
     
     IF @labourHeaderId IS NOT NULL
     BEGIN
-        SELECT @labourCost = SUM(ISNULL(TotalCost, 0)),
-               @overheadCost = SUM(ISNULL(DirectLaborOHCost, 0))
-        FROM SubWorkOrderLabor
-        WHERE SubWorkOrderLaborHeaderId = @labourHeaderId AND BillableId = 1 AND IsActive = 1 AND IsDeleted = 0;
-    END
-    
+        SELECT @labourCost = SUM(ISNULL([TotalCost], 0)),
+               @overheadCost = SUM(ISNULL([DirectLaborOHCost], 0))
+        FROM [dbo].[SubWorkOrderLabor] WITH(NOLOCK)
+        WHERE [SubWorkOrderLaborHeaderId] = @labourHeaderId AND [BillableId] = 1 AND [IsActive] = 1 AND [IsDeleted] = 0;
+    END   
     -- Calculate Total Cost
-    DECLARE @totalCost DECIMAL(18,2);
+    DECLARE @totalCost DECIMAL(18,2) = 0;
     SET @totalCost = @partsCost + @chargesCost + @freightCost + @labourCost;
     SET @revenue = @partsCost + @labourCost + @otherCost;
     SET @margin = @revenue - (@partsCost + @labourCost + @otherCost);
@@ -80,41 +83,40 @@ BEGIN
     SET @laborRevePer = CASE WHEN @revenue = 0 THEN 0 ELSE (@labourCost / @revenue) * 100 END;
     SET @overHeadPer = CASE WHEN @revenue = 0 THEN 0 ELSE (@overheadCost / @revenue) * 100 END;
     SET @marginPer = CASE WHEN @revenue = 0 THEN 0 ELSE (@margin / @revenue) * 100 END;
-    SET @directCostPer = CASE WHEN @revenue = 0 THEN 0 ELSE ((@partsCost + @labourCost + @otherCost) / @revenue) * 100 END;
-    
+    SET @directCostPer = CASE WHEN @revenue = 0 THEN 0 ELSE ((@partsCost + @labourCost + @otherCost) / @revenue) * 100 END;    
     -- Update or Insert Work Order Cost Details
-    IF EXISTS (SELECT 1 FROM SubWorkOrderMPNCostDetail WHERE WorkOrderId = @workOrderId AND SubWOPartNoId = @subWOPartNoId)
-    BEGIN
-        UPDATE SubWorkOrderMPNCostDetail
-        SET ActualMargin = @actMargin,
-            ActualMarginPercentage = @actMarginPer,
-            ActualRevenue = @actRevenue,
-            ChargesCost = @chargesCost,
-            DirectCost = (@partsCost + @labourCost + @otherCost),
-            DirectCostPercentage = @directCostPer,
-            FreightCost = @freightCost,
-            LaborCost = @labourCost,
-            LaborRevPercentage = @laborRevePer,
-            Margin = @margin,
-            MarginPercentage = @marginPer,
-            OtherCost = @otherCost,
-            OverHeadCost = @overheadCost,
-            OverHeadPercentage = @overHeadPer,
-            PartsCost = @partsCost,
-            PartsRevPercentage = @partsRevePer,
-            Revenue = @revenue,
-            TotalCost = @totalCost,
-            UpdatedBy = @updatedBy,
-            UpdatedDate = GETUTCDATE()
-        WHERE WorkOrderId = @workOrderId AND SubWOPartNoId = @subWOPartNoId;
+    IF EXISTS (SELECT 1 FROM [dbo].[SubWorkOrderMPNCostDetail] WITH(NOLOCK) WHERE [WorkOrderId] = @workOrderId AND [SubWOPartNoId] = @subWOPartNoId)
+    BEGIN		
+        UPDATE [dbo].[SubWorkOrderMPNCostDetail]
+        SET [ActualMargin] = @actMargin,
+            [ActualMarginPercentage] = @actMarginPer,
+            [ActualRevenue] = @actRevenue,
+            [ChargesCost] = @chargesCost,
+            [DirectCost] = (@partsCost + @labourCost + @otherCost),
+            [DirectCostPercentage] = @directCostPer,
+            [FreightCost] = @freightCost,
+            [LaborCost] = @labourCost,
+            [LaborRevPercentage] = @laborRevePer,
+            [Margin] = @margin,
+            [MarginPercentage] = @marginPer,
+            [OtherCost] = @otherCost,
+            [OverHeadCost] = @overheadCost,
+            [OverHeadPercentage] = @overHeadPer,
+            [PartsCost] = @partsCost,
+            [PartsRevPercentage] = @partsRevePer,
+            [Revenue] = @revenue,
+            [TotalCost] = @totalCost,
+            [UpdatedBy] = @updatedBy,
+            [UpdatedDate] = GETUTCDATE()
+        WHERE [WorkOrderId] = @workOrderId AND [SubWOPartNoId] = @subWOPartNoId;
     END
     ELSE
     BEGIN
-        INSERT INTO SubWorkOrderMPNCostDetail (
-            WorkOrderId, SubWorkOrderId, SubWOPartNoId, ChargesCost, DirectCost, DirectCostPercentage,
-            ActualMargin, ActualMarginPercentage,ActualRevenue, FreightCost, LaborCost, LaborRevPercentage, Margin, MarginPercentage, OtherCost, OverHeadCost,
-            OverHeadPercentage, PartsCost, PartsRevPercentage, Revenue, TotalCost, CreatedBy, CreatedDate,
-            UpdatedBy, UpdatedDate, IsActive, IsDeleted, MasterCompanyId
+        INSERT INTO [dbo].[SubWorkOrderMPNCostDetail] (
+            [WorkOrderId], [SubWorkOrderId], [SubWOPartNoId], [ChargesCost], [DirectCost], [DirectCostPercentage],
+            [ActualMargin], [ActualMarginPercentage],[ActualRevenue], [FreightCost], [LaborCost], [LaborRevPercentage], [Margin], [MarginPercentage], [OtherCost], [OverHeadCost],
+            [OverHeadPercentage], [PartsCost], [PartsRevPercentage], [Revenue], [TotalCost], [CreatedBy], [CreatedDate],
+            [UpdatedBy], [UpdatedDate], [IsActive], [IsDeleted], [MasterCompanyId]
         )
         VALUES (
             @workOrderId, @subWorkOrderId, @subWOPartNoId, @chargesCost, (@partsCost + @labourCost + @otherCost), @directCostPer,
@@ -123,6 +125,33 @@ BEGIN
             @updatedBy, GETUTCDATE(), 1, 0, IIF(@masterCompanyId = 0, 1, @masterCompanyId)
         );
 
-		EXEC usp_CalculateSubWorkOrderCostsDetail @WorkOrderId = @workOrderId, @SubWorkOrderId = @subWorkOrderId, @SubWOPartNoId = @subWOPartNoId, @UpdatedBy = @updatedBy, @MasterCompanyId = @masterCompanyId
+		EXEC [dbo].[usp_CalculateSubWorkOrderCostsDetail] @WorkOrderId = @workOrderId, @SubWorkOrderId = @subWorkOrderId, @SubWOPartNoId = @subWOPartNoId, @UpdatedBy = @updatedBy, @MasterCompanyId = @masterCompanyId
     END
+
+	END
+	COMMIT  TRANSACTION
+		END TRY    
+		BEGIN CATCH      
+			IF @@trancount > 0
+				PRINT 'ROLLBACK'
+                    ROLLBACK TRAN;
+              DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+              , @AdhocComments     VARCHAR(150)    = 'UpdateSubWorkOrderMPNCostDetail' 
+			  ,@ProcedureParameters VARCHAR(3000) = '@Parameter1 = ''' + CAST(ISNULL(@workOrderId, '') AS VARCHAR(100))
+			                                      + '@Parameter2 = ''' + CAST(ISNULL(@subWorkOrderId, '') AS VARCHAR(100)) 
+												  + '@Parameter3 = ''' + CAST(ISNULL(@subWOPartNoId, '') AS VARCHAR(100)) 
+												  + '@Parameter4 = ''' + CAST(ISNULL(@updatedBy, '') AS VARCHAR(100)) 
+												  + '@Parameter5 = ''' + CAST(ISNULL(@masterCompanyId, '') AS VARCHAR(100))               
+              , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+              exec spLogException 
+                       @DatabaseName           = @DatabaseName
+                     , @AdhocComments          = @AdhocComments
+                     , @ProcedureParameters    = @ProcedureParameters
+                     , @ApplicationName        = @ApplicationName
+                     , @ErrorLogID                    = @ErrorLogID OUTPUT ;
+              RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)
+              RETURN(1);
+        END CATCH     
 END

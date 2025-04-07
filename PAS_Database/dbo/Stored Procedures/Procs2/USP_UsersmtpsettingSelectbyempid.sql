155e8b2a94d9b43570b966ecca8a5ac49645fbc5
shrey.chandegara <shrey.chandegara@silverxis.com>
2025-04-07 15:37:44 +0530

    PN-12082

--- PAS_Database/dbo/Stored Procedures/Procs2/USP_UsersmtpsettingSelectbyempid.sql
+++ PAS_Database/dbo/Stored Procedures/Procs2/USP_UsersmtpsettingSelectbyempid.sql
@@ -1,6 +1,5 @@
-﻿
---exec USP_UsersmtpsettingSelectbyempid 3
-create    Procedure USP_UsersmtpsettingSelectbyempid
+﻿--exec USP_UsersmtpsettingSelectbyempid 228
+CREATE    Procedure [dbo].[USP_UsersmtpsettingSelectbyempid]
 @EmployeeId bigint 
 AS
 BEGIN
@@ -9,7 +8,7 @@ SET NOCOUNT ON
 
 	BEGIN TRY
 
-	select isnull(smtpsettingId,0)smtpsettingId,Employee.EmployeeId,smtpserver,emailpassword,isnull(portno,0)portno,email,emailtype,verifyemail from Employee 
+	select isnull(smtpsettingId,0)smtpsettingId,Employee.EmployeeId,smtpserver,emailpassword,isnull(portno,0)portno,email,emailtype,verifyemail,ISNULL(IsIncludeInCC,0) AS IsIncludeInCC from Employee 
 	left join Usersmtpsetting on Employee.EmployeeId=Usersmtpsetting.EmployeeId
 	where Employee.EmployeeId=@EmployeeId
 			

5e85d9e942bf24803dcc429afd08bcfdf5c6217c
Rajesh Gami <rajesh@silverxis.com>
2025-04-07 15:07:33 +0530

    PN-11862, PN-11295, PN-11917

---
+++ PAS_Database/dbo/Stored Procedures/RPT_GetCommonSubWorkOrderFormTypePrintView.sql
@@ -0,0 +1,160 @@
+﻿
+/*************************************************************           
+ ** File:   [RPT_GetCommonSubWorkOrderFormTypePrintView]       
+ ** Author: RAJESH GAMI
+ ** Description: This stored procedure is used retrieve Common Sub Work Order Form Type (Task) for SSRS report
+ ** Purpose:         
+ ** Date:   03 APR 2025
+
+ ** PARAMETERS:           
+ @UserType varchar(60)   
+         
+ ** RETURN VALUE:           
+  
+ **************************************************************           
+  ** Change History           
+ **************************************************************           
+ ** PR   Date           Author		         Change Description            
+ ** --   ----------    -----------		--------------------------------          
+    1    03 APR 2025   RAJESH GAMI			Created    
+-- EXEC  [dbo].[RPT_GetCommonSubWorkOrderFormTypePrintView] 4769
+**************************************************************/
+CREATE       PROCEDURE [dbo].[RPT_GetCommonSubWorkOrderFormTypePrintView]
+	@SubWorkorderId BIGINT = 0,
+	@WorkorderId BIGINT = 0,
+	@SubWOPartNoId BIGINT = 0
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	SET NOCOUNT ON;
+
+		BEGIN TRY
+		BEGIN TRANSACTION
+			BEGIN
+				;WITH CTE AS (
+					SELECT 
+						ISNULL(WOT.SubWorkOrderTaskId, 0) AS WorkOrderTaskId,
+						ISNULL(WOT.WorkOrderId, 0) AS WorkOrderId,
+						ISNULL(WOT.SubWOPartNoId, 0) AS WorkOrderPartNumberId,
+						ISNULL(WOT.TaskId, 0) AS TaskId,
+						ISNULL(WOT.SequenceNumber, 0) AS SequenceNumber,
+						WOT.OpenDate AS OpenDate,
+						ISNULL(WOT.OpenBy, '') AS OpenBy,
+						ISNULL(WOT.IsIncludeInPrint, 0) AS IsIncludeInPrint,
+						ISNULL(WOT.HasInstruction, 0) AS HasInstruction,
+						ISNULL(WOT.TaskName, '') AS TaskName,
+						ISNULL(WOTD.TechId, 0) AS TechId,
+						ISNULL(WOTD.TechName, '') AS TechName,
+						WOTD.TechUpdatedDate AS TechUpdatedDate,
+						ISNULL(WOTD.InspectorId, 0) AS InspectorId,
+						ISNULL(WOTD.InspectorName, '') AS InspectorName,
+						WOTD.InspectorUpdatedDate AS InspectorUpdatedDate,
+						ISNULL(WOTD.Descrepancy, '') AS Descrepancy,
+						ISNULL(WOTD.Resolution, '') AS Resolution,
+						ISNULL(WOT.MasterCompanyId, 0) AS MasterCompanyId,
+						ISNULL(WOT.CreatedBy, '') AS CreatedBy,
+						ISNULL(WOT.CreatedDate, '') AS CreatedDate,
+						ISNULL(WOT.UpdatedBy, '') AS UpdatedBy,
+						ISNULL(WOT.UpdatedDate, '') AS UpdatedDate,
+						WOTI.SubWorkOrderTaskInstructionId WorkOrderTaskInstructionId,
+						ISNULL(WOTI.ParentId, 0) AS ParentId,
+						ISNULL(WOTI.IsParent, 0) AS IsParent,
+						ISNULL(WOTI.InstructionTitle, '') AS InstructionTitle,
+						ISNULL(WOTI.SequenceNumber, 0) AS ChildSequenceNumber,
+						ISNULL(WOTI.InstructionDetails, '') AS InstructionDetails,
+						ISNULL(WOTI.TechId, 0) AS ChildTechId,
+						ISNULL(WOTI.TechName, '') AS ChildTechName,
+						WOTI.TechUpdatedDate AS ChildTechUpdatedDate,
+						ISNULL(WOTI.InspectorId, 0) AS ChildInspectorId,
+						ISNULL(WOTI.InspectorName, '') AS ChildInspectorName,
+						WOTI.InspectorUpdatedDate AS ChildInspectorUpdatedDate,
+						ISNULL(WOTI.PrintInWO, 0) AS PrintInWO,
+						ISNULL(WOTI.PrintInWOQ, 0) AS PrintInWOQ
+					FROM dbo.SubWorkOrderTask WOT WITH (NOLOCK)
+					INNER JOIN dbo.SubWorkOrderTaskDetails WOTD WITH (NOLOCK) ON WOT.SubWorkOrderTaskId = WOTD.SubWorkOrderTaskId
+					LEFT JOIN dbo.SubWorkOrderTaskInstruction WOTI WITH (NOLOCK) ON WOT.SubWorkOrderTaskId = WOTI.SubWorkOrderTaskId AND ISNULL(WOTI.PrintInWO,0) = 1
+					WHERE WOT.WorkOrderId = @WorkOrderId AND WOT.SubWorkOrderId = @SubWorkorderId AND WOT.IsActive = 1 AND WOT.IsDeleted = 0 AND ISNULL(WOTD.PrintInWO,0) = 1 AND ISNULL(WOT.SubWOPartNoId,0) = @SubWOPartNoId
+				),
+				RecursiveCTE AS (				
+
+					SELECT 
+						c.*
+						,CAST(c.SequenceNumber AS NVARCHAR(MAX)) + '.' + CAST(ROW_NUMBER() OVER (PARTITION BY c.SequenceNumber ORDER BY c.ChildSequenceNumber) AS NVARCHAR(MAX)) AS SrNo
+					FROM CTE c
+					WHERE c.ParentId = 0
+					UNION ALL
+				
+					SELECT 
+						c.*,
+						 CAST(r.SrNo + '.' + 
+							 CAST(c.ChildSequenceNumber AS NVARCHAR(MAX)) AS NVARCHAR(MAX)
+							 )
+							 AS SrNo
+					FROM CTE c
+					INNER JOIN RecursiveCTE r ON c.ParentId = r.WorkOrderTaskInstructionId
+				)
+				SELECT 
+					WorkOrderTaskId,
+					WorkOrderId,
+					WorkOrderPartNumberId,
+					TaskId,
+					SequenceNumber,
+					OpenDate,
+					OpenBy,
+					IsIncludeInPrint,
+					HasInstruction,
+					TaskName,
+					TechId,
+					TechName,
+					TechUpdatedDate,
+					InspectorId,
+					InspectorName,
+					InspectorUpdatedDate,
+					Descrepancy,
+					Resolution,
+					MasterCompanyId,
+					CreatedBy,
+					CreatedDate,
+					UpdatedBy,
+					UpdatedDate,
+					ISNULL(WorkOrderTaskInstructionId,0)WorkOrderTaskInstructionId,
+					ParentId,
+					IsParent,
+					InstructionTitle,
+					ChildSequenceNumber,
+					REPLACE(REPLACE(ISNULL(InstructionDetails,''), '<p>', ''),'</p>','<br />') as InstructionDetails,
+					ChildTechId,
+					ChildTechName,
+					ChildTechUpdatedDate,
+					ChildInspectorId,
+					ChildInspectorName,
+					ChildInspectorUpdatedDate,
+					PrintInWO,
+					PrintInWOQ,
+					SrNo
+				FROM RecursiveCTE
+				ORDER BY SequenceNumber;
+			END
+		COMMIT  TRANSACTION
+
+		END TRY    
+		BEGIN CATCH      
+			IF @@trancount > 0
+				PRINT 'ROLLBACK'
+				ROLLBACK TRAN;
+				DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+              , @AdhocComments     VARCHAR(150)    = 'RPT_GetCommonSubWorkOrderFormTypePrintView' 
+              , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ ISNULL(@WorkorderId, '') + ''
+              , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+              exec spLogException 
+                       @DatabaseName           =  @DatabaseName
+                     , @AdhocComments          =  @AdhocComments
+                     , @ProcedureParameters	   =  @ProcedureParameters
+                     , @ApplicationName        =  @ApplicationName
+                     , @ErrorLogID             =  @ErrorLogID OUTPUT ;
+              RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)
+              RETURN(1);
+		END CATCH
+END
---
+++ PAS_Database/dbo/Stored Procedures/SP_GetSubWorkOrderMPNsById.sql
@@ -0,0 +1,122 @@
+﻿
+/*************************************************************           
+ ** File:   [SP_GetSubWorkOrderMPNsById]           
+ ** Author: Rajesh Gami
+ ** Description: This stored procedure is used to Get SubWork Order MPNs By Subworkorder Id
+ ** Date:   21 MAR 2025
+ ** PARAMETERS:           
+ ** RETURN VALUE:
+ **************************************************************           
+  ** Change History           
+ **************************************************************           
+ ** PR   Date          Author  		 Change Description            
+ ** --   --------      -------		 ---------------------------     
+    1    21 MAR 2025   Rajesh Gami     Created
+**************************************************************
+ EXEC SP_GetSubWorkOrderMPNsById 246
+ **************************************************************/
+CREATE     PROCEDURE [dbo].[SP_GetSubWorkOrderMPNsById] 
+@SubWorkOrderId bigint =0
+AS
+BEGIN
+  SET NOCOUNT ON;
+  SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+  BEGIN TRY
+  BEGIN TRANSACTION
+	BEGIN
+
+		IF (@SubWorkOrderId >0)
+		BEGIN
+				SELECT 
+					wop.SubWOPartNoId,
+					ISNULL(wop.IsFinishGood,0)IsFinishGood,
+					ISNULL(wop.IsClosed,0) AS isWOClose,
+					im.PartNumber,
+					im.PartDescription,
+					im.ManufacturerName,
+					ISNULL(ig.Description, '') AS ItemGroup,
+					ISNULL(im.RevisedPart, '') AS RevisePartNo,
+					con.Description AS Condition,
+					wop.ItemMasterId,
+					wop.ConditionId,
+					ISNULL(wop.Quantity,0)Quantity,
+					wop.SubWorkOrderScopeId,
+					wop.SubWorkOrderStageId,
+					wop.SubWorkOrderStatusId,
+					wop.SubWorkOrderPriorityId,
+					wop.CustomerRequestDate,
+					wop.PromisedDate,
+					wop.EstimatedCompletionDate,
+					wop.EstimatedShipDate,
+					wop.CMMIds,
+					wop.CreatedBy,
+					wop.CreatedDate,
+					ISNULL(wop.IsActive,0)IsActive,
+					ISNULL(wop.IsDeleted,0)IsDeleted,
+					ISNULL(wop.IsDER,0)IsDER,
+					ISNULL(wop.IsPMA,0)IsPMA,
+					wop.MasterCompanyId,
+					wop.NTE,
+					wop.StockLineId,
+					wop.SubWorkOrderId,
+					wop.TATDaysCurrent,
+					wop.TATDaysStandard,
+					wop.TechnicianId,
+					wop.TechStationId,
+					ISNULL(emp.FirstName + ' ' + emp.LastName, '') AS TechnicianName,
+					wop.UpdatedBy,
+					wop.UpdatedDate,
+					wop.WorkflowId,
+					wop.WorkOrderId,
+					0 WorkOrderMaterialsId,
+					ISNULL(wop.RevisedSerialNumber, ISNULL(sl.SerialNumber, '')) AS SerialNumber,
+					ISNULL(sl.StockLineNumber, '') AS StockLineNumber,
+					ISNULL(sl.ControlNumber, '') AS ControlNumber,
+					ISNULL(sl.IdNumber, '') AS ControlerId,
+					(CASE WHEN wop.WorkflowId > 0 THEN (SELECT TOP 1 WorkflowExpirationDate FROM dbo.Workflow WITH(NOLOCK) WHERE WorkflowId = wop.WorkflowId) END) AS WorkflowExpirationDate,
+					sl.SiteId,
+					ISNULL(ssi.Name, '') AS Site,
+					ISNULL(wh.Name, '') AS Warehouse,
+					ISNULL(lo.Name, '') AS Location,
+					ISNULL(sh.Name, '') AS Shelf,
+					ISNULL(bi.Name, '') AS Bin,
+					ISNULL(wop.IsTraveler,0)IsTraveler,
+					ISNULL(wop.IsManualForm,0)IsManualForm,
+					ISNULL(wop.islocked, 0) AS IsLocked
+				FROM dbo.SubWorkOrderPartNumber wop WITH(NOLOCK)
+				JOIN dbo.ItemMaster im WITH(NOLOCK) ON wop.ItemMasterId = im.ItemMasterId
+				JOIN dbo.Condition con WITH(NOLOCK) ON wop.ConditionId = con.ConditionId
+				LEFT JOIN dbo.Itemgroup ig WITH(NOLOCK) ON im.ItemGroupId = ig.ItemGroupId
+				LEFT JOIN dbo.StockLine sl WITH(NOLOCK) ON wop.StockLineId = sl.StockLineId
+				LEFT JOIN dbo.Site ssi WITH(NOLOCK) ON sl.SiteId = ssi.SiteId
+				LEFT JOIN dbo.Warehouse wh WITH(NOLOCK) ON sl.WarehouseId = wh.WarehouseId
+				LEFT JOIN dbo.Location lo WITH(NOLOCK) ON sl.LocationId = lo.LocationId
+				LEFT JOIN dbo.Shelf sh WITH(NOLOCK) ON sl.ShelfId = sh.ShelfId
+				LEFT JOIN dbo.Bin bi WITH(NOLOCK) ON sl.BinId = bi.BinId
+				LEFT JOIN dbo.Employee emp WITH(NOLOCK) ON wop.TechnicianId = emp.EmployeeId
+				WHERE wop.SubWorkOrderId = @SubWorkOrderId;
+		END
+		
+	END
+	COMMIT  TRANSACTION
+  END TRY
+  BEGIN CATCH
+		IF @@trancount > 0
+			PRINT 'ROLLBACK'
+			ROLLBACK TRAN;
+		DECLARE @ErrorLogID int,
+            @DatabaseName varchar(100) = DB_NAME()
+            -----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+            ,@AdhocComments varchar(150) = '[SP_GetSubWorkOrderMPNsById]',
+            @ProcedureParameters varchar(3000) = '@SubWorkOrderId = ''' + CAST(ISNULL(@SubWorkOrderId, '') AS varchar(100)),
+            @ApplicationName varchar(100) = 'PAS'
+    -----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+    EXEC spLogException @DatabaseName = @DatabaseName,
+                        @AdhocComments = @AdhocComments,
+                        @ProcedureParameters = @ProcedureParameters,
+                        @ApplicationName = @ApplicationName,
+                        @ErrorLogID = @ErrorLogID OUTPUT;
+    RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1, @ErrorLogID)
+    RETURN (1);
+  END CATCH
+END

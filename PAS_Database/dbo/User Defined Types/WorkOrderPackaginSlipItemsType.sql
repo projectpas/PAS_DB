ecfcae5141f59d3149406fea69ad149856609c57
devendra <devendra@silverxis.com>
2025-04-16 18:49:16 +0530

    PN-11912

---
+++ PAS_Database/dbo/User Defined Types/WorkOrderPackaginSlipItemsType.sql
@@ -0,0 +1,14 @@
+﻿CREATE TYPE [dbo].[WorkOrderPackaginSlipItemsType] AS TABLE (
+    [PackagingSlipItemId] BIGINT         NULL,
+    [PackagingSlipId]     BIGINT         NULL,
+    [WOPickTicketId]      BIGINT         NULL,
+    [WOPartNoId]          BIGINT         NULL,
+    [MasterCompanyId]     INT            NULL,
+    [CreatedBy]           VARCHAR (256)  NULL,
+    [UpdatedBy]           VARCHAR (256)  NULL,
+    [CreatedDate]         DATETIME2 (7)  NULL,
+    [UpdatedDate]         DATETIME2 (7)  NULL,
+    [IsActive]            BIT            NULL,
+    [IsDeleted]           BIT            NULL,
+    [PDFPath]             NVARCHAR (MAX) NULL);
+

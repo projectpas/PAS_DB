94885de28cc66359348b1cb4a269e53059413cd4
DivyeshSilverxis <divyesh@silverxis.com>
2025-04-18 12:54:57 +0530

    PN-11729

--- PAS_Database/dbo/Views/vw_AssetAttributeTypeAudit.sql
+++ PAS_Database/dbo/Views/vw_AssetAttributeTypeAudit.sql
@@ -1,29 +1,30 @@
-﻿
-
-
-CREATE VIEW [dbo].[vw_AssetAttributeTypeAudit]
+﻿CREATE VIEW [dbo].[vw_AssetAttributeTypeAudit]
 AS
-SELECT --AAT.* , 
+SELECT
 	AAT.AssetAttributeTypeId AS PkID,
 	AAT.AssetAttributeTypeId AS ID,
-	ATY.TangibleClassName ,
-	CT.Name AS ConventionTypeName,
-	DM.AssetDepreciationMethodName,
-	FI.Name AS AssetDepreciationFrequencyName,
-	PER.PercentValue,
-	AQGL.AccountName AS AcquiredGLAccount,
-	DPGL.AccountName AS DeprExpenseGLAccount,
-	ADGL.AccountName AS AdDepsGLAccount,
-	ASGL.AccountName AS AssetSaleGLAccount,
-	WOGL.AccountName AS AssetWriteOffGLAccount,
-	WDGL.AccountName AS AssetWriteDownLAccount,
-	LegalEntity = STUFF((SELECT ', ' + Name
-			   FROM dbo.LegalEntity LE 
-			   WHERE LE.LegalEntityId IN (SELECT Item FROM DBO.SPLITSTRING(AAT.SelectedCompanyIds,','))
-			  FOR XML PATH('')), 1, 2, '')
-FROM dbo.AssetAttributeType AAT WITH(NOLOCK)
-	JOIN dbo.TangibleClass ATY WITH(NOLOCK) ON AAT.TangibleClassId=ATY.TangibleClassId
-	JOIN dbo.ConventionType CT WITH(NOLOCK) ON AAT.ConventionType=CT.ConventionTypeId
+	LegalEntity = STUFF((SELECT ', ' + [Name] FROM dbo.LegalEntity LE WITH(NOLOCK) WHERE LE.LegalEntityId IN (SELECT Item FROM DBO.SPLITSTRING(AAT.SelectedCompanyIds,',')) FOR XML PATH('')), 1, 2, ''),
+	ATY.TangibleClassName AS [Tangible Class],
+	AAT.[AssetAttributeTypeName] AS [Name],
+	AAT.[Description] AS [Description],	
+	ADC.[AssetDepConventionName] AS [Convention Type],
+	DM.[AssetDepreciationMethodName] AS [Depreciation Method],
+	PER.PercentValue AS [Residual Percentage],
+	AAT.[AssetLife] AS [Asset Life],
+	FI.[Name] AS [Depreciation Frequency],
+	AQGL.AccountName AS [Acquired GL Account],
+	DPGL.AccountName AS [Depr Expense GL Account],
+	ADGL.AccountName AS [Accum Depr GL Account],
+	ASGL.AccountName AS [Asset Sale GL Account],
+	WOGL.AccountName AS [Asset Write Off GL Account],
+	WDGL.AccountName AS [Asset Write Down GL Account],
+	AAT.UpdatedBy AS [Updated By],
+	AAT.UpdatedDate	AS [Updated Date],
+	AAT.IsActive AS [Active],
+	AAT.IsDeleted AS [Deleted]
+FROM dbo.AssetAttributeTypeAudit AAT WITH(NOLOCK)
+	JOIN dbo.TangibleClass ATY WITH(NOLOCK) ON AAT.TangibleClassId=ATY.TangibleClassId	
+	JOIN [dbo].[AssetDepConvention] ADC WITH(NOLOCK) ON AAT.ConventionType = ADC.AssetDepConventionId	
 	JOIN dbo.AssetDepreciationMethod DM WITH(NOLOCK) ON  AAT.DepreciationMethod=DM.AssetDepreciationMethodId
 	JOIN dbo.AssetDepreciationFrequency FI WITH(NOLOCK) ON AAT.DepreciationFrequencyId=FI.AssetDepreciationFrequencyId
 	JOIN dbo.GLAccount AQGL WITH(NOLOCK) ON  AAT.AcquiredGLAccountId=AQGL.GLAccountId

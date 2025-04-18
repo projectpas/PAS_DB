5bb69be12b773da0d474a24dc43219c00d91a027
DivyeshSilverxis <divyesh@silverxis.com>
2025-04-18 13:19:08 +0530

    PN-11729

--- PAS_Database/dbo/Views/vw_AssetAttributeTypeAudit.sql
+++ PAS_Database/dbo/Views/vw_AssetAttributeTypeAudit.sql
@@ -1,30 +1,31 @@
-﻿CREATE VIEW [dbo].[vw_AssetAttributeTypeAudit]
+﻿
+CREATE VIEW [dbo].[vw_AssetAttributeTypeAudit]
 AS
 SELECT
-	AAT.AssetAttributeTypeId AS PkID,
-	AAT.AssetAttributeTypeId AS ID,
+	[AAT].[AssetAttributeTypeId] AS PkID,
+	[AAT].[AssetAttributeTypeId] AS ID,
 	LegalEntity = STUFF((SELECT ', ' + [Name] FROM dbo.LegalEntity LE WITH(NOLOCK) WHERE LE.LegalEntityId IN (SELECT Item FROM DBO.SPLITSTRING(AAT.SelectedCompanyIds,',')) FOR XML PATH('')), 1, 2, ''),
-	ATY.TangibleClassName AS [Tangible Class],
-	AAT.[AssetAttributeTypeName] AS [Name],
-	AAT.[Description] AS [Description],	
-	ADC.[AssetDepConventionName] AS [Convention Type],
-	DM.[AssetDepreciationMethodName] AS [Depreciation Method],
-	PER.PercentValue AS [Residual Percentage],
-	AAT.[AssetLife] AS [Asset Life],
-	FI.[Name] AS [Depreciation Frequency],
-	AQGL.AccountName AS [Acquired GL Account],
-	DPGL.AccountName AS [Depr Expense GL Account],
-	ADGL.AccountName AS [Accum Depr GL Account],
-	ASGL.AccountName AS [Asset Sale GL Account],
-	WOGL.AccountName AS [Asset Write Off GL Account],
-	WDGL.AccountName AS [Asset Write Down GL Account],
-	AAT.UpdatedBy AS [Updated By],
-	AAT.UpdatedDate	AS [Updated Date],
-	AAT.IsActive AS [Active],
-	AAT.IsDeleted AS [Deleted]
+	[ATY].[TangibleClassName] AS [Tangible Class],
+	[AAT].[AssetAttributeTypeName] AS [Name],
+	[AAT].[Description] AS [Description],	
+	[ADC].[AssetDepConventionName] AS [Convention Type],
+	[DM].[AssetDepreciationMethodName] AS [Depreciation Method],
+	[PER].[PercentValue] AS [Residual Percentage],
+	[AAT].[AssetLife] AS [Asset Life],
+	[FI].[Name] AS [Depreciation Frequency],
+	[AQGL].[AccountName] AS [Acquired GL Account],
+	[DPGL].[AccountName] AS [Depr Expense GL Account],
+	[ADGL].[AccountName] AS [Accum Depr GL Account],
+	[ASGL].[AccountName] AS [Asset Sale GL Account],
+	[WOGL].[AccountName] AS [Asset Write Off GL Account],
+	[WDGL].[AccountName] AS [Asset Write Down GL Account],
+	[AAT].[UpdatedBy] AS [Updated By],
+	[AAT].[UpdatedDate]	AS [Updated Date],
+	[AAT].[IsActive] AS [Active],
+	[AAT].[IsDeleted] AS [Deleted]
 FROM dbo.AssetAttributeTypeAudit AAT WITH(NOLOCK)
 	JOIN dbo.TangibleClass ATY WITH(NOLOCK) ON AAT.TangibleClassId=ATY.TangibleClassId	
-	JOIN [dbo].[AssetDepConvention] ADC WITH(NOLOCK) ON AAT.ConventionType = ADC.AssetDepConventionId	
+	JOIN dbo.AssetDepConvention ADC WITH(NOLOCK) ON AAT.ConventionType = ADC.AssetDepConventionId	
 	JOIN dbo.AssetDepreciationMethod DM WITH(NOLOCK) ON  AAT.DepreciationMethod=DM.AssetDepreciationMethodId
 	JOIN dbo.AssetDepreciationFrequency FI WITH(NOLOCK) ON AAT.DepreciationFrequencyId=FI.AssetDepreciationFrequencyId
 	JOIN dbo.GLAccount AQGL WITH(NOLOCK) ON  AAT.AcquiredGLAccountId=AQGL.GLAccountId

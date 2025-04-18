257a3e49c81cf82a05aabf9bce74610563d869b0
sahdev <sahdev@silverxis.com>
2025-04-18 16:38:03 +0530

    PN-12090

---
+++ PAS_Database/dbo/Stored Procedures/USP_GetEmployeeCertificationWithModels.sql
@@ -0,0 +1,72 @@
+﻿/*************************************************************           
+ ** File:   [USP_GetEmployeeCertificationWithModels]           
+ ** Author:   Sahdev Saliya
+ ** Description: This stored procedure is used to Get Certification records by ID
+ ** Purpose:         
+ ** Date:   18-04-2025       
+          
+ ** RETURN VALUE:           
+  
+ **************************************************************             
+  ** Change History             
+ **************************************************************             
+ ** S NO   Date            Author          Change Description              
+ ** --   --------         -------          --------------------------------            
+    1    18-04-2025    Sahdev Saliya       Created  
+
+**************************************************************/    
+CREATE     PROCEDURE [dbo].[USP_GetEmployeeCertificationWithModels]
+    @EmployeeId INT,
+    @EmployeeCertificationId INT
+AS
+BEGIN
+    SET NOCOUNT ON;  
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	BEGIN TRY  
+		SELECT 
+		     EC.EmployeeCertificationTypeId
+			,EC.[EmployeeCertificationId]
+			,EC.[EmployeeId]
+			,EC.[CertificationNumber] as cert
+            ,ECT.[Description] as certType 
+			,EC.[CertifyingInstitution]
+			,EC.[CertificationDate] AS certDate
+			,EC.[CreatedBy]
+			,EC.[CreatedDate]
+			,EC.[IsExpirationDate]
+			,EC.[ExpirationDate]
+			,EC.[IsCertificationInForce] AS inforce
+			,EC.[Memo]
+			,EC.[IsActive]  
+			,EC.[IsDeleted] 
+			,EC.[MasterCompanyId]
+			,EC.[UpdatedBy]
+			,EC.[UpdatedDate]
+		FROM [dbo].[EmployeeCertification] EC WITH(NOLOCK)  
+				LEFT JOIN [dbo].[EmployeeCertificationType] ECT WITH(NOLOCK) ON EC.EmployeeCertificationTypeId = ECT.EmployeeCertificationTypeId
+		WHERE EC.EmployeeId = @EmployeeId
+		  AND EC.EmployeeCertificationId = @EmployeeCertificationId;
+	END TRY 
+	BEGIN CATCH  
+   
+    DECLARE @ErrorLogID int,  
+            @DatabaseName varchar(100) = DB_NAME(),  
+            -----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------  
+            @AdhocComments varchar(150) = 'USP_GetEmployeeTrainingWithAircraftModels',  
+            @ProcedureParameters varchar(3000) = '@Parameter1 = ''' + CAST(ISNULL(@EmployeeId, '') AS varchar(100)) +    
+            '@Parameter2 = ''' + CAST(ISNULL(@EmployeeCertificationId, '') AS varchar(100)),  
+            @ApplicationName varchar(100) = 'PAS'   
+    -----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------  
+    EXEC Splogexception @DatabaseName = @DatabaseName,  
+                        @AdhocComments = @AdhocComments,  
+                        @ProcedureParameters = @ProcedureParameters,  
+                        @ApplicationName = @ApplicationName,  
+                        @ErrorLogID = @ErrorLogID OUTPUT;  
+  
+    RAISERROR (  
+    'Unexpected Error Occured in the database. Please let the support team know of the error number : %d'  
+    , 16, 1, @ErrorLogID)  
+  
+    RETURN (1);  
+  END CATCH   
+END
---
+++ PAS_Database/dbo/Stored Procedures/USP_updateActionForEmployeeCertification.sql
@@ -0,0 +1,84 @@
+﻿/*************************************************************           
+ ** File:   [USP_updateActionForEmployeeCertification]           
+ ** Author:   Sahdev Saliya
+ ** Description: This stored procedure is used to add update Employee Certification List
+ ** Purpose:         
+ ** Date:   18-04-2025       
+          
+ ** RETURN VALUE:           
+  
+ **************************************************************             
+  ** Change History             
+ **************************************************************             
+ ** S NO   Date            Author          Change Description              
+ ** --   --------         -------          --------------------------------            
+    1    18-04-2025    Sahdev Saliya       Created  
+
+**************************************************************/ 
+CREATE     PROCEDURE [dbo].[USP_updateActionForEmployeeCertification]
+	@EmployeeCertificationId BIGINT,
+	@ActionPerform VARCHAR(256) = NULL,
+	@UpdatedBy VARCHAR(256) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+	BEGIN TRY
+		
+		IF(@ActionPerform = 'isDelete')
+		BEGIN
+			UPDATE EmployeeCertification
+			SET 
+			IsDeleted = 1,
+			UpdatedDate = GETUTCDATE(),
+			UpdatedBy = CASE WHEN @UpdatedBy IS NOT NULL THEN @UpdatedBy ELSE UpdatedBy END
+			WHERE [EmployeeCertificationId] = @EmployeeCertificationId;
+		END
+		ELSE IF(@ActionPerform = 'isRestore')
+		BEGIN
+			UPDATE EmployeeCertification
+			SET 
+			IsDeleted = 0,
+			UpdatedDate = GETUTCDATE(),
+			UpdatedBy = CASE WHEN @UpdatedBy IS NOT NULL THEN @UpdatedBy ELSE UpdatedBy END
+			WHERE [EmployeeCertificationId] = @EmployeeCertificationId;
+		END
+		ELSE IF(@ActionPerform = 'isActive')
+		BEGIN
+			UPDATE EmployeeCertification
+			SET 
+			IsActive = 1,
+			UpdatedDate = GETUTCDATE(),
+			UpdatedBy = CASE WHEN @UpdatedBy IS NOT NULL THEN @UpdatedBy ELSE UpdatedBy END
+			WHERE [EmployeeCertificationId] = @EmployeeCertificationId;
+		END
+		ELSE IF(@ActionPerform = 'isInActive')
+		BEGIN
+			UPDATE EmployeeCertification
+			SET 
+			IsActive = 0,
+			UpdatedDate = GETUTCDATE(),
+			UpdatedBy = CASE WHEN @UpdatedBy IS NOT NULL THEN @UpdatedBy ELSE UpdatedBy END
+			WHERE [EmployeeCertificationId] = @EmployeeCertificationId;
+		END
+
+	END TRY   
+	BEGIN CATCH      
+	         DECLARE @ErrorLogID INT
+			,@DatabaseName VARCHAR(100) = db_name()
+			-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+			,@AdhocComments VARCHAR(150) = 'USP_updateActionForEmployeeCertification'
+			,@ProcedureParameters VARCHAR(3000) = '@Parameter1 = '', '
+			,@ApplicationName VARCHAR(100) = 'PAS'
+		-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+		EXEC spLogException @DatabaseName = @DatabaseName
+			,@AdhocComments = @AdhocComments
+			,@ProcedureParameters = @ProcedureParameters
+			,@ApplicationName = @ApplicationName
+			,@ErrorLogID = @ErrorLogID OUTPUT;
+
+		RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d',16,1,@ErrorLogID)
+		RETURN (1);           
+	END CATCH
+END;

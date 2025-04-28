e3d0fb53bba7b65d83d2f76492072423093ef4ad
amitghediyasilverxis <amit.ghediya@silverxis.com>
2025-04-28 15:42:18 +0530

    PN-12410 handle EXCHANGES QUOTE TO SO: ERROR.

--- PAS_Database/dbo/Stored Procedures/Procs1/CopyExchangeAgreementDocument.sql
+++ PAS_Database/dbo/Stored Procedures/Procs1/CopyExchangeAgreementDocument.sql
@@ -1,4 +1,24 @@
-﻿CREATE    PROCEDURE [dbo].[CopyExchangeAgreementDocument]
+﻿/*************************************************************             
+ ** File:   [CopyExchangeAgreementDocument]             
+ ** Author:  
+ ** Description:  
+ ** Purpose:           
+ ** Date:       
+            
+ ** PARAMETERS:
+           
+ ** RETURN VALUE:             
+ **************************************************************             
+ ** Change History             
+ **************************************************************             
+ ** PR   Date         Author				Change Description              
+ ** --   --------     -------			--------------------------------     
+					 Unkown					Created
+    1    28/04/2025  Amit Ghediya			Update for insert more column.  
+       
+-- EXEC CopyExchangeAgreementDocument 1,129,456,502
+************************************************************************/  
+CREATE    PROCEDURE [dbo].[CopyExchangeAgreementDocument]
 @MasterCompanyId int=1,
 @ExchangeQuoteId bigint=21,
 @ExchangeSalesOrderId bigint=21,
@@ -10,20 +30,24 @@ SET NOCOUNT ON;
 	BEGIN TRY
 	DECLARE @EXCHANGEQUOTEMODULEID BIGINT=64;
 	DECLARE @EXCHANGESALESORDERMODULEID BIGINT=73;
-	DECLARE @DocType bigint;
-	SET @DocType=(SELECT DocumentTypeId From DocumentType WHERE MasterCompanyId=@MasterCompanyId AND [Name]='Exchange Agreement');
-	--PRINT @DocType;
-	DECLARE @AttachmentId bigint;
-	SET @AttachmentId = (select TOP 1 AttachmentId from CommonDocumentDetails where ModuleId=@EXCHANGEQUOTEMODULEID and ReferenceId=@ExchangeQuoteId AND DocumentTypeId=@DocType);
+	DECLARE @DocType BIGINT;
+	DECLARE @AttachmentId BIGINT;
+
+	SET @DocType=(SELECT [DocumentTypeId] FROM [DBO].[DocumentType] WITH(NOLOCK) WHERE MasterCompanyId=@MasterCompanyId AND [Name]='Exchange Agreement');
 	
-	INSERT INTO Attachment
-	SELECT @EXCHANGESALESORDERMODULEID,@ExchangeSalesOrderId,@MasterCompanyId,CreatedBy,GETDATE(),UpdatedBy,GETDATE(),IsActive,IsDeleted FROM Attachment WHERE AttachmentId=@AttachmentId;
+	SET @AttachmentId = (SELECT TOP 1 AttachmentId FROM [DBO].[CommonDocumentDetails] WITH(NOLOCK) WHERE ModuleId=@EXCHANGEQUOTEMODULEID AND ReferenceId=@ExchangeQuoteId AND DocumentTypeId=@DocType);
 	
-	INSERT INTO AttachmentDetails
-	SELECT IDENT_CURRENT('Attachment'),FileName,Description,Link,FileFormat,FileSize,FileType,GETDATE(),GETDATE(),CreatedBy,UpdatedBy,IsActive,IsDeleted,[Name],Memo,TypeId FROM AttachmentDetails WHERE AttachmentId=@AttachmentId;
+	INSERT INTO [DBO].[Attachment]([ModuleId],[ReferenceId],[MasterCompanyId],[CreatedBy],[CreatedDate],[UpdatedBy],[UpdatedDate],[IsActive],[IsDeleted],[SubModuleId],[SubReferenceId])
+	SELECT @EXCHANGESALESORDERMODULEID,@ExchangeSalesOrderId,@MasterCompanyId,[CreatedBy],GETDATE(),[UpdatedBy],GETDATE(),[IsActive],[IsDeleted],[SubModuleId],[SubReferenceId] 
+	FROM [DBO].[Attachment] WITH(NOLOCK) WHERE AttachmentId=@AttachmentId;
 	
-	INSERT INTO CommonDocumentDetails
-	SELECT @EXCHANGESALESORDERMODULEID,@ExchangeSalesOrderId,IDENT_CURRENT('Attachment'),DocName,DocMemo,DocDescription,@MasterCompanyId,CreatedBy,UpdatedBy,GETDATE(),GETDATE(),IsActive,IsDeleted,DocumentTypeId,ExpirationDate,ReferenceIndex,ModuleType FROM CommonDocumentDetails WHERE AttachmentId=@AttachmentId;
+	INSERT INTO [DBO].[AttachmentDetails]([AttachmentId],[FileName],[Description],[Link],[FileFormat],[FileSize],[FileType],[CreatedDate],[UpdatedDate],[CreatedBy],[UpdatedBy],[IsActive],[IsDeleted],[Name],[Memo],[TypeId])
+	SELECT IDENT_CURRENT('Attachment'),[FileName],[Description],[Link],[FileFormat],[FileSize],[FileType],GETDATE(),GETDATE(),[CreatedBy],[UpdatedBy],[IsActive],[IsDeleted],[Name],[Memo],[TypeId]
+	FROM [DBO].[AttachmentDetails] WITH(NOLOCK) WHERE AttachmentId=@AttachmentId;
+	 
+	INSERT INTO [DBO].[CommonDocumentDetails]([ModuleId],[ReferenceId],[AttachmentId],[DocName],[DocMemo],[DocDescription],[MasterCompanyId],[CreatedBy],[UpdatedBy],[CreatedDate],[UpdatedDate],[IsActive],[IsDeleted],[DocumentTypeId],[ExpirationDate],[ReferenceIndex],[ModuleType],[SubModuleId],[SubReferenceId])
+	SELECT @EXCHANGESALESORDERMODULEID,@ExchangeSalesOrderId,IDENT_CURRENT('Attachment'),[DocName],[DocMemo],[DocDescription],@MasterCompanyId,[CreatedBy],[UpdatedBy],GETDATE(),GETDATE(),[IsActive],[IsDeleted],[DocumentTypeId],[ExpirationDate],[ReferenceIndex],[ModuleType],[SubModuleId],[SubReferenceId] 
+	FROM [DBO].[CommonDocumentDetails] WITH(NOLOCK) WHERE AttachmentId=@AttachmentId;
 
 	SELECT @AttachmentIds = IDENT_CURRENT('Attachment');
 

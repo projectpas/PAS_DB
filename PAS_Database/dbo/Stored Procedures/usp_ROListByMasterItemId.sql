bdbb331e18cfc03cd3eafd28cd81c7b016fe3313
sahdev <sahdev@silverxis.com>
2025-05-05 15:58:08 +0530

    PN-12113

---
+++ PAS_Database/dbo/Stored Procedures/usp_ROListByMasterItemId.sql
@@ -0,0 +1,60 @@
+﻿/*************************************************************           
+ ** File:   [usp_ROListByMasterItemId]           
+ ** Author:   Sahdev Saliya
+ ** Description: This stored procedure is used to RO List By MasterItemId List
+ ** Purpose:         
+ ** Date:   05-05-2025       
+          
+ ** RETURN VALUE:           
+  
+ **************************************************************             
+  ** Change History             
+ **************************************************************             
+ ** S NO   Date            Author          Change Description              
+ ** --   --------         -------          --------------------------------            
+    1    05-05-2025    Sahdev Saliya       Created  
+
+**************************************************************/  
+CREATE   PROCEDURE dbo.[usp_ROListByMasterItemId]
+    @ItemMasterId BIGINT
+AS
+BEGIN
+  SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+  SET NOCOUNT ON;
+
+        BEGIN TRY
+
+				SELECT DISTINCT
+				RO.RepairOrderId,
+				RO.RepairOrderNumber
+				FROM [dbo].RepairOrder RO WITH(NOLOCK)
+				INNER JOIN [dbo].RepairOrderPart ROP WITH(NOLOCK) ON RO.RepairOrderId = ROP.RepairOrderId
+				LEFT JOIN [dbo].ItemMaster ITM WITH(NOLOCK) ON ROP.ItemMasterId = ITM.ItemMasterId AND ITM.ItemMasterId = @ItemMasterId
+				LEFT JOIN [dbo].AssetInventory ASI WITH(NOLOCK) ON ROP.ItemMasterId = ASI.AssetRecordId AND ASI.AssetRecordId = @ItemMasterId
+				WHERE RO.IsDeleted = 0 AND (@ItemMasterId IS NULL 
+					   OR ITM.ItemMasterId IS NOT NULL 
+					   OR ASI.AssetRecordId IS NOT NULL)
+        END TRY    
+
+     BEGIN CATCH      
+			IF @@trancount > 0
+				PRINT 'ROLLBACK'
+				DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
+
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+              , @AdhocComments     VARCHAR(150)    = 'usp_ROListByMasterItemId' 
+              , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = ''' + CAST(ISNULL(@ItemMasterId, '') AS varchar(100)) 
+			 
+              , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+
+              exec spLogException 
+                       @DatabaseName			= @DatabaseName
+                     , @AdhocComments			= @AdhocComments
+                     , @ProcedureParameters		= @ProcedureParameters
+                     , @ApplicationName			= @ApplicationName
+                     , @ErrorLogID              = @ErrorLogID OUTPUT ;
+              RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)
+              RETURN
+	 END CATCH
+END

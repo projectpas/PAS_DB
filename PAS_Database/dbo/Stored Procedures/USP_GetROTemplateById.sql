89636e91de13da6d9dd788f66bda9b6ef9265817
amitghediyasilverxis <amit.ghediya@silverxis.com>
2025-05-06 19:37:37 +0530

    PN-12475 added for RO Template changes.

---
+++ PAS_Database/dbo/Stored Procedures/USP_GetROTemplateById.sql
@@ -0,0 +1,75 @@
+﻿/*************************************************************           
+ ** File:   [USP_GetROTemplateById]           
+ ** Author:   Amit Ghediya
+ ** Description: This stored procedure is used to Get Ro Template records by ID
+ ** Purpose:         
+ ** Date:   06-05-2025       
+          
+ ** RETURN VALUE:           
+  
+ **************************************************************             
+  ** Change History             
+ **************************************************************             
+ ** S NO   Date            Author          Change Description              
+ ** --   --------         -------          --------------------------------            
+    1    06-05-2025		Amit Ghediya       Created  
+
+**************************************************************/    
+CREATE    PROCEDURE [dbo].[USP_GetROTemplateById]
+    @RepairOrderTemplateId BIGINT
+AS
+BEGIN
+    SET NOCOUNT ON;  
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	BEGIN TRY  
+		SELECT 
+		     ROT.[RepairOrderTemplateId],
+	  		 ROT.ItemMasterId,
+			 IM.partnumber,
+			 ROT.WorkPerformedId,
+			 CBT.CapabilityTypeDesc AS 'WorkToPerform',
+			 ROT.CustomerId,  
+			 CM.[Name] AS 'CustomerName',
+			 ROT.PublicationRecordId,  
+			 PUB.PublicationId,
+			 ROT.VendorId, 
+			 VN.VendorName,
+			 ROT.Instruction,
+			 ROT.[IsActive],
+			 ROT.[IsDeleted],
+			 ROT.[MasterCompanyId],
+			 ROT.CreatedBy,
+			 ROT.CreatedDate,
+			 ROT.UpdatedBy,
+			 ROT.UpdatedDate
+		FROM [dbo].[RepairOrderTemplate] ROT WITH(NOLOCK)  
+		LEFT JOIN [dbo].[ItemMaster] IM WITH(NOLOCK) ON IM.[ItemMasterId] = ROT.[ItemMasterId]
+		LEFT JOIN [dbo].[Customer] CM WITH(NOLOCK) ON CM.[CustomerId] = ROT.[CustomerId]
+		LEFT JOIN [dbo].[Vendor] VN WITH(NOLOCK) ON VN.[VendorId] = ROT.[VendorId]
+		LEFT JOIN [dbo].[Publication] PUB WITH(NOLOCK) ON PUB.[PublicationRecordId] = ROT.[PublicationRecordId]
+		LEFT JOIN [dbo].[CapabilityType] CBT WITH(NOLOCK) ON CBT.[CapabilityTypeId] = ROT.[WorkPerformedId]
+		WHERE ROT.RepairOrderTemplateId = @RepairOrderTemplateId;
+	END TRY 
+	BEGIN CATCH  
+   
+    DECLARE @ErrorLogID int,  
+            @DatabaseName varchar(100) = DB_NAME(),  
+            -----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------  
+            @AdhocComments varchar(150) = 'USP_GetROTemplateById',  
+            @ProcedureParameters varchar(3000) = '@Parameter1 = ''' + CAST(ISNULL(@RepairOrderTemplateId, '') AS varchar(100)) +    
+            '@Parameter2 = ''' + CAST(ISNULL(@RepairOrderTemplateId, '') AS varchar(100)),  
+            @ApplicationName varchar(100) = 'PAS'   
+    -----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------  
+    EXEC Splogexception @DatabaseName = @DatabaseName,  
+                        @AdhocComments = @AdhocComments,  
+                        @ProcedureParameters = @ProcedureParameters,  
+                        @ApplicationName = @ApplicationName,  
+                        @ErrorLogID = @ErrorLogID OUTPUT;  
+  
+    RAISERROR (  
+    'Unexpected Error Occured in the database. Please let the support team know of the error number : %d'  
+    , 16, 1, @ErrorLogID)  
+  
+    RETURN (1);  
+  END CATCH   
+END
---
+++ PAS_Database/dbo/Tables/RepairOrderTemplate.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[RepairOrderTemplate] (
+    [RepairOrderTemplateId]     BIGINT         IDENTITY (1, 1) NOT NULL,
+    [RepairOrderTemplateNumber] VARCHAR (256)  NOT NULL,
+    [ItemMasterId]              BIGINT         NOT NULL,
+    [WorkPerformedId]           BIGINT         NULL,
+    [CustomerId]                BIGINT         NULL,
+    [PublicationRecordId]       BIGINT         NULL,
+    [VendorId]                  BIGINT         NULL,
+    [Instruction]               NVARCHAR (MAX) NULL,
+    [MasterCompanyId]           INT            NOT NULL,
+    [CreatedBy]                 VARCHAR (256)  NOT NULL,
+    [UpdatedBy]                 VARCHAR (256)  NOT NULL,
+    [CreatedDate]               DATETIME2 (7)  NOT NULL,
+    [UpdatedDate]               DATETIME2 (7)  NOT NULL,
+    [IsActive]                  BIT            DEFAULT ((1)) NOT NULL,
+    [IsDeleted]                 BIT            DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [ROT_RepairOrderTemplate] PRIMARY KEY CLUSTERED ([RepairOrderTemplateId] ASC),
+    CONSTRAINT [FK_RepairOrderTemplate_MasterCompany] FOREIGN KEY ([MasterCompanyId]) REFERENCES [dbo].[MasterCompany] ([MasterCompanyId])
+);
+

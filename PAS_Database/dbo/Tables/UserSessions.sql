433d6c2a4cdbc002c291af2ae2f7d3e7413f791e
Vishal Suthar <vishal@silverxis.com>
2025-05-19 23:27:43 +0530

    PN-12478 Changes for Single Active Login Session for User

---
+++ PAS_Database/dbo/Tables/UserSessions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserSessions] (
+    [UserSessionsId] BIGINT           IDENTITY (1, 1) NOT NULL,
+    [UserId]         UNIQUEIDENTIFIER NULL,
+    [JwtToken]       NVARCHAR (MAX)   NULL,
+    [LastLoginTime]  DATETIME         NULL,
+    CONSTRAINT [PK_UserSessions] PRIMARY KEY CLUSTERED ([UserSessionsId] ASC)
+);
+

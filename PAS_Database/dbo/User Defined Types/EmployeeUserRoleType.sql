2939f3446338a1c0087184722b15a8dd87fcf4f0
sahdev <sahdev@silverxis.com>
2025-06-24 12:56:33 +0530

    PN-13133

---
+++ PAS_Database/dbo/User Defined Types/EmployeeUserRoleType.sql
@@ -0,0 +1,11 @@
+﻿CREATE TYPE [dbo].[EmployeeUserRoleType] AS TABLE (
+    [EmployeeUserRoleId] BIGINT        NULL,
+    [EmployeeId]         BIGINT        NULL,
+    [RoleId]             BIGINT        NULL,
+    [IsActive]           BIT           NULL,
+    [IsDeleted]          BIT           NULL,
+    [CreatedBy]          VARCHAR (256) NULL,
+    [UpdatedBy]          VARCHAR (256) NULL,
+    [UpdatedDate]        DATETIME2 (7) NULL,
+    [CreatedDate]        DATETIME2 (7) NULL);
+

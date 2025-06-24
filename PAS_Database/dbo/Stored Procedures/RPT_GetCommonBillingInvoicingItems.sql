02365c7c948867a09a9b2071663a138b98c9f890
Moin Bloch <moin@silverxis.com>
2025-06-25 02:22:08 +0530

    PN-12215

--- PAS_Database/dbo/Stored Procedures/RPT_GetCommonBillingInvoicingItems.sql
+++ PAS_Database/dbo/Stored Procedures/RPT_GetCommonBillingInvoicingItems.sql
@@ -13,8 +13,9 @@
     1    02/06/2025   Moin Bloch    Created
 	2    18/06/2025   Moin Bloch    Fix For Condition in pdf
 	3    18/06/2025   Moin Bloch    Added WorkFlowWorkOrderId
+	4    24/06/2025   Moin Bloch    Fix For Duplicate Part
      
---   EXEC [dbo].[RPT_GetCommonBillingInvoicingItems] 60,15
+--   EXEC [dbo].[RPT_GetCommonBillingInvoicingItems] 89,15
 ********************************************************************************************/
 CREATE PROCEDURE [dbo].[RPT_GetCommonBillingInvoicingItems]
 @BillingInvoicingId BIGINT = NULL,
@@ -36,7 +37,7 @@ BEGIN
 			DECLARE @FinalCondCert INT
 			SELECT @FinalCondCert = [WorkOrderSettlementId] FROM [dbo].[WorkOrderSettlement] WITH(NOLOCK) WHERE [WorkOrderSettlementName] = 'Final Cond/Cert'
 			
-	    SELECT BII.[BillingInvoicingItemId]
+	    SELECT DISTINCT BII.[BillingInvoicingItemId]
 			  ,BII.[BillingInvoicingId]
 			  ,BII.[ModuleId]
 			  ,BII.[ReferenceId]
@@ -45,7 +46,7 @@ BEGIN
 			  ,BII.[ItemMasterId]
 			  ,BII.[StocklineId]
 			  ,BII.[ConditionId]
-			  ,BII.[CostPlusType]
+			  ,UPPER(BII.[CostPlusType]) [CostPlusType]
 			  ,ISNULL(BII.[UnitPrice],0) [UnitPrice]
 			  ,ISNULL(BII.[QtyBilled],0) [QtyBilled]
 			  ,ISNULL(BII.[PartCost],0) [PartCost]
@@ -81,28 +82,31 @@ BEGIN
 			  ,ISNULL(BII.[IsVersionIncrease],0) [IsVersionIncrease]
 			  ,ISNULL(BII.[IsPerformaInvoice],0) [IsPerformaInvoice]
 			  ,BII.[MasterCompanyId]			  
-			  ,WOP.[RevisedPartNumber] [PNumber]			
-			  ,WOP.[RevisedPartDescription]  [PNDescription]
-			  ,WOP.[RevisedSerialNumber]  [SerialNumber]
-			  ,CASE WHEN WOP.[RevisedConditionId] IS NOT NULL THEN  CASE WHEN COND.[Memo] <> '' THEN COND.[Memo] ELSE COND.[Code] END
-			        WHEN WOS.[WorkOrderSettlementId] IS NOT NULL THEN WOS.[conditionName]
-				ELSE (SELECT TOP 1 CASE WHEN c.[Memo] <> '' THEN c.[Memo] ELSE c.[Code] END FROM  [dbo].[Condition] c WITH(NOLOCK) 
+			  ,UPPER(WOP.[RevisedPartNumber]) [PNumber]			
+			  ,UPPER(WOP.[RevisedPartDescription])  [PNDescription]
+			  ,UPPER(WOP.[RevisedSerialNumber])  [SerialNumber]
+			  ,CASE WHEN WOP.[RevisedConditionId] IS NOT NULL THEN  CASE WHEN COND.[Memo] <> '' THEN UPPER(COND.[Memo]) ELSE UPPER(COND.[Code]) END
+			        WHEN WOS.[WorkOrderSettlementId] IS NOT NULL THEN UPPER(WOS.[conditionName])
+				ELSE (SELECT TOP 1 CASE WHEN c.[Memo] <> '' THEN UPPER(c.[Memo]) ELSE UPPER(c.[Code]) END FROM [dbo].[Condition] c WITH(NOLOCK) 
 					   WHERE c.[ConditionId] = BII.[ConditionId] AND c.[MasterCompanyId] = BII.[MasterCompanyId])
 				END [Cond]				  			  							
 			  ,WO.[Notes]
 			  ,UOM.[ShortName] [PurchaseUnitOfMeasure]
 			  ,BII.WorkFlowWorkOrderId
-			  ,ISNULL(WOC.[WorkOrderChargesId],0) [IsCharges]
+			  ,CASE WHEN ISNULL(BII.[IsMiscChargesCheck],0) = 0 THEN 0
+			        WHEN ISNULL(BII.[IsPerformaInvoice],0) = 1 THEN 0 
+				    ELSE ISNULL(BII.[MiscChargesCostPlus],0)
+			   END [IsCharges]			
 		   FROM [dbo].[BillingInvoicingItems] BII WITH(NOLOCK) 
-		  INNER JOIN [dbo].[WorkOrder] WO WITH(NOLOCK) ON BII.[ReferenceId] = WO.[WorkOrderId]
-		  INNER JOIN [dbo].[WorkOrderPartNumber] WOP WITH(NOLOCK) ON BII.[SubReferenceId] = WOP.[ID]
+		  INNER JOIN [dbo].[WorkOrder] WO WITH(NOLOCK) ON BII.[ReferenceId] = WO.[WorkOrderId] AND BII.ModuleId = @WOModuleId
+		  INNER JOIN [dbo].[WorkOrderPartNumber] WOP WITH(NOLOCK) ON BII.[SubReferenceId] = WOP.[ID] AND BII.ModuleId = @WOModuleId
 		  INNER JOIN [dbo].[WorkOrderWorkFlow] WOF WITH(NOLOCK) ON WOP.[ID] = WOF.[WorkOrderPartNoId]
 		   LEFT JOIN [dbo].[WorkOrderSettlementDetails] WOS WITH(NOLOCK) ON WOP.[ID] = wos.[workOrderPartNoId] AND WOS.[WorkOrderSettlementId] = @FinalCondCert
 		   LEFT JOIN [dbo].[Condition] COND WITH(NOLOCK) ON WOP.[RevisedConditionId] = COND.[ConditionId]
 		   LEFT JOIN [dbo].[ItemMaster] ITM WITH(NOLOCK) ON WOP.[RevisedItemmasterid] = ITM.[ItemMasterId]
 		   LEFT JOIN [dbo].[UnitOfMeasure] UOM WITH(NOLOCK) ON [ITM].[PurchaseUnitOfMeasureId] = UOM.[UnitOfMeasureId]
-		   LEFT JOIN [dbo].[WorkOrderCharges] WOC WITH(NOLOCK) ON BII.[WorkFlowWorkOrderId] = WOC.[WorkFlowWorkOrderId] AND woc.[IsDeleted]=0  
-		  WHERE BII.[BillingInvoicingId] = @BillingInvoicingId
+		   --LEFT JOIN [dbo].[WorkOrderCharges] WOC WITH(NOLOCK) ON BII.[WorkFlowWorkOrderId] = WOC.[WorkFlowWorkOrderId] AND woc.[IsDeleted] = 0  AND BII.ModuleId = @WOModuleId
+		  WHERE BII.[BillingInvoicingId] = @BillingInvoicingId AND BII.ModuleId = @WOModuleId
 		  		  
 	END 
 	END TRY    

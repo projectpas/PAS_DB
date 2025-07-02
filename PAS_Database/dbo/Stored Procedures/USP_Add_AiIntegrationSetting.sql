8c27b9ac672e426921e6f02e3d2422303b82443e
saliyabhargav <bhargav.saliya@silverxis.com>
2025-07-02 10:00:12 +0530

    PN-13261

---
+++ PAS_Database/dbo/Stored Procedures/USP_Add_AiIntegrationSetting.sql
@@ -0,0 +1,66 @@
+﻿CREATE   PROCEDURE [dbo].[USP_Add_AiIntegrationSetting]
+	@AiIntegrationSettingId BIGINT,
+	@CreatedBy VARCHAR(50),
+	@UpdatedBy VARCHAR(50),
+	@MasterCompanyId int,
+	@IsEnableDisableAIintegration BIT,
+	@IsReviewRequired BIT,
+	@IsAutoEmailSend BIT,
+	@FromDate DATETIME2(7),
+	@ToDate DATETIME2(7)
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+		BEGIN TRANSACTION;
+
+		MERGE INTO dbo.AiIntegrationSetting AS TARGET
+		USING (SELECT @AiIntegrationSettingId AS AiIntegrationSettingId, @MasterCompanyId AS MasterCompanyId) AS SOURCE
+			ON TARGET.AiIntegrationSettingId = SOURCE.AiIntegrationSettingId 
+			   AND TARGET.MasterCompanyId = SOURCE.MasterCompanyId
+		WHEN MATCHED THEN
+			UPDATE SET 
+				TARGET.FromDate = @FromDate,
+				TARGET.ToDate = @ToDate,
+				TARGET.IsEnableDisableAIintegration = @IsEnableDisableAIintegration,
+				TARGET.IsReviewRequired = @IsReviewRequired,
+				TARGET.IsAutoEmailSend = @IsAutoEmailSend,
+				TARGET.UpdatedDate = GETUTCDATE(),
+				TARGET.UpdatedBy = @UpdatedBy
+		WHEN NOT MATCHED BY TARGET THEN
+			INSERT ([FromDate], [ToDate], [IsEnableDisableAIintegration], [IsReviewRequired], [IsAutoEmailSend], [MasterCompanyId], [CreatedBy], [UpdatedBy], [CreatedDate], [UpdatedDate], [IsActive], [IsDeleted])
+			VALUES (@FromDate, @ToDate, @IsEnableDisableAIintegration, @IsReviewRequired, @IsAutoEmailSend, @MasterCompanyId, @CreatedBy, @UpdatedBy, GETUTCDATE(), GETUTCDATE(), 1, 0);
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH        
+	IF @@trancount > 0  
+    PRINT 'ROLLBACK'  
+		ROLLBACK TRAN;  
+		DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name()   
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------  
+		, @AdhocComments     VARCHAR(150)    = 'USP_Add_AiIntegrationSetting'   
+		, @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ CAST(ISNULL(@AiIntegrationSettingId, '') as Varchar(100)) + 
+											  '@Parameter2 = '''+ CAST(ISNULL(@CreatedBy, '') as Varchar(100))+		
+											  '@Parameter3 = '''+ CAST(ISNULL(@UpdatedBy, '') as Varchar(100))+		
+											  '@Parameter4 = '''+ CAST(ISNULL(@MasterCompanyId, '') as Varchar(100))+		
+											  '@Parameter5 = '''+ CAST(ISNULL(@IsEnableDisableAIintegration, '') as Varchar(100))+		
+											  '@Parameter6 = '''+ CAST(ISNULL(@IsReviewRequired, '') as Varchar(100))+		
+											  '@Parameter7 = '''+ CAST(ISNULL(@IsAutoEmailSend, '') as Varchar(100))+		
+											  '@Parameter8 = '''+ CAST(ISNULL(@MasterCompanyId, '') as Varchar(100))+		
+											  '@Parameter9 = '''+ CAST(ISNULL(@FromDate, '') as Varchar(100))+		
+											  '@Parameter10 = '''+ CAST(ISNULL(@ToDate, '') as Varchar(100))
+		, @ApplicationName VARCHAR(100) = 'PAS'  
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------  
+		exec spLogException   
+		@DatabaseName           = @DatabaseName  
+		, @AdhocComments          = @AdhocComments  
+		, @ProcedureParameters = @ProcedureParameters  
+		, @ApplicationName        =  @ApplicationName  
+		, @ErrorLogID                    = @ErrorLogID OUTPUT ;  
+		RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)  
+		RETURN(1);  
+	END CATCH 
+END
---
+++ PAS_Database/dbo/Stored Procedures/USP_GetAiIntegrationSettingList.sql
@@ -0,0 +1,70 @@
+﻿/*********************     
+** Author:  <BHARGAV SALIYA >    
+** Create date: <July-01-2025>    
+** Description: <get Ai Integration Setting Data by mastercompanyId>    
+    
+EXEC [USP_GetPNLabelSettingData]   
+**********************   
+** Change History   
+**********************     
+** PR   Date              Author          Change Description    
+** --   --------          -------         --------------------------------  
+** 1	July-01-2025	BHARGAV SALIYA    Create
+
+exec dbo.USP_GetPNLabelSettingData 1  
+**********************/   
+
+CREATE   PROCEDURE [dbo].[USP_GetAiIntegrationSettingList]
+@MasterCompanyId bigint
+AS
+BEGIN
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	SET NOCOUNT ON;
+
+		BEGIN TRY
+		BEGIN TRANSACTION
+			BEGIN 
+				
+				SELECT 
+					 AI.AiIntegrationSettingId
+					,AI.[FromDate]
+					,AI.[ToDate]
+					,AI.[IsEnableDisableAIintegration]
+					,AI.[IsReviewRequired]
+					,AI.[IsAutoEmailSend]
+					,AI.[MasterCompanyId]
+					,AI.[CreatedBy]
+					,AI.[UpdatedBy]
+					,AI.[CreatedDate]
+					,AI.[UpdatedDate]
+					,AI.[IsActive]
+					,AI.[IsDeleted]
+				FROM dbo.AiIntegrationSetting AI WITH(NOLOCK)
+				WHERE AI.MasterCompanyId = @MasterCompanyId
+                
+			END
+		COMMIT  TRANSACTION
+
+		END TRY    
+		BEGIN CATCH      
+			IF @@trancount > 0
+				--PRINT 'ROLLBACK'
+				ROLLBACK TRAN;
+				DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
+
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+              , @AdhocComments     VARCHAR(150)    = 'USP_GetAiIntegrationSettingList' 
+              , @ProcedureParameters VARCHAR(3000)  = '@MasterCompanyId = '''+ ISNULL(@MasterCompanyId, '') + ''
+              , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+
+              exec spLogException 
+                       @DatabaseName			= @DatabaseName
+                     , @AdhocComments			= @AdhocComments
+                     , @ProcedureParameters		= @ProcedureParameters
+                     , @ApplicationName         = @ApplicationName
+                     , @ErrorLogID              = @ErrorLogID OUTPUT ;
+              RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)
+              RETURN(1);
+		END CATCH
+END

319da9ff5e0bfe60ac5abcff53b6e24eb5f03bd6
Moin Bloch <moin@silverxis.com>
2025-07-10 19:12:50 +0530

    PN-13473

---
+++ PAS_Database/dbo/Stored Procedures/GetWorkorderQuoteCurrectiveAction.sql
@@ -0,0 +1,217 @@
+﻿/*************************************************************   
+** Author:  Moin Bloch
+** Create date: <07/10/2025>  
+** Description: <Get Work order Release Form Data>  
+  
+EXEC [[GetWorkorderQuoteCurrectiveAction]] 
+************************************************************** 
+** Change History 
+**************************************************************   
+** PR   Date        Author          Change Description  
+** --   --------    -------         --------------------------------
+** 01   10/07/2025  Moin Bloch      Created
+
+ EXEC [dbo].[GetWorkorderReleaseFromData] 9737,9847
+ EXEC [dbo].[GetWorkorderQuoteCurrectiveAction] 9737,9847
+**************************************************************/ 
+CREATE   PROC [dbo].[GetWorkorderQuoteCurrectiveAction]
+@WorkorderId BIGINT = NULL,  
+@workOrderPartNumberId BIGINT = NULL
+AS  
+BEGIN  
+ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED  
+ SET NOCOUNT ON;  
+  
+  BEGIN TRY  
+		DECLARE @MasterCompanyId INT;  
+		DECLARE @MTIMasterCompanyId INT; 
+
+		DECLARE @CMMIds VARCHAR(200) = NULL;			
+		DECLARE @IsMultiple BIT = NULL;
+		DECLARE @EmailBody NVARCHAR(MAX)=''		
+		DECLARE @ECMasterCompanyId INT = 19
+		DECLARE @NeoMasterCompanyId INT = 20
+		
+		SELECT @MasterCompanyId = [MasterCompanyId] FROM [DBO].[WorkOrder] CTT WITH(NOLOCK) WHERE [WorkorderId] = @WorkorderId;
+				
+		IF OBJECT_ID(N'tempdb..#tmprCMMIDsDetails') IS NOT NULL
+		BEGIN
+			DROP TABLE #tmprCMMIDsDetails
+		END		
+		
+		CREATE TABLE #tmprCMMIDsDetails
+		(
+			[ID] BIGINT NOT NULL IDENTITY, 
+			[CMMId] BIGINT NULL
+	    )
+
+		SELECT @CMMIds = wop.[CMMIds]
+		FROM [dbo].[WorkOrderPartNumber] wop WITH(NOLOCK) 
+		WHERE wop.[WorkOrderId] = @WorkOrderId AND wop.[ID]=@workOrderPartNumberId AND [MasterCompanyId] = @MasterCompanyId
+
+		IF(@CMMIds = '')
+		BEGIN
+			SET @CMMIds = NULL
+		END
+
+		IF(@CMMIds IS NOT NULL)
+		BEGIN
+			INSERT INTO #tmprCMMIDsDetails ([CMMId])
+			SELECT [PublicationRecordId] 
+			FROM [dbo].[Publication] WHERE [PublicationRecordId] IN (SELECT Item FROM DBO.SPLITSTRING(@CMMIds, ','))  
+			ORDER BY PublicationTypeId DESC
+		END			
+		
+		IF(@CMMIds IS NOT NULL)
+		BEGIN			
+			IF CHARINDEX(',', @CMMIds) > 0
+			BEGIN
+				SET @IsMultiple = 1
+			END
+			ELSE
+			BEGIN
+				SET @IsMultiple = 0
+			END
+		END
+
+		IF(@MasterCompanyId = @ECMasterCompanyId OR @MasterCompanyId = @NeoMasterCompanyId)
+		BEGIN
+			IF(@CMMIds IS NOT NULL)
+			BEGIN
+				IF(@IsMultiple = 1)
+				BEGIN
+					SELECT @EmailBody = [EmailBody] FROM 
+						[dbo].[PublicationTemplate] PT WITH(NOLOCK) 
+						WHERE [MasterCompanyId] = @MasterCompanyId AND [PublicationTypeId] = 0
+				END
+				ELSE
+				BEGIN
+					SELECT @EmailBody = [EmailBody]
+						FROM [dbo].[Publication] PC WITH(NOLOCK)
+						LEFT JOIN [dbo].[PublicationTemplate] PT WITH(NOLOCK) ON PT.[PublicationTypeId] = PC.[PublicationTypeId] and PT.[IsActive] = 1 AND PT.[IsDeleted] = 0
+						WHERE PC.[PublicationRecordId] = CAST(@CMMIds AS BIGINT) AND PC.[MasterCompanyId] = @MasterCompanyId 
+				END		
+			END
+		END
+		ELSE
+		BEGIN			
+			IF(@CMMIds IS NOT NULL)
+			BEGIN
+				SELECT TOP 1 @EmailBody = [EmailBody] FROM [dbo].[PublicationTemplate] PT WITH(NOLOCK) WHERE [MasterCompanyId] = @MasterCompanyId	
+			END
+			ELSE
+			BEGIN
+				SELECT @EmailBody = '';
+			END
+		END
+
+		IF(@MasterCompanyId = @ECMasterCompanyId OR @MasterCompanyId = @NeoMasterCompanyId)
+		BEGIN
+			IF(@IsMultiple IS NULL OR @IsMultiple = 0 )
+			BEGIN
+				SELECT ISNULL(UPPER(pub.PublicationId),0) AS PublicationId,
+				       CASE WHEN ISNULL(wop.[RevisedConditionId],0) > 0 THEN UPPER(C.[Memo]) ELSE UPPER(wosc.[conditionName]) END AS ConditionName,
+					   ISNULL(CONVERT(VARCHAR(20),UPPER(pub.RevisionNum)),'-') RevisionNum,
+					   UPPER(ISNULL(REPLACE(CONVERT(VARCHAR(100),pub.revisionDate,106),' ','/'),'-')) RevisionDate,
+					   '' SecondPublicationId,
+					   '' SecondRevisionNum,
+					   '' SecondRevisionDate,
+					   wo.[WorkOrderNum],
+					   '' IsEasaUKLicenseType,
+					   ISNULL(pub.[PublishedById],0) PublishedById,
+					   ven.[VendorName],
+					   mf.[Name] [ManufacturerName],
+					   pub.[PublishedByOthers],
+					   @IsMultiple AS IsMultiple,	
+					   @EmailBody AS EmailBody
+				FROM [dbo].[WorkOrderPartNumber] wop WITH(NOLOCK)   
+					  LEFT JOIN [dbo].[WorkOrder] wo  WITH(NOLOCK) ON wo.WorkOrderId = wop.WorkOrderId  
+					  LEFT JOIN [dbo].[WorkOrderSettlementDetails] wosc WITH(NOLOCK) ON wop.WorkOrderId = wosc.WorkOrderId AND wop.ID = wosc.workOrderPartNoId AND wosc.WorkOrderSettlementId = 9  					 
+					  LEFT JOIN [dbo].[Publication] pub WITH(NOLOCK) ON pub.PublicationRecordId = @CMMIds 
+					  LEFT JOIN [dbo].[Vendor] ven WITH(NOLOCK) ON pub.PublishedByRefId = ven.VendorId  
+					  LEFT JOIN [dbo].[Manufacturer] mf WITH(NOLOCK) ON pub.PublishedByRefId = mf.ManufacturerId 					 
+					  LEFT JOIN [dbo].[Condition] C WITH(NOLOCK) ON C.ConditionId = wop.RevisedConditionId
+				 WHERE wop.WorkOrderId = @WorkOrderId AND wop.ID=@workOrderPartNumberId 
+			END
+			ELSE
+			BEGIN			
+				  DECLARE @CMMID1 BIGINT = 0 
+				  DECLARE @CMMID2 BIGINT = 0 
+
+				  SELECT @CMMID1 = CMMId FROM #tmprCMMIDsDetails WHERE [ID] = 1;
+				  SELECT @CMMID2 = CMMId FROM #tmprCMMIDsDetails WHERE [ID] = 2;
+		  				  
+				  SELECT ISNULL(UPPER(pub.PublicationId),0) AS PublicationId,
+			             CASE WHEN ISNULL(wop.[RevisedConditionId],0) > 0 THEN UPPER(C.[Memo]) ELSE UPPER(wosc.[conditionName]) END AS ConditionName,
+				 		 ISNULL(CONVERT(VARCHAR(20),UPPER(pub.RevisionNum)),'-') RevisionNum,
+						 UPPER(ISNULL(REPLACE(CONVERT(VARCHAR(100),pub.revisionDate,106),' ','/'),'-')) RevisionDate,
+						 ISNULL(UPPER(pub2.PublicationId),0) AS SecondPublicationId,
+						 ISNULL(CONVERT(VARCHAR(20),UPPER(pub2.RevisionNum)),'-') SecondRevisionNum,
+						 UPPER(ISNULL(REPLACE(CONVERT(VARCHAR(100),pub2.revisionDate,106),' ','/'),'-')) SecondRevisionDate,
+						 wo.[WorkOrderNum],
+						 '' IsEasaUKLicenseType,
+						 ISNULL(pub.[PublishedById],0) PublishedById,
+						 ven.[VendorName],
+						 mf.[Name] ManufacturerName,
+				         pub.[PublishedByOthers],
+						 @IsMultiple AS IsMultiple,
+						 @EmailBody AS EmailBody
+			FROM [dbo].[WorkOrderPartNumber] wop WITH(NOLOCK)   
+				  LEFT JOIN [dbo].[WorkOrder] wo  WITH(NOLOCK) ON wo.WorkOrderId = wop.WorkOrderId  
+				  LEFT JOIN [dbo].[WorkOrderSettlementDetails] wosc WITH(NOLOCK) ON wop.WorkOrderId = wosc.WorkOrderId AND wop.ID = wosc.workOrderPartNoId AND wosc.WorkOrderSettlementId = 9  
+				  LEFT JOIN [dbo].[Publication] pub WITH(NOLOCK) ON pub.[PublicationRecordId] = @CMMID1 
+				  LEFT JOIN [dbo].[Publication] pub2 WITH(NOLOCK) ON pub2.[PublicationRecordId] = @CMMID2 				  
+				  LEFT JOIN [dbo].[Vendor] ven WITH(NOLOCK) ON pub.PublishedByRefId = ven.VendorId  
+				  LEFT JOIN [dbo].[Manufacturer] mf WITH(NOLOCK) ON pub.PublishedByRefId = mf.ManufacturerId 
+				  LEFT JOIN [dbo].[Condition] C WITH(NOLOCK) ON C.ConditionId = wop.RevisedConditionId
+			 WHERE wop.WorkOrderId = @WorkOrderId AND wop.ID=@workOrderPartNumberId 
+			END	
+		END
+		ELSE
+		BEGIN
+			   SELECT ISNULL(UPPER(pub.PublicationId),0) AS PublicationId,
+			          CASE WHEN ISNULL(wop.[RevisedConditionId],0) > 0 THEN UPPER(C.[Memo]) ELSE UPPER(wosc.[conditionName]) END AS ConditionName,
+					  ISNULL(CONVERT(VARCHAR(20),UPPER(pub.RevisionNum)),'-') RevisionNum,
+					  UPPER(ISNULL(REPLACE(CONVERT(VARCHAR(100),pub.revisionDate,106),' ','/'),'-')) RevisionDate,	
+					  '' SecondPublicationId,
+					  '' SecondRevisionNum,
+					  '' SecondRevisionDate,		
+					  wo.[WorkOrderNum],
+					  '' IsEasaUKLicenseType,
+					  ISNULL(pub.[PublishedById],0) PublishedById,
+					  ven.[VendorName],
+					  mf.[Name] [ManufacturerName],
+					  pub.[PublishedByOthers],					  
+					  @IsMultiple AS IsMultiple,
+					  @EmailBody AS EmailBody					   
+				FROM [dbo].[WorkOrderPartNumber] wop WITH(NOLOCK)   
+					  LEFT JOIN [dbo].[WorkOrder] wo  WITH(NOLOCK) ON wo.WorkOrderId = wop.WorkOrderId  
+					  LEFT JOIN [dbo].[WorkOrderSettlementDetails] wosc WITH(NOLOCK) ON wop.WorkOrderId = wosc.WorkOrderId AND wop.ID = wosc.workOrderPartNoId AND wosc.WorkOrderSettlementId = 9  
+					  LEFT JOIN [dbo].[Publication] pub WITH(NOLOCK) ON pub.PublicationRecordId = @CMMIds 
+					  LEFT JOIN [dbo].[Vendor] ven WITH(NOLOCK) ON pub.PublishedByRefId = ven.VendorId  
+					  LEFT JOIN [dbo].[Manufacturer] mf WITH(NOLOCK) ON pub.PublishedByRefId = mf.ManufacturerId 
+					  LEFT JOIN [dbo].[Condition] C WITH(NOLOCK) ON C.ConditionId = wop.RevisedConditionId
+				 WHERE wop.WorkOrderId = @WorkOrderId AND wop.ID=@workOrderPartNumberId 
+		END		
+					
+  END TRY      
+  BEGIN CATCH        
+   IF @@trancount > 0  
+    PRINT 'ROLLBACK'  
+    DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name()   
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------  
+              , @AdhocComments     VARCHAR(150)    = 'GetWorkorderQuoteCurrectiveAction'                
+			  , @ProcedureParameters VARCHAR(3000) = '@Parameter1 = ''' + CAST(ISNULL(@WorkorderId, '') AS VARCHAR(100))
+			                                       + '@Parameter2 = ''' + CAST(ISNULL(@workOrderPartNumberId, '') AS VARCHAR(100)) 
+              , @ApplicationName VARCHAR(100) = 'PAS'  
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------  
+              exec spLogException   
+                       @DatabaseName           = @DatabaseName  
+                     , @AdhocComments          = @AdhocComments  
+                     , @ProcedureParameters = @ProcedureParameters  
+                     , @ApplicationName        =  @ApplicationName  
+                     , @ErrorLogID                    = @ErrorLogID OUTPUT ;  
+              RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)  
+              RETURN(1);  
+  END CATCH  
+END

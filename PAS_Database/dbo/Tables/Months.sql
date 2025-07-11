8cee4ea6509970d8df9b9ee2d1ad4d16cb38d585
saliyabhargav <bhargav.saliya@silverxis.com>
2025-07-11 19:53:50 +0530

    PN-13347

---
+++ PAS_Database/dbo/Tables/Months.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Months] (
+    [MonthId]         BIGINT        IDENTITY (1, 1) NOT NULL,
+    [MonthName]       VARCHAR (100) NOT NULL,
+    [MonthShortName]  VARCHAR (50)  NOT NULL,
+    [MonthNumber]     INT           NOT NULL,
+    [MasterCompanyId] INT           NOT NULL,
+    [CreatedBy]       VARCHAR (256) NOT NULL,
+    [UpdatedBy]       VARCHAR (256) NOT NULL,
+    [CreatedDate]     DATETIME2 (7) CONSTRAINT [DF_Months_CreatedDate] DEFAULT (getdate()) NOT NULL,
+    [UpdatedDate]     DATETIME2 (7) CONSTRAINT [DF_Months_UpdatedDate] DEFAULT (getdate()) NOT NULL,
+    [IsActive]        BIT           CONSTRAINT [DF_Months_IsActive] DEFAULT ((1)) NOT NULL,
+    [IsDeleted]       BIT           CONSTRAINT [DF_Months_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_Months] PRIMARY KEY CLUSTERED ([MonthId] ASC)
+);
+

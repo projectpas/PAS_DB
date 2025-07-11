e1f17ec1c92a6c0851b5d6094f79b8b40c4f932f
amitghediyasilverxis <amit.ghediya@silverxis.com>
2025-07-11 19:56:41 +0530

    PN-13436 handle work order mpn stockline document.

--- PAS_Database/dbo/Stored Procedures/GetStocklineDocumentDetailsByReferenceId.sql
+++ PAS_Database/dbo/Stored Procedures/GetStocklineDocumentDetailsByReferenceId.sql
@@ -15,7 +15,7 @@ EXEC [GetStocklineDocumentDetailsByReferenceId]  925,1,57
 
 **************************************************************/ 
 
-CREATE    PROCEDURE [dbo].[GetStocklineDocumentDetailsByReferenceId]
+CREATE     PROCEDURE [dbo].[GetStocklineDocumentDetailsByReferenceId]
 	@ReferenceId BIGINT = NULL,
 	@MasterCompanyId BIGINT = NULL,
 	@ModuleId BIGINT = NULL
@@ -32,6 +32,7 @@ BEGIN
 				@MasterLoopID INT = 0,
 				@ChildMasterLoopID INT,
 				@ItemMasterId BIGINT = 0,
+				@WorkOrderId BIGINT = 0,
 				@PartsStocklineId VARCHAR(MAX) = '';
 
 		SELECT @Stockline_ModuleId = [AttachmentModuleId] FROM DBO.AttachmentModule WITH(NOLOCK) WHERE [Name] = 'StockLine';
@@ -125,128 +126,31 @@ BEGIN
 			 DECLARE @WorkOrderMaterialsId BIGINT = 0;
 			 
 			 ---START Material PN Stockline	
-				--Get FROM Wo Material data
-				IF OBJECT_ID(N'tempdb..#tmpWorkOrderMaterials') IS NOT NULL
-				BEGIN
-					DROP TABLE #tmpWorkOrderMaterials
-				END
-					  	  
-				CREATE TABLE #tmpWorkOrderMaterials
-				(
-					ID BIGINT NOT NULL IDENTITY, 
-					WorkOrderMaterialsId BIGINT NULL,
-					ItemMasterId BIGINT NULL
-				)
 
-				--Get FROM WO Material Stockline data
-				IF OBJECT_ID(N'tempdb..#tmpWorkOrderMaterialStockLine') IS NOT NULL
-				BEGIN
-					DROP TABLE #tmpWorkOrderMaterialStockLine
-				END
-					  	  
-				CREATE TABLE #tmpWorkOrderMaterialStockLine
-				(
-					ID BIGINT NOT NULL IDENTITY, 
-					StocklineId BIGINT NULL
-				)
+				--Get WorkorderId from Workflow table,
+				SELECT @WorkOrderId = WorkOrderId FROM [DBO].[WorkOrderWorkFlow] WITH(NOLOCK) 
+				WHERE [WorkFlowWorkOrderId] = @ReferenceId;
 
 				--Insert PN(Material) Data
-				INSERT INTO #tmpWorkOrderMaterials (WorkOrderMaterialsId,ItemMasterId) 
-							  SELECT WorkOrderMaterialsId,ItemMasterId
-				FROM [DBO].[WorkOrderMaterials] VRP WITH(NOLOCK) 
-				WHERE VRP.WorkFlowWorkOrderId = @ReferenceId;
+				INSERT INTO #tmpSalesOrderStockline (StocklineId) 
+							  SELECT VRP.StockLineId
+				FROM [DBO].[WorkOrderMaterialStockLine] VRP WITH(NOLOCK) 
+				JOIN WorkOrderMaterials MST WITH(NOLOCK) ON MST.WorkOrderMaterialsId = VRP.WorkOrderMaterialsId
+				WHERE MST.WorkOrderId = @WorkOrderId; -- get workorderid
 
-				SELECT  @MasterLoopID = MAX(ID) FROM #tmpWorkOrderMaterials;
-				WHILE(@MasterLoopID > 0)
-				BEGIN
-					SELECT @WorkOrderMaterialsId = WorkOrderMaterialsId,
-						   @ItemMasterId = ItemMasterId
-					FROM #tmpWorkOrderMaterials WITH(NOLOCK) WHERE [ID] = @MasterLoopID;
+				INSERT INTO #tmpSalesOrderStockline (StocklineId) 
+							  SELECT StockLineId
+				FROM [DBO].[WorkOrderPartNumber] VRP WITH(NOLOCK) 
+				WHERE VRP.WorkOrderId = @WorkOrderId; -- get workorderid
 
-					--Truncate #tmpWorkOrderMaterialStockLine
-					TRUNCATE TABLE #tmpWorkOrderMaterialStockLine;
-
-					INSERT INTO #tmpWorkOrderMaterialStockLine (StocklineId) 
-						 SELECT StocklineId
-					FROM [DBO].[WorkOrderMaterialStockLine] WOST WITH(NOLOCK) 
-					WHERE WOST.WorkOrderMaterialsId = @WorkOrderMaterialsId;
-
-					SELECT  @ChildMasterLoopID = MAX(ID) FROM #tmpWorkOrderMaterialStockLine;
-					WHILE(@ChildMasterLoopID > 0)
-					BEGIN
-						   INSERT INTO #tmpSalesOrderStockline (StocklineId) 
-					   			SELECT StocklineId
-						   FROM #tmpWorkOrderMaterialStockLine WITH(NOLOCK) WHERE [ID] = @ChildMasterLoopID
-					   
-						   SET @ChildMasterLoopID = @ChildMasterLoopID - 1;
-					END
-					SET @MasterLoopID = @MasterLoopID - 1;
-				END
 			---END Material PN Stockline
 
 			---START KIT PN Stockline
-				DECLARE @KITMasterLoopID INT = 0,
-						@KITChildMasterLoopID INT,
-						@WorkOrderMaterialsKitId BIGINT = 0;
-
-				IF OBJECT_ID(N'tempdb..#tmpWorkOrderMaterialsKit') IS NOT NULL
-				BEGIN
-					DROP TABLE #tmpWorkOrderMaterialsKit
-				END
-						  	  
-				CREATE TABLE #tmpWorkOrderMaterialsKit
-				(
-					ID BIGINT NOT NULL IDENTITY, 
-					WorkOrderMaterialskitId BIGINT NULL,
-					ItemMasterId BIGINT NULL
-				)
-
-				--Get FROM WOPartKIT Stockline data
-				IF OBJECT_ID(N'tempdb..#tmpWorkOrderMaterialStockLineKit') IS NOT NULL
-				BEGIN
-					DROP TABLE #tmpWorkOrderMaterialStockLineKit
-				END
-						  	  
-				CREATE TABLE #tmpWorkOrderMaterialStockLineKit
-				(
-					ID BIGINT NOT NULL IDENTITY, 
-					StocklineId BIGINT NULL
-				)
-
-				--Insert PN(KIT) Data
-				INSERT INTO #tmpWorkOrderMaterialsKit (WorkOrderMaterialskitId,ItemMasterId) 
-							  SELECT WorkOrderMaterialskitId,ItemMasterId
-				FROM [DBO].[WorkOrderMaterialsKit] WOP WITH(NOLOCK) 
-				WHERE WOP.WorkFlowWorkOrderId = @ReferenceId;
-
-				SELECT  @KITMasterLoopID = MAX(ID) FROM #tmpWorkOrderMaterialsKit;
-				WHILE(@KITMasterLoopID > 0)
-				BEGIN
-					SELECT @WorkOrderMaterialsKitId = WorkOrderMaterialskitId,
-						   @ItemMasterId = ItemMasterId
-					FROM #tmpWorkOrderMaterialsKit WITH(NOLOCK) WHERE [ID] = @KITMasterLoopID;
-
-					--Truncate #tmpWorkOrderMaterialStockLineKit
-					TRUNCATE TABLE #tmpWorkOrderMaterialStockLineKit;
-
-					INSERT INTO #tmpWorkOrderMaterialStockLineKit (StocklineId) 
-						 SELECT StocklineId
-					FROM [DBO].[WorkOrderMaterialStockLineKit] WOST WITH(NOLOCK) 
-					WHERE WOST.WorkOrderMaterialskitId = @WorkOrderMaterialsKitId;
-
-					SELECT  @KITChildMasterLoopID = MAX(ID) FROM #tmpWorkOrderMaterialStockLineKit;
-					WHILE(@KITChildMasterLoopID > 0)
-					BEGIN
-						   INSERT INTO #tmpSalesOrderStockline (StocklineId) 
-						   		SELECT StocklineId
-						   FROM #tmpWorkOrderMaterialStockLineKit WITH(NOLOCK) WHERE [ID] = @KITChildMasterLoopID
-						   
-						   SET @KITChildMasterLoopID = @KITChildMasterLoopID - 1;
-					END
-
-					SET @KITMasterLoopID = @KITMasterLoopID - 1;
-				END
-				
+				INSERT INTO #tmpSalesOrderStockline (StocklineId) 
+							  SELECT MST.StockLineId
+				FROM [DBO].[WorkOrderMaterialStockLineKit] MST WITH(NOLOCK) 
+				JOIN [DBO].[WorkOrderMaterialsKit] VRP WITH(NOLOCK) ON MST.WorkOrderMaterialsKitId = VRP.WorkOrderMaterialsKitId
+				WHERE VRP.WorkOrderId = @WorkOrderId; -- get workorderid
 			---END KIT PN Stockline
 
 			SELECT @PartsStocklineId = STRING_AGG([StocklineId], ',') FROM #tmpSalesOrderStockline

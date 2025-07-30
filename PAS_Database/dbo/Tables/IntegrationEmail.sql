4285cfade33ec5e822cb89e86641d5fa10d6bcf2
Hemant <Hemant@silverxis.com>
2025-07-30 12:54:43 +0530

    Integration Changes

---
+++ PAS_Database/dbo/Tables/IntegrationEmail.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[IntegrationEmail] (
+    [IntegrationEmailID] BIGINT         IDENTITY (1, 1) NOT NULL,
+    [Subject]            VARCHAR (500)  NOT NULL,
+    [EmailBody]          NVARCHAR (MAX) NOT NULL,
+    [ToEmail]            NVARCHAR (320) NOT NULL,
+    [FromEmail]          NVARCHAR (320) NOT NULL,
+    [CC]                 NVARCHAR (320) NULL,
+    [BCC]                NVARCHAR (320) NULL,
+    [EmailReadBy]        NVARCHAR (320) NULL,
+    [ReferenceId]        BIGINT         NOT NULL,
+    [ModuleId]           INT            NOT NULL,
+    [EmailStatus]        BIT            NULL,
+    [HasAttachments]     BIT            NULL,
+    [AttachmentName]     NVARCHAR (320) NULL,
+    [AttachmentPath]     NVARCHAR (320) NULL,
+    [MasterCompanyId]    INT            NOT NULL,
+    [CreatedBy]          VARCHAR (256)  NOT NULL,
+    [UpdatedBy]          VARCHAR (256)  NOT NULL,
+    [CreatedDate]        DATETIME2 (7)  CONSTRAINT [DF_IntegrationEmail_CreatedDate] DEFAULT (getutcdate()) NOT NULL,
+    [UpdatedDate]        DATETIME2 (7)  CONSTRAINT [DF_IntegrationEmail_UpdatedDate] DEFAULT (getutcdate()) NOT NULL,
+    [IsActive]           BIT            CONSTRAINT [DF_IntegrationEmail_IsActive] DEFAULT ((1)) NOT NULL,
+    [IsDeleted]          BIT            CONSTRAINT [DF_IntegrationEmail_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_IntegrationEmail] PRIMARY KEY CLUSTERED ([IntegrationEmailID] ASC),
+    CONSTRAINT [FK_IntegrationEmail_MasterCompany] FOREIGN KEY ([MasterCompanyId]) REFERENCES [dbo].[MasterCompany] ([MasterCompanyId])
+);
+
---
+++ PAS_Database/dbo/Tables/IntegrationEmailSmtpConfigrationAudit.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[IntegrationEmailSmtpConfigrationAudit] (
+    [IntegrationEmailConfigAuditId] BIGINT         IDENTITY (1, 1) NOT NULL,
+    [IntegrationEmailConfigId]      BIGINT         NOT NULL,
+    [SmtpUserEmail]                 NVARCHAR (MAX) NOT NULL,
+    [smtpserver]                    NVARCHAR (500) NOT NULL,
+    [SmtpEmailPassword]             NVARCHAR (500) NOT NULL,
+    [SmtpPort]                      INT            NOT NULL,
+    [UseSsl]                        BIT            NULL,
+    [MasterCompanyId]               INT            NOT NULL,
+    [CreatedBy]                     VARCHAR (256)  NOT NULL,
+    [UpdatedBy]                     VARCHAR (256)  NOT NULL,
+    [CreatedDate]                   DATETIME2 (7)  NOT NULL,
+    [UpdatedDate]                   DATETIME2 (7)  NOT NULL,
+    [IsActive]                      BIT            NOT NULL,
+    [IsDeleted]                     BIT            NOT NULL,
+    CONSTRAINT [PK_IntegrationEmailConfigrationAudit] PRIMARY KEY CLUSTERED ([IntegrationEmailConfigAuditId] ASC)
+);
+

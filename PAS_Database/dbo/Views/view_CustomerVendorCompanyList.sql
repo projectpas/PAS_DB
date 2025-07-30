8d39dcfa029230da3173b8e9760015786a925a6b
Vishal Suthar <vishal@silverxis.com>
2025-07-30 23:41:25 +0530

    PN-13601 Changes for Import Stocklines

---
+++ PAS_Database/dbo/Views/view_CustomerVendorCompanyList.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW view_CustomerVendorCompanyList AS
+SELECT CustomerId AS ReferenceId, [Name] AS RefName, MasterCompanyId
+FROM DBO.Customer WITH (NOLOCK)
+UNION
+SELECT VendorId AS ReferenceId, [VendorName] AS RefName, MasterCompanyId
+FROM DBO.Vendor WITH (NOLOCK)
+UNION
+SELECT LegalEntityId AS ReferenceId, [Name] AS RefName, MasterCompanyId
+FROM DBO.LegalEntity WITH (NOLOCK)

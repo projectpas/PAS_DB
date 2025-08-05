f456e1b7d91186411f2ade4887d8214a442a7585
Moin Bloch <moin@silverxis.com>
2025-08-05 12:25:34 +0530

    Email Configration

---
+++ PAS_Database/dbo/Stored Procedures/USP_GetIntegrationEmailConfigration.sql
@@ -0,0 +1,70 @@
+﻿/*************************************************************                 
+ ** File:   [USP_GetIntegrationEmailConfigration]                 
+ ** Author:   Moin Bloch
+ ** Description: Get Integration Email Configration 
+ ** Purpose:               
+ ** Date:    04/08/2025      
+ **************************************************************                 
+  ** Change History                 
+ **************************************************************                 
+ ** PR   Date         Author  Change	Description                  
+ ** --   --------     -------  ------	--------------------------                
+    1    04/08/2025   Moin Bloch   	    Created      
+    
+-- EXEC USP_GetIntegrationEmailConfigration 1,0,1
+**************************************************************/                   
+CREATE   PROCEDURE [dbo].[USP_GetIntegrationEmailConfigration] 
+@MasterCompanyId INT = NULL,
+@IntegrationEmailConfigId INT = NULL,
+@Opr INT = NULL
+AS      
+BEGIN      
+ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED      
+ SET NOCOUNT ON;      
+ BEGIN TRY      
+    
+	IF(@Opr = 1)
+	BEGIN
+		SELECT [IntegrationEmailConfigId]
+			  ,[SmtpUserEmail]
+			  ,[smtpserver]
+			  ,[SmtpEmailPassword]
+			  ,[SmtpPort]
+			  ,ISNULL([UseSsl],0) [UseSsl]
+			  ,[MasterCompanyId]
+			  ,[CreatedBy]
+			  ,[UpdatedBy]
+			  ,[CreatedDate]
+			  ,[UpdatedDate]
+			  ,[IsActive]
+			  ,[IsDeleted]
+		  FROM [dbo].[IntegrationEmailSmtpConfigration] IE WITH(NOLOCK)      
+	  WHERE IE.[MasterCompanyId] = @MasterCompanyId
+		AND IE.[IsDeleted] = 0
+		AND IE.[IsActive] = 1   
+
+	END
+	IF(@Opr = 2)
+	BEGIN
+		UPDATE [dbo].[IntegrationEmailSmtpConfigration] SET [IsDeleted] = 1 WHERE [IntegrationEmailConfigId] = @IntegrationEmailConfigId
+	END
+	          
+ END TRY          
+ BEGIN CATCH      
+  DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name()       
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------      
+        , @AdhocComments     VARCHAR(150)    = 'USP_GetIntegrationEmailConfigration'       
+        ,@ProcedureParameters VARCHAR(3000) = '@Parameter1 = ''' + CAST(ISNULL(1, '') AS varchar(100))             
+        + '@Parameter2 = ''' + CAST(ISNULL(@MasterCompanyId  , '') AS varchar(100))   
+        , @ApplicationName VARCHAR(100) = 'PAS'      
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------      
+        exec spLogException       
+                @DatabaseName           = @DatabaseName      
+                , @AdhocComments          = @AdhocComments      
+                , @ProcedureParameters = @ProcedureParameters      
+                , @ApplicationName        =  @ApplicationName      
+                , @ErrorLogID                    = @ErrorLogID OUTPUT ;      
+        RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)      
+        RETURN(1);      
+ END CATCH      
+END

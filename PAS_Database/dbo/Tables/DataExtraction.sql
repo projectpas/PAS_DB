8ca2ea9f4508ad40ac05a44e034d765980831097
Vishal Suthar <vishal@silverxis.com>
2025-08-06 03:02:44 +0530

    PN-13787 Added table for dynamic fields to be extracted using AI

---
+++ PAS_Database/dbo/Tables/DataExtraction.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[DataExtraction] (
+    [Id]              BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Field]           VARCHAR (200) NULL,
+    [Question]        VARCHAR (MAX) NULL,
+    [Type]            VARCHAR (100) NULL,
+    [MasterCompanyId] INT           NULL,
+    [CreatedBy]       VARCHAR (256) NOT NULL,
+    [CreatedDate]     DATETIME2 (7) CONSTRAINT [DF_DataExtraction_CreatedDate_1] DEFAULT (getdate()) NOT NULL,
+    [UpdatedBy]       VARCHAR (256) NOT NULL,
+    [UpdatedDate]     DATETIME2 (7) CONSTRAINT [DF_DataExtraction_UpdatedDate_1] DEFAULT (getdate()) NOT NULL,
+    [IsDeleted]       BIT           CONSTRAINT [DF_DataExtraction_IsDeleted_1] DEFAULT ((0)) NOT NULL,
+    [IsActive]        BIT           CONSTRAINT [DF_DataExtraction_IsActive_1] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_DataExtraction] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

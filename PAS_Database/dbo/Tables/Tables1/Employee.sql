45305ba78be55f99809f627f4a51a63d88fec0a0
saliyabhargav <bhargav.saliya@silverxis.com>
2025-08-27 16:28:36 +0530

    PN-13964

--- PAS_Database/dbo/Tables/Tables1/Employee.sql
+++ PAS_Database/dbo/Tables/Tables1/Employee.sql
@@ -50,12 +50,15 @@
     [TwoFactorAuthentication]            BIT             DEFAULT ((0)) NOT NULL,
     [TwoFactorAuthenticationType]        INT             NULL,
     [TwoFactorAuthenticatorKey]          NVARCHAR (100)  NULL,
+    [IsScrapCertification]               BIT             NULL,
     CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([EmployeeId] ASC),
     CONSTRAINT [FK__Employee__LegalE__3FC721DF] FOREIGN KEY ([LegalEntityId]) REFERENCES [dbo].[LegalEntity] ([LegalEntityId]),
     CONSTRAINT [FK_Employee_JobTitle] FOREIGN KEY ([JobTitleId]) REFERENCES [dbo].[JobTitle] ([JobTitleId]),
     CONSTRAINT [FK_Employee_MasterCompany] FOREIGN KEY ([MasterCompanyId]) REFERENCES [dbo].[MasterCompany] ([MasterCompanyId]),
     CONSTRAINT [FK_Employee_Station] FOREIGN KEY ([StationId]) REFERENCES [dbo].[EmployeeStation] ([EmployeeStationId])
 );
+
+
 
 
 

6b6e1aa037d57b68c54e1d95e9a858f6d0c4d842
saliyabhargav <bhargav.saliya@silverxis.com>
2025-08-28 09:57:31 +0530

    PN-13793

---
+++ PAS_Database/dbo/Tables/ItemMasterRanking.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[ItemMasterRanking] (
+    [ItemMasterRankingId] BIGINT        IDENTITY (1, 1) NOT NULL,
+    [ItemMasterId]        BIGINT        NOT NULL,
+    [RankingId]           INT           NOT NULL,
+    [MasterCompanyId]     INT           NOT NULL,
+    [CreatedBy]           VARCHAR (256) NULL,
+    [UpdatedBy]           VARCHAR (256) NULL,
+    [CreatedDate]         DATETIME2 (7) NOT NULL,
+    [UpdatedDate]         DATETIME2 (7) NOT NULL,
+    [IsActive]            BIT           NULL,
+    [IsDeleted]           BIT           CONSTRAINT [DF_ItemMasterRanking_IsDeleted] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_ItemMasterRanking] PRIMARY KEY CLUSTERED ([ItemMasterRankingId] ASC)
+);
+
---
+++ PAS_Database/dbo/Views/vw_RankingAudit.sql
@@ -0,0 +1,6 @@
+﻿CREATE   VIEW [dbo].[vw_RankingAudit]
+AS
+	SELECT tt.RankingAuditId  AS PkID, tt.RankingId AS ID	,tt.[Description]
+	,tt.CreatedBy AS [Created By],
+	tt.CreatedDate AS [Created Date],tt.UpdatedBy AS [Updated By], tt.UpdatedDate AS [Updated Date], tt.IsActive AS [Is Active], tt.IsDeleted AS [Is Deleted]
+	FROM [DBO].RankingAudit tt WITH (NOLOCK)

0a0c0eb5f28415676e77271d029e96b5b73b7053
devendra silverxis <devendra@silverxis.com>
2025-10-30 19:44:00 +0530

    PN-14555

---
+++ PAS_Database/dbo/Stored Procedures/usp_GetOpenAIPrompts.sql
@@ -0,0 +1,61 @@
+﻿/*************************************************************           
+ ** File:   [usp_GetOpenAIPrompts]           
+ ** Author:   Devendra Shekh
+ ** Description: This SP is used to get the Open AI Prompt By Name
+ ** Purpose:         
+ ** Date:   29-Oct-2025 
+ **************************************************************           
+ ** Change History           
+ **************************************************************           
+ ** PR   Date				Author				Change Description            
+ ** --   --------			-------				--------------------------------          
+    1    29-Oct-2025		Devendra Shekh		Created
+
+EXEC [DBO].[usp_GetOpenAIPrompts] 1, 'SupportTicket'
+**************************************************************/
+CREATE   PROCEDURE [dbo].[usp_GetOpenAIPrompts]
+(
+    @MasterCompanyId INT = NULL,
+    @Name VARCHAR(100) = NULL
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	BEGIN TRY 
+
+		SELECT
+			[PromptId],
+			[Name],
+			[PromptText],
+			[Model],
+			[APIUrl],
+			[MasterCompanyId],
+			[CreatedBy],
+			[CreatedDate],
+			[UpdatedBy],
+			[UpdatedDate],
+			[IsDeleted],
+			[IsActive]
+		FROM [dbo].[OpenAIPrompt] WITH (NOLOCK)
+		WHERE [Name] = @Name AND [IsActive] = 1 AND [IsDeleted] = 0
+
+	END TRY
+	BEGIN CATCH	
+		DECLARE @ErrorLogID INT
+		,@DatabaseName VARCHAR(100) = db_name()
+		-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+		,@AdhocComments VARCHAR(150) = 'usp_GetOpenAIPrompts'
+		,@ProcedureParameters VARCHAR(3000) = '@Parameter1 = ''' + CAST(ISNULL(@MasterCompanyId, '') as varchar(255))
+			+ '@Parameter2 = ''' + CAST(ISNULL(@Name, '') as varchar(100)) 
+		,@ApplicationName VARCHAR(100) = 'PAS'
+		-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+		EXEC spLogException @DatabaseName = @DatabaseName
+			,@AdhocComments = @AdhocComments
+			,@ProcedureParameters = @ProcedureParameters
+			,@ApplicationName = @ApplicationName
+			,@ErrorLogID = @ErrorLogID OUTPUT;
+
+		RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d',16,1,@ErrorLogID)
+		RETURN (1);
+	END CATCH
+END
---
+++ PAS_Database/dbo/Stored Procedures/usp_GetSupportEmail.sql
@@ -0,0 +1,74 @@
+﻿/*************************************************************           
+ ** File:   [usp_GetSupportEmail]           
+ ** Author:   Devendra Shekh
+ ** Description: This SP is used to get the SupportEmail Data 
+ ** Purpose:         
+ ** Date:   29-Oct-2025 
+ **************************************************************           
+ ** Change History           
+ **************************************************************           
+ ** PR   Date				Author				Change Description            
+ ** --   --------			-------				--------------------------------          
+    1    29-Oct-2025		Devendra Shekh		Created
+
+EXEC [DBO].[usp_GetSupportEmail] 1, 'email.poweraerosuites@gmail.com'
+**************************************************************/
+CREATE   PROCEDURE [dbo].[usp_GetSupportEmail]
+(
+    @MasterCompanyId INT = NULL,
+    @EmailReadBy NVARCHAR(320) = NULL
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+	BEGIN TRY 
+
+		SELECT TOP 1
+			[SupportEmailId],
+			[CustomerTicketId],
+			[MessageId],
+			[Subject],
+			[EmailBody],
+			[ToEmail],
+			[FromEmail],
+			[CC],
+			[BCC],
+			[EmailReadBy],
+			[HasAttachments],
+			[EmailSection],
+			[ReceivedDate],
+			[MasterCompanyId],
+			[CreatedBy],
+			[UpdatedBy],
+			[CreatedDate],
+			[UpdatedDate],
+			[IsActive],
+			[IsDeleted],
+			[IsRead],
+			[IsProcessed],
+			[EmailStatusId],
+			[AttemptCount]
+		FROM [dbo].[SupportEmail] WITH (NOLOCK)
+		WHERE [EmailReadBy] = @EmailReadBy AND [IsActive] = 1 AND [IsDeleted] = 0
+		ORDER BY [ReceivedDate] DESC;
+
+	END TRY
+		BEGIN CATCH	
+		DECLARE @ErrorLogID INT
+		,@DatabaseName VARCHAR(100) = db_name()
+		-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+		,@AdhocComments VARCHAR(150) = 'usp_GetSupportEmail'
+		,@ProcedureParameters VARCHAR(3000) = '@Parameter1 = ''' + CAST(ISNULL(@MasterCompanyId, '') as varchar(255))
+			+ '@Parameter2 = ''' + CAST(ISNULL(@EmailReadBy, '') as varchar(100)) 
+		,@ApplicationName VARCHAR(100) = 'PAS'
+		-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+		EXEC spLogException @DatabaseName = @DatabaseName
+			,@AdhocComments = @AdhocComments
+			,@ProcedureParameters = @ProcedureParameters
+			,@ApplicationName = @ApplicationName
+			,@ErrorLogID = @ErrorLogID OUTPUT;
+
+		RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d',16,1,@ErrorLogID)
+		RETURN (1);
+	END CATCH
+END

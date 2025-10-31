d1d2fd1c4f30a1978f5ad74f7dd820618209ae55
sahdev <sahdev@silverxis.com>
2025-10-31 19:27:47 +0530

    PN-14203, PN-14208

---
+++ PAS_Database/dbo/Stored Procedures/USP_GetCommonForStocklineByItemMasterId.sql
@@ -0,0 +1,113 @@
+﻿/*************************************************************           
+ ** File:   [USP_GetCommonForStocklineByItemMasterId]           
+ ** Author:   Sahdev Saliya
+ ** Description: This stored procedure is used to Get CommonForStocklineByItemMasterId List
+ ** Purpose:         
+ ** Date:   31-10-2025       
+          
+ ** RETURN VALUE:           
+  
+ **************************************************************             
+  ** Change History             
+ **************************************************************             
+ ** S NO   Date            Author          Change Description              
+ ** --   --------         -------          --------------------------------            
+    1    31-10-2025    Sahdev Saliya       Created  
+
+	exec [dbo].[USP_GetCommonForStocklineByItemMasterId]
+**************************************************************/
+CREATE   PROCEDURE [DBO].[USP_GetCommonForStocklineByItemMasterId]
+    @ItemMasterId BIGINT = NULL
+
+AS
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+		SELECT TOP 1
+			iM.ItemMasterId,
+			iM.PartNumber,
+			iM.PartDescription,
+			iM.RevisedPartId,
+			imxl.ExchangeCoreCost AS CoreUnitCost,
+			iM.ItemMasterAssetTypeId AS AcquistionTypeId,
+			imps.SP_CalSPByPP_UnitSalePrice AS UnitSalesPrice,
+			imps.ConditionId,
+			imps.PP_UnitPurchasePrice AS POUnitCost,
+			rPart.PartNumber AS RevisedPart,
+			iM.ItemGroup,
+			iM.ItemMasterAssetTypeId,
+			iM.GLAccountId,
+			(gl.AccountCode + ' - ' + gl.AccountName) AS GLAccount,
+			(SELECT STRING_AGG(inte.Description, ',')
+				FROM [DBO].[ItemMaster] v WITH (NOLOCK)
+				INNER JOIN [DBO].ItemMasterIntegrationPortal mp WITH (NOLOCK) ON v.ItemMasterId = mp.ItemMasterId
+				INNER JOIN [DBO].IntegrationPortal inte WITH (NOLOCK) ON mp.IntegrationPortalId = inte.IntegrationPortalId
+				WHERE v.ItemMasterId = iM.ItemMasterId
+			) AS IntegrationPortal,
+
+			(SELECT STRING_AGG(CAST(inte.IntegrationPortalId AS VARCHAR(20)), ',')
+				FROM [DBO].[ItemMaster] v WITH (NOLOCK)
+				INNER JOIN [DBO].ItemMasterIntegrationPortal mp WITH (NOLOCK) ON v.ItemMasterId = mp.ItemMasterId
+				INNER JOIN [DBO].IntegrationPortal inte WITH (NOLOCK) ON mp.IntegrationPortalId = inte.IntegrationPortalId
+				WHERE v.ItemMasterId = iM.ItemMasterId
+			) AS IntegrationPortalIds,
+			iM.ShelfLife,
+			iM.ExpirationDate,
+			iM.IsSerialized,
+			ISNULL(imx.ExportECCN, '') AS ExportECCN,
+			ISNULL(imx.ITARNumber, '') AS ITARNumber,
+			iM.NationalStockNumber,
+			iM.TagDays,
+			iM.ManufacturingDays,
+			iM.DaysReceived,
+			iM.OpenDays,
+			iM.SiteId,
+			iM.WarehouseId,
+			iM.LocationId,
+			iM.ShelfId,
+			iM.BinId,
+			iM.IsManufacturingDateAvailable,
+			iM.IsOEM,
+			iM.IsOemPNId,
+			iM.IsPma,
+			iM.IsDER,
+			iM.IsTimeLife,
+			iM.IsActive,
+			iM.ManufacturerId,
+			iM.PurchaseUnitOfMeasureId,
+			iM.IsExpirationDateAvailable,
+			iM.IsReceivedDateAvailable,
+			iM.IsTagDateAvailable,
+			iM.InventoryGLSettingId,
+			iM.ItemClassificationName AS Classification
+
+    FROM [DBO].[ItemMaster] iM WITH (NOLOCK)
+        LEFT JOIN [DBO].[ItemMaster] rPart WITH (NOLOCK) ON iM.RevisedPartId = rPart.ItemMasterId
+        LEFT JOIN [DBO].[ItemMasterExchangeLoan] imxl WITH (NOLOCK) ON iM.ItemMasterId = imxl.ItemMasterId
+        LEFT JOIN [DBO].[ItemMasterPurchaseSale] imps WITH (NOLOCK) ON iM.ItemMasterId = imps.ItemMasterId
+        LEFT JOIN [DBO].[ItemMasterExportInfo] imx WITH (NOLOCK) ON iM.ItemMasterId = imx.ItemMasterId
+        LEFT JOIN [DBO].[GLAccount] gl WITH (NOLOCK) ON iM.GLAccountId = gl.GLAccountId
+    WHERE iM.ItemMasterId = @ItemMasterId;
+    END TRY 
+
+    BEGIN CATCH
+		DECLARE @ErrorLogID INT, @DatabaseName VARCHAR(100) = db_name() 
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+              , @AdhocComments     VARCHAR(150)    = 'USP_GetCommonForStocklineByItemMasterId'
+			  , @ProcedureParameters VARCHAR(3000) = '@Parameter1 = '''
+              , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+		EXEC spLogException @DatabaseName = @DatabaseName
+			,@AdhocComments = @AdhocComments
+			,@ProcedureParameters = @ProcedureParameters
+			,@ApplicationName = @ApplicationName
+			,@ErrorLogID = @ErrorLogID OUTPUT;
+
+		RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1, @ErrorLogID)
+
+		RETURN (1); 
+	END CATCH
+
+END
---
+++ PAS_Database/dbo/Stored Procedures/USP_GetNhaTlaAltEquPartHistory.sql
@@ -0,0 +1,122 @@
+﻿/*************************************************************           
+ ** File:   [USP_GetNhaTlaAltEquPartHistory]           
+ ** Author:   Sahdev Saliya
+ ** Description: This stored procedure is used to GetNhaTlaAltEquPartHistory List
+ ** Purpose:         
+ ** Date:   31-10-2025       
+          
+ ** RETURN VALUE:           
+  
+ **************************************************************             
+  ** Change History             
+ **************************************************************             
+ ** S NO   Date            Author          Change Description              
+ ** --   --------         -------          --------------------------------            
+    1    31-10-2025    Sahdev Saliya       Created  
+
+	exec [dbo].[USP_GetNhaTlaAltEquPartHistory]
+**************************************************************/
+CREATE   PROCEDURE [dbo].[USP_GetNhaTlaAltEquPartHistory]
+   @ItemMappingId BIGINT = NULL,
+   @EmployeeId BIGINT = NULL,
+   @Opr INT = NULL
+AS
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+    SET NOCOUNT ON;
+	BEGIN TRY
+	DECLARE @ModuleId INT 
+	SELECT @ModuleId = [AttachmentModuleId] FROM dbo.AttachmentModule WITH(NOLOCK) WHERE [Name] = 'NhaTlaAltEquItemMapping'
+
+	IF (@Opr=1) 
+	BEGIN
+
+    DECLARE @CurrntEmpTimeZoneDesc VARCHAR(100) = '';
+	SELECT
+        @CurrntEmpTimeZoneDesc = COALESCE(
+            ETZ.[Description],  -- Employee's timezone
+            LTZ.[Description]   -- Fallback to LegalEntity timezone
+        )
+		FROM dbo.Employee E WITH (NOLOCK)
+		LEFT JOIN dbo.TimeZone ETZ WITH (NOLOCK)
+			ON E.TimeZoneId = ETZ.TimeZoneId
+		LEFT JOIN dbo.LegalEntity LE WITH (NOLOCK)
+			ON E.LegalEntityId = LE.LegalEntityId
+		LEFT JOIN dbo.TimeZone LTZ WITH (NOLOCK)
+			ON LE.TimeZoneId = LTZ.TimeZoneId
+		WHERE E.EmployeeId = @EmployeeId;
+	    
+	SELECT  alt.NhaTlaAltEquAuditId,
+			alt.ItemMappingId,
+			im.ItemMasterId,
+			im1.ManufacturerId,
+			alt.MappingItemMasterId,
+			alt.MappingType,
+			im.PartNumber,
+			im.PartDescription,
+			man.[Name] AS Manufacturer,
+			im1.PartNumber AS AltPartNo,
+			im1.PartDescription AS AltPartDescription,
+			alt.IsActive,
+			alt.IsDeleted,
+			alt.CreatedBy,
+			CreatedDate = CAST(DBO.ConvertUTCtoLocal(alt.CreatedDate, @CurrntEmpTimeZoneDesc) AS DATETIME),
+			alt.UpdatedBy,
+			UpdatedDate = CAST(DBO.ConvertUTCtoLocal(alt.UpdatedDate, @CurrntEmpTimeZoneDesc) AS DATETIME),						
+			ic.[Description] AS ItemClassification,
+			alt.CustomerId,
+			cust.[Name]
+		FROM dbo.NhaTlaAltEquAudit alt WITH (NOLOCK)
+			INNER JOIN dbo.ItemMaster im WITH (NOLOCK) ON alt.ItemMasterId = im.ItemMasterId
+			INNER JOIN dbo.ItemMaster im1 WITH (NOLOCK) ON alt.MappingItemMasterId = im1.ItemMasterId
+			INNER JOIN dbo.Manufacturer man WITH (NOLOCK) ON im1.ManufacturerId = man.ManufacturerId
+			INNER JOIN dbo.ItemClassification ic WITH (NOLOCK) ON im1.ItemClassificationId = ic.ItemClassificationId
+			LEFT JOIN dbo.Customer cust WITH (NOLOCK) ON alt.CustomerId = cust.CustomerId
+		WHERE alt.ItemMappingId = @ItemMappingId
+      
+	END
+	IF (@Opr = 2)
+	BEGIN
+		SELECT AD.[AttachmentDetailId]
+			  ,AD.[AttachmentId]
+			  ,AD.[FileName]
+			  ,AD.[Description]
+			  ,AD.[Link]
+			  ,AD.[FileFormat]
+			  ,AD.[FileSize]
+			  ,AD.[FileType]
+			  ,AD.[CreatedDate]
+			  ,AD.[UpdatedDate]
+			  ,AD.[CreatedBy]
+			  ,AD.[UpdatedBy]
+			  ,AD.[IsActive]
+			  ,AD.[IsDeleted]
+			  ,AD.[Name]
+			  ,AD.[Memo]
+			  ,AD.[TypeId] 
+			FROM dbo.Attachment ATT WITH (NOLOCK)
+            INNER JOIN dbo.AttachmentDetails AD WITH (NOLOCK) ON ATT.AttachmentId = AD.AttachmentId
+            WHERE ATT.ReferenceId = @ItemMappingId
+              AND ATT.ModuleId = @ModuleId
+            ORDER BY AD.AttachmentDetailId DESC;
+	END
+
+   END TRY
+    BEGIN CATCH
+			DECLARE @ErrorLogID INT, @DatabaseName VARCHAR(100) = db_name() 
+	-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+				  , @AdhocComments     VARCHAR(150)    = 'USP_GetNhaTlaAltEquPartHistory'
+				  , @ProcedureParameters VARCHAR(3000) = '@Parameter1 = '''
+				  , @ApplicationName VARCHAR(100) = 'PAS'
+	-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+			EXEC spLogException @DatabaseName = @DatabaseName
+				,@AdhocComments = @AdhocComments
+				,@ProcedureParameters = @ProcedureParameters
+				,@ApplicationName = @ApplicationName
+				,@ErrorLogID = @ErrorLogID OUTPUT;
+
+			RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1, @ErrorLogID)
+
+			RETURN (1); 
+	 END CATCH
+END

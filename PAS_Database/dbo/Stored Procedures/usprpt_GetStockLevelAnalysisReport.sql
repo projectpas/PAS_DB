5ca81f4ceb66f260eb1ade5158ede89e5a4c9beb
Amit Gediya <amit.ghediya@silverxis.comm>
2025-11-06 14:13:14 +0530

    PN-14649 added for stock analysis report.

--- PAS_Database/dbo/Stored Procedures/usprpt_GetStockLevelAnalysisReport.sql
+++ PAS_Database/dbo/Stored Procedures/usprpt_GetStockLevelAnalysisReport.sql
@@ -25,7 +25,7 @@ BEGIN
   SET NOCOUNT ON;
   SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 
-    declare @level1 VARCHAR(MAX) = NULL,
+    DECLARE @level1 VARCHAR(MAX) = NULL,
 	@level2 VARCHAR(MAX) = NULL,
 	@level3 VARCHAR(MAX) = NULL,
 	@level4 VARCHAR(MAX) = NULL,
@@ -35,32 +35,56 @@ BEGIN
 	@Level8 VARCHAR(MAX) = NULL,
 	@Level9 VARCHAR(MAX) = NULL,
 	@Level10 VARCHAR(MAX) = NULL,
+
+	@PN VARCHAR(MAX) = NULL,
+	@Condition VARCHAR(MAX) = NULL,
+
+	@Site VARCHAR(MAX) = NULL,
+	@Warehouse VARCHAR(MAX) = NULL,
+	@Location VARCHAR(MAX) = NULL,
+	@Shelf VARCHAR(MAX) = NULL,
+	@Bin VARCHAR(MAX) = NULL,
+
 	@IsDownload BIT = NULL
 
   BEGIN TRY
-      
 	select 
-   
-	@level1=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level1' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level1 end,
-	@level2=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level2' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level2 end,
-	@level3=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level3' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level3 end,
-	@level4=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level4' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level4 end,
-	@level5=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level5' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level5 end,
-	@level6=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level6' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level6 end,
-	@level7=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level7' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level7 end,
-	@level8=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level8' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level8 end,
-	@level9=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level9' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level9 end,
-	@level10=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level10' 
-	then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level10 end
+		@level1=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level1' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level1 end,
+		@level2=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level2' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level2 end,
+		@level3=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level3' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level3 end,
+		@level4=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level4' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level4 end,
+		@level5=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level5' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level5 end,
+		@level6=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level6' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level6 end,
+		@level7=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level7' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level7 end,
+		@level8=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level8' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level8 end,
+		@level9=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level9' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level9 end,
+		@level10=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Level10' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @level10 end,
+
+		@PN=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='PN' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @PN end,
+		@Condition=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Condition' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @Condition end,
+
+		@Site=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Site' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @Site end,
+		@Warehouse=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Warehouse' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @Warehouse end,
+		@Location=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Location' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @Location end,
+		@Shelf=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Shelf' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @Shelf end,
+		@Bin=case when filterby.value('(FieldName/text())[1]','VARCHAR(100)')='Bin' 
+		then filterby.value('(FieldValue/text())[1]','VARCHAR(100)') else @Bin end
 
   FROM
       @xmlFilter.nodes('/ArrayOfFilter/Filter')AS TEMPTABLE(filterby)
@@ -90,15 +114,56 @@ BEGIN
 	  SET @PageSize = CASE WHEN NULLIF(@PageSize,0) IS NULL THEN 10 ELSE @PageSize END
 	  SET @PageNumber = CASE WHEN NULLIF(@PageNumber,0) IS NULL THEN 1 ELSE @PageNumber END
 
-	  ;WITH rptCTE (TotalRecordsCount, pn, pndescription, masterCompanyId) AS (
+	  ;WITH rptCTE (TotalRecordsCount, pn, pndescription,manufacturer,cond,stockuom, masterCompanyId,stocklevel,leadtimedays,reorderpoint,reorderqty,minqtyorder,
+					qtyonhand,qtyonavail,reorder,qtytoorder,site,warehouse,location,shelf,bin,level1, level2, level3, level4, level5, level6, level7, level8,
+			     level9, level10) AS (
       SELECT COUNT(1) OVER () AS TotalRecordsCount,
         UPPER(stl.partnumber) AS 'pn',
         UPPER(stl.PNDescription) AS 'pndescription',
-		stl.MasterCompanyId
+		UPPER(stl.Manufacturer) AS 'manufacturer',
+		UPPER(stl.Condition) AS 'cond',
+		stl.UnitOfMeasure AS 'stockuom',
+		stl.MasterCompanyId AS 'masterCompanyId',
+		IM.StockLevel AS 'stocklevel',
+		IM.LeadTimeDays AS 'leadtimedays',
+		IM.ReorderPoint AS 'reorderpoint',
+		IM.ReorderQuantiy AS 'reorderqty',
+		IM.MinimumOrderQuantity AS 'minqtyorder',
+		stl.QuantityOnHand AS 'qtyonhand',
+		stl.QuantityAvailable AS 'qtyonavail',
+		CASE WHEN stl.QuantityAvailable <= IM.StockLevel THEN 'YES' ELSE 'NO' END AS 'reorder',
+		CASE 
+			WHEN 
+				(ISNULL(IM.StockLevel,0) - ISNULL(stl.QuantityAvailable,0)) > ISNULL(IM.MinimumOrderQuantity,0) 
+			THEN (ISNULL(IM.StockLevel,0) - ISNULL(stl.QuantityAvailable,0)) ELSE ISNULL(IM.MinimumOrderQuantity,0)
+		END	AS 'qtytoorder',
+		UPPER(stl.[Site]) As 'site',
+		UPPER(stl.[Warehouse]) As 'warehouse',
+		UPPER(stl.[Location]) As 'location',
+		UPPER(stl.[Shelf]) As 'shelf',
+		UPPER(stl.[Bin]) As 'bin',
+		UPPER(MSD.Level1Name) AS level1,     
+		UPPER(MSD.Level2Name) AS level2,    
+		UPPER(MSD.Level3Name) AS level3,    
+		UPPER(MSD.Level4Name) AS level4,    
+		UPPER(MSD.Level5Name) AS level5,    
+		UPPER(MSD.Level6Name) AS level6,    
+		UPPER(MSD.Level7Name) AS level7,   
+		UPPER(MSD.Level8Name) AS level8,    
+		UPPER(MSD.Level9Name) AS level9,    
+		UPPER(MSD.Level10Name) AS level10
 		FROM DBO.Stockline stl WITH (NOLOCK)
+		INNER JOIN dbo.ItemMaster IM ON IM.ItemMasterId = stl.ItemMasterId
 	    INNER JOIN dbo.StocklineManagementStructureDetails MSD WITH (NOLOCK) ON MSD.ModuleID = @ModuleID AND MSD.ReferenceID = stl.StockLineId
 		LEFT JOIN dbo.EntityStructureSetup ES ON ES.EntityStructureId=MSD.EntityMSID
-		WHERE stl.mastercompanyid = @mastercompanyid and stl.IsParent =1 AND stl.IsDeleted=0
+		WHERE stl.mastercompanyid = @mastercompanyid and stl.IsParent =1 and stl.isActive =1 AND stl.IsDeleted=0
+			AND (ISNULL(@PN,'') ='' OR stl.ItemMasterId  = @PN)
+			AND (ISNULL(@Condition,'') ='' OR stl.ConditionId IN (SELECT Item FROM DBO.SPLITSTRING(@Condition,','))) 
+			AND (ISNULL(@Site,'') ='' OR stl.SiteId IN (SELECT Item FROM DBO.SPLITSTRING(@Site,',')))   
+			AND (ISNULL(@Warehouse,'') ='' OR stl.WarehouseId IN (SELECT Item FROM DBO.SPLITSTRING(@Warehouse,',')))   
+			AND (ISNULL(@Location,'') ='' OR stl.LocationId IN (SELECT Item FROM DBO.SPLITSTRING(@Location,',')))   
+			AND (ISNULL(@Shelf,'') ='' OR stl.ShelfId IN (SELECT Item FROM DBO.SPLITSTRING(@Shelf,',')))   
+			AND (ISNULL(@Bin,'') ='' OR stl.BinId IN (SELECT Item FROM DBO.SPLITSTRING(@Bin,',')))
 	        AND  (ISNULL(@Level1,'') ='' OR MSD.[Level1Id] IN (SELECT Item FROM DBO.SPLITSTRING(@Level1,',')))
 			AND  (ISNULL(@Level2,'') ='' OR MSD.[Level2Id] IN (SELECT Item FROM DBO.SPLITSTRING(@Level2,',')))
 			AND  (ISNULL(@Level3,'') ='' OR MSD.[Level3Id] IN (SELECT Item FROM DBO.SPLITSTRING(@Level3,',')))
@@ -110,15 +175,15 @@ BEGIN
 			AND  (ISNULL(@Level9,'') ='' OR MSD.[Level9Id] IN (SELECT Item FROM DBO.SPLITSTRING(@Level9,',')))
 			AND  (ISNULL(@Level10,'') =''  OR MSD.[Level10Id] IN (SELECT Item FROM DBO.SPLITSTRING(@Level10,',')))
 			)
-			,FinalCTE(TotalRecordsCount, pn, pndescription, masterCompanyId) 
-			  AS (SELECT DISTINCT TotalRecordsCount, pn, pndescription, masterCompanyId FROM rptCTE)
+			,FinalCTE(TotalRecordsCount, pn, pndescription,manufacturer,cond,stockuom,stocklevel,leadtimedays,reorderpoint,reorderqty,minqtyorder,qtyonhand,qtyonavail,reorder,qtytoorder,site,warehouse,location,shelf,bin,level1, level2, level3, level4, level5, level6, level7, level8,level9, level10, masterCompanyId) 
+			  AS (SELECT DISTINCT TotalRecordsCount, pn, pndescription,manufacturer,cond,stockuom,stocklevel,leadtimedays,reorderpoint,reorderqty,minqtyorder,qtyonhand,qtyonavail,reorder,qtytoorder,site,warehouse,location,shelf,bin,level1, level2, level3, level4, level5, level6, level7, level8,level9, level10, masterCompanyId FROM rptCTE)
 
 			,WithTotal (masterCompanyId) 
 			  AS (SELECT masterCompanyId
 				FROM FinalCTE
 				GROUP BY masterCompanyId)
 
-			  SELECT COUNT(2) OVER () AS TotalRecordsCount, pn, pndescription
+			  SELECT COUNT(2) OVER () AS TotalRecordsCount, pn, pndescription,manufacturer,cond,stockuom,stocklevel,leadtimedays,reorderpoint,reorderqty,minqtyorder,qtyonhand,qtyonavail,reorder,qtytoorder,site,warehouse,location,shelf,bin,level1, level2, level3, level4, level5, level6, level7, level8,level9, level10
 				FROM FinalCTE FC
 					INNER JOIN WithTotal WC ON FC.masterCompanyId = WC.masterCompanyId
 				ORDER BY pn DESC

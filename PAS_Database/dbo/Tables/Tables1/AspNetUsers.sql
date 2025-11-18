c3d0fbca1bae39dd608fbd29af6c24577f4637d6
Amit Gediya <amit.ghediya@silverxis.comm>
2025-11-18 13:21:26 +0530

    PN-14749 update for aspnetuser details into master db.

--- PAS_Database/dbo/Tables/Tables1/AspNetUsers.sql
+++ PAS_Database/dbo/Tables/Tables1/AspNetUsers.sql
@@ -32,6 +32,8 @@
 
 
 
+
+
 GO
 CREATE UNIQUE NONCLUSTERED INDEX [UserNameIndex]
     ON [dbo].[AspNetUsers]([NormalizedUserName] ASC) WHERE ([NormalizedUserName] IS NOT NULL);
@@ -41,3 +43,100 @@ GO
 CREATE NONCLUSTERED INDEX [EmailIndex]
     ON [dbo].[AspNetUsers]([NormalizedEmail] ASC);
 
+
+GO
+--drop  trigger TR_AspNetUsers_To_MainDB
+CREATE   TRIGGER [dbo].[TR_AspNetUsers_To_MainDB]
+ON [dbo].[AspNetUsers]
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+		-------------------------------------------
+        -- 1. INSERT new users into PAS_UAT (Master)
+        -------------------------------------------
+        INSERT INTO MTI.dbo.AspNetUsers
+        (
+            Id,
+			ConcurrencyStamp,
+			UserName,
+			NormalizedUserName,
+			Email,
+			NormalizedEmail,
+			EmailConfirmed,
+			PhoneNumberConfirmed,
+			TwoFactorEnabled,
+			LockoutEnabled,
+			AccessFailedCount,
+			EmployeeId,
+			FullName,
+			IsEnabled,
+			MasterCompanyId,
+			SecurityStamp,
+			PasswordHash,
+			CreatedDate,
+			UpdatedDate
+        )
+        SELECT
+            i.Id,
+			i.ConcurrencyStamp,
+			i.UserName,
+			i.NormalizedUserName,
+			i.Email,
+			i.NormalizedEmail,
+			i.EmailConfirmed,
+			i.PhoneNumberConfirmed,
+			i.TwoFactorEnabled,
+			i.LockoutEnabled,
+			i.AccessFailedCount,
+			i.EmployeeId,
+			i.FullName,
+			i.IsEnabled,
+			i.MasterCompanyId,
+			i.SecurityStamp,
+			i.PasswordHash,
+			i.CreatedDate,
+			i.UpdatedDate
+        FROM inserted i
+        WHERE NOT EXISTS (
+				SELECT 1 
+				FROM MTI.dbo.AspNetUsers u
+				WHERE u.EmployeeId = i.EmployeeId 
+				  AND u.MasterCompanyId = i.MasterCompanyId
+		);
+
+		-------------------------------------------
+        -- 2. UPDATE existing users in PAS_UAT (Master)
+        -------------------------------------------
+        UPDATE u
+        SET 
+            u.ConcurrencyStamp    = i.ConcurrencyStamp,
+            u.UserName            = i.UserName,
+            u.NormalizedUserName  = i.NormalizedUserName,
+            u.Email               = i.Email,
+            u.NormalizedEmail     = i.NormalizedEmail,
+            u.EmailConfirmed      = i.EmailConfirmed,
+            u.PhoneNumberConfirmed= i.PhoneNumberConfirmed,
+            u.TwoFactorEnabled    = i.TwoFactorEnabled,
+            u.LockoutEnabled      = i.LockoutEnabled,
+            u.AccessFailedCount   = i.AccessFailedCount,
+            u.EmployeeId          = i.EmployeeId,
+            u.FullName            = i.FullName,
+            u.IsEnabled           = i.IsEnabled,
+            u.MasterCompanyId     = i.MasterCompanyId,
+            u.SecurityStamp       = i.SecurityStamp,
+            u.PasswordHash        = i.PasswordHash,
+            u.CreatedDate         = i.CreatedDate,
+            u.UpdatedDate         = i.UpdatedDate
+        FROM MTI.dbo.AspNetUsers u
+        INNER JOIN inserted i ON u.Id = i.Id;
+
+    END TRY
+    BEGIN CATCH
+        DECLARE @msg NVARCHAR(MAX) = ERROR_MESSAGE();
+
+    END CATCH
+END

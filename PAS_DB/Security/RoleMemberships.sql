102255e41ec8348c910b1d80bae2b7744e68425e
Hemant <Hemant@silverxis.com>
2026-01-21 22:54:33 +0530

    Deletd roles

--- PAS_DB/Security/RoleMemberships.sql
+++ PAS_DB/Security/RoleMemberships.sql
@@ -14,30 +14,3 @@
 
 
 
-GO
-ALTER ROLE [db_securityadmin] ADD MEMBER [Donald];
-
-
-GO
-ALTER ROLE [db_owner] ADD MEMBER [Donald];
-
-
-GO
-ALTER ROLE [db_owner] ADD MEMBER [pasadmin];
-
-
-GO
-ALTER ROLE [db_datawriter] ADD MEMBER [Donald];
-
-
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [Donald];
-
-
-GO
-ALTER ROLE [db_backupoperator] ADD MEMBER [Donald];
-
-
-GO
-ALTER ROLE [db_accessadmin] ADD MEMBER [Donald];
-

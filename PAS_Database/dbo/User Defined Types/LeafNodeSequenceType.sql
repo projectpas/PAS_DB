33d0ab61116dbd5c4966bf37d5c5c9ae0317471f
saliyabhargav <bhargav.saliya@silverxis.com>
2026-01-22 11:19:29 +0530

    PN-15152

---
+++ PAS_Database/dbo/User Defined Types/LeafNodeSequenceType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TYPE [dbo].[LeafNodeSequenceType] AS TABLE (
+    [LeafNodeId]           BIGINT        NULL,
+    [Name]                 VARCHAR (256) NULL,
+    [PrintSequenceNumber]  INT           NULL,
+    [MasterCompanyId]      INT           NULL,
+    [ReportingStructureId] BIGINT        NULL);
+

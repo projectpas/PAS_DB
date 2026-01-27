298cbcc1ea2af4c74ca6394fd5ed47fd048e2e36
Aayushi Patel <ayushi@silverxis.com>
2026-01-27 18:03:42 +0530

    PN-15265

--- PAS_DB/dbo/Stored Procedures/Procs3/USP_SearchBulkStockData.sql
+++ PAS_DB/dbo/Stored Procedures/Procs3/USP_SearchBulkStockData.sql
@@ -14,9 +14,8 @@
     1    12/10/2023     AMIT GHEDIYA			Created
 	2    06/12/2023     AMIT GHEDIYA			Modify(Added Adjustment Type column)
 	3	 09/04/2025	    Ekta Chandegra	        Convert date using dbo.ConvertUTCtoLocal
-       
+    4    26/01/2026     Ayushi Patel            Enhancement: Added ViewType-based data handling (SUMMARY / DETAILS).   
 -- EXEC USP_SearchBulkStockData
-  
 ************************************************************************/  
 CREATE    PROCEDURE [dbo].[USP_SearchBulkStockData]
 	@PageNumber int = NULL,
@@ -33,7 +32,18 @@ CREATE    PROCEDURE [dbo].[USP_SearchBulkStockData]
 	@IsDeleted bit = NULL,
 	@MasterCompanyId bigint = NULL,
 	@AdjustmentType varchar(150) = NULL,
-	@EmployeeId bigint
+	@EmployeeId bigint,
+    @ViewType VARCHAR(20) = 'SUMMARY',
+    @PartNumber        VARCHAR(50) = NULL,
+    @PartDescription   VARCHAR(150) = NULL,
+    @Condition         VARCHAR(50) = NULL,
+    @StockLineNumber   VARCHAR(50) = NULL,
+    @ControlNumber     VARCHAR(50) = NULL,
+    @NewQty        VARCHAR(50) = NULL,
+    @UnitCost      VARCHAR(50) = NULL,
+    @LastMSLevel   VARCHAR(200) = NULL,
+    @AllMSLevels   VARCHAR(500) = NULL
+
 AS
 BEGIN  
  SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED  
@@ -81,6 +91,9 @@ BEGIN
     BEGIN   
      SET @StatusId = NULL  
     END   
+
+	IF UPPER(@ViewType) = 'SUMMARY'
+      BEGIN
 
 		;WITH Result AS(  
 		SELECT stadt.[Name] AS 'AdjustmentType',
@@ -137,7 +150,157 @@ BEGIN
      OFFSET @RecordFrom ROWS   
      FETCH NEXT @PageSize ROWS ONLY  
    END  
-  
+
+  ELSE
+      BEGIN
+	  ;WITH Result AS
+            (
+                SELECT
+                    stadt.[Name] AS AdjustmentType,
+                    bsadj.BulkStkLineAdjId,
+                    bsadj.BulkStkLineAdjNumber,
+                    CAST(DBO.ConvertUTCtoLocal(bsadj.CreatedDate, @CurrntEmpTimeZoneDesc) AS DATETIME) AS CreatedDate,
+
+                    IM.PartNumber,
+                    IM.PartDescription,
+
+                    BSAD.NewQty,
+                    BSAD.UnitCost,
+                    BSAD.LastMSLevel,
+                    BSAD.AllMSLevels,
+
+                    STL.[Condition],
+                    STL.StockLineNumber,
+                    STL.ControlNumber,
+                    bsadj.StatusId
+                FROM dbo.BulkStockLineAdjustment bsadj WITH (NOLOCK)
+                INNER JOIN dbo.StockLineAdjustmentType stadt
+                    ON bsadj.StockLineAdjustmentTypeId = stadt.StockLineAdjustmentTypeId
+                INNER JOIN dbo.BulkStockLineAdjustmentDetails BSAD WITH (NOLOCK)
+                    ON bsadj.BulkStkLineAdjId = BSAD.BulkStkLineAdjId
+                INNER JOIN dbo.StockLine STL WITH (NOLOCK)
+                    ON BSAD.StockLineId = STL.StockLineId
+                INNER JOIN dbo.ItemMaster IM WITH (NOLOCK)
+                    ON STL.ItemMasterId = IM.ItemMasterId
+                WHERE
+                    bsadj.MasterCompanyId = @MasterCompanyId
+                    AND ISNULL(bsadj.IsDeleted, 0) = 0
+                    AND (@StatusId IS NULL OR bsadj.StatusId = @StatusId )
+            ),
+            FinalResult AS
+            (
+                SELECT
+                    AdjustmentType,
+                    BulkStkLineAdjId,
+                    BulkStkLineAdjNumber,
+                    CreatedDate,
+                    PartNumber,
+                    PartDescription,
+                    NewQty,
+                    UnitCost,
+                    LastMSLevel,
+                    AllMSLevels,
+                    [Condition],
+                    StockLineNumber,
+                    ControlNumber,
+                    StatusId
+                FROM Result
+                WHERE
+                (
+                    @GlobalFilter <> '' AND
+                    (
+                        LOWER(BulkStkLineAdjNumber) LIKE '%' + LOWER(@GlobalFilter) + '%'
+                        OR LOWER(PartNumber) LIKE '%' + LOWER(@GlobalFilter) + '%'
+                        OR LOWER(PartDescription) LIKE '%' + LOWER(@GlobalFilter) + '%'
+                        OR LOWER(StockLineNumber) LIKE '%' + LOWER(@GlobalFilter) + '%'
+                        OR LOWER(ControlNumber) LIKE '%' + LOWER(@GlobalFilter) + '%'
+                        OR LOWER(AdjustmentType) LIKE '%' + LOWER(@GlobalFilter) + '%'
+                        OR LOWER(LastMSLevel) LIKE '%' + LOWER(@GlobalFilter) + '%'
+                        OR LOWER(AllMSLevels) LIKE '%' + LOWER(@GlobalFilter) + '%'
+                        OR CAST(NewQty AS VARCHAR(50)) LIKE '%' + @GlobalFilter + '%'
+                        OR CAST(UnitCost AS VARCHAR(50)) LIKE '%' + @GlobalFilter + '%'
+                    )
+                )
+                OR
+                (
+                    @GlobalFilter = '' AND
+                    (ISNULL(@BulkStkLineAdjNumber, '') = '' OR BulkStkLineAdjNumber LIKE '%' + @BulkStkLineAdjNumber + '%')
+                    AND (ISNULL(@AdjustmentType, '') = '' OR AdjustmentType LIKE '%' + @AdjustmentType + '%')
+                    AND (ISNULL(@PartNumber, '') = '' OR PartNumber LIKE '%' + @PartNumber + '%')
+                    AND (ISNULL(@PartDescription, '') = '' OR PartDescription LIKE '%' + @PartDescription + '%')
+                    AND (ISNULL(@Condition, '') = '' OR [Condition] LIKE '%' + @Condition + '%')
+                    AND (ISNULL(@StockLineNumber, '') = '' OR StockLineNumber LIKE '%' + @StockLineNumber + '%')
+                    AND (ISNULL(@ControlNumber, '') = '' OR ControlNumber LIKE '%' + @ControlNumber + '%')
+                    AND (ISNULL(@LastMSLevel, '') = '' OR LastMSLevel LIKE '%' + @LastMSLevel + '%')
+                    AND (ISNULL(@AllMSLevels, '') = '' OR AllMSLevels LIKE '%' + @AllMSLevels + '%')
+                    AND (ISNULL(@NewQty, '') = '' OR CAST(NewQty AS VARCHAR(50)) LIKE '%' + @NewQty + '%')
+                    AND (ISNULL(@UnitCost, '') = '' OR CAST(UnitCost AS VARCHAR(50)) LIKE '%' + @UnitCost + '%')
+                )
+            ),
+            ResultCount AS
+            (
+                SELECT COUNT(BulkStkLineAdjId) AS NumberOfItems FROM FinalResult
+            )
+            SELECT
+                AdjustmentType,
+                BulkStkLineAdjId,
+                BulkStkLineAdjNumber,
+                CreatedDate,
+                PartNumber,
+                PartDescription,
+                NewQty,
+                UnitCost,
+                LastMSLevel,
+                AllMSLevels,
+                [Condition],
+                StockLineNumber,
+                ControlNumber,
+                StatusId,
+                NumberOfItems
+            FROM FinalResult, ResultCount
+            ORDER BY
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'ADJUSTMENTTYPE') THEN AdjustmentType END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'ADJUSTMENTTYPE') THEN AdjustmentType END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'BULKSTKLINEADJNUMBER') THEN BulkStkLineAdjNumber END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'BULKSTKLINEADJNUMBER') THEN BulkStkLineAdjNumber END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'CREATEDDATE') THEN CreatedDate END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'CREATEDDATE') THEN CreatedDate END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'PARTNUMBER') THEN PartNumber END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'PARTNUMBER') THEN PartNumber END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'PARTDESCRIPTION') THEN PartDescription END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'PARTDESCRIPTION') THEN PartDescription END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'CONDITION') THEN [Condition] END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'CONDITION') THEN [Condition] END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'STOCKLINENUMBER') THEN StockLineNumber END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'STOCKLINENUMBER') THEN StockLineNumber END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'CONTROLNUMBER') THEN ControlNumber END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'CONTROLNUMBER') THEN ControlNumber END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'NEWQTY') THEN NewQty END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'NEWQTY') THEN NewQty END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'UNITCOST') THEN UnitCost END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'UNITCOST') THEN UnitCost END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'LASTMSLEVEL') THEN LastMSLevel END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'LASTMSLEVEL') THEN LastMSLevel END DESC,
+
+                CASE WHEN (@SortOrder = 1 AND @SortColumn = 'ALLMSLEVELS') THEN AllMSLevels END ASC,
+                CASE WHEN (@SortOrder = -1 AND @SortColumn = 'ALLMSLEVELS') THEN AllMSLevels END DESC
+
+
+            OFFSET @RecordFrom ROWS
+            FETCH NEXT @PageSize ROWS ONLY;
+
+        END
+        END
   END TRY      
   BEGIN CATCH        
    IF @@trancount > 0  

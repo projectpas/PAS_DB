c36891446e9ab2958b404f48be39d7ebd9cf58b1
Aayushi Patel <ayushi@silverxis.com>
2026-01-29 14:10:12 +0530

    added new table WOReleaseForm , also updated Get SP

---
+++ PAS_DB/dbo/Tables/WOReleaseForm.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[WOReleaseForm] (
+    [WOReleaseFormId] BIGINT        IDENTITY (1, 1) NOT NULL,
+    [FormName]        VARCHAR (200) NOT NULL,
+    [Description]     VARCHAR (500) NULL,
+    [IsActive]        BIT           DEFAULT ((1)) NOT NULL,
+    [MasterCompanyId] BIGINT        NOT NULL,
+    [CreatedBy]       VARCHAR (256) NULL,
+    [CreatedDate]     DATETIME2 (7) DEFAULT (sysdatetime()) NOT NULL,
+    [UpdatedBy]       VARCHAR (256) NULL,
+    [UpdatedDate]     DATETIME2 (7) NULL,
+    [IsDeleted]       BIT           NULL,
+    CONSTRAINT [PK_WOReleaseForm] PRIMARY KEY CLUSTERED ([WOReleaseFormId] ASC)
+);
+

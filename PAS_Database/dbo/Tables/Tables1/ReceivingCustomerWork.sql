b2b14635ffa87ff691b33e93611ef14890407edd
Divyesh <Divyesh@Silverxis.com>
2026-02-12 20:51:05 +0530

    PN-15430

--- PAS_Database/dbo/Tables/Tables1/ReceivingCustomerWork.sql
+++ PAS_Database/dbo/Tables/Tables1/ReceivingCustomerWork.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[ReceivingCustomerWork] (
+CREATE TABLE [dbo].[ReceivingCustomerWork] (
     [ReceivingCustomerWorkId] BIGINT          IDENTITY (1, 1) NOT NULL,
     [EmployeeId]              BIGINT          NOT NULL,
     [CustomerId]              BIGINT          NOT NULL,
@@ -135,29 +135,129 @@
 
 
 
+
+
 GO
-
-
-----------------------------------------------
-
-CREATE TRIGGER [dbo].[Trg_ReceivingCustomerWorkAudit]
-
-   ON  [dbo].[ReceivingCustomerWork]
-
-   AFTER INSERT,UPDATE
-
-AS 
-
+CREATE   TRIGGER [dbo].[trg_Audit_dbo_ReceivingCustomerWork]
+ON [dbo].[ReceivingCustomerWork]
+AFTER INSERT, UPDATE, DELETE
+AS
 BEGIN
+    SET NOCOUNT ON;
+    ;WITH
+    d AS (SELECT d.[ReceivingCustomerWorkId],d.[EmployeeId],d.[CustomerId],d.[ReceivingNumber],d.[CustomerContactId],d.[ItemMasterId],d.[RevisePartId],d.[IsSerialized],d.[SerialNumber],d.[Quantity],d.[ConditionId],d.[SiteId],d.[WarehouseId],d.[LocationId],d.[Shelfid],d.[BinId],d.[OwnerTypeId],d.[Owner],d.[IsCustomerStock],d.[TraceableToTypeId],d.[TraceableTo],d.[ObtainFromTypeId],d.[ObtainFrom],d.[IsMFGDate],d.[MFGDate],d.[MFGTrace],d.[MFGLotNo],d.[IsExpDate],d.[ExpDate],d.[IsTimeLife],d.[TagDate],d.[TagType],d.[TagTypeIds],d.[TimeLifeDate],d.[TimeLifeOrigin],d.[TimeLifeCyclesId],d.[Memo],d.[PartCertificationNumber],d.[ManagementStructureId],d.[StockLineId],d.[WorkOrderId],d.[MasterCompanyId],d.[CreatedBy],d.[UpdatedBy],d.[CreatedDate],d.[UpdatedDate],d.[IsActive],d.[IsDeleted],d.[IsSkipSerialNo],d.[IsSkipTimeLife],d.[Reference],d.[CertifiedBy],d.[ReceivedDate],d.[CustReqDate],d.[Level1],d.[Level2],d.[Level3],d.[Level4],d.[EmployeeName],d.[CustomerName],d.[WorkScopeId],d.[CustomerCode],d.[ManufacturerName],d.[InspectedById],d.[CertifiedDate],d.[ObtainFromName],d.[OwnerName],d.[TraceableToName],d.[PartNumber],d.[WorkScope],d.[Condition],d.[Site],d.[Warehouse],d.[Location],d.[Shelf],d.[Bin],d.[InspectedBy],d.[InspectedDate],d.[TaggedById],d.[TaggedBy],d.[ACTailNum],d.[TaggedByType],d.[TaggedByTypeName],d.[CertifiedById],d.[CertifiedTypeId],d.[CertifiedType],d.[CertTypeId],d.[CertType],d.[RemovalReasonId],d.[RemovalReasons],d.[RemovalReasonsMemo],d.[ExchangeSalesOrderId],d.[CustReqTagTypeId],d.[CustReqTagType],d.[CustReqCertTypeId],d.[CustReqCertType],d.[RepairOrderPartRecordId],d.[IsExchangeBatchEntry],d.[IsPiecePart],d.[IsRepairManagement],d.[IsSkipShippingReference],d.[CSN],d.[TSN],d.[CSO],d.[TSO] FROM deleted d),
+    i AS (SELECT i.[ReceivingCustomerWorkId],i.[EmployeeId],i.[CustomerId],i.[ReceivingNumber],i.[CustomerContactId],i.[ItemMasterId],i.[RevisePartId],i.[IsSerialized],i.[SerialNumber],i.[Quantity],i.[ConditionId],i.[SiteId],i.[WarehouseId],i.[LocationId],i.[Shelfid],i.[BinId],i.[OwnerTypeId],i.[Owner],i.[IsCustomerStock],i.[TraceableToTypeId],i.[TraceableTo],i.[ObtainFromTypeId],i.[ObtainFrom],i.[IsMFGDate],i.[MFGDate],i.[MFGTrace],i.[MFGLotNo],i.[IsExpDate],i.[ExpDate],i.[IsTimeLife],i.[TagDate],i.[TagType],i.[TagTypeIds],i.[TimeLifeDate],i.[TimeLifeOrigin],i.[TimeLifeCyclesId],i.[Memo],i.[PartCertificationNumber],i.[ManagementStructureId],i.[StockLineId],i.[WorkOrderId],i.[MasterCompanyId],i.[CreatedBy],i.[UpdatedBy],i.[CreatedDate],i.[UpdatedDate],i.[IsActive],i.[IsDeleted],i.[IsSkipSerialNo],i.[IsSkipTimeLife],i.[Reference],i.[CertifiedBy],i.[ReceivedDate],i.[CustReqDate],i.[Level1],i.[Level2],i.[Level3],i.[Level4],i.[EmployeeName],i.[CustomerName],i.[WorkScopeId],i.[CustomerCode],i.[ManufacturerName],i.[InspectedById],i.[CertifiedDate],i.[ObtainFromName],i.[OwnerName],i.[TraceableToName],i.[PartNumber],i.[WorkScope],i.[Condition],i.[Site],i.[Warehouse],i.[Location],i.[Shelf],i.[Bin],i.[InspectedBy],i.[InspectedDate],i.[TaggedById],i.[TaggedBy],i.[ACTailNum],i.[TaggedByType],i.[TaggedByTypeName],i.[CertifiedById],i.[CertifiedTypeId],i.[CertifiedType],i.[CertTypeId],i.[CertType],i.[RemovalReasonId],i.[RemovalReasons],i.[RemovalReasonsMemo],i.[ExchangeSalesOrderId],i.[CustReqTagTypeId],i.[CustReqTagType],i.[CustReqCertTypeId],i.[CustReqCertType],i.[RepairOrderPartRecordId],i.[IsExchangeBatchEntry],i.[IsPiecePart],i.[IsRepairManagement],i.[IsSkipShippingReference],i.[CSN],i.[TSN],i.[CSO],i.[TSO] FROM inserted i),
+    paired AS (
+        SELECT
+            COALESCE(i.ReceivingCustomerWorkId, d.ReceivingCustomerWorkId ) AS ReceivingCustomerWorkId,
+            (SELECT d.* FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS old_row_json,
+            (SELECT i.* FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS new_row_json, 
+            CASE
+                WHEN i.ReceivingCustomerWorkId IS NOT NULL AND d.ReceivingCustomerWorkId IS NOT NULL THEN 'U'
+                WHEN i.ReceivingCustomerWorkId IS NOT NULL AND d.ReceivingCustomerWorkId IS NULL     THEN 'I'
+                WHEN i.ReceivingCustomerWorkId IS NULL     AND d.ReceivingCustomerWorkId IS NOT NULL THEN 'D'
+            END AS Action,
 
-	INSERT INTO [dbo].[ReceivingCustomerWorkAudit] 
+            (SELECT COALESCE(i.ReceivingCustomerWorkId, d.ReceivingCustomerWorkId) AS ReceivingCustomerWorkId
+                FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS PKJson
+        FROM d
+        FULL OUTER JOIN i
+            ON i.ReceivingCustomerWorkId = d.ReceivingCustomerWorkId
+    ),
 
-    SELECT *
-
-	FROM INSERTED 
-
-	SET NOCOUNT ON;
-
-
-
-END
+    oldv AS (
+        SELECT
+            p.PKJson,
+            p.ReceivingCustomerWorkId,
+            v.[key]  AS ColumnName,
+            v.value  AS OldValue
+        FROM paired p
+        CROSS APPLY OPENJSON(p.old_row_json) v
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM dbo.IgnoreColumn ign
+            WHERE ign.SchemaName = N'dbo'
+                AND ign.TableName  = N'ReceivingCustomerWork'
+                AND ign.ColumnName = N'ReceivingCustomerWorkId'
+        )),
+    newv AS (
+        SELECT
+            p.PKJson,
+            p.ReceivingCustomerWorkId ,
+            v.[key]  AS ColumnName,
+            v.value  AS NewValue
+        FROM paired p
+        CROSS APPLY OPENJSON(p.new_row_json) v
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM dbo.IgnoreColumn ign
+            WHERE ign.SchemaName = N'dbo'
+                AND ign.TableName  = N'ReceivingCustomerWork'
+                AND ign.ColumnName = N'ReceivingCustomerWorkId'
+        )),
+    merged AS (
+        SELECT
+            COALESCE(n.PKJson, o.PKJson)                AS PKJson,
+            COALESCE(n.ColumnName, o.ColumnName)        AS ColumnName,
+            o.OldValue,
+            n.NewValue,
+            p.Action
+        FROM paired p
+        LEFT JOIN oldv o
+            ON o.ReceivingCustomerWorkId = p.ReceivingCustomerWorkId
+        LEFT JOIN newv n
+            ON n.ReceivingCustomerWorkId = p.ReceivingCustomerWorkId
+            AND n.ColumnName = o.ColumnName
+        UNION ALL
+        SELECT
+            n.PKJson,
+            n.ColumnName,
+            NULL AS OldValue,
+            n.NewValue,
+            p.Action
+        FROM paired p
+        LEFT JOIN newv n
+            ON n.ReceivingCustomerWorkId = p.ReceivingCustomerWorkId
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM oldv o2
+            WHERE o2.ReceivingCustomerWorkId = p.ReceivingCustomerWorkId
+                AND o2.ColumnName    = n.ColumnName
+        )
+    )
+    INSERT dbo.AuditLog (SchemaName, TableName, PKJson, ColumnName, Action, OldValue, NewValue)
+    SELECT
+        N'dbo' AS SchemaName,
+        N'ReceivingCustomerWork' AS TableName,
+        m.PKJson,
+        m.ColumnName,
+        m.Action,        
+        CASE             
+            WHEN m.ColumnName = 'RevisePartId' THEN rpOld.PartNumber
+            WHEN m.ColumnName = 'CustomerContactId' THEN LTRIM(RTRIM(ISNULL(cOld.FirstName,'') + ' ' + ISNULL(cOld.LastName,'')))            
+            ELSE m.OldValue
+        END AS OldValue,        
+        CASE            
+            WHEN m.ColumnName = 'RevisePartId' THEN rpNew.PartNumber
+            WHEN m.ColumnName = 'CustomerContactId' THEN LTRIM(RTRIM(ISNULL(cNew.FirstName,'') + ' ' + ISNULL(cNew.LastName,'')))           
+            ELSE m.NewValue
+        END AS NewValue
+    FROM merged m    
+    LEFT JOIN dbo.ItemMaster rpOld ON m.ColumnName = 'RevisePartId' AND TRY_CAST(m.OldValue AS BIGINT) = rpOld.ItemMasterId 
+    LEFT JOIN dbo.ItemMaster rpNew ON m.ColumnName = 'RevisePartId' AND TRY_CAST(m.NewValue AS BIGINT) = rpNew.ItemMasterId
+    LEFT JOIN dbo.CustomerContact ccOld ON m.ColumnName = 'CustomerContactId' AND TRY_CAST(m.OldValue AS INT) = ccOld.CustomerContactId
+    LEFT JOIN dbo.Contact cOld ON ccOld.ContactId = cOld.ContactId
+    LEFT JOIN dbo.CustomerContact ccNew ON m.ColumnName = 'CustomerContactId' AND TRY_CAST(m.NewValue AS INT) = ccNew.CustomerContactId
+    LEFT JOIN dbo.Contact cNew ON ccNew.ContactId = cNew.ContactId
+    WHERE 
+        m.ColumnName <> 'ReceivingCustomerWorkId' and (
+        (m.Action = 'U' AND (
+               (m.OldValue IS NULL AND m.NewValue IS NOT NULL)
+            OR (m.OldValue IS NOT NULL AND m.NewValue IS NULL)
+            OR (m.OldValue <> m.NewValue)
+        ))
+        OR
+        (m.Action = 'I' AND m.NewValue IS NOT NULL)
+        OR
+        (m.Action = 'D' AND m.OldValue IS NOT NULL));
+END;

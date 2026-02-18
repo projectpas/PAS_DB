ce7ae6d710b277cb65a0cbb06eed5db42188d6d6
Nakul <nakul@silverxis.com>
2026-02-18 16:35:33 +0530

    PN-15182-BETA

---
+++ PAS_DB/dbo/Tables/WIPCategory.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[WIPCategory] (
+    [WIPCategoryId]   BIGINT        IDENTITY (1, 1) NOT NULL,
+    [WIPCategory]     VARCHAR (256) NULL,
+    [CreatedBy]       VARCHAR (256) NULL,
+    [CreatedDate]     DATETIME2 (7) NULL,
+    [UpdatedBy]       VARCHAR (256) NULL,
+    [UpdatedDate]     DATETIME2 (7) NULL,
+    [IsActive]        BIT           NULL,
+    [IsDeleted]       BIT           NULL,
+    [MasterCompanyId] INT           NULL,
+    PRIMARY KEY CLUSTERED ([WIPCategoryId] ASC)
+);
+

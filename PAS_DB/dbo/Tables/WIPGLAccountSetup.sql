b8bf248926773dcebffcdeb52732cec088c971e0
Nakul <nakul@silverxis.com>
2026-02-18 16:45:52 +0530

    PN-15182(BETA)

---
+++ PAS_DB/dbo/Tables/WIPGLAccountSetup.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[WIPGLAccountSetup] (
+    [WIPGLAccountId]  BIGINT        IDENTITY (1, 1) NOT NULL,
+    [WIPCategoryId]   BIGINT        NULL,
+    [GLAccountId]     BIGINT        NULL,
+    [CreatedBy]       VARCHAR (256) NULL,
+    [CreatedDate]     DATETIME2 (7) NULL,
+    [UpdatedBy]       VARCHAR (256) NULL,
+    [UpdatedDate]     DATETIME2 (7) NULL,
+    [IsActive]        BIT           NULL,
+    [IsDeleted]       BIT           NULL,
+    [MasterCompanyId] INT           NULL,
+    PRIMARY KEY CLUSTERED ([WIPGLAccountId] ASC)
+);
+
+
+GO
+/****** Object:  Trigger [dbo].[Trg_WIPGLAccountSetupAudit]]   Script Date: 27-01-2026 ******/
+CREATE   TRIGGER [dbo].[Trg_WIPGLAccountSetupAudit] ON [dbo].[WIPGLAccountSetup]
+
+   AFTER INSERT,UPDATE  
+
+AS   
+
+BEGIN  
+
+ INSERT INTO [dbo].[WIPGLAccountSetupAudit]  
+ SELECT * FROM INSERTED  
+ SET NOCOUNT ON;  
+
+END
---
+++ PAS_DB/dbo/Tables/WIPGLAccountSetupAudit.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[WIPGLAccountSetupAudit] (
+    [WIPGLAccountSetupAuditId] BIGINT        IDENTITY (1, 1) NOT NULL,
+    [WIPGLAccountId]           BIGINT        NULL,
+    [WIPCategoryId]            BIGINT        NULL,
+    [GLAccountId]              BIGINT        NULL,
+    [CreatedBy]                VARCHAR (256) NULL,
+    [CreatedDate]              DATETIME2 (7) NULL,
+    [UpdatedBy]                VARCHAR (256) NULL,
+    [UpdatedDate]              DATETIME2 (7) NULL,
+    [IsActive]                 BIT           NULL,
+    [IsDeleted]                BIT           NULL,
+    [MasterCompanyId]          INT           NULL,
+    PRIMARY KEY CLUSTERED ([WIPGLAccountSetupAuditId] ASC)
+);
+

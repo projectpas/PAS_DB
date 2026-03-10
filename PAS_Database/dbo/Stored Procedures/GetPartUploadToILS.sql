cb6c17a04a8498b98b999d00b0296052e55119c7
Vishal Suthar <vishal@silverxis.com>
2026-03-11 03:08:41 +0530

    PN-11778 PartsBase Integration Changes

--- PAS_Database/dbo/Stored Procedures/GetPartUploadToILS.sql
+++ PAS_Database/dbo/Stored Procedures/GetPartUploadToILS.sql
@@ -10,92 +10,176 @@
  **************************************************************           
   ** Change History           
  **************************************************************           
- ** PR   Date         Author		   Change Description            
- ** --   --------     -------		   -------------------------------          
-    1    25/06/2025   Amit Ghediya     Created
+ ** PR   Date         Author			Change Description            
+ ** --   --------     -------			-------------------------------          
+    1    25/06/2025   Amit Ghediya		Created
+    2    10/03/2026   Vishal Suthar     Modified to include both ILS and PartsBase
     
- EXEC GetPartUploadToILS 20
+ EXEC GetPartUploadToILS 1, 'PARTSBASE'
 
 **************************************************************/ 
     
 CREATE   PROCEDURE [dbo].[GetPartUploadToILS]       
-	@MasterCompanyId BIGINT
+	@MasterCompanyId BIGINT,
+	@Portal VARCHAR(50)
 AS    
 BEGIN
 	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED	
 	SET NOCOUNT ON  
 	BEGIN TRY
-
-			DECLARE @Condition_Code VARCHAR(100) = 'AR,NE,NS,OH,SVC',
-					@NHA_MappingType INT = 1,
-					@SiteId BIGINT = 0,
-					@WarehouseId BIGINT = 0,
-					@LocationId BIGINT = 0,
-					@ShelfId BIGINT = 0,
-					@BinId BIGINT = 0;
-
-			SELECT @SiteId = [SiteId],@WarehouseId = [WarehouseId],@LocationId = [LocationId],@ShelfId = [ShelfId],@BinId = [BinId] FROM [dbo].[StocklineSettings] WITH(NOLOCK) WHERE [MasterCompanyId] = @MasterCompanyId;
-
-			IF(@SiteId = 0)
+			
+			IF (@Portal = 'ILS')
 			BEGIN
-				SET @SiteId = NULL;
-			END
+				DECLARE @Condition_Code VARCHAR(100) = 'AR,NE,NS,OH,SVC',
+						@NHA_MappingType INT = 1,
+						@SiteId BIGINT = 0,
+						@WarehouseId BIGINT = 0,
+						@LocationId BIGINT = 0,
+						@ShelfId BIGINT = 0,
+						@BinId BIGINT = 0;
 
-			IF(@WarehouseId = 0)
+				SELECT @SiteId = [SiteId],@WarehouseId = [WarehouseId],@LocationId = [LocationId],@ShelfId = [ShelfId],@BinId = [BinId] FROM [dbo].[StocklineSettings] WITH(NOLOCK) WHERE [MasterCompanyId] = @MasterCompanyId;
+
+				IF(@SiteId = 0)
+				BEGIN
+					SET @SiteId = NULL;
+				END
+
+				IF(@WarehouseId = 0)
+				BEGIN
+					SET @WarehouseId = NULL;
+				END
+
+				IF(@LocationId = 0)
+				BEGIN
+					SET @LocationId = NULL;
+				END
+
+				IF(@ShelfId = 0)
+				BEGIN
+					SET @ShelfId = NULL;
+				END
+
+				IF(@BinId = 0)
+				BEGIN
+					SET @BinId = NULL;
+				END
+
+				SELECT TOP 2
+					1 AS ITEMNUMBER,
+					'' AS 'COMPANYID',
+					IM.Partnumber AS 'PARTNUMBER',
+					IM1.partnumber AS 'ALTERNATEPARTNUMBER',
+					STK.Condition AS CONDITIONCD,
+					'Y' AS ILSLISTCD,
+					0 AS PARTVALUE,
+					IM.PartDescription AS 'DESCRIPTION',
+					--IM.PartAlternatePartId,
+					'' AS CAGE,
+					STK.Quantity AS QUANTITY,
+					STK.UnitOfMeasure AS UNITOFMEASURE,
+					STK.UnitCost AS PRICE,
+					'' AS PARTCATEGORY,
+					'' AS 'CONTROL',
+					'' AS EXCHANGESEL
+					--STK.Condition,
+					--STK.ConditionId
+				FROM [dbo].[Stockline] STK WITH(NOLOCK)
+				INNER JOIN [dbo].[ItemMaster] IM WITH(NOLOCK) ON IM.ItemMasterId = STK.ItemMasterId
+				LEFT JOIN [dbo].[Nha_Tla_Alt_Equ_ItemMapping] TLA WITH(NOLOCK) ON STK.ItemMasterId = TLA.ItemMasterId AND TLA.MappingType = @NHA_MappingType
+				LEFT JOIN [dbo].[ItemMaster] IM1 WITH(NOLOCK) ON IM1.ItemMasterId = TLA.MappingItemMasterId
+				INNER JOIN [dbo].[Condition] CON WITH(NOLOCK) ON CON.ConditionId = STK.ConditionId
+				WHERE STK.MasterCompanyId = @MasterCompanyId 
+				AND @SiteId IS NULL OR STK.SiteId = @SiteId
+				AND @WarehouseId IS NULL OR STK.WarehouseId = @WarehouseId
+				AND @LocationId IS NULL OR STK.LocationId = @LocationId
+				AND @ShelfId IS NULL OR STK.ShelfId = @ShelfId
+				AND @BinId IS NULL OR STK.BinId = @BinId
+				AND STK.isActive = 1
+				AND STK.isDeleted = 0
+				AND STk.MasterCompanyId = @MasterCompanyId
+				AND CON.[Description] IN(SELECT item FROM SplitString(@Condition_Code,','))
+				ORDER BY STK.CreatedDate DESC
+			END
+			ELSE IF (@Portal = 'PARTSBASE')
 			BEGIN
-				SET @WarehouseId = NULL;
-			END
+				DECLARE @Condition_Code_PartsBase VARCHAR(100) = 'AR,NE,NS,OH,SVC',
+						@NHA_MappingType_PartsBase INT = 1,
+						@SiteId_PartsBase BIGINT = 0,
+						@WarehouseId_PartsBase BIGINT = 0,
+						@LocationId_PartsBase BIGINT = 0,
+						@ShelfId_PartsBase BIGINT = 0,
+						@BinId_PartsBase BIGINT = 0;
 
-			IF(@LocationId = 0)
-			BEGIN
-				SET @LocationId = NULL;
-			END
+				SELECT @SiteId_PartsBase = [PartsBaseSiteId],@WarehouseId_PartsBase = [PartsBaseWarehouseId],@LocationId_PartsBase = [PartsBaseLocationId],@ShelfId_PartsBase = [PartsBaseShelfId],@BinId_PartsBase = [PartsBaseBinId] FROM [dbo].[StocklineSettings] WITH(NOLOCK) WHERE [MasterCompanyId] = @MasterCompanyId;
 
-			IF(@ShelfId = 0)
-			BEGIN
-				SET @ShelfId = NULL;
-			END
+				IF(@SiteId_PartsBase = 0)
+				BEGIN
+					SET @SiteId_PartsBase = NULL;
+				END
 
-			IF(@BinId = 0)
-			BEGIN
-				SET @BinId = NULL;
-			END
+				IF(@WarehouseId_PartsBase = 0)
+				BEGIN
+					SET @WarehouseId_PartsBase = NULL;
+				END
 
-			SELECT TOP 2
-				1 AS ITEMNUMBER,
-				'' AS 'COMPANYID',
-				IM.Partnumber AS 'PARTNUMBER',
-				IM1.partnumber AS 'ALTERNATEPARTNUMBER',
-				STK.Condition AS CONDITIONCD,
-				'Y' AS ILSLISTCD,
-				0 AS PARTVALUE,
-				IM.PartDescription AS 'DESCRIPTION',
-				--IM.PartAlternatePartId,
-				'' AS CAGE,
-				STK.Quantity AS QUANTITY,
-				STK.UnitOfMeasure AS UNITOFMEASURE,
-				STK.UnitCost AS PRICE,
-				'' AS PARTCATEGORY,
-				'' AS 'CONTROL',
-				'' AS EXCHANGESEL
-				--STK.Condition,
-				--STK.ConditionId
-			FROM [dbo].[Stockline] STK WITH(NOLOCK)
-			INNER JOIN [dbo].[ItemMaster] IM WITH(NOLOCK) ON IM.ItemMasterId = STK.ItemMasterId
-			LEFT JOIN [dbo].[Nha_Tla_Alt_Equ_ItemMapping] TLA WITH(NOLOCK) ON STK.ItemMasterId = TLA.ItemMasterId AND TLA.MappingType = @NHA_MappingType
-			LEFT JOIN [dbo].[ItemMaster] IM1 WITH(NOLOCK) ON IM1.ItemMasterId = TLA.MappingItemMasterId
-			INNER JOIN [dbo].[Condition] CON WITH(NOLOCK) ON CON.ConditionId = STK.ConditionId
-			WHERE STK.MasterCompanyId = @MasterCompanyId 
-			AND @SiteId IS NULL OR STK.SiteId = @SiteId
-			AND @WarehouseId IS NULL OR STK.WarehouseId = @WarehouseId
-			AND @LocationId IS NULL OR STK.LocationId = @LocationId
-			AND @ShelfId IS NULL OR STK.ShelfId = @ShelfId
-			AND @BinId IS NULL OR STK.BinId = @BinId
-			AND STK.isActive = 1
-			AND STK.isDeleted = 0
-			AND STk.MasterCompanyId = @MasterCompanyId
-			AND CON.[Description] IN(SELECT item FROM SplitString(@Condition_Code,','))
-			ORDER BY STK.CreatedDate DESC
+				IF(@LocationId_PartsBase = 0)
+				BEGIN
+					SET @LocationId_PartsBase = NULL;
+				END
+
+				IF(@ShelfId_PartsBase = 0)
+				BEGIN
+					SET @ShelfId_PartsBase = NULL;
+				END
+
+				IF(@BinId_PartsBase = 0)
+				BEGIN
+					SET @BinId_PartsBase = NULL;
+				END
+
+				SELECT TOP 2
+					1 AS ITEMNUMBER,
+					'' AS 'COMPANYID',
+					IM.Partnumber AS 'PartNumber',
+					IM1.partnumber AS 'AlternatePartNumber',
+					STK.Condition AS Condition,
+					'Y' AS ILSLISTCD,
+					0 AS PARTVALUE,
+					IM.PartDescription AS 'PartDescription',
+					'' AS CAGE,
+					STK.Quantity AS Quantity,
+					STK.UnitOfMeasure AS UOM,
+					STK.UnitCost AS Price,
+					'' AS PARTCATEGORY,
+					'' AS 'CONTROL',
+					'' AS EXCHANGESEL,
+					STK.Manufacturer AS Manufacturer,
+					STK.Location AS Location,
+					'' AS EngineModel,
+					STK.SerialNumber AS SerialNumber,
+					STK.PartCertificationNumber AS Certification,
+					STK.TraceableTo AS Trace,
+					'' AS Images,
+					'' AS Documents,
+					'' AS Capabilities
+				FROM [dbo].[Stockline] STK WITH(NOLOCK)
+				INNER JOIN [dbo].[ItemMaster] IM WITH(NOLOCK) ON IM.ItemMasterId = STK.ItemMasterId
+				LEFT JOIN [dbo].[Nha_Tla_Alt_Equ_ItemMapping] TLA WITH(NOLOCK) ON STK.ItemMasterId = TLA.ItemMasterId AND TLA.MappingType = @NHA_MappingType_PartsBase
+				LEFT JOIN [dbo].[ItemMaster] IM1 WITH(NOLOCK) ON IM1.ItemMasterId = TLA.MappingItemMasterId
+				INNER JOIN [dbo].[Condition] CON WITH(NOLOCK) ON CON.ConditionId = STK.ConditionId
+				WHERE STK.MasterCompanyId = @MasterCompanyId 
+				AND @SiteId_PartsBase IS NULL OR STK.SiteId = @SiteId_PartsBase
+				AND @WarehouseId_PartsBase IS NULL OR STK.WarehouseId = @WarehouseId_PartsBase
+				AND @LocationId_PartsBase IS NULL OR STK.LocationId = @LocationId_PartsBase
+				AND @ShelfId_PartsBase IS NULL OR STK.ShelfId = @ShelfId_PartsBase
+				AND @BinId_PartsBase IS NULL OR STK.BinId = @BinId_PartsBase
+				AND STK.isActive = 1
+				AND STK.isDeleted = 0
+				AND STk.MasterCompanyId = @MasterCompanyId
+				AND CON.[Description] IN(SELECT item FROM SplitString(@Condition_Code_PartsBase,','))
+				ORDER BY STK.CreatedDate DESC
+			END
 	END TRY    
 	BEGIN CATCH      
 			IF @@trancount > 0				

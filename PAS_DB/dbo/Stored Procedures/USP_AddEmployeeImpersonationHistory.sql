d839bbf942f42d1c08433cd1c6674c11002289e1
saliyabhargav <bhargav.saliya@silverxis.com>
2026-03-13 17:49:03 +0530

    PN-15456 For Dev

---
+++ PAS_DB/dbo/Stored Procedures/USP_AddEmployeeImpersonationHistory.sql
@@ -0,0 +1,113 @@
+﻿/*************************************************************             
+ ** File:   [USP_AddEmployeeImpersonationHistory]           
+ ** Author:   Bhargav Saliya 
+ ** Description: This stored procedure is used to add a record when we perform impersonation.
+ ** Jira Id: PN-15456
+ ** Purpose:           
+ ** Date:   
+            
+ ** PARAMETERS:             
+ @UserType varchar(60)     
+           
+ ** RETURN VALUE:             
+    
+ **************************************************************             
+  ** Change History             
+ **************************************************************             
+ ** PR   Date         Author              Change Description              
+ ** --   --------     -------          --------------------------------     
+    1    26/02/2026   Bhargav Saliya       PN-15456: Created
+    2    12/03/2026   Bhargav Saliya       PN-15747: Modified
+**************************************************************/
+CREATE     PROCEDURE [dbo].[USP_AddEmployeeImpersonationHistory]  
+@ImpersonatedEmployeeId BIGINT,  
+@MasterCompanyId INT,
+@ImpersonatedByEmployeeId BIGINT,
+@CurrentUserMasterCompanyId INT
+AS  
+BEGIN  
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED  
+	SET NOCOUNT ON;  
+	BEGIN TRY  
+	DECLARE @ImpersonatedBy VARCHAR(256) ;
+	SET @ImpersonatedBy = (SELECT ISNULL(FirstName,'') + ' ' + ISNULL(LastName,'') as [Name] FROM dbo.[Employee] WITH(NOLOCK) WHERE EmployeeId = @ImpersonatedByEmployeeId and MasterCompanyId =  @CurrentUserMasterCompanyId);
+
+	DECLARE @Impersonated VARCHAR(256);
+
+	IF(@MasterCompanyId > 0)
+	BEGIN
+		DECLARE @ConnectionString NVARCHAR(MAX) = NULL;
+		DECLARE @TargetDBName SYSNAME = NULL;
+
+		IF @MasterCompanyId IS NOT NULL
+		BEGIN
+			SELECT @ConnectionString = [ConnectionString]
+			FROM dbo.MasterCompany WITH (NOLOCK)
+			WHERE MasterCompanyId = @MasterCompanyId;
+		END
+
+		SET @TargetDBName = (SELECT REPLACE(value, 'Initial Catalog=', '') AS InitialCatalogm FROM STRING_SPLIT(@ConnectionString, ';') WHERE value LIKE 'Initial Catalog=%');
+
+		DECLARE @sql NVARCHAR(MAX);
+		DECLARE @paramDefs NVARCHAR(MAX);
+
+		SET @sql = N'
+		SELECT @Impersonated = ISNULL(FirstName, '''') + '' '' + ISNULL(LastName, '''')
+		FROM ' + QUOTENAME(@TargetDBName) + '.dbo.Employee WITH(NOLOCK)
+		WHERE EmployeeId = @ImpersonatedEmployeeId
+		AND MasterCompanyId = @MasterCompanyId;
+		';
+
+		SET @paramDefs = N'
+			@ImpersonatedEmployeeId BIGINT,
+			@MasterCompanyId INT,
+			@Impersonated NVARCHAR(200) OUTPUT
+		';
+
+		EXEC sp_executesql
+		@sql,
+		@paramDefs,
+		@ImpersonatedEmployeeId = @ImpersonatedEmployeeId,
+		@MasterCompanyId = @MasterCompanyId,
+		@Impersonated = @Impersonated OUTPUT;
+
+		EXEC sp_executesql
+		@sql,
+		@paramDefs,
+		@ImpersonatedEmployeeId = @ImpersonatedEmployeeId,
+		@MasterCompanyId = @MasterCompanyId,
+		@Impersonated = @Impersonated OUTPUT;
+		--SET @Impersonated = (SELECT ISNULL(FirstName,'') + ' ' + ISNULL(LastName,'') as [Name] FROM dbo.[Employee] WITH(NOLOCK) WHERE EmployeeId = @ImpersonatedEmployeeId and MasterCompanyId =  @MasterCompanyId);
+	END
+
+	DECLARE @CompanyName varchar(500) = (SELECT [CompanyName] FROM dbo.[MasterCompany] WITH(NOLOCK) WHERE MasterCompanyId = @CurrentUserMasterCompanyId);
+	DECLARE @CompanyCode varchar(100) = (SELECT [MasterCompanyCode] FROM dbo.[MasterCompany] WITH(NOLOCK) WHERE MasterCompanyId = @CurrentUserMasterCompanyId);
+
+	IF(@ImpersonatedEmployeeId > 0 AND @ImpersonatedByEmployeeId > 0)
+	BEGIN
+		INSERT INTO dbo.[EmployeeImpersonationHistory]
+			([ImpersonatedByEmployeeId],[ImpersonatedBy],[ImpersonatedEmployeeId],[Impersonated],[CompanyName],[CompanyCode],[CompanyId],[IsActive],[MasterCompanyId],[CreatedBy],[CreatedDate],[UpdatedBy],[UpdatedDate])
+		VALUES(@ImpersonatedByEmployeeId,@ImpersonatedBy,@ImpersonatedEmployeeId,@Impersonated,@CompanyName,@CompanyCode,@MasterCompanyId,1,@CurrentUserMasterCompanyId,@ImpersonatedBy,GETUTCDATE(),@ImpersonatedBy,GETUTCDATE())
+	END
+	
+
+	END TRY      
+	BEGIN CATCH        
+	IF @@trancount > 0  
+    PRINT 'ROLLBACK'  
+    DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name()   
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------  
+              , @AdhocComments     VARCHAR(150)    = 'USP_AddEmployeeImpersonationHistory'   
+              , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ ISNULL(@ImpersonatedEmployeeId, '') + ''  
+              , @ApplicationName VARCHAR(100) = 'PAS'  
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------  
+              exec spLogException   
+                       @DatabaseName           =  @DatabaseName  
+                     , @AdhocComments          =  @AdhocComments  
+                     , @ProcedureParameters    =  @ProcedureParameters  
+                     , @ApplicationName        =  @ApplicationName  
+                     , @ErrorLogID             =  @ErrorLogID OUTPUT ;  
+              RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)  
+              RETURN(1);  
+  END CATCH  
+END
---
+++ PAS_DB/dbo/Tables/EmployeeImpersonationHistory.sql
@@ -0,0 +1,129 @@
+﻿CREATE TABLE [dbo].[EmployeeImpersonationHistory] (
+    [EmployeeImpersonationId]  BIGINT        IDENTITY (1, 1) NOT NULL,
+    [ImpersonatedByEmployeeId] BIGINT        NOT NULL,
+    [ImpersonatedBy]           VARCHAR (256) NOT NULL,
+    [ImpersonatedEmployeeId]   BIGINT        NOT NULL,
+    [Impersonated]             VARCHAR (256) NOT NULL,
+    [CompanyName]              VARCHAR (256) NOT NULL,
+    [CompanyCode]              VARCHAR (100) NOT NULL,
+    [CompanyId]                INT           NULL,
+    [IsActive]                 BIT           DEFAULT ((1)) NOT NULL,
+    [MasterCompanyId]          INT           NULL,
+    [CreatedBy]                VARCHAR (256) NOT NULL,
+    [CreatedDate]              DATETIME      NOT NULL,
+    [UpdatedBy]                VARCHAR (256) NOT NULL,
+    [UpdatedDate]              DATETIME      NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeImpersonationId] ASC)
+);
+
+
+GO
+CREATE     TRIGGER [dbo].[trg_Audit_dbo_EmployeeImpersonationHistory]
+        ON [dbo].[EmployeeImpersonationHistory]
+        AFTER INSERT, UPDATE, DELETE
+        AS
+        BEGIN
+            SET NOCOUNT ON;
+            ;WITH
+            d AS (SELECT d.[EmployeeImpersonationId],d.[ImpersonatedByEmployeeId],d.[ImpersonatedBy],d.[ImpersonatedEmployeeId],d.[Impersonated],d.[CompanyName],d.[CompanyCode],d.[CompanyId],d.[IsActive],d.[MasterCompanyId],d.[CreatedBy],d.[CreatedDate],d.[UpdatedBy],d.[UpdatedDate] FROM deleted d),
+            i AS (SELECT i.[EmployeeImpersonationId],i.[ImpersonatedByEmployeeId],i.[ImpersonatedBy],i.[ImpersonatedEmployeeId],i.[Impersonated],i.[CompanyName],i.[CompanyCode],i.[CompanyId],i.[IsActive],i.[MasterCompanyId],i.[CreatedBy],i.[CreatedDate],i.[UpdatedBy],i.[UpdatedDate] FROM inserted i),
+            paired AS (
+                SELECT
+                    COALESCE(i.EmployeeImpersonationId, d.EmployeeImpersonationId ) AS EmployeeImpersonationId,
+                    (SELECT d.* FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS old_row_json,
+                    (SELECT i.* FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS new_row_json, 
+                    CASE
+                        WHEN i.EmployeeImpersonationId IS NOT NULL AND d.EmployeeImpersonationId IS NOT NULL THEN 'U'
+                        WHEN i.EmployeeImpersonationId IS NOT NULL AND d.EmployeeImpersonationId IS NULL     THEN 'I'
+                        WHEN i.EmployeeImpersonationId IS NULL     AND d.EmployeeImpersonationId IS NOT NULL THEN 'D'
+                    END AS Action,
+
+                    (SELECT COALESCE(i.ImpersonatedEmployeeId, d.ImpersonatedEmployeeId) AS ImpersonatedEmployeeId
+                     FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) AS PKJson
+                FROM d
+                FULL OUTER JOIN i
+                    ON i.EmployeeImpersonationId = d.EmployeeImpersonationId
+            ),
+
+            oldv AS (
+                SELECT
+                    p.PKJson,
+                    p.EmployeeImpersonationId,
+                    v.[key]  AS ColumnName,
+                    v.value  AS OldValue
+                FROM paired p
+                CROSS APPLY OPENJSON(p.old_row_json) v
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM dbo.IgnoreColumn ign WITH(NOLOCK)
+                    WHERE ign.SchemaName = N'dbo'
+                      AND ign.TableName  = N'EmployeeImpersonationHistory'
+                      AND ign.ColumnName = N'EmployeeImpersonationId'
+                )),
+            newv AS (
+                SELECT
+                    p.PKJson,
+                    p.EmployeeImpersonationId ,
+                    v.[key]  AS ColumnName,
+                    v.value  AS NewValue
+                FROM paired p
+                CROSS APPLY OPENJSON(p.new_row_json) v
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM dbo.IgnoreColumn ign WITH(NOLOCK)
+                    WHERE ign.SchemaName = N'dbo'
+                      AND ign.TableName  = N'EmployeeImpersonationHistory'
+                      AND ign.ColumnName = N'EmployeeImpersonationId'
+                )),
+            merged AS (
+                SELECT
+                    COALESCE(n.PKJson, o.PKJson)                AS PKJson,
+                    COALESCE(n.ColumnName, o.ColumnName)        AS ColumnName,
+                    o.OldValue,
+                    n.NewValue,
+                    p.Action
+                FROM paired p
+                LEFT JOIN oldv o
+                    ON o.EmployeeImpersonationId = p.EmployeeImpersonationId
+                LEFT JOIN newv n
+                    ON n.EmployeeImpersonationId = p.EmployeeImpersonationId
+                   AND n.ColumnName = o.ColumnName
+                UNION ALL
+                SELECT
+                    n.PKJson,
+                    n.ColumnName,
+                    NULL AS OldValue,
+                    n.NewValue,
+                    p.Action
+                FROM paired p
+                LEFT JOIN newv n
+                    ON n.EmployeeImpersonationId = p.EmployeeImpersonationId
+                WHERE NOT EXISTS (
+                    SELECT 1
+                    FROM oldv o2
+                    WHERE o2.EmployeeImpersonationId = p.EmployeeImpersonationId
+                      AND o2.ColumnName    = n.ColumnName
+                )
+            )
+            INSERT dbo.AuditLog (SchemaName, TableName, PKJson, ColumnName, Action, OldValue, NewValue)
+            SELECT
+                N'dbo' AS SchemaName,
+                N'EmployeeImpersonationHistory' AS TableName,
+                m.PKJson,
+                m.ColumnName,
+                m.Action,
+                m.OldValue,
+                m.NewValue
+            FROM merged m
+            WHERE
+                m.ColumnName <> '<Enter your PrimaryKEY>' and (
+                (m.Action = 'U' AND (
+                     (m.OldValue IS NULL AND m.NewValue IS NOT NULL)
+                  OR (m.OldValue IS NOT NULL AND m.NewValue IS NULL)
+                  OR (m.OldValue <> m.NewValue)
+                ))
+                OR
+                (m.Action = 'I' AND m.NewValue IS NOT NULL)
+                OR
+                (m.Action = 'D' AND m.OldValue IS NOT NULL));
+        END;

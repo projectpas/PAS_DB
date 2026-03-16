1c4591f6ba6158c4a073277f2606e151e5413372
saliyabhargav <bhargav.saliya@silverxis.com>
2026-03-16 14:21:49 +0530

    PN-15456 For UAT

--- PAS_Database/dbo/Stored Procedures/USP_AddEmployeeImpersonationHistory.sql
+++ PAS_Database/dbo/Stored Procedures/USP_AddEmployeeImpersonationHistory.sql
@@ -30,60 +30,21 @@ BEGIN
 	SET NOCOUNT ON;  
 	BEGIN TRY  
 	DECLARE @ImpersonatedBy VARCHAR(256) ;
-	SET @ImpersonatedBy = (SELECT ISNULL(FirstName,'') + ' ' + ISNULL(LastName,'') as [Name] FROM dbo.[Employee] WITH(NOLOCK) WHERE EmployeeId = @ImpersonatedByEmployeeId and MasterCompanyId =  @CurrentUserMasterCompanyId);
+	DECLARE @Impersonated VARCHAR(256);	
+	DECLARE @CompanyName VARCHAR(500);
+	DECLARE @CompanyCode VARCHAR(100)
 
-	DECLARE @Impersonated VARCHAR(256);
+	SELECT @ImpersonatedBy = (SELECT ISNULL(FirstName,'') + ' ' + ISNULL(LastName,'')
+	FROM dbo.[Employee] WITH(NOLOCK) 
+	WHERE EmployeeId = @ImpersonatedByEmployeeId and MasterCompanyId =  @CurrentUserMasterCompanyId);
+	
+	SELECT @Impersonated = ASP.FullName
+	FROM dbo.[AspNetUsers] ASP WITH(NOLOCK) 
+	WHERE EmployeeId = @ImpersonatedEmployeeId and MasterCompanyId =  @MasterCompanyId;
 
-	IF(@MasterCompanyId > 0)
-	BEGIN
-		DECLARE @ConnectionString NVARCHAR(MAX) = NULL;
-		DECLARE @TargetDBName SYSNAME = NULL;
+	SELECT @CompanyName = [CompanyName], @CompanyCode = [MasterCompanyCode] FROM dbo.[MasterCompany] WITH(NOLOCK) WHERE MasterCompanyId = @MasterCompanyId;
 
-		IF @MasterCompanyId IS NOT NULL
-		BEGIN
-			SELECT @ConnectionString = [ConnectionString]
-			FROM dbo.MasterCompany WITH (NOLOCK)
-			WHERE MasterCompanyId = @MasterCompanyId;
-		END
-
-		SET @TargetDBName = (SELECT REPLACE(value, 'Initial Catalog=', '') AS InitialCatalogm FROM STRING_SPLIT(@ConnectionString, ';') WHERE value LIKE 'Initial Catalog=%');
-
-		DECLARE @sql NVARCHAR(MAX);
-		DECLARE @paramDefs NVARCHAR(MAX);
-
-		SET @sql = N'
-		SELECT @Impersonated = ISNULL(FirstName, '''') + '' '' + ISNULL(LastName, '''')
-		FROM ' + QUOTENAME(@TargetDBName) + '.dbo.Employee WITH(NOLOCK)
-		WHERE EmployeeId = @ImpersonatedEmployeeId
-		AND MasterCompanyId = @MasterCompanyId;
-		';
-
-		SET @paramDefs = N'
-			@ImpersonatedEmployeeId BIGINT,
-			@MasterCompanyId INT,
-			@Impersonated NVARCHAR(200) OUTPUT
-		';
-
-		EXEC sp_executesql
-		@sql,
-		@paramDefs,
-		@ImpersonatedEmployeeId = @ImpersonatedEmployeeId,
-		@MasterCompanyId = @MasterCompanyId,
-		@Impersonated = @Impersonated OUTPUT;
-
-		EXEC sp_executesql
-		@sql,
-		@paramDefs,
-		@ImpersonatedEmployeeId = @ImpersonatedEmployeeId,
-		@MasterCompanyId = @MasterCompanyId,
-		@Impersonated = @Impersonated OUTPUT;
-		--SET @Impersonated = (SELECT ISNULL(FirstName,'') + ' ' + ISNULL(LastName,'') as [Name] FROM dbo.[Employee] WITH(NOLOCK) WHERE EmployeeId = @ImpersonatedEmployeeId and MasterCompanyId =  @MasterCompanyId);
-	END
-
-	DECLARE @CompanyName varchar(500) = (SELECT [CompanyName] FROM dbo.[MasterCompany] WITH(NOLOCK) WHERE MasterCompanyId = @CurrentUserMasterCompanyId);
-	DECLARE @CompanyCode varchar(100) = (SELECT [MasterCompanyCode] FROM dbo.[MasterCompany] WITH(NOLOCK) WHERE MasterCompanyId = @CurrentUserMasterCompanyId);
-
-	IF(@ImpersonatedEmployeeId > 0 AND @ImpersonatedByEmployeeId > 0)
+	IF(ISNULL(@ImpersonatedEmployeeId, 0) > 0 AND ISNULL(@ImpersonatedByEmployeeId, 0) > 0)
 	BEGIN
 		INSERT INTO dbo.[EmployeeImpersonationHistory]
 			([ImpersonatedByEmployeeId],[ImpersonatedBy],[ImpersonatedEmployeeId],[Impersonated],[CompanyName],[CompanyCode],[CompanyId],[IsActive],[MasterCompanyId],[CreatedBy],[CreatedDate],[UpdatedBy],[UpdatedDate])

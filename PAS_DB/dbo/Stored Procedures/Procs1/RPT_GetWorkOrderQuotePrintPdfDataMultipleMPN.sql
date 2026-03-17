ff45f5f73139a4030b729fecb569f083acb663e7
Aayushi Patel <ayushi@silverxis.com>
2026-03-17 19:27:17 +0530

    PN-15745

--- PAS_DB/dbo/Stored Procedures/Procs1/RPT_GetWorkOrderQuotePrintPdfDataMultipleMPN.sql
+++ PAS_DB/dbo/Stored Procedures/Procs1/RPT_GetWorkOrderQuotePrintPdfDataMultipleMPN.sql
@@ -22,8 +22,9 @@
     8    10-JUL-2025     Moin Bloch         Updated MEMO To PublicationNotes
 	9	 23-JUL-2025     Devendra Shekh		Added Case for Memo
 	10	 14-OCT-2025     RAJESH GAMI		Return Estimated Ship Date
-	11	 02-March-2026   Ayushi Patel		PN-15602 PN-15604 Retuen ItemNo  
---EXEC [RPT_GetWorkOrderQuotePrintPdfDataMultipleMPN] 7884,'',0  
+	11	 02-March-2026   Ayushi Patel		PN-15745 Retuen ItemNo  , Added OrderBY ID
+--EXEC [RPT_GetWorkOrderQuotePrintPdfDataMultipleMPN] 2297,'',0  
+exec RPT_GetWorkOrderQuotePrintPdfDataMultipleMPN @WorkOrderQuoteId=2300,@workOrderPartNoIds=N'10439',@isByPartIds=0
 **************************************************************/  
 CREATE    PROCEDURE [dbo].[RPT_GetWorkOrderQuotePrintPdfDataMultipleMPN]  
  @WorkOrderQuoteId bigint,  
@@ -364,7 +365,8 @@ BEGIN
 					FinalLaborTotal,
 					RowNumber,
 					Memo, IsPrintCorrectiveAction,EstimatedShipDate
-				FROM #tmpQuoteIds; 
+				FROM #tmpQuoteIds
+				ORDER BY ID;
 
 		END
 		ELSE
@@ -493,7 +495,8 @@ BEGIN
         WHEN LTRIM(RTRIM(CAST(EstimatedShipDate AS VARCHAR))) = '' THEN NULL
         ELSE EstimatedShipDate
     END AS EstimatedShipDate
-				FROM #tmpQuotetblMulti; 
+				FROM #tmpQuotetblMulti
+				ORDER BY ID;
 
 		END
 

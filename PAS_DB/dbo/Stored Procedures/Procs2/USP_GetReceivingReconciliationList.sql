68a4cc26ed4258ab3205f3ce30ffb442f96cf8b4
Amit Gediya <amit.ghediya@silverxis.comm>
2026-03-19 13:41:20 +0530

    RR revert for isactive

--- PAS_DB/dbo/Stored Procedures/Procs2/USP_GetReceivingReconciliationList.sql
+++ PAS_DB/dbo/Stored Procedures/Procs2/USP_GetReceivingReconciliationList.sql
@@ -128,7 +128,7 @@ BEGIN
 								INNER JOIN [dbo].[RoleManagementStructure] RMS WITH (NOLOCK) ON puo.ManagementStructureId = RMS.EntityStructureId
 								INNER JOIN [dbo].[EmployeeUserRole] EUR WITH (NOLOCK) ON EUR.RoleId = RMS.RoleId AND EUR.EmployeeId = @EmployeeId
 								WHERE ReceivingReconciliationId=RRH.ReceivingReconciliationId
-						)R WHERE (@StatusID IS NULL OR RRH.StatusId = @StatusID) AND RRH.IsActive = 1 AND RRH.IsDeleted = 0)
+						)R WHERE (@StatusID IS NULL OR RRH.StatusId = @StatusID) AND RRH.IsDeleted = 0)
 						,PartCTE AS(
 						SELECT SQ.ReceivingReconciliationId,(CASE WHEN COUNT(DISTINCT po.PurchaseOrderId) > 1 Then 'Multiple' ELse A.PartNumber End)  as 'PartNumberType',A.PartNumber from [dbo].[ReceivingReconciliationHeader] SQ WITH (NOLOCK)
 						LEFT JOIN [dbo].[ReceivingReconciliationDetails] SP WITH (NOLOCK) On SQ.ReceivingReconciliationId=SP.ReceivingReconciliationId

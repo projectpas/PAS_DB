1e9f9787a774a9dc90796be78663f67d08d24d69
projectpas <54223580+projectpas@users.noreply.github.com>
2026-03-27 17:03:02 +0530

    Update USP_UpdateAircraftRegistryState.sql

--- PAS_Database/dbo/Stored Procedures/USP_UpdateAircraftRegistryState.sql
+++ PAS_Database/dbo/Stored Procedures/USP_UpdateAircraftRegistryState.sql
@@ -1,71 +1,83 @@
-﻿/***********************************************************
-** File:  [USP_UpdateAircraftRegistryState]
-** Author: Priyansh Patel
-** Description: Update Aircraft Registry State by Id
-** Purpose:  
-** Date:   2026-03-26
-
-** RETURN VALUE: 
-**************************************************************
+/************************************************************
+** File:        [USP_UpdateAircraftRegistryState]
+** Author:      Priyansh Patel
+** Description: Update IsDeleted / IsActive state on an
+**              AircraftRegistryHeader row by Id.
+**
 ** Change History
-**************************************************************
-** PR   Date			Author			Change Description
-** --   --------		-------			--------------------------------
-   1   26-03-2025    Priyansh Patel	    Created - [PN-15841]
-
- exec  USP_UpdateAircraftRegistryState 710
-***************************************************************/
-CREATE       PROCEDURE [dbo].[USP_UpdateAircraftRegistryState]
+************************************************************
+** PR   Date         Author          Description
+** --   ----------   -------------   -------------------------
+** 1    26-03-2025   Priyansh Patel  Created [PN-15841]
+************************************************************/
+CREATE OR ALTER PROCEDURE [dbo].[USP_UpdateAircraftRegistryState]
     @AircraftRegistryId BIGINT,
     @MasterCompanyId    INT,
     @IsDeleted          BIT = NULL,
     @IsActive           BIT = NULL
 AS
 BEGIN
-    SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
-	SET NOCOUNT ON  
-	  BEGIN TRY
-     BEGIN TRAN;
+    -- NOTE: READ UNCOMMITTED has no effect on DML statements.
+    -- UPDATE always acquires row-level exclusive locks regardless
+    -- of session isolation level. Removed to avoid confusion.
+    SET NOCOUNT ON;
 
-        UPDATE [dbo].[AircraftRegistryHeader] SET  
-                IsDeleted = CASE 
-                    WHEN @IsDeleted IS NOT NULL THEN @IsDeleted  
-                    ELSE IsDeleted 
-                END,
+    BEGIN TRY
 
-                IsActive = CASE 
-                    WHEN @IsActive IS NOT NULL THEN @IsActive  
-                    ELSE IsActive
-                END,
-            UpdatedDate = GETUTCDATE()
-        WHERE AircraftRegistryId = @AircraftRegistryId 
-          AND MasterCompanyId = @MasterCompanyId;
+        DECLARE @AffectedRows INT;
 
-        IF @@ROWCOUNT > 0
-            SELECT @AircraftRegistryId AS AircraftRegistryId;
-        ELSE
-            SELECT -1 AS AircraftRegistryId;
+        BEGIN TRANSACTION;
 
-        COMMIT TRAN;
+            UPDATE [dbo].[AircraftRegistryHeader]
+            SET
+                IsDeleted   = CASE WHEN @IsDeleted IS NOT NULL THEN @IsDeleted ELSE IsDeleted END,
+                IsActive    = CASE WHEN @IsActive  IS NOT NULL THEN @IsActive  ELSE IsActive  END,
+                UpdatedDate = GETUTCDATE()
+            WHERE
+                AircraftRegistryId = @AircraftRegistryId
+                AND MasterCompanyId = @MasterCompanyId;
+
+            -- @@ROWCOUNT must be captured immediately; any subsequent
+            -- statement (including SELECT) resets it to 0.
+            SET @AffectedRows = @@ROWCOUNT;
+
+        COMMIT TRANSACTION;
+
+        -- Return the Id on success, -1 when no row matched the WHERE clause.
+        SELECT CASE WHEN @AffectedRows > 0 THEN @AircraftRegistryId ELSE -1 END AS AircraftRegistryId;
 
     END TRY
-    BEGIN CATCH      
-		IF @@trancount > 0
-			PRINT 'ROLLBACK'
-			ROLLBACK TRAN;
-			DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
------------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
-            , @AdhocComments     VARCHAR(150)    = 'USP_UpdateAircraftRegistryState' 
-            , @ProcedureParameters VARCHAR(3000)  = '@Parameter1 = '''+ ISNULL(CAST(@AircraftRegistryId AS VARCHAR(20)) , '') + ''
-            , @ApplicationName VARCHAR(100) = 'PAS'
------------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
-            exec spLogException 
-                    @DatabaseName           = @DatabaseName
-                    , @AdhocComments          = @AdhocComments
-                    , @ProcedureParameters = @ProcedureParameters
-                    , @ApplicationName        =  @ApplicationName
-                    , @ErrorLogID                    = @ErrorLogID OUTPUT ;
-            RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)
-            RETURN(1);
-	END CATCH	
-END
+    BEGIN CATCH
+
+        IF @@TRANCOUNT > 0
+        BEGIN
+            ROLLBACK TRANSACTION;
+        END;
+
+        DECLARE
+            @ErrorLogID          INT,
+            @DatabaseName        VARCHAR(100)  = DB_NAME(),
+            @AdhocComments       VARCHAR(150)  = 'USP_UpdateAircraftRegistryState',
+            @ProcedureParameters VARCHAR(3000) =
+                '@AircraftRegistryId = ' + ISNULL(CAST(@AircraftRegistryId AS VARCHAR(20)), 'NULL')
+                + ', @MasterCompanyId = ' + ISNULL(CAST(@MasterCompanyId   AS VARCHAR(20)), 'NULL')
+                + ', @IsDeleted = '       + ISNULL(CAST(@IsDeleted         AS VARCHAR(5)),  'NULL')
+                + ', @IsActive = '        + ISNULL(CAST(@IsActive          AS VARCHAR(5)),  'NULL'),
+            @ApplicationName     VARCHAR(100)  = 'PAS';
+
+        EXEC spLogException
+            @DatabaseName        = @DatabaseName,
+            @AdhocComments       = @AdhocComments,
+            @ProcedureParameters = @ProcedureParameters,
+            @ApplicationName     = @ApplicationName,
+            @ErrorLogID          = @ErrorLogID OUTPUT;
+
+        RAISERROR(
+            'Unexpected error in the database. Please provide error number %d to the support team.',
+            16, 1, @ErrorLogID
+        );
+
+        RETURN 1;
+
+    END CATCH;
+END;

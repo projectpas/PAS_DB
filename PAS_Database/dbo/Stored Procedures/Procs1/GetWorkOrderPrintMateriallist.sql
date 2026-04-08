91de9d5d3c1b7536f4f9fc954861df1d0da05d0a
Aayushi Patel <ayushi@silverxis.com>
2026-04-08 12:13:20 +0530

    PN-15908

--- PAS_Database/dbo/Stored Procedures/Procs1/GetWorkOrderPrintMateriallist.sql
+++ PAS_Database/dbo/Stored Procedures/Procs1/GetWorkOrderPrintMateriallist.sql
@@ -60,7 +60,7 @@ BEGIN
 						CASE WHEN ISNULL(STK.[IsTurnIn],0) = 1 THEN ISNULL(STK.[WorkOrderNumber],'') ELSE '' END AS WorkOrderNumber,
 						STK.SerialNumber,
 						--WOMS.QuantityTurnIn AS TenderedQTY
-						ISNULL([dbo].[fn_ConvertUOM](SUM(WOMS.QuantityTurnIn),STk.[StockUnitOfMeasure], STk.[ConsumeUnitOfMeasure],0,STk.[MasterCompanyId]),0) AS TenderedQTY
+						ISNULL([dbo].[fn_ConvertUOM](ISNULL(WOMS.QuantityTurnIn,0),ISNULL(STk.[StockUnitOfMeasure], 0),ISNULL(STk.[ConsumeUnitOfMeasure], 0),0,ISNULL(STk.[MasterCompanyId], 0)),0) AS TenderedQTY
 				FROM [dbo].[WorkOrderMaterialStockLine] WOMS WITH(NOLOCK)
 				INNER JOIN [dbo].[WorkOrderMaterials] WOM WITH(NOLOCK) ON WOM.WorkOrderMaterialsId = WOMS.WorkOrderMaterialsId
 				INNER JOIN [dbo].[Stockline] STK WITH(NOLOCK) ON STk.StockLineId = WOMS.StockLineId

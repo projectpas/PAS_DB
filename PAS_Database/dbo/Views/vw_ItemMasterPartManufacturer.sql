95ff2760208a9007ce165bb928c4d466367804af
Aayushi Patel <ayushi@silverxis.com>
2026-05-14 16:46:47 +0530

    PN-16321

---
+++ PAS_Database/dbo/Views/vw_ItemMasterPartManufacturer.sql
@@ -0,0 +1,25 @@
+﻿
+CREATE VIEW dbo.vw_ItemMasterPartManufacturer
+AS
+
+WITH PartCounts AS
+(
+    SELECT
+        PartNumber,
+        COUNT(*) AS PartCount
+    FROM dbo.ItemMaster
+    GROUP BY PartNumber
+)
+
+SELECT
+    IM.*,
+
+    CASE
+        WHEN PC.PartCount > 1
+            THEN CONCAT(IM.PartNumber, ' - ', IM.ManufacturerName)
+        ELSE IM.PartNumber
+    END AS PartManufacture
+
+FROM dbo.ItemMaster IM
+INNER JOIN PartCounts PC
+    ON IM.PartNumber = PC.PartNumber;

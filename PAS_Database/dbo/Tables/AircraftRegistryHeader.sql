8ae5132b52c6d1bca209fa06d6b8905acd822790
Amit Gediya <amit.ghediya@silverxis.comm>
2026-05-22 16:20:11 +0530

    PN-16533 added for hh/mm in ac general info.

--- PAS_Database/dbo/Tables/AircraftRegistryHeader.sql
+++ PAS_Database/dbo/Tables/AircraftRegistryHeader.sql
@@ -31,11 +31,15 @@
     [UpdatedDate]            DATETIME2 (7)   CONSTRAINT [DF_AircraftRegistry_UpdatedDate] DEFAULT (sysdatetime()) NOT NULL,
     [AircraftRegistryNumber] VARCHAR (30)    NULL,
     [LastMaintenanceDate]    DATETIME        NULL,
+    [TotalTSNMM]             DECIMAL (18, 6) NULL,
+    [TotalCSNMM]             DECIMAL (18, 6) NULL,
     CONSTRAINT [PK_AircraftRegistry] PRIMARY KEY CLUSTERED ([AircraftRegistryId] ASC),
     CONSTRAINT [FK_AircraftRegistry_AircraftStatus] FOREIGN KEY ([AircraftStatusId]) REFERENCES [dbo].[AircraftStatus] ([AircraftStatusId]),
     CONSTRAINT [FK_AircraftRegistry_MaintenanceStatus] FOREIGN KEY ([MaintenanceStatusId]) REFERENCES [dbo].[MaintenanceStatus] ([MaintenanceStatusId]),
     CONSTRAINT [FK_AircraftRegistry_MasterCompany] FOREIGN KEY ([MasterCompanyId]) REFERENCES [dbo].[MasterCompany] ([MasterCompanyId])
 );
+
+
 
 
 

f41e6aa73fd3a67488e108082465f53ef39d3c1a
priyansh <patel.priyansh@silverxis.com>
2026-05-28 17:54:49 +0530

    PN-16589

---
+++ PAS_DB/dbo/Stored Procedures/USP_GetAllAircraft.sql
@@ -0,0 +1,53 @@
+﻿/*************************************************************           
+ ** File:		 [USP_GetAllAircraft]           
+ ** Author:		 Amit Ghediya
+ ** Description: This Stored Procedure Is Used To Get USP_GetAllAircraft By MasterCompany.
+ ** Purpose:         
+ ** Date:   04-10-2026
+ **************************************************************           
+ ** Change History           
+ **************************************************************           
+ ** PR   Date				Author				Change Description            
+ ** --   -------------		----------------	--------------------------------          
+    1    04-10-2026			Amit Ghediya			Created
+    
+ -- EXEC [USP_GetAllAircraft] @MasterCompanyId=1
+**************************************************************/
+CREATE     PROCEDURE [dbo].[USP_GetAllAircraft]
+	@MasterCompanyId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	BEGIN TRY	
+		SELECT
+			m.[AircraftTypeId],
+			m.[Description] AS [Name],
+			ISNULL(m.[memo], '') AS Comments,
+			ISNULL(m.[IsActive], 0) AS IsActive,
+			ISNULL(m.[IsDeleted], 0) AS IsDeleted,
+			m.[MasterCompanyId]
+		FROM [DBO].[aircrafttype] AS m WITH (NOLOCK)
+		WHERE ISNULL(m.[IsDeleted], 0) = 0 AND m.[MasterCompanyId] = @MasterCompanyId
+		ORDER BY m.[AircraftTypeId] DESC;
+	END TRY 
+	BEGIN CATCH
+	
+		DECLARE @ErrorLogID INT, @DatabaseName VARCHAR(100) = db_name() 
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+              , @AdhocComments     VARCHAR(150)    = 'USP_GetAllAircraft'
+			  , @ProcedureParameters VARCHAR(3000) = '@Parameter1 = '''
+              , @ApplicationName VARCHAR(100) = 'PAS'
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+		EXEC spLogException @DatabaseName = @DatabaseName
+			,@AdhocComments = @AdhocComments
+			,@ProcedureParameters = @ProcedureParameters
+			,@ApplicationName = @ApplicationName
+			,@ErrorLogID = @ErrorLogID OUTPUT;
+
+		RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1, @ErrorLogID)
+
+		RETURN (1); 
+	END CATCH
+
+END
---
+++ PAS_DB/dbo/Stored Procedures/usp_GetAircraftCycleTimeById.sql
@@ -0,0 +1,103 @@
+﻿/*************************************************************
+** File:        [usp_GetAircraftCycleTimeById]
+** Description:
+** Purpose:
+** Date:
+**
+** RETURN VALUE:
+**************************************************************
+** Change History
+**************************************************************
+** PR   Date         Author         Change Description
+** --   ----------   -------------  --------------------------------
+** 1    14/04/2026  Amit Ghediya		Created 
+** 2    14/04/2026  Amit Ghediya		Get LastFlownDate (PN-16156)
+** 3    28/04/2026  Amit Ghediya		Get Minutes related data,LastFlownDate (PN-16151)
+*************************************************************/
+CREATE     PROCEDURE [dbo].[usp_GetAircraftCycleTimeById]
+(
+    @AircraftCycleTimeMappingsId BIGINT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+		
+		-- Main Cycle Data
+		SELECT 
+			AircraftCycleTimeMappingsId,
+			ModuleId,
+			RefrenceId,
+			CycleDate,
+			Hours,
+			Minutes,
+			CurruntHours,
+			CurruntMinutes,
+			CumulativeHours,
+			CumulativeMinutes,
+			Cycles,
+			CyclesMinutes,
+			CurruntCycles,
+			CurruntCyclesMinutes,
+			CumulativeCycles,
+			CumulativeCyclesMinutes,
+			Memo,
+			MasterCompanyId,
+			CreatedBy,
+			UpdatedBy,
+			LastFlownDate = (SELECT TOP 1 LastFlownDate FROM dbo.AircraftInstalledPartDetails WITH(NOLOCK) WHERE [AircraftRegistryId] = [RefrenceId])
+		FROM dbo.AircraftCycleTimeMappings WITH(NOLOCK)
+		WHERE RefrenceId = @AircraftCycleTimeMappingsId
+		  AND IsActive = 1 AND IsDeleted = 0;
+
+		-- Engine Data
+		SELECT 
+			AESM.AircraftEngineStartsMappingsId,
+			AESM.AircraftCycleTimeMappingsId,
+			AESM.EngineName,
+			AESM.Hours,
+			AESM.Minutes,
+			AESM.CurruntHours,
+			AESM.CurruntMinutes,
+			AESM.CumulativeHours,
+			AESM.CumulativeMinutes,
+			AESM.Starts,
+			AESM.CurruntStarts,
+			AESM.CumulativeStarts,
+			AESM.Memo,
+			AESM.MasterCompanyId,
+			AESM.CreatedBy,
+			AESM.UpdatedBy
+		FROM dbo.AircraftEngineStartsMappings AESM WITH(NOLOCK)
+		INNER JOIN dbo.AircraftCycleTimeMappings ACTM WITH(NOLOCK) ON ACTM.AircraftCycleTimeMappingsId = AESM.AircraftCycleTimeMappingsId
+		WHERE ACTM.RefrenceId = @AircraftCycleTimeMappingsId
+		  AND AESM.IsActive = 1 AND AESM.IsDeleted = 0;
+    END TRY
+    BEGIN CATCH
+        DECLARE
+            @ErrorLogID INT,
+            @DatabaseName VARCHAR(100) = DB_NAME(),
+            @AdhocComments VARCHAR(150) = 'usp_GetAircraftCycleTimeById',
+            @ProcedureParameters VARCHAR(3000),
+            @ApplicationName VARCHAR(100) = 'PAS';
+
+        SET @ProcedureParameters =
+              '@AircraftCycleTimeMappingsId=' + CAST(ISNULL(@AircraftCycleTimeMappingsId, 0) AS VARCHAR(20));
+        EXEC spLogException
+             @DatabaseName        = @DatabaseName,
+             @AdhocComments       = @AdhocComments,
+             @ProcedureParameters = @ProcedureParameters,
+             @ApplicationName     = @ApplicationName,
+             @ErrorLogID          = @ErrorLogID OUTPUT;
+
+        RAISERROR
+        (
+            'Unexpected error occurred in the database. Please let the support team know the error number: %d',
+            16,
+            1,
+            @ErrorLogID
+        );
+
+        RETURN 1;
+    END CATCH
+END;

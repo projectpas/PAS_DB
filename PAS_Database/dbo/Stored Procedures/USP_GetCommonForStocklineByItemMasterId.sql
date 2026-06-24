d19f1205c32e69014bda0bdad2f0cf0af2626b59
Nakul <nakul@silverxis.com>
2026-06-24 15:59:09 +0530

    Removed duplicate changes.

--- PAS_Database/dbo/Stored Procedures/USP_GetCommonForStocklineByItemMasterId.sql
+++ PAS_Database/dbo/Stored Procedures/USP_GetCommonForStocklineByItemMasterId.sql
@@ -15,7 +15,6 @@
     1    31-10-2025    Sahdev Saliya       Created  
 	2    10 DEc @025   Rajesh Gami		   Return DecimalPlaces from UnitOfMeasure
 	3    03-06-2026    Sahdev Saliya       Added Model [PN-16667]
-	4	 24-06-2026    Nakul Chandigra	   Added StockUnitOfMeasureId in select (PN-16914)
 	exec [dbo].[USP_GetCommonForStocklineByItemMasterId]
 **************************************************************/
 CREATE   PROCEDURE [dbo].[USP_GetCommonForStocklineByItemMasterId]
@@ -88,9 +87,8 @@ BEGIN
 			ISNULL(uom.DecimalPlaces,2) as DecimalPlaces,
 			ISNULL(iM.StockUnitOfMeasureId,0) StockUnitOfMeasureId,
 			ISNULL(iM.ConsumeUnitOfMeasureId,0) ConsumeUnitOfMeasureId,
-			iM.Model,
-			ISNULL(iM.StockUnitOfMeasureId,0) StockUnitOfMeasureId
-    FROM [DBO].[ItemMaster] iM WITH (NOLOCK)
+			iM.Model
+	    FROM [DBO].[ItemMaster] iM WITH (NOLOCK)
         LEFT JOIN [DBO].[ItemMaster] rPart WITH (NOLOCK) ON iM.RevisedPartId = rPart.ItemMasterId
         LEFT JOIN [DBO].[ItemMasterExchangeLoan] imxl WITH (NOLOCK) ON iM.ItemMasterId = imxl.ItemMasterId
         LEFT JOIN [DBO].[ItemMasterPurchaseSale] imps WITH (NOLOCK) ON iM.ItemMasterId = imps.ItemMasterId

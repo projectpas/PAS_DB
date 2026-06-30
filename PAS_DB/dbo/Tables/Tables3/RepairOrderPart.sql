5622fcab98ad1efd3904bc856b2050ff5ab2bb6b
Abhishek <Abhishek@silverxis.com>
2026-06-30 17:20:27 +0530

    PN-16786 Piece Parts Changes

--- PAS_DB/dbo/Tables/Tables3/RepairOrderPart.sql
+++ PAS_DB/dbo/Tables/Tables3/RepairOrderPart.sql
@@ -112,6 +112,7 @@
     [IsFromAircraft]                 BIT             NULL,
     [AircraftInstalledPartDetailsId] BIGINT          NULL,
     [IsPiecePart]                    BIT             CONSTRAINT [DF_RepairOrderPart_IsPiecePart] DEFAULT ((0)) NOT NULL,
+    [ParentRepairOrderPartId]        BIGINT          DEFAULT (NULL) NULL,
     CONSTRAINT [PK_RepairOrderpart] PRIMARY KEY CLUSTERED ([RepairOrderPartRecordId] ASC),
     CONSTRAINT [FK_RepairOrderPart_FunctionalCurrency] FOREIGN KEY ([FunctionalCurrencyId]) REFERENCES [dbo].[Currency] ([CurrencyId]),
     CONSTRAINT [FK_RepairOrderPart_GlAccount] FOREIGN KEY ([GlAccountId]) REFERENCES [dbo].[GLAccount] ([GLAccountId]),
@@ -123,6 +124,8 @@
     CONSTRAINT [FK_RepairOrderPart_SubWorkOrderId] FOREIGN KEY ([SubWorkOrderId]) REFERENCES [dbo].[SubWorkOrder] ([SubWorkOrderId]),
     CONSTRAINT [FK_RepairOrderPart_WorkOrderId] FOREIGN KEY ([WorkOrderId]) REFERENCES [dbo].[WorkOrder] ([WorkOrderId])
 );
+
+
 
 
 

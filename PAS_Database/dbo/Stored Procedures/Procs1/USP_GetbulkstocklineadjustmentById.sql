83aa85491c8f6418cdc53087dd609421eecca2c7
Moin Bloch <moin@silverxis.com>
2026-07-04 19:08:21 +0530

    PN-16824

--- PAS_Database/dbo/Stored Procedures/Procs1/USP_GetbulkstocklineadjustmentById.sql
+++ PAS_Database/dbo/Stored Procedures/Procs1/USP_GetbulkstocklineadjustmentById.sql
@@ -16,8 +16,9 @@
     1    08/10/2023     AMIT GHEDIYA			Created
 	2    31/10/2023     AMIT GHEDIYA			Added Isseralized for Qty check.
 	3    22/12/2023     Bhargav Salya			Added NewUnitCostTotransfer Field
+	4    16/06/2026     Moin Bloch			    Added [BulkStockScrapCertificateId] Field PN-16824
        
--- EXEC USP_GetbulkstocklineadjustmentById 8,2  
+-- EXEC USP_GetbulkstocklineadjustmentById 110,2  
   
 ************************************************************************/  
 CREATE    PROCEDURE [dbo].[USP_GetbulkstocklineadjustmentById]  
@@ -84,11 +85,13 @@ BEGIN
 			   BSAD.[QuantityOnHand],
 			   BSAD.[UnitOfMeasure],
 			   BSAD.[NewUnitCostTotransfer],
-			   BSAD.[AdjustmentReasonId]
+			   BSAD.[AdjustmentReasonId],
+			   BSSS.[BulkStockScrapCertificateId]
 		  FROM [dbo].[BulkStockLineAdjustmentDetails] BSAD WITH (NOLOCK)  
 		  INNER JOIN [dbo].[Stockline] STL WITH (NOLOCK) ON  STL.StockLineId = BSAD.StockLineId
 		  INNER JOIN [dbo].[ItemMaster] IM WITH (NOLOCK) ON STL.[ItemMasterId] = IM.[ItemMasterId]
-		  LEFT JOIN [dbo].[Manufacturer] MF WITH (NOLOCK) ON STL.ManufacturerId = MF.ManufacturerId
+		   LEFT JOIN [dbo].[Manufacturer] MF WITH (NOLOCK) ON STL.ManufacturerId = MF.ManufacturerId
+		   LEFT JOIN [dbo].[BulkStockScrapCertificate] BSSS WITH (NOLOCK) ON BSSS.[BulkStkLineAdjDetailsId] = BSAD.[BulkStkLineAdjDetailsId] AND BSSS.[IsActive] = 1 AND BSSS.[IsDeleted]=0
 		  WHERE BSAD.[BulkStkLineAdjId] = @BulkStkLineAdjId AND BSAD.[IsActive] = 1;
 	END
 END TRY      

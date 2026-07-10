551d47bc320aba5bc17ed096d037c8a6feb8a93b
priyansh <patel.priyansh@silverxis.com>
2026-07-10 19:16:30 +0530

    PN-17039

--- PAS_Database/dbo/Stored Procedures/USP_GetTaskInstructionMasterList.sql
+++ PAS_Database/dbo/Stored Procedures/USP_GetTaskInstructionMasterList.sql
@@ -13,6 +13,8 @@
     3    04-Mar-2025		RAJESH GAMI      		Sequence Number logic change (Group by Task)
 	4    10-Mar-2025		Divyesh Kathiriya		Update CreatedDate and UpdateDate based on Employee time zone
     5    11-Mar-2025		RAJESH GAMI      		IsDefaultInstruction Added
+    6    10-July-2026		Priyansh Patel      	Added the tasks with no instruction also [PN-17039]
+
 	exec dbo.USP_GetTaskInstructionMasterList @MasterCompanyId=1,@IsDeleted=1
 
 **************************************************************/
@@ -24,70 +26,72 @@ AS
 BEGIN	
 	SET NOCOUNT ON;
 	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED	
-	BEGIN TRY
-		DECLARE @EmpLegalEntiyId BIGINT = 0;
-		DECLARE @CurrntEmpTimeZoneDesc VARCHAR(100) = '';
-		
-		SELECT @EmpLegalEntiyId = LegalEntityId FROM DBO.Employee WHERE EmployeeId = @EmployeeId;
-		SELECT 
-				@CurrntEmpTimeZoneDesc = COALESCE(
-					ETZ.[Description],  -- Prefer Employee's TimeZone description if available
-					LTZ.[Description]   -- Fallback to LegalEntity's TimeZone description
-				)
-			FROM 
-				dbo.Employee E WITH (NOLOCK) 
-			LEFT JOIN 
-				dbo.TimeZone ETZ WITH (NOLOCK) 
-				ON E.TimeZoneId = ETZ.TimeZoneId
-			LEFT JOIN 
-				dbo.LegalEntity LE WITH (NOLOCK) 
-				ON E.LegalEntityId = LE.LegalEntityId
-			LEFT JOIN 
-				dbo.TimeZone LTZ WITH (NOLOCK) 
-				ON LE.TimeZoneId = LTZ.TimeZoneId
-			WHERE 
-				E.EmployeeId = @EmployeeId; -- Use appropriate filter for the specific employee
+    BEGIN TRY
+        DECLARE @EmpLegalEntiyId BIGINT = 0;
+        DECLARE @CurrntEmpTimeZoneDesc VARCHAR(100) = '';
 
+        SELECT @EmpLegalEntiyId = LegalEntityId FROM dbo.Employee WHERE EmployeeId = @EmployeeId;
 
-		IF @IsDeleted IS NULL  
-		Begin  
-		 Set @IsDeleted=0  
-		End  
-			;WITH CTE AS (
-				SELECT 
-					TIM.TaskInstructionId,
-					TIM.ParentId,
-					TIM.IsParent,
-					TIM.TaskId,
-					T.[Description] TaskName,
-					TIM.Title InstructionTitle,
-					TIM.SequenceNumber AS SequenceNumber,
-					TIM.[Description] InstructionDetails,
-					TIM.MasterCompanyId,
-					TIM.CreatedBy,
-					TIM.UpdatedBy,
-					CASE WHEN @EmployeeId != 0 AND @CurrntEmpTimeZoneDesc != '' THEN 
-						CASE WHEN CAST(TIM.CreatedDate AS DATE) = CAST('0001-01-01 00:00:00' AS DATE) THEN NULL ELSE (CAST(DBO.ConvertUTCtoLocal(TIM.CreatedDate, @CurrntEmpTimeZoneDesc) AS DATETIME)) END 
-					ELSE (CAST(TIM.CreatedDate AS DATETIME)) END CreatedDate,
-					CASE WHEN @EmployeeId != 0 AND @CurrntEmpTimeZoneDesc != '' THEN 
-						CASE WHEN CAST(TIM.UpdatedDate AS DATE) = CAST('0001-01-01 00:00:00' AS DATE) THEN NULL ELSE (CAST(DBO.ConvertUTCtoLocal(TIM.UpdatedDate, @CurrntEmpTimeZoneDesc) AS DATETIME)) END 
-					ELSE (CAST(TIM.UpdatedDate AS DATETIME)) END UpdatedDate,
-					TIM.IsActive,
-					TIM.IsDeleted,
-					DENSE_RANK() OVER (ORDER BY TIM.TaskId) AS TaskSrNo,
-					ISNULL(IsDefaultInstruction,0)IsDefaultInstruction,
-					ISNULL(IsParentInstruction,0)IsParentInstruction
-				FROM dbo.TaskInstructionMaster TIM WITH(NOLOCK)
-				LEFT JOIN DBO.Task T WITH(NOLOCK) 
-					ON T.TaskId = TIM.TaskId
-				WHERE TIM.MasterCompanyId = @MasterCompanyId 
-					AND ISNULL(TIM.IsActive, 0) = 1 
-					AND ISNULL(TIM.IsDeleted, 0) = @IsDeleted
+        SELECT
+                @CurrntEmpTimeZoneDesc = COALESCE(
+                    ETZ.[Description],  -- Prefer Employee's TimeZone description if available
+                    LTZ.[Description]   -- Fallback to LegalEntity's TimeZone description
+                )
+            FROM
+                dbo.Employee E WITH (NOLOCK)
+            LEFT JOIN
+                dbo.TimeZone ETZ WITH (NOLOCK)
+                ON E.TimeZoneId = ETZ.TimeZoneId
+            LEFT JOIN
+                dbo.LegalEntity LE WITH (NOLOCK)
+                ON E.LegalEntityId = LE.LegalEntityId
+            LEFT JOIN
+                dbo.TimeZone LTZ WITH (NOLOCK)
+                ON LE.TimeZoneId = LTZ.TimeZoneId
+            WHERE
+                E.EmployeeId = @EmployeeId;
 
-			)
+        IF @IsDeleted IS NULL
+        BEGIN
+            SET @IsDeleted = 0;
+        END
 
-			SELECT 	* INTO #LeafTempTbl FROM CTE;			
-			SELECT * FROM #LeafTempTbl ORDER BY TaskId,SequenceNumber;
+        ;WITH CTE AS (
+            SELECT
+                TIM.TaskInstructionId,
+                TIM.ParentId,
+                TIM.IsParent,
+                T.TaskId,
+                T.[Description] TaskName,
+                TIM.Title InstructionTitle,
+                TIM.SequenceNumber AS SequenceNumber,
+                TIM.[Description] InstructionDetails,
+                TIM.MasterCompanyId,
+                T.CreatedBy,
+                T.UpdatedBy,
+                CASE WHEN @EmployeeId != 0 AND @CurrntEmpTimeZoneDesc != '' THEN
+                    CASE WHEN CAST(TIM.CreatedDate AS DATE) = CAST('0001-01-01 00:00:00' AS DATE) THEN NULL ELSE (CAST(DBO.ConvertUTCtoLocal(T.CreatedDate, @CurrntEmpTimeZoneDesc) AS DATETIME)) END
+                ELSE (CAST(TIM.CreatedDate AS DATETIME)) END CreatedDate,
+                CASE WHEN @EmployeeId != 0 AND @CurrntEmpTimeZoneDesc != '' THEN
+                    CASE WHEN CAST(TIM.UpdatedDate AS DATE) = CAST('0001-01-01 00:00:00' AS DATE) THEN NULL ELSE (CAST(DBO.ConvertUTCtoLocal(T.UpdatedDate, @CurrntEmpTimeZoneDesc) AS DATETIME)) END
+                ELSE (CAST(TIM.UpdatedDate AS DATETIME)) END UpdatedDate,
+                TIM.IsActive,
+                TIM.IsDeleted,
+                DENSE_RANK() OVER (ORDER BY T.TaskId) AS TaskSrNo,
+                ISNULL(TIM.IsDefaultInstruction, 0) IsDefaultInstruction,
+                ISNULL(TIM.IsParentInstruction, 0) IsParentInstruction
+            FROM dbo.Task T WITH (NOLOCK)
+            LEFT JOIN dbo.TaskInstructionMaster TIM WITH (NOLOCK)
+                ON TIM.TaskId = T.TaskId
+                AND TIM.MasterCompanyId = @MasterCompanyId
+                AND ISNULL(TIM.IsActive, 0) = 1
+                AND ISNULL(TIM.IsDeleted, 0) = @IsDeleted
+            WHERE T.MasterCompanyId = @MasterCompanyId 
+              AND ISNULL(T.IsActive, 0) = 1
+                AND ISNULL(T.IsDeleted, 0) = @IsDeleted
+        )
+        SELECT * INTO #LeafTempTbl FROM CTE;
+        SELECT * FROM #LeafTempTbl ORDER BY TaskId, SequenceNumber;
 		
 	END TRY    
 	BEGIN CATCH      

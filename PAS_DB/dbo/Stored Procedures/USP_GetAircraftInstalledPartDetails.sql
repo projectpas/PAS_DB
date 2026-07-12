86b73b155511aa48e876716c5d882d170a0b5e30
Amit Gediya <amit.ghediya@silverxis.comm>
2026-07-12 22:27:33 +0530

    more update

--- PAS_DB/dbo/Stored Procedures/USP_GetAircraftInstalledPartDetails.sql
+++ PAS_DB/dbo/Stored Procedures/USP_GetAircraftInstalledPartDetails.sql
@@ -32,6 +32,7 @@
 ** 18   2026-06-03   Amit Ghediya       Update for get latest wo created from ACIC [PN-16699]
 ** 19   30/06/2026	 Amit Ghediya	    Update for Engine data [PN-17075]
 ** 20   07/07/2026	  Kishor Makwana	[PN-17162] Updated for Get ServiceLifeUnitMonthsOrDays, ServiceLifeLimit
+** 21   10/07/2026	  Amit Ghediya		Update condition
 
 *********************/
 CREATE       PROCEDURE [dbo].[USP_GetAircraftInstalledPartDetails]
@@ -88,10 +89,10 @@ BEGIN
         (
             SELECT
                 AIPD.AircraftInstalledPartDetailsId,
-				ISNULL(ARH.MakeType, ERH.MakeType) AS MakeType,
-				ISNULL(ARH.AircraftModel, ERH.EngineModel) AS Model,
-				ISNULL(ARH.TailNum, ERH.TailNum) AS TailNum,
-				ISNULL(ARH.SerialNum, ERH.SerialNum) AS SerialNum,
+				CASE WHEN ISNULL(@IsFromAircraft,0) = 1 THEN ARH.MakeType ELSE ERH.MakeType END AS MakeType,
+				CASE WHEN ISNULL(@IsFromAircraft,0) = 1 THEN ARH.AircraftModel ELSE ERH.EngineModel END AS Model,
+				CASE WHEN ISNULL(@IsFromAircraft,0) = 1 THEN ARH.TailNum ELSE ERH.EngineName END AS TailNum,
+				CASE WHEN ISNULL(@IsFromAircraft,0) = 1 THEN ARH.SerialNum ELSE ERH.SerialNum END AS SerialNum,
                 AIPD.ATAChapterId,
 				CONCAT_WS(' - ',
 				   NULLIF(IMAM.Level1, ''),
@@ -102,9 +103,9 @@ BEGIN
                 AIPD.PartDescription,
 				AIPD.SequenceNum,
 				AIPD.ItemMasterId,
-				ISNULL(ARH.AircraftRegistryId, ERH.EngineRegistryId) AS AircraftRegistryId,
+				CASE WHEN ISNULL(@IsFromAircraft,0) = 1 THEN ARH.AircraftRegistryId ELSE ERH.EngineRegistryId END AS AircraftRegistryId,
 				ERH.EngineRegistryId,
-				COALESCE(ARH.AircraftRegistryNumber, ERH.EngineRegistryNumber, '') AS AircraftRegistryNumber,
+				CASE WHEN ISNULL(@IsFromAircraft,0) = 1 THEN ARH.AircraftRegistryNumber ELSE ERH.EngineRegistryNumber END AS AircraftRegistryNumber,
 				STK.Condition,
 				STK.ConditionId,
 				STK.StockLineNumber,
@@ -112,7 +113,6 @@ BEGIN
 				STK.SerialNumber,
 				STK.StockLineId,
 				CSTK.StockLineId AS ReStockLineId,
-				--CASE WHEN ISNULL(@IsFromAircraft,0) = 1 THEN CSTK.StockLineId ELSE CSTK.StockLineId END
 				STK.QuantityAvailable,
 				STK.QuantityOnHand,
 				CASE WHEN STK.IsCustomerStock = 1 THEN 'Yes' ELSE 'No' END AS IsCustomerStock, 
@@ -184,7 +184,7 @@ BEGIN
 			INNER JOIN dbo.ItemMaster IM WITH (NOLOCK) ON AIPD.ItemMasterId = IM.ItemMasterId
 			LEFT JOIN dbo.AircraftStatus AST WITH (NOLOCK) ON AST.AircraftStatusId = ARH.AircraftStatusId
 			LEFT JOIN dbo.Stockline STK WITH (NOLOCK) ON STK.StockLineId = AIPD.StockLineId
-			LEFT JOIN [dbo].[Stockline] CSTK WITH (NOLOCK) ON CSTK.[StockLineId] = ISNULL(ARH.[StockLineId], ERH.[StockLineId])
+			LEFT JOIN [dbo].[Stockline] CSTK WITH (NOLOCK) ON CSTK.[StockLineId] = CASE WHEN ISNULL(@IsFromAircraft,0) = 1 THEN ARH.[StockLineId] ELSE  ERH.[StockLineId] END
 			LEFT JOIN dbo.PurchaseOrderPart POP WITH (NOLOCK) ON POP.AircraftInstalledPartDetailsId = AIPD.AircraftInstalledPartDetailsId
 			LEFT JOIN dbo.PurchaseOrder PO WITH (NOLOCK) ON PO.PurchaseOrderId = POP.PurchaseOrderId
 			LEFT JOIN dbo.RepairOrderPart ROP WITH (NOLOCK) ON ROP.AircraftInstalledPartDetailsId = AIPD.AircraftInstalledPartDetailsId

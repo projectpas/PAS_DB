68bc97af9dda3d2c6a36909039631f6e3f3b70db
projectpas <54223580+projectpas@users.noreply.github.com>
2026-07-27 07:17:13 +0530

    Refactor GetNonStockPNManufacturerCombinationCreated
    Refactored stored procedure to remove CROSS JOIN and replaced it with sequential CTEs to eliminate redundant Cartesian product. Updated change history to reflect the latest modifications.

--- PAS_DB/dbo/Stored Procedures/Procs1/GetNonStockPNManufacturerCombinationCreated.sql
+++ PAS_DB/dbo/Stored Procedures/Procs1/GetNonStockPNManufacturerCombinationCreated.sql
@@ -1,71 +1,77 @@
-﻿
-
 /*************************************************************           
  ** File:   [GetNonStockPNManufacturerCombinationCreated]           
  ** Author:  Moin Bloch
  ** Description: This stored procedure is used to get CurrentStlNo
  ** Purpose:         
  ** Date:   02/02/2022        
-          
- ** PARAMETERS: @@PurchaseOrderId bigint
-         
+ ** PARAMETERS: @MasterCompanyId INT
  ** RETURN VALUE:           
  **************************************************************           
  ** Change History           
  **************************************************************           
- ** PR   Date         Author		Change Description            
- ** --   --------     -------		--------------------------------
-    1    02/02/2022  Moin Bloch     Created
-    2    24/July/2026	 RAJESH GAMI	[PN-17350] - Converted legacy dbo.NonStockInventory/dbo.ItemMasterNonStock references to dbo.Stockline/dbo.ItemMaster with ISNULL(IsNonStock,0)=1
-
+ ** PR   Date          Author		Change Description            
+ ** --   --------      -------		--------------------------------
+    1    02/02/2022    Moin Bloch     Created
+    2    24/July/2026  RAJESH GAMI    [PN-17350] - Converted legacy dbo.NonStockInventory/dbo.ItemMasterNonStock references to dbo.Stockline/dbo.ItemMaster with ISNULL(IsNonStock,0)=1
+    3    27/July/2026  <You>          Removed CROSS JOIN; replaced with sequential CTEs. Cartesian product was redundant.
 -- EXEC [GetNonStockPNManufacturerCombinationCreated] 179
 ************************************************************************/
 CREATE PROCEDURE [dbo].[GetNonStockPNManufacturerCombinationCreated]
-@MasterCompanyId INT = NULL
+    @MasterCompanyId INT = NULL
 AS
 BEGIN
-	SET NOCOUNT ON;
-	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
-	BEGIN TRY
-		;WITH CTE_Stockline (ItemMasterId, ManufacturerId, StockLineId) AS
-		(
-			SELECT ItemMasterId = ac.ItemMasterId,
-				   ac.ManufacturerId,
-				MAX(ac.StockLineId) StockLineId
-			FROM (SELECT DISTINCT ItemMasterId FROM DBO.Stockline WITH (NOLOCK) WHERE ISNULL(IsNonStock,0) = 1) ac1 CROSS JOIN
-				(SELECT DISTINCT ManufacturerId FROM DBO.Stockline WITH (NOLOCK) WHERE ISNULL(IsNonStock,0) = 1) ac2 LEFT JOIN
-				DBO.Stockline ac WITH (NOLOCK)
-				ON ac.ItemMasterId = ac1.ItemMasterId AND ac.ManufacturerId = ac2.ManufacturerId AND ISNULL(ac.IsNonStock,0) = 1
-			WHERE ac.MasterCompanyId = @MasterCompanyId
-			GROUP BY ac.ItemMasterId, ac.ManufacturerId
-			HAVING COUNT(ac.ItemMasterId) > 0
-		)
+    SET NOCOUNT ON;
+    SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+    BEGIN TRY
 
-		SELECT CSTL.ItemMasterId,
-				CSTL.ManufacturerId,
-				StockLineNumber = STL.StockLineNumber,
-				ISNULL(IM.CurrentStlNo, 0) AS CurrentStlNo,
-				IM.isSerialized
-		FROM CTE_Stockline CSTL INNER JOIN DBO.Stockline STL WITH (NOLOCK)
-		INNER JOIN DBO.ItemMaster IM ON STL.ItemMasterId = IM.ItemMasterId AND STL.ManufacturerId = IM.ManufacturerId AND ISNULL(IM.IsNonStock,0) = 1
-		ON CSTL.StockLineId = STL.StockLineId
-		WHERE ISNULL(STL.IsNonStock,0) = 1
+        ;WITH CTE_NonStockLine AS
+        (
+            -- Step 1: Stage only the relevant non-stock rows for this company
+            SELECT  ItemMasterId,
+                    ManufacturerId,
+                    StockLineId
+            FROM    dbo.Stockline WITH (NOLOCK)
+            WHERE   ISNULL(IsNonStock, 0) = 1
+              AND   MasterCompanyId = @MasterCompanyId
+        ),
+        CTE_Stockline AS
+        (
+            -- Step 2: Latest StockLineId per ItemMaster / Manufacturer combination
+            SELECT  ItemMasterId,
+                    ManufacturerId,
+                    MAX(StockLineId) AS StockLineId
+            FROM    CTE_NonStockLine
+            GROUP BY ItemMasterId, ManufacturerId
+        )
+        SELECT  CSTL.ItemMasterId,
+                CSTL.ManufacturerId,
+                StockLineNumber = STL.StockLineNumber,
+                ISNULL(IM.CurrentStlNo, 0) AS CurrentStlNo,
+                IM.isSerialized
+        FROM    CTE_Stockline CSTL
+                INNER JOIN dbo.Stockline STL WITH (NOLOCK)
+                    ON  STL.StockLineId = CSTL.StockLineId
+                    AND ISNULL(STL.IsNonStock, 0) = 1
+                INNER JOIN dbo.ItemMaster IM WITH (NOLOCK)
+                    ON  IM.ItemMasterId   = STL.ItemMasterId
+                    AND IM.ManufacturerId = STL.ManufacturerId
+                    AND ISNULL(IM.IsNonStock, 0) = 1;
 
-	END TRY
-	BEGIN CATCH
-			DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
+    END TRY
+    BEGIN CATCH
+            DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name() 
 -----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
             , @AdhocComments     VARCHAR(150)    = 'GetNonStockPNManufacturerCombinationCreated' 
             , @ProcedureParameters VARCHAR(3000)  = ''
             , @ApplicationName VARCHAR(100) = 'PAS'
 -----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
-            exec spLogException 
+            EXEC spLogException 
                     @DatabaseName			= @DatabaseName
                     , @AdhocComments			= @AdhocComments
                     , @ProcedureParameters		= @ProcedureParameters
-                    , @ApplicationName			=  @ApplicationName
+                    , @ApplicationName			= @ApplicationName
                     , @ErrorLogID              = @ErrorLogID OUTPUT ;
-            RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)
+            RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1, @ErrorLogID)
             RETURN(1);
     END CATCH 
 END

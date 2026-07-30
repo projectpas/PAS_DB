4ec6a0ab528f317f25230f7165a9381050b075e3
Amit Gediya <amit.ghediya@silverxis.comm>
2026-07-30 16:36:57 +0530

    more update

--- PAS_DB/dbo/Stored Procedures/USP_GetOpenWorksheetsForAircraft.sql
+++ PAS_DB/dbo/Stored Procedures/USP_GetOpenWorksheetsForAircraft.sql
@@ -12,13 +12,14 @@
 ** 1    24/07/2026  Amit Ghediya      Created
 ** 2    30/07/2026  Amit Ghediya      Update for get MakeTypeId,ModelId data match Ws only [PN-17477]
 ************************************************************/
-CREATE   PROCEDURE [dbo].[USP_GetOpenWorksheetsForAircraft]
+CREATE    PROCEDURE [dbo].[USP_GetOpenWorksheetsForAircraft]
     @PageNumber         INT          = 1,
     @PageSize           INT          = 10,
     @SortColumn         VARCHAR(50)  = 'createdDate',
     @SortOrder          VARCHAR(4)   = 'DESC',
     @AircraftRegistryId BIGINT,
-    @MasterCompanyId    INT
+    @MasterCompanyId    INT,
+    @IsFromAircraft     BIT
 AS
 BEGIN
 
@@ -27,13 +28,25 @@ BEGIN
     BEGIN TRY
 
 		DECLARE @MakeTypeId BIGINT = 0,
-				@ModelId BIGINT = 0;
+				@ModelId BIGINT = 0,
+				@Model VARCHAR(100);
 
         SET @PageNumber = CASE WHEN @PageNumber < 1 THEN 1 ELSE @PageNumber END;
         SET @PageSize   = CASE WHEN @PageSize < 1 THEN 10 WHEN @PageSize > 500 THEN 500 ELSE @PageSize END;
         SET @SortOrder  = CASE WHEN UPPER(@SortOrder) = 'ASC' THEN 'ASC' ELSE 'DESC' END;
 
-		SELECT @MakeTypeId = [MakeTypeId],@ModelId = [AircraftModelId] FROM [dbo].[AircraftRegistryHeader] WITH (NOLOCK) WHERE [AircraftRegistryId]  = @AircraftRegistryId
+		IF ISNULL(@IsFromAircraft, 1) = 1
+		BEGIN
+			SELECT @MakeTypeId = [MakeTypeId], @ModelId = [AircraftModelId]
+			FROM [dbo].[AircraftRegistryHeader] WITH (NOLOCK)
+			WHERE [AircraftRegistryId] = @AircraftRegistryId
+		END
+		ELSE
+		BEGIN
+			SELECT @Model = [EngineModel]
+			FROM [dbo].[EngineRegistryHeader] WITH (NOLOCK)
+			WHERE [EngineRegistryId] = @AircraftRegistryId
+		END
 
         ;WITH OpenWorksheets AS
         (
@@ -42,7 +55,9 @@ BEGIN
                 WH.WorksheetNumber,
                 WH.TailNum,
                 WH.MakeType,
+				WH.AircraftModel,
                 WH.SerialNum,
+				ERH.EngineName,
                 CASE WHEN ISNULL(WH.IsFromAircraft,0) = 1 THEN ARH.CustomerName ELSE ERH.CustomerName END AS CustomerName,
                 WSS.Status,
                 WH.CreatedDate,
@@ -56,8 +71,13 @@ BEGIN
 				AND ISNULL(WH.IsFromAircraft,0) = 0
             LEFT JOIN [dbo].[WorkSheetStatus] WSS WITH (NOLOCK)
                 ON WSS.WorkSheetStatusId = WH.WorkSheetStatusId
-            WHERE WH.MakeTypeId = @MakeTypeId 
-			  AND WH.AircraftModelId = @ModelId
+            WHERE
+			  (
+				(ISNULL(@IsFromAircraft,1) = 1 AND WH.MakeTypeId = @MakeTypeId AND WH.AircraftModelId = @ModelId)
+				OR
+				(ISNULL(@IsFromAircraft,1) = 0 AND WH.AircraftModel = @Model)
+			  )
+			  AND ISNULL(WH.IsFromAircraft,0) = ISNULL(@IsFromAircraft,1)
               AND WH.MasterCompanyId = @MasterCompanyId
               AND WH.IsDeleted = 0
               AND WH.WorkSheetStatusId = 1 -- Open
@@ -66,8 +86,10 @@ BEGIN
             WorksheetHeaderId,
             WorksheetNumber,
             TailNum,
+			AircraftModel,
             MakeType,
             SerialNum,
+			EngineName,
             CustomerName,
             Status,
             CreatedDate,
@@ -92,6 +114,7 @@ BEGIN
             @AdhocComments       VARCHAR(150)  = 'USP_GetOpenWorksheetsForAircraft',
             @ProcedureParameters VARCHAR(3000) =
                 '@AircraftRegistryId = ' + ISNULL(CAST(@AircraftRegistryId AS VARCHAR(20)), 'NULL')
+                + ', @IsFromAircraft = ' + ISNULL(CAST(@IsFromAircraft AS VARCHAR(5)), 'NULL')
                 + ', @MasterCompanyId = ' + ISNULL(CAST(@MasterCompanyId AS VARCHAR(20)), 'NULL'),
             @ApplicationName     VARCHAR(100)  = 'PAS';
 

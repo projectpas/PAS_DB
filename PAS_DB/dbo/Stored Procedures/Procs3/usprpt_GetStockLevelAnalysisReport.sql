2f5ce61fd2a72d5eb129b2b678e70a5aa82f9f0a
priyansh <patel.priyansh@silverxis.com>
2026-07-31 17:29:41 +0530

    PN-17228

--- PAS_DB/dbo/Stored Procedures/Procs3/usprpt_GetStockLevelAnalysisReport.sql
+++ PAS_DB/dbo/Stored Procedures/Procs3/usprpt_GetStockLevelAnalysisReport.sql
@@ -18,7 +18,7 @@
 	4    01/July/2026			 RAJESH GAMI						[PN-17008] - Merge Non Stock Inventory to ItemMaster : Get only Stock Inventory Data Where IsNonStock = 0
 	5    09/July/2026			 RAJESH GAMI						[PN-17009] - Merge Non-Stock Inventory to Stockline : Get only Stock Inventory Data Where IsNonStock = 0
 	6    24/July/2026			 RAJESH GAMI						[PN-17350] - Removed obsolete ItemMaster/Stockline IsNonStock=0 filters (2) to allow Non-Stock items in Stock Level Analysis Report
-	7    31-May-2026		Priyansh Patel			[PN-17228] - Fix the issue with record not coming when stockline created manually
+	7    31-July-2026		Priyansh Patel			[PN-17228] - Fix the issue with record not coming when stockline created manually
 EXECUTE   [dbo].[usprpt_GetStockLevelAnalysisReport] '2','2010-01-01','2022-04-26',null,1,10
 **************************************************************/
 CREATE   PROCEDURE [dbo].[usprpt_GetStockLevelAnalysisReport] 

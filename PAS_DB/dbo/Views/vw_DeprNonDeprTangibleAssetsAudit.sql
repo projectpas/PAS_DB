28b957130928ba0de6cf54810ea8b828482465f9
Abhishek Jirawala <abhishek@silverxis.com>
2026-08-01 15:21:41 +0530

    PN-16823 Asset Changes

--- PAS_DB/dbo/Views/vw_DeprNonDeprTangibleAssetsAudit.sql
+++ PAS_DB/dbo/Views/vw_DeprNonDeprTangibleAssetsAudit.sql
@@ -1,37 +1,47 @@
 ﻿
 
+
 CREATE VIEW [dbo].[vw_DeprNonDeprTangibleAssetsAudit]
 AS
 SELECT
-	[DAT].[AuditDeprNonDeprTangibleAssetsId] AS PkID,
-	[DAT].[DeprNonDeprTangibleAssetsId] AS ID,
-	[ATY].[TangibleClassName] AS [Tangible Class],
-	[DAT].[AssetAttributeTypeName] AS [Name],
-	[DAT].[Description] AS [Description],
-	[DM].[AssetDepreciationMethodName] AS [Depreciation Method],
-	[PER].[PercentValue] AS [Residual Percentage],
-	[DAT].[AssetLife] AS [Asset Life],
-	[FI].[Name] AS [Depreciation Frequency],
-	[CLGL].[AccountName] AS [Calibrated GL Account],
-	[AQGL].[AccountName] AS [Acquired GL Account],
-	[DPGL].[AccountName] AS [Depr Expense GL Account],
-	[ADGL].[AccountName] AS [Accum Depr GL Account],
-	[ASGL].[AccountName] AS [Asset Sale GL Account],
-	[WOGL].[AccountName] AS [Asset Write Off GL Account],
-	[WDGL].[AccountName] AS [Asset Write Down GL Account],
-	[DAT].[UpdatedBy] AS [Updated By],
-	[DAT].[UpdatedDate] AS [Updated Date],
-	[DAT].[IsActive] AS [Active],
-	[DAT].[IsDeleted] AS [Deleted]
-FROM dbo.DeprNonDeprTangibleAssetsAudit DAT WITH(NOLOCK)
-	LEFT JOIN dbo.TangibleClass ATY WITH(NOLOCK) ON DAT.TangibleClassId=ATY.TangibleClassId
-	LEFT JOIN dbo.AssetDepreciationMethod DM WITH(NOLOCK) ON DAT.AssetDeprMethodId=DM.AssetDepreciationMethodId
-	LEFT JOIN dbo.AssetDepreciationFrequency FI WITH(NOLOCK) ON DAT.DepreciationFrequencyId=FI.AssetDepreciationFrequencyId
-	LEFT JOIN dbo.GLAccount CLGL WITH(NOLOCK) ON DAT.CalibratedGLAccountId=CLGL.GLAccountId
-	LEFT JOIN dbo.GLAccount AQGL WITH(NOLOCK) ON DAT.AcquiredGLAccountId=AQGL.GLAccountId
-	LEFT JOIN dbo.GLAccount DPGL WITH(NOLOCK) ON DAT.DeprExpenseGLAccountId=DPGL.GLAccountId
-	LEFT JOIN dbo.GLAccount ADGL WITH(NOLOCK) ON DAT.AccumDeprGLAccountId=ADGL.GLAccountId
-	LEFT JOIN dbo.GLAccount ASGL WITH(NOLOCK) ON DAT.AssetSaleGLAccountId=ASGL.GLAccountId
-	LEFT JOIN dbo.GLAccount WOGL WITH(NOLOCK) ON DAT.AssetWriteOffGLAccountId=WOGL.GLAccountId
-	LEFT JOIN dbo.GLAccount WDGL WITH(NOLOCK) ON DAT.AssetWriteDownGLAccountId=WDGL.GLAccountId
-	LEFT JOIN dbo.[Percent] PER WITH(NOLOCK) ON DAT.ResidualPercentage=PER.PercentId
+	AAT.[AuditDeprNonDeprTangibleAssetsId] AS PkID
+	,AAT.[DeprNonDeprTangibleAssetsId] AS ID
+	,AAT.[AssetAttributeTypeId]
+	  ,ATC.[DeprNonDeprTangibleClassTypeName] 'TangibleClassName'
+	  ,AT2.[AssetAttributeTypeName]
+	  ,AAT.[Description] AS [Description]
+	  ,ADM.[AssetDepreciationMethodName] 'DepreciationMethodName'
+
+	  ,AAT.[ResidualPercentage]
+	  ,PER.[PercentValue] 'ResidualPercentageValue'
+	  ,AAT.[AssetLife]
+	  ,FI.[Name] 'DepreciationFrequencyName'
+
+	  ,CGL.[AccountCode] +'-'+ CGL.[AccountName] 'CalibratedGLAccountName'
+	  ,ADGL.[AccountCode] +'-'+ ADGL.[AccountName] 'AccumDeprGLAccountName'
+	  
+	  ,AGL.[AccountCode] +'-'+ AGL.[AccountName] 'AcquiredGLAccountName'
+	  ,DGL.[AccountCode] +'-'+ DGL.[AccountName] 'DeprExpenseGLAccountName'
+	  ,ASG.[AccountCode] +'-'+ ASG.[AccountName] 'AssetSaleGLAccountName'
+	  ,AWG.[AccountCode] +'-'+ AWG.[AccountName] 'AssetWriteOffGLAccountName'
+	  ,ARG.[AccountCode] +'-'+ ARG.[AccountName] 'AssetWriteDownGLAccountName'
+      ,AAT.[CreatedBy]
+      ,AAT.[CreatedDate]
+      ,AAT.[UpdatedBy]
+      ,AAT.[UpdatedDate]
+      ,AAT.[IsActive]
+      ,AAT.[IsDeleted]
+FROM dbo.DeprNonDeprTangibleAssetsAudit AAT WITH(NOLOCK)
+	LEFT JOIN [dbo].[AssetAttributeType] AT2 WITH (NOLOCK) ON AAT.AssetAttributeTypeId = AT2.AssetAttributeTypeId
+  --LEFT JOIN [dbo].[TangibleClass] ATC WITH (NOLOCK) ON AAT.TangibleClassId = ATC.TangibleClassId
+  LEFT JOIN [dbo].[DeprNonDeprTangibleClassTypes] ATC WITH (NOLOCK) ON AAT.TangibleClassId = ATC.DeprNonDeprTangibleClassTypeId
+  LEFT JOIN [dbo].[AssetDepreciationMethod] ADM WITH (NOLOCK) ON AAT.AssetDeprMethodId = ADM.AssetDepreciationMethodId
+  LEFT JOIN [dbo].[Percent] PER WITH (NOLOCK) ON AAT.ResidualPercentage = PER.PercentId
+  LEFT JOIN [dbo].[AssetDepreciationFrequency] FI WITH (NOLOCK) ON AAT.DepreciationFrequencyId = FI.AssetDepreciationFrequencyId
+  LEFT JOIN [dbo].[GLAccount] AGL WITH (NOLOCK) ON AAT.AcquiredGLAccountId = AGL.GLAccountId
+  LEFT JOIN [dbo].[GLAccount] DGL WITH (NOLOCK) ON AAT.DeprExpenseGLAccountId = DGL.GLAccountId
+  LEFT JOIN [dbo].[GLAccount] ASG WITH (NOLOCK) ON AAT.AssetSaleGLAccountId = ASG.GLAccountId
+  LEFT JOIN [dbo].[GLAccount] AWG WITH (NOLOCK) ON AAT.AssetWriteOffGLAccountId = AWG.GLAccountId
+  LEFT JOIN [dbo].[GLAccount] ARG WITH (NOLOCK) ON AAT.AssetWriteDownGLAccountId = ARG.GLAccountId
+  LEFT JOIN [dbo].[GLAccount] CGL WITH (NOLOCK) ON AAT.CalibratedGLAccountId = CGL.GLAccountId
+  LEFT JOIN [dbo].[GLAccount] ADGL WITH (NOLOCK) ON AAT.AccumDeprGLAccountId = ADGL.GLAccountId

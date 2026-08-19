9261ba74101681bf37322f9e2e6d3c06c0a9dc79
Abhishek Jirawala <abhishek@silverxis.com>
2026-08-19 17:34:26 +0530

    PN-16823 Asset Changes (perf indexes, extracted from ee382cfdd)
    Cherry-picked only the Asset-related hunks (AssetInventory/BatchStatus
    performance indexes) from ee382cfdd, which was a tangled merge commit
    that also carried unrelated SalesOrder/PurchaseOrder/RepairOrder/
    Stockline/GoodsReceipt changes from SPRINT_67. Those unrelated changes
    were excluded.
    
    (cherry picked from commit ee382cfdd6002ba9e6dedc0956d8c9ac535a2c8a)

--- PAS_Database/dbo/Tables/Tables1/AssetInventory.sql
+++ PAS_Database/dbo/Tables/Tables1/AssetInventory.sql
@@ -242,3 +242,37 @@ CREATE NONCLUSTERED INDEX [IX_Asset_POPart_Date]
     ON [dbo].[AssetInventory]([PurchaseOrderPartRecordId] ASC, [CreatedDate] ASC)
     INCLUDE([Name], [Description], [CreatedBy], [UnitCost]);
 
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AssetInventory_ROPartRec_Perf]
+    ON [dbo].[AssetInventory]([RepairOrderPartRecordId] ASC)
+    INCLUDE([Name], [Description], [CreatedBy], [CreatedDate], [UnitCost]) WITH (FILLFACTOR = 90, DATA_COMPRESSION = PAGE);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AssetInventory_ROPartRec_Created_Perf]
+    ON [dbo].[AssetInventory]([RepairOrderPartRecordId] ASC, [CreatedDate] ASC)
+    INCLUDE([Name], [Description], [CreatedBy], [UnitCost]) WITH (FILLFACTOR = 90, DATA_COMPRESSION = PAGE);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AssetInventory_POPartRec_Perf]
+    ON [dbo].[AssetInventory]([PurchaseOrderPartRecordId] ASC)
+    INCLUDE([Name], [Description], [CreatedBy], [CreatedDate], [UnitCost]) WITH (FILLFACTOR = 90, DATA_COMPRESSION = PAGE);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AssetInventory_POPartRec_Created_Perf]
+    ON [dbo].[AssetInventory]([PurchaseOrderPartRecordId] ASC, [CreatedDate] ASC)
+    INCLUDE([Name], [Description], [CreatedBy], [UnitCost]) WITH (FILLFACTOR = 90, DATA_COMPRESSION = PAGE);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AssetInventory_Created_ROPartRec_Perf]
+    ON [dbo].[AssetInventory]([CreatedDate] ASC, [RepairOrderPartRecordId] ASC) WITH (FILLFACTOR = 90, DATA_COMPRESSION = PAGE);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_AssetInventory_Created_POPartRec_Perf]
+    ON [dbo].[AssetInventory]([CreatedDate] ASC, [PurchaseOrderPartRecordId] ASC) WITH (FILLFACTOR = 90, DATA_COMPRESSION = PAGE);
+

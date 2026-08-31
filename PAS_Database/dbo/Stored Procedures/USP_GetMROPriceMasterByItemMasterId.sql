ff0c6fada4d19187e94c9d1734627fea4a830ab6
Aayushi Patel <ayushi@silverxis.com>
2026-08-31 13:22:09 +0530

    PN-16987

---
+++ PAS_Database/dbo/Stored Procedures/USP_GetMROPriceMasterByItemMasterId.sql
@@ -0,0 +1,102 @@
+﻿/*********************           
+ ** File:   [USP_GetMROPriceMasterByItemMasterId]           
+ ** Author: Priyansh Patel
+ ** Description: This stored procedure returns all MRO Price Master records
+ **              by ItemMasterId 
+ ** Date:   26/09/2025
+
+ **********************           
+  ** Change History           
+ **********************           
+ ** PR   Date          Author  			Change Description            
+ ** --   --------      -------			---------------------------     
+    1    26/09/2025    Priyansh Patel   Created
+	2	 10/11/2025	   Priyansh Patel	Updated column name UnitPrice to FlatRatePrice
+    3    13/11/2025    Ayushi Patel     Sort By created Date
+	4    14/11/2025    Moin Bloch       Added Some Field
+	5    01/July/2026			 RAJESH GAMI						[PN-17008] - Merge Non Stock Inventory to ItemMaster : Get only Stock Inventory Data Where IsNonStock = 0
+    6    28/08/2026  Ayushi Patel       [PN-16987]added effective date in the select statement
+**********************/
+-- Example: EXEC USP_GetMROPriceMasterByItemMasterId 97005, 0, 1
+
+CREATE   PROCEDURE [dbo].[USP_GetMROPriceMasterByItemMasterId] 
+    @ItemMasterId BIGINT = NULL, 
+	@IsDeleted BIT,
+	@MasterCompanyId int = 0
+AS
+BEGIN
+    SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+
+		IF @ItemMasterId=0
+		BEGIN
+			SET @ItemMasterId=NULL
+		END
+
+        BEGIN
+            SELECT 
+                  MPM.[MROPriceMasterId],
+                  MPM.[ItemMasterId],
+				  (ISNULL(UPPER(IM.[PartNumber]),'')) 'PartNumber',
+				  (ISNULL(UPPER(IM.[PartDescription]),'')) 'PartDescription',
+				  (ISNULL(UPPER(IM.[ManufacturerName]),'')) 'ManufacturerName',
+                  MPM.[MasterCompanyId],
+                  MPM.[CustomerId],
+				  CM.[Name] [CustomerName],
+				  WS.[WorkScopeCode] [WorkScopeName],
+				  CR.[Code] [CurrencyName],
+                  MPM.[WorkscopeId],
+                  MPM.[FlatRatePrice],
+				  MPM.[CurrencyId],
+                  MPM.[StartDate],
+				  MPM.[EndDate],
+				  MPM.[EffectiveDate],
+                  MPM.[CreatedBy],
+                  MPM.[CreatedDate],
+                  MPM.[UpdatedBy],
+                  MPM.[UpdatedDate],
+                  MPM.[IsActive],
+                  MPM.[IsDeleted]
+            FROM [dbo].[MROPriceMaster] MPM WITH(NOLOCK)
+			INNER JOIN [dbo].[ItemMaster] IM WITH(NOLOCK) ON MPM.ItemMasterId = IM.ItemMasterId
+			 LEFT JOIN [dbo].[Customer] CM   WITH(NOLOCK) ON CM.CustomerId = MPM.CustomerId
+			 LEFT JOIN [dbo].[WorkScope] WS  WITH(NOLOCK) ON WS.WorkScopeId = MPM.WorkScopeId
+			 LEFT JOIN [dbo].[Currency] CR   WITH(NOLOCK) ON CR.CurrencyId = MPM.CurrencyId
+			WHERE (@ItemMasterId IS NULL OR  MPM.[ItemMasterId] = @ItemMasterId)
+              AND MPM.[MasterCompanyId] = @MasterCompanyId
+              AND MPM.[IsActive] = 1
+              AND MPM.[IsDeleted] = @IsDeleted
+             AND ISNULL(IM.IsNonStock,0) = 0
+            ORDER BY MPM.[ItemMasterId], MPM.[CreatedDate] DESC;
+        END
+    END TRY
+ BEGIN CATCH
+        IF @@TRANCOUNT > 0
+        BEGIN
+            PRINT 'ROLLBACK';
+            ROLLBACK TRAN;
+        END
+
+        DECLARE @ErrorLogID INT,
+                @DatabaseName VARCHAR(100) = DB_NAME(),
+		-----------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------
+                @AdhocComments VARCHAR(150) = '[USP_GetMROPriceMasterByItemMasterId]',
+                @ProcedureParameters VARCHAR(3000) = 
+                    '@ItemMasterId=''' + CAST(ISNULL(@ItemMasterId, 0) AS VARCHAR(100)) + ''',
+                     @MasterCompanyId=''' + CAST(ISNULL(@MasterCompanyId, 0) AS VARCHAR(100)) + ''',
+                     @IsDeleted=''' + CAST(ISNULL(@IsDeleted, 0) AS VARCHAR(100)) + '''',
+                @ApplicationName VARCHAR(100) = 'PAS';
+        -----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------
+        EXEC spLogException 
+            @DatabaseName = @DatabaseName,
+            @AdhocComments = @AdhocComments,
+            @ProcedureParameters = @ProcedureParameters,
+            @ApplicationName = @ApplicationName,
+            @ErrorLogID = @ErrorLogID OUTPUT;
+
+        RAISERROR ('Unexpected Error Occurred in the database. Please let the support team know of the error number : %d', 16, 1, @ErrorLogID);
+        RETURN (1);
+    END CATCH
+END
---
+++ PAS_Database/dbo/Tables/MROPriceMaster.sql
@@ -0,0 +1,53 @@
+CREATE TABLE [dbo].[MROPriceMaster] (
+    [MROPriceMasterId] BIGINT          IDENTITY (1, 1) NOT NULL,
+    [ItemMasterId]     BIGINT          NOT NULL,
+    [MasterCompanyId]  INT             NOT NULL,
+    [CustomerId]       BIGINT          NULL,
+    [WorkscopeId]      BIGINT          NOT NULL,
+    [FlatRatePrice]    DECIMAL (18, 6) NOT NULL,
+    [CurrencyId]       INT             NOT NULL,
+    [StartDate]        DATETIME2 (7)   NOT NULL,
+    [CreatedBy]        VARCHAR (50)    NOT NULL,
+    [CreatedDate]      DATETIME2 (7)   CONSTRAINT [DF_MROPriceMaster_CreatedDate] DEFAULT (getutcdate()) NOT NULL,
+    [UpdatedBy]        VARCHAR (50)    NOT NULL,
+    [UpdatedDate]      DATETIME2 (7)   CONSTRAINT [DF_MROPriceMaster_UpdatedDate] DEFAULT (getutcdate()) NOT NULL,
+    [IsActive]         BIT             CONSTRAINT [DF__MROPriceMaster__IsActive] DEFAULT ((1)) NOT NULL,
+    [IsDeleted]        BIT             CONSTRAINT [DF__MROPriceMaster__IsDeleted] DEFAULT ((0)) NOT NULL,
+    [EndDate]          DATETIME2 (7)   NULL,
+    [EffectiveDate]    DATETIME2 (7)   NULL,
+    CONSTRAINT [PK_MROPriceMaster] PRIMARY KEY CLUSTERED ([MROPriceMasterId] ASC),
+    CONSTRAINT [FK_MROPriceMaster_Currency] FOREIGN KEY ([CurrencyId]) REFERENCES [dbo].[Currency] ([CurrencyId]),
+    CONSTRAINT [FK_MROPriceMaster_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customer] ([CustomerId]),
+    CONSTRAINT [FK_MROPriceMaster_ItemMaster] FOREIGN KEY ([ItemMasterId]) REFERENCES [dbo].[ItemMaster] ([ItemMasterId]),
+    CONSTRAINT [FK_MROPriceMaster_MasterCompany] FOREIGN KEY ([MasterCompanyId]) REFERENCES [dbo].[MasterCompany] ([MasterCompanyId]),
+    CONSTRAINT [FK_MROPriceMaster_WorkScope] FOREIGN KEY ([WorkscopeId]) REFERENCES [dbo].[WorkScope] ([WorkScopeId]),
+    CONSTRAINT [Unique_MROPriceMaster] UNIQUE NONCLUSTERED ([ItemMasterId] ASC, [CustomerId] ASC, [MasterCompanyId] ASC, [WorkscopeId] ASC, [IsDeleted] ASC)
+);
+
+
+
+
+GO
+
+
+CREATE TRIGGER [dbo].[Trg_MROPriceMasterAudit]
+
+   ON  [dbo].[MROPriceMaster]
+
+   AFTER INSERT,DELETE,UPDATE
+
+AS
+
+BEGIN
+
+	INSERT INTO [dbo].[MROPriceMasterAudit]
+
+	SELECT * FROM INSERTED
+
+
+
+	SET NOCOUNT ON;
+
+
+
+END

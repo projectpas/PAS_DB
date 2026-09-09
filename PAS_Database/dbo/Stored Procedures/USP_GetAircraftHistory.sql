d4df9c21da43d32b741a4520ce66a6e41d809c18
Amit Gediya <amit.ghediya@silverxis.comm>
2026-09-09 22:46:54 +0530

    PN-17828 update for lease changes.

---
+++ PAS_Database/dbo/Stored Procedures/USP_GetAircraftHistory.sql
@@ -0,0 +1,160 @@
+﻿/*************************************************************             
+ ** File:   [USP_GetAircraftHistory]             
+ ** Author:  Amit Ghediya  
+ ** Description: This stored procedure is used AC History data  
+ ** Purpose:           
+ ** Date:   05/06/2026        
+            
+ ** RETURN VALUE:             
+ **************************************************************             
+ ** Change History             
+ **************************************************************             
+ ** PR   Date         Author  Change Description              
+ ** --   --------     -------  --------------------------------            
+    1    05/06/2026   Amit Ghediya    Created  
+	
+-- EXEC USP_GetHistory 1,1,'',0,'',1,1  
+************************************************************************/  
+CREATE    PROCEDURE [dbo].[USP_GetAircraftHistory]  
+ @PageNumber INT,  
+ @PageSize INT,  
+ @SortColumn VARCHAR(50)=null,  
+ @SortOrder INT,  
+ @GlobalFilter VARCHAR(50) = null,   
+ @HistoryId VARCHAR(20),  
+ @RefferenceId BIGINT,  
+ @AircraftRegistryNumber VARCHAR(MAX) = null,  
+ @PartNumber VARCHAR(MAX) = null,  
+ @OldValue VARCHAR(MAX) = null,  
+ @NewValue VARCHAR(MAX) = null,  
+ @Activity VARCHAR(30) = null,   
+ @HistoryText VARCHAR(MAX) = null,  
+ @MasterCompanyId INT,  
+ @CreatedDate DATETIME=null,  
+ @UpdatedDate  DATETIME=null,  
+ @CreatedBy VARCHAR(50)=null,  
+ @UpdatedBy VARCHAR(50)=null,
+ @EmployeeId bigint=null
+AS  
+BEGIN  
+ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED  
+ SET NOCOUNT ON;  
+ BEGIN TRY  
+  BEGIN TRANSACTION  
+   BEGIN  
+   DECLARE @RecordFrom INT;  
+
+	DECLARE @CurrntEmpTimeZoneDesc VARCHAR(100) = '';
+		
+	SELECT @CurrntEmpTimeZoneDesc = COALESCE(ETZ.[Description], LTZ.[Description]) FROM dbo.Employee E WITH (NOLOCK) 
+		LEFT JOIN dbo.TimeZone ETZ WITH (NOLOCK) ON E.TimeZoneId = ETZ.TimeZoneId
+		LEFT JOIN dbo.LegalEntity LE WITH (NOLOCK) ON E.LegalEntityId = LE.LegalEntityId
+		LEFT JOIN dbo.TimeZone LTZ WITH (NOLOCK) ON LE.TimeZoneId = LTZ.TimeZoneId
+	WHERE E.EmployeeId = @EmployeeId; 
+
+    SET @RecordFrom = (@PageNumber-1) * @PageSize;  
+      
+    IF @SortColumn IS NULL  
+    BEGIN  
+     SET @SortColumn=UPPER('CreatedDate')  
+    END   
+    Else  
+    BEGIN   
+     SET @SortColumn=UPPER(@SortColumn)  
+    END  
+  
+    ;With Result AS(  
+    SELECT HS.[HistoryId],  
+		 HS.[ModuleId],  
+		 --HS.[RefferenceId],  
+		 AR.AircraftRegistryNumber,  
+		 0 AS [partnumber],  
+		 HS.[OldValue],  
+		 HS.[NewValue],  
+		 HS.[HistoryText],  
+		 HS.[FieldsName],  
+		 HS.[CreatedBy],  
+		 HS.[CreatedDate],  
+		 HS.[UpdatedBy],  
+		 CASE WHEN CAST(HS.[UpdatedDate] AS DATE) = CAST('0001-01-01 00:00:00' AS DATE)THEN NULL ELSE (CAST(DBO.ConvertUTCtoLocal(HS.[UpdatedDate], @CurrntEmpTimeZoneDesc) AS DATETIME))END [UpdatedDate],
+         HS.[Activity]
+	FROM [dbo].[AircraftHistory] HS WITH (NOLOCK)  
+	JOIN dbo.AircraftRegistryHeader AR WITH (NOLOCK) ON AR.AircraftRegistryId = HS.RefferenceId
+    WHERE HS.RefferenceId = @RefferenceId 
+	AND HS.MasterCompanyId = @MasterCompanyId    
+    ),  
+    FinalResult AS (  
+    SELECT HistoryId, ModuleId, AircraftRegistryNumber, partnumber, OldValue, NewValue,Activity,HistoryText, FieldsName  
+      ,CreatedDate, UpdatedDate, CreatedBy, UpdatedBy FROM Result  
+    WHERE (  
+     (@GlobalFilter <>'' AND ((HistoryId LIKE '%' +@GlobalFilter+'%') OR   
+       (OldValue LIKE '%' +@GlobalFilter+'%') OR  
+       (NewValue LIKE '%' +@GlobalFilter+'%') OR  
+	   (Activity LIKE '%' +@GlobalFilter+'%') OR 
+       (HistoryText LIKE '%' +@GlobalFilter+'%')  
+       ))  
+       OR     
+       (@GlobalFilter='' AND   
+       (ISNULL(@OldValue,'') ='' OR OldValue LIKE  '%'+@OldValue+'%') AND  
+       (ISNULL(@NewValue,'') ='' OR NewValue LIKE  '%'+@NewValue+'%') AND  
+       (ISNULL(@PartNumber,'') ='' OR partnumber LIKE  '%'+@PartNumber+'%') AND  
+       (ISNULL(@AircraftRegistryNumber,'') ='' OR AircraftRegistryNumber LIKE  '%'+@AircraftRegistryNumber+'%') AND  
+       (ISNULL(@UpdatedBy,'') ='' OR UpdatedBy LIKE  '%'+@UpdatedBy+'%') AND  
+       (ISNULL(@UpdatedDate,'') ='' OR CAST(UpdatedDate AS DATE)=CAST(@UpdatedDate AS DATE)) AND  
+	   (ISNULL(@Activity,'') ='' OR Activity LIKE  '%'+@Activity+'%') AND 
+       (ISNULL(@HistoryText,'') ='' OR HistoryText LIKE '%'+@HistoryText+'%'))  
+       )),  
+      ResultCount AS (SELECT COUNT(HistoryId) AS NumberOfItems FROM FinalResult)  
+      SELECT HistoryId, ModuleId, AircraftRegistryNumber, partnumber, OldValue, NewValue, Activity, HistoryText, FieldsName,CreatedDate, UpdatedDate, CreatedBy, UpdatedBy,  
+      NumberOfItems FROM FinalResult, ResultCount  
+  
+     ORDER BY    
+     CASE WHEN (@SortOrder=1 AND @SortColumn='HistoryId')  THEN HistoryId END DESC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='OldValue')  THEN OldValue END ASC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='NewValue')  THEN NewValue END ASC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='AircraftRegistryNumber')  THEN AircraftRegistryNumber END ASC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='PartNumber')  THEN partnumber END ASC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='HistoryText')  THEN HistoryText END ASC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='CREATEDDATE')  THEN CreatedDate END ASC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='UPDATEDDATE')  THEN UpdatedDate END ASC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='CREATEDBY')  THEN CreatedBy END ASC,  
+     CASE WHEN (@SortOrder=1 AND @SortColumn='UPDATEDBY')  THEN UpdatedBy END ASC, 
+	 CASE WHEN (@SortOrder=1 AND @SortColumn='ACTIVITY')  THEN Activity END ASC, 
+	   
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='HistoryId')  THEN HistoryId END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='OldValue')  THEN OldValue END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='NewValue')  THEN NewValue END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='AircraftRegistryNumber')  THEN AircraftRegistryNumber END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='PartNumber')  THEN partnumber END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='HistoryText')  THEN HistoryText END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='CREATEDDATE')  THEN CreatedDate END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='UPDATEDDATE')  THEN UpdatedDate END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='CREATEDBY')  THEN CreatedBy END DESC,  
+     CASE WHEN (@SortOrder=-1 AND @SortColumn='UPDATEDBY')  THEN UpdatedBy END DESC, 
+	 CASE WHEN (@SortOrder=-1 AND @SortColumn='ACTIVITY')  THEN Activity END DESC
+
+     OFFSET @RecordFrom ROWS   
+     FETCH NEXT @PageSize ROWS ONLY  
+    END  
+    COMMIT  TRANSACTION  
+    END TRY      
+ BEGIN CATCH        
+  IF @@trancount > 0  
+   PRINT 'ROLLBACK'  
+   ROLLBACK TRAN;  
+   DECLARE   @ErrorLogID  INT, @DatabaseName VARCHAR(100) = db_name()   
+-----------------------------------PLEASE CHANGE THE VALUES FROM HERE TILL THE NEXT LINE----------------------------------------  
+            , @AdhocComments     VARCHAR(150)    = 'USP_GetHistory'   
+            , @ProcedureParameters VARCHAR(3000) = '@HistoryId = ''' + CAST(ISNULL(@HistoryId, '') AS VARCHAR(100))  
+            , @ApplicationName VARCHAR(100) = 'PAS'  
+-----------------------------------PLEASE DO NOT EDIT BELOW----------------------------------------  
+            exec spLogException   
+                    @DatabaseName           = @DatabaseName  
+                    , @AdhocComments          = @AdhocComments  
+                    , @ProcedureParameters = @ProcedureParameters  
+                    , @ApplicationName        =  @ApplicationName  
+                    , @ErrorLogID                    = @ErrorLogID OUTPUT ;  
+            RAISERROR ('Unexpected Error Occured in the database. Please let the support team know of the error number : %d', 16, 1,@ErrorLogID)  
+            RETURN(1);  
+ END CATCH  
+END

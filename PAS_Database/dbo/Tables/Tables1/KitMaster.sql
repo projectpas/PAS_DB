e842bbc6ee85d5c3236182973f27b8005fa5dbff
Hemant <Hemant@silverxis.com>
2026-09-11 18:18:02 +0530

    Updated decimal number 18.2 to 28.2

--- PAS_Database/dbo/Tables/Tables1/KitMaster.sql
+++ PAS_Database/dbo/Tables/Tables1/KitMaster.sql
@@ -15,7 +15,7 @@
     [IsDeleted]       BIT             NULL,
     [CustomerId]      BIGINT          NULL,
     [CustomerName]    VARCHAR (250)   NULL,
-    [KitCost]         DECIMAL (18, 6) NOT NULL,
+    [KitCost]         DECIMAL (28, 6) NOT NULL,
     [KitDescription]  VARCHAR (MAX)   NULL,
     [WorkScopeId]     BIGINT          NULL,
     [WorkScopeName]   VARCHAR (250)   NULL,
